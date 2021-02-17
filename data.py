a38a4c13a7a063f95bdbcbce1fb1bb007e28e3a9
LKuemmel <lena.kuemmel@openwb.de>
2021-02-17 08:41:22 +0100

    Dictionaries

--- data.py
+++ data.py
@@ -1,16 +1,17 @@
 """ Verschachtelte Listen, die die Daten zur Berechnung des Alogrithmus enthalten.
+Dictionary: Zugriff erfolgt bei Dictionary über Keys, nicht über Indizes wie bei Listen. Das hat den Vorteil, dass Instanzen gelöscht werden können, der Zugriff aber nicht verändert werden musss.
 """
 
-cp_data=[]
-cp_template_data=[]
-pv_data=[]
-pv_module_data=[]
-ev_data=[]
-ev_template_data=[]
-ev_charge_template_data=[]
-meter_data=[]
-meter_module_data=[]
-bat_data=[]
-bat_module_data=[]
-evu_data=[]
-evu_module_data=[]
+cp_data={}
+cp_template_data={}
+pv_data={}
+pv_module_data={}
+ev_data={}
+ev_template_data={}
+ev_charge_template_data={}
+meter_data={}
+meter_module_data={}
+bat_data={}
+bat_module_data={}
+evu_data={}
+evu_module_data={}
--- subdata.py
+++ subdata.py
@@ -3,8 +3,10 @@
 
 from datetime import datetime
 import paho.mqtt.client as mqtt
+import re
 import threading
 
+import ev
 import prepare
 
 class subData():
@@ -13,19 +15,19 @@ class subData():
     """
 
     #Instanzen
-    cp_data=[]
-    cp_template_data=[]
-    pv_data=[]
-    pv_module_data=[]
-    ev_data=[]
-    ev_template_data=[]
-    ev_charge_template_data=[]
-    meter_data=[]
-    meter_module_data=[]
-    bat_data=[]
-    bat_module_data=[]
-    evu_data=[]
-    evu_module_data=[]
+    cp_data={}
+    cp_template_data={}
+    pv_data={}
+    pv_module_data={}
+    ev_data={}
+    ev_template_data={}
+    ev_charge_template_data={}
+    meter_data={}
+    meter_module_data={}
+    bat_data={}
+    bat_module_data={}
+    evu_data={}
+    evu_module_data={}
 
     def __init__(self):
         self.ticker_prepare = threading.Event()
@@ -64,6 +66,8 @@ class subData():
     def on_message(self, client, userdata, msg):
         """ wartet auf eingehende Topics und ruft Funktionen zur weiteren Verarbeitung auf.
         """
+        topic=msg.topic
+        payload=msg.payload
         # log all messages before any error forces this process to die
         if (len(msg.payload.decode("utf-8")) >= 1):
             theTime = datetime.now()
@@ -74,33 +78,127 @@ class subData():
 
         print(msg.topic, msg.payload)
 
+        if "openWB/vehicle" in topic:
+            processVehicleTopic(topic, payload)
+        elif "openWB/chargepoint" in topic:
+            processChargepointTopic(topic, payload)
+        elif "openWB/pv" in topic:
+            processPvTopic(topic, payload)
+        elif "openWB/bat" in topic:
+            processBatTopic(topic, payload)
+        elif "openWB/general" in topic:
+            processGeneralTopic(topic, payload)
+        elif "openWB/optional" in topic:
+            processOptionalTopic(topic, payload)
+        elif "openWB/counter" in topic:
+            processCounterTopic(topic, payload)
+        elif "openWB/graph" in topic:
+            processGraphTopic(topic, payload)
+        elif "openWB/smarthome" in topic:
+            processSmarthomeTopic(topic, payload)
+        else:
+            print("Unknown topic: "+topic+", "+str(msg.payload.decode("utf-8")))
+
+    def get_index(self, topic):
+        """extrahiert den Index aus einemTtopic (zwischen zwei //)
+        """
+        index=re.search('(?!\/)([0-9]+)(?=\/)', topic)
+        return index.group()
+
+    def processVehicleTopic(self, topic, payload):
+        """
+        """
+        if topic=="openWB/vehicle/general/chargeMode":
+
+        elif topic=="openWB/vehicle/standard/matchEV":
+
+        elif topic=="openWB/vehicle/standard/chargeTemplate":
+
+        elif topic=="openWB/vehicle/standard/evTemplate":
+
+        elif topic=="openWB/vehicle/template/chargeTemplate":
+            index=self.get_index(topic)
+            if "cp"+index not in ev_charge_template_data:
+                ev_charge_template_data["cp"+index] = ev.chargeTemplate()
+            if "name" in topic:
+
+            elif "loadStandard" in topic:
+
+            elif "prio" in topic:
+
+            elif "chargeMode" in topic:
+                if "immediateLoad/current" in topic:
+
+                elif "immediateLoad/limit" in topic:
+
+        elif topic=="openWB/vehicle/template/evTemplate":
+
+
+        else:
+
+    def processChargepointTopic(self, topic, payload):
+        """
+        """
+        pass
+
+    def processPvTopic(self, topic, payload):
+        """
+        """
+        pass
+
+    def processBatTopic(self, topic, payload):
+        """
+        """
+        pass
+
+    def processGeneralTopic(self, topic, payload):
+        """
+        """
+        pass
+
+    def processOptionalTopic(self, topic, payload):
+        """
+        """
+        pass
+
+    def processCounterTopic(self, topic, payload):
+        """
+        """
+        pass
+
+    def processGraphTopic(self, topic, payload):
+        """
+        """
+        pass
+
+    def processSmarthomeTopic(self, topic, payload):
+        """
+        """
+        pass
+
         #keine Reihenfolge imner gucken, ob es Instanz schon gibt
 
-        # if topic==/chargepoint/1:
-        # 	if payload==1:
-        #         cp1=chargepoint()
-        # 		data.cp_data.append(cp1)
-        # 	else
-        # 		data.cp_data[0].remove(cp1)
-        # elif topic == /chargepoint/1/config/template/autolock:
-        #     if cp1 in data.cp_data:
-        #         cp1=chargepoint()
-        # 		data.cp_data.append(cp1)
-        # 	if payload == active:
-        # 		data.cp_data[0].autolock=true
-        # 	else
-        # 		data.cp_data[0].autolock=false
-        # elif topic==/chargepoint/1/config/template:
-        # 	if payload!="none":
-        # 		data.cp_data[0].template=payload
-        #elif topic="openWB/general/controlInterval":
-        #   
-            # self.ticker_prepare.clear()
-            # seconds = mqttpayload
-            # self.ticker_prepare.set()
-            # while not self.ticker_prepare.wait(seconds):
-            #     self.prep.setup_algorithm()
-        
-
-
-
+        if topic=="openWB/chargepoint/1":
+            if payload==True:
+                cp1=chargepoint()
+                data.cp_data.append(cp1)
+            else
+                data.cp_data[0].remove(cp1)
+        elif topic == "openWB/chargepoint/1/config/template/autolock":
+            if cp1 in data.cp_data:
+                cp1=chargepoint()
+                data.cp_data.append(cp1)
+            if payload == True:
+                data.cp_data[0].autolock=true
+            else
+                data.cp_data[0].autolock=false
+        elif topic=="openWB/chargepoint/1/config/template":
+            if payload!="none":
+                data.cp_data[0].template=payload
+        elif topic="openWB/general/controlInterval":
+            
+            self.ticker_prepare.clear()
+            seconds = mqttpayload
+            self.ticker_prepare.set()
+            while not self.ticker_prepare.wait(seconds):
+                self.prep.setup_algorithm()
