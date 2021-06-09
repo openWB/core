3f30bf9409e36af81f0330533bb608a429604cc8
kevinwieland <kevin.wieland@openwb.de>
2021-06-09 07:26:20 +0200

    1p3p weitersenden an externe ladepunkte

--- runs/setchargepoints.py
+++ runs/setchargepoints.py
@@ -6,15 +6,18 @@ import json
 import sys
 chargepoint=sys.argv[2]
 ll=sys.argv[1]
-
+action=sys.argv[3]
 #print('cp'+str(chargepoint)+' ll: '+str(ll))
 cpconfig=subscribe.simple("openWB/chargepoint/"+str(chargepoint)+"/config/connection_module/selected", hostname="localhost").payload.decode("utf-8")
 chargepointconfig = json.loads(cpconfig)
 if ( chargepointconfig['selected'] == "external_openwb" ):
-    if ( chargepointconfig['config']['chargepoint'] == "2" ):
-        publish.single("openWB/set/isss/Lp2Current", str(ll), hostname=str(chargepointconfig['config']['ip']))
-    else:
-        publish.single("openWB/set/isss/Current", str(ll), hostname=str(chargepointconfig['config']['ip']))
+    if ( action == "ll"):
+        if ( chargepointconfig['config']['chargepoint'] == "2" ):
+            publish.single("openWB/set/isss/Lp2Current", str(ll), hostname=str(chargepointconfig['config']['ip']))
+        else:
+            publish.single("openWB/set/isss/Current", str(ll), hostname=str(chargepointconfig['config']['ip']))
+    if ( action == "1p3p"):
+        publish.single("openWB/set/isss/U1p3p", str(ll), hostname=str(chargepointconfig['config']['ip']))
 
 
 
