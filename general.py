9de0b1f6009f7f9373a3e44e99f164e3a73aab7d
LKuemmel <lena.kuemmel@openwb.de>
2021-06-10 16:41:10 +0200

    phases for stop and standby, don't start auto phase switch with one phase

--- general.py
+++ general.py
@@ -13,7 +13,9 @@ class general():
 
 
     def get_phases_chargemode(self, chargemode):
-        """ gibt die Anazhl Phasen zurück, mit denen im jeweiligen Lademodus geladen wird
+        """ gibt die Anazhl Phasen zurück, mit denen im jeweiligen Lademodus geladen wird. 
+        Wenn der Lademodus Stop oder Standby ist, wird 0 zurückgegeben, da in diesem Fall 
+        die bisher genutzte Phasenzahl weiter genutzt wird, bis der Algorithmus eine Umschaltung vorgibt.
 
         Parameter
         ---------
@@ -25,7 +27,10 @@ class general():
         int: Anzahl Phasen
         """
         try:
-            return self.data["chargemode_config"][chargemode]["phases_to_use"]
+            if chargemode == "stop" or chargemode == "standby":
+                return 0
+            else:
+                return self.data["chargemode_config"][chargemode]["phases_to_use"]
         except Exception as e:
             log.exception_logging(e)
             return 1
