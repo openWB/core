ea46a1795b044aa01fc0bf3c3bbf5123ebcf609d
LKuemmel <lena.kuemmel@openwb.de>
2021-06-14 15:31:11 +0200

    fix phase switch consider min pv

--- chargepoint.py
+++ chargepoint.py
@@ -261,7 +261,7 @@ class chargepoint():
                         pub.pub("openWB/set/vehicle/"+str(charging_ev.ev_num) + "/control_parameter/timestamp_perform_phase_switch", "0")
                         # Aktuelle Ladeleistung und Differenz wieder freigeben.
                         if charging_ev.data["control_parameter"]["phases"] == 3:
-                            data.pv_data["all"].data["set"]["reserved_evu_overhang"] -= charging_ev.ev_template.data["min_current"] * 3 * 230
+                            data.pv_data["all"].data["set"]["reserved_evu_overhang"] -= charging_ev.data["control_parameter"]["required_current"] * 3 * 230
                         else:
                             data.pv_data["all"].data["set"]["reserved_evu_overhang"] -= charging_ev.ev_template.data["max_current_one_phase"] * 230
                 # Wenn eine Umschaltung im Gange ist, muss erst gewartet werden, bis diese fertig ist.
@@ -275,7 +275,7 @@ class chargepoint():
                             self.data["get"]["state_str"] = message
                             # Timestamp für die Durchführungsdauer
                             # Ladeleistung reservieren, da während der Umschaltung die Ladung pausiert wird.
-                            data.pv_data["all"].data["set"]["reserved_evu_overhang"] += charging_ev.ev_template.data["min_current"] * 3 * 230
+                            data.pv_data["all"].data["set"]["reserved_evu_overhang"] += charging_ev.data["control_parameter"]["required_current"] * 3 * 230
                             charging_ev.data["control_parameter"]["timestamp_perform_phase_switch"] = timecheck.create_timestamp()
                             pub.pub("openWB/set/vehicle/"+str(charging_ev.ev_num) + "/control_parameter/timestamp_perform_phase_switch", charging_ev.data["control_parameter"]["timestamp_perform_phase_switch"])
                         else:
