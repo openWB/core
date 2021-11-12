c0c475cfb2b7249c6841325d1a20ef3c284794a6
LKuemmel <lena.kuemmel@openwb.de>
2021-11-12 11:49:20 +0100

    clean up repo

--- packages/modules/cp/mqtt.py
+++
@@ -1,6 +0,0 @@
-from ...helpermodules import pub
-
-def mqtt_state(cp):
-    cp_num = cp.cp_num
-    pub.pub("openWB/set/chargepoint/"+str(cp_num)+"/get/fault_state", 0)
-    pub.pub("openWB/set/chargepoint/"+str(cp_num)+"/get/fault_str", "Kein Fehler")
