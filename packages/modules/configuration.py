97d8e20f4ae5759dcb16570af5158801129b27e0
LKuemmel <lena.kuemmel@openwb.de>
2021-12-17 16:40:01 +0100

    add new devices

--- packages/modules/configuration.py
+++ packages/modules/configuration.py
@@ -14,6 +14,9 @@ def _pub_configurable_soc_modules() -> None:
     try:
         soc_modules = [
             {
+                "value": "evnotify",
+                "text": "EVNotify"
+            },{
                 "value": "tesla",
                 "text": "Tesla"
             }
@@ -55,6 +58,110 @@ def _pub_configurable_devices_components() -> None:
                 ]
             },
             {
+                "value": "fronius",
+                "text": "Fronius",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": " Fronius Speicher"
+                    },
+                    {
+                        "value": "counter_s0",
+                        "text": "Fronius S0 Zähler"
+                    },
+                    {
+                        "value": "counter_sm",
+                        "text": "Fronius Smart Meter"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Fronius Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "http",
+                "text": "HTTP",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "HTTP-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "HTTP-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "HTTP-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "huawei",
+                "text": "Huawei",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Huawei-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "Huawei-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Huawei-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "janitza",
+                "text": "Janitza",
+                "component": [
+                    {
+                        "value": "counter",
+                        "text": "Janitza-Zähler"
+                    }
+                ]
+            },
+            {
+                "value": "json",
+                "text": "JSON",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "JSON-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "JSON-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "JSON-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "mqtt",
+                "text": "MQTT-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "MQTT-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "MQTT-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "MQTT-Wechselrichter"
+                    }
+                ]
+            },
+            {
                 "value": "openwb",
                 "text": "openWB Kit",
                 "component": [
@@ -91,6 +198,136 @@ def _pub_configurable_devices_components() -> None:
                 ]
             },
             {
+                "value": "openwb_pv_evu",
+                "text": "openWB PV-Zähler am EVU-Kit",
+                "component": [
+                    {
+                        "value": "inverter",
+                        "text": "PV-Zähler"
+                    }
+                ]
+            },
+            {
+                "value": "powerdog",
+                "text": "Powerdog-Device",
+                "component": [
+                    {
+                        "value": "counter",
+                        "text": "Powerdog-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Powerdog-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "saxpower",
+                "text": "Saxpower-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Saxpower-Speicher"
+                    }
+                ]
+            },
+            {
+                "value": "siemens",
+                "text": "Siemens-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Siemens-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "Siemens-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Siemens-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "solax",
+                "text": "Solax-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Solax-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "Solax-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Solax-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "studer",
+                "text": "Studer-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Studer-Speicher"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Studer-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "sungrow",
+                "text": "Sungrow-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Sungrow-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "Sungrow-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Sungrow-Wechselrichter"
+                    }
+                ]
+            },
+            {
+                "value": "sunny_island",
+                "text": "Sunny Island-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Sunny Island-Speicher"
+                    }
+                ]
+            },
+            {
+                "value": "victron",
+                "text": "Victron-Device",
+                "component": [
+                    {
+                        "value": "bat",
+                        "text": "Victron-Speicher"
+                    },
+                    {
+                        "value": "counter",
+                        "text": "Victron-Zähler"
+                    },
+                    {
+                        "value": "inverter",
+                        "text": "Victron-Wechselrichter"
+                    }
+                ]
+            },
+            {
                 "value": "virtual",
                 "text": "Virtuelles Modul",
                 "component": [
@@ -100,24 +337,7 @@ def _pub_configurable_devices_components() -> None:
                     }
                 ]
             },
-            {
-                "value": "mqtt",
-                "text": "MQTT-Device",
-                "component": [
-                    {
-                        "value": "bat",
-                        "text": "MQTT-Speicher"
-                    },
-                    {
-                        "value": "counter",
-                        "text": "MQTT-Zähler"
-                    },
-                    {
-                        "value": "inverter",
-                        "text": "MQTT-Wechselrichter"
-                    }
-                ]
-            }
+            
         ]
         Pub().pub("openWB/set/system/configurable/devices_components", devices_components)
     except Exception:
