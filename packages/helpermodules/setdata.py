1e9d9936f2fd59c56f548abc432b15aca71ab1e6
LKuemmel <lena.kuemmel@openwb.de>
2021-12-20 13:48:08 +0100

    fix setdata

--- packages/helpermodules/setdata.py
+++ packages/helpermodules/setdata.py
@@ -510,7 +510,7 @@ class SetData:
             elif ("/get/daily_yield" in msg.topic or
                     "/get/power_all" in msg.topic or
                     "/get/counter" in msg.topic or
-                    "/get/imported" in msg.topic):
+                    "/get/exported" in msg.topic):
                 self._validate_value(msg, float, [(0, float("inf"))])
             elif "/get/phases_in_use" in msg.topic:
                 self._validate_value(msg, int, [(0, 3)])
