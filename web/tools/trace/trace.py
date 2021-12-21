14e1db2e8e1e27d58043905c7fe35a9a506515de
Lutz Bender <github@lutz-bender.de>
2021-12-21 13:10:39 +0100

    prepare installation

--- web/tools/trace/trace.py
+++ web/tools/trace/trace.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python
 import sys
-import os
 import time
-import getopt
-import socket
-import ConfigParser
-import struct
-import binascii
 from pymodbus.client.sync import ModbusTcpClient
 
 ipaddress = str(sys.argv[1])
