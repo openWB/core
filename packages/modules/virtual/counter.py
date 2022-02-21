d2b8f81fbf4d416092e4cf6884b26eaec84722c5
LKuemmel <lena.kuemmel@openwb.de>
2022-02-21 12:11:59 +0100

    calc values of virtual counter by it's childrens

--- packages/modules/virtual/counter.py
+++ packages/modules/virtual/counter.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+from operator import add
+
 from control import data
 from helpermodules.log import MainLogger
 from modules.common import simcount
 from modules.common.component_state import CounterState
 from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.store import get_counter_value_store
+from modules.common.component_type import ComponentType
 
 
 def get_default_config() -> dict:
@@ -13,12 +16,19 @@ def get_default_config() -> dict:
         "type": "counter",
         "id": None,
         "configuration": {
-            "home_consumption": 400
+            "external_consumption": 400
         }
     }
 
 
 class VirtualCounter:
+
+    # Gedrehter Anschluss der Ladepunkte:
+    # Phase 1 LP -> LP-P 0 = EVU-P 0, LP-P 1 = EVU-P 1, LP-P 2 = EVU-P 2
+    # Phase 1 LP -> LP-P 0 = EVU-P 1, LP-P 1 = EVU-P 2, LP-P 2 = EVU-P 0
+    # Phase 3 LP -> LP-P 0 = EVU-P 2, LP-P 1 = EVU-P 0, LP-P 2 = EVU-P 1
+    cp_tp_evu_phase_mapping = {"1": [0, 1, 2], "2": [1, 2, 0], "3": [2, 0, 1]}
+
     def __init__(self, device_id: int, component_config: dict) -> None:
         self.__device_id = device_id
         self.component_config = component_config
@@ -30,58 +40,53 @@ class VirtualCounter:
                                             self.component_config["type"])
 
     def update(self):
-        # "Angeschlossene" Ladepunkte ermitteln
-        counter_name = "counter"+str(self.component_config["id"])
-        chargepoints = data.data.counter_data["all"].get_chargepoints_of_counter(counter_name)
-        # Ladeleistungen, Ströme addieren
-        currents = [0.0]*3
-        power = 0
-        for cp in chargepoints:
-            chargepoint = data.data.cp_data[cp]
-            # Gedrehter Anschluss der Ladepunkte:
-            # Phase 1 LP = Phase 1 EVU -> LP-P 0 = EVU-P 0, LP-P 1 = EVU-P 1, LP-P 2 = EVU-P 2
-            # Phase 1 LP = Phase 2 EVU -> LP-P 0 = EVU-P 1, LP-P 1 = EVU-P 2, LP-P 2 = EVU-P 0
-            # Phase 3 LP = Phase 3 EVU -> LP-P 0 = EVU-P 2, LP-P 1 = EVU-P 0, LP-P 2 = EVU-P 1
-            if chargepoint.data["config"]["phase_1"] == 1:
-                evu_phases = [0, 1, 2]
-            elif chargepoint.data["config"]["phase_1"] == 2:
-                evu_phases = [1, 2, 0]
-            elif chargepoint.data["config"]["phase_1"] == 3:
-                evu_phases = [2, 0, 1]
+        self.currents = [0.0]*3
+        self.incomplete_currents = False
+        self.power = 0
+
+        def add_current_power(element):
+            if element.data["get"]["currents"]:
+                self.currents = list(map(add, self.currents, element.data["get"]["currents"]))
             else:
-                raise FaultState.error("Für den virtuellen Zähler muss der Anschluss der Phasen vom Ladepunkt " +
-                                       str(chargepoint.cp_num) + " an die Phasen der EVU angegeben werden.")
-            currents[0] = currents[0] + chargepoint.data["get"]["currents"][evu_phases[0]]
-            currents[1] = currents[1] + chargepoint.data["get"]["currents"][evu_phases[1]]
-            currents[2] = currents[2] + chargepoint.data["get"]["currents"][evu_phases[2]]
+                self.currents = [0, 0, 0]
+                self.incomplete_currents = True
+            self.power += element.data["get"]["power"]
 
-            power = power + chargepoint.data["get"]["power"]
+        counter_all = data.data.counter_data["all"]
+        elements = counter_all.get_entry_of_element(self.component_config["id"])["children"]
+        for element in elements:
+            if element["type"] == ComponentType.CHARGEPOINT.value:
+                chargepoint = data.data.cp_data[f"cp{element['id']}"]
+                try:
+                    evu_phases = self.cp_tp_evu_phase_mapping[str(chargepoint.data["config"]["phase_1"])]
+                except KeyError:
+                    raise FaultState.error(f"Für den virtuellen Zähler muss der Anschluss der Phasen von Ladepunkt "
+                                           f"{chargepoint.cp_num} an die Phasen der EVU angegeben werden.")
+                self.currents = [self.currents[i] + chargepoint.data["get"]
+                                 ["currents"][evu_phases[i]] for i in range(0, 3)]
 
-        if self.component_config["configuration"]["home_consumption"]:
-            pv_power = data.data.pv_data["all"].data["get"]["power"]
-            bat_power = data.data.bat_data["all"].data["get"]["power"]
-            power = pv_power + bat_power + power + self.component_config["configuration"]["home_consumption"]
-            power_modules_phase = (
-                pv_power + bat_power + self.component_config["configuration"]["home_consumption"])/3/230
-            currents = [currents[i] + power_modules_phase for i in range(0, 3)]
+                self.power = self.power + chargepoint.data["get"]["power"]
+            elif element["type"] == ComponentType.BAT.value:
+                add_current_power(data.data.bat_data[f"bat{element['id']}"])
+            elif element["type"] == ComponentType.COUNTER.value:
+                add_current_power(data.data.counter_data[f"counter{element['id']}"])
+            elif element["type"] == ComponentType.INVERTER.value:
+                add_current_power(data.data.pv_data[f"pv{element['id']}"])
 
-        powers = [230*c for c in currents]
-        topic_str = "openWB/set/system/device/{}/component/{}/".format(
-            self.__device_id, self.component_config["id"]
-        )
+        topic_str = "openWB/set/system/device/{}/component/{}/".format(self.__device_id, self.component_config["id"])
         imported, exported = self.__sim_count.sim_count(
-            power,
+            self.power,
             topic=topic_str,
             data=self.simulation,
             prefix="bezug"
         )
-
         counter_state = CounterState(
-            currents=currents,
-            powers=powers,
             imported=imported,
             exported=exported,
-            power=power
+            power=(self.power + self.component_config["configuration"]["external_consumption"])
         )
+        if self.incomplete_currents is False:
+            counter_state.currents = self.currents
+
         MainLogger().debug("Virtual Leistung[W]: " + str(counter_state.power))
         self.__store.set(counter_state)
