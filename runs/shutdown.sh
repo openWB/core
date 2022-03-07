18e8c43b161c3e277f2c8d8cdb45b02900846d1d
Lutz Bender <user.email=github@lutz-bender.de>
2022-03-07 10:08:30 +0100

    add system commands

--- runs/shutdown.sh
+++ runs/shutdown.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-sudo rm /var/log/openWB.log
-sudo touch /var/log/openWB.log
-sudo chmod 777 /var/log/openWB.log
-$(sleep 5 && sudo shutdown now)&
+sudo poweroff &
--- runs/update_self.sh
+++ runs/update_self.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-OPENWBBASEDIR=$(cd `dirname $0`/../ && pwd)
-# backup some files before fetching new release
-# module soc_eq
-cp modules/soc_eq/soc_eq_acc_lp1 /tmp/soc_eq_acc_lp1
-cp modules/soc_eq/soc_eq_acc_lp2 /tmp/soc_eq_acc_lp2
 
 # fetch new release from GitHub
 sudo git fetch origin
-sudo git reset --hard origin/$1
 
-# set permissions
-cd /var/www/html/
-sudo chown -R pi:pi openWB 
-sudo chown -R www-data:www-data ${OPENWBBASEDIR}/web/backup
-sudo chown -R www-data:www-data ${OPENWBBASEDIR}/web/tools/upload
+# stop openwb2 service
+sudo service openwb2 stop
 
-# restore saved files after fetching new release
-# module soc_eq
-sudo cp /tmp/soc_eq_acc_lp1 ${OPENWBBASEDIR}/modules/soc_eq/soc_eq_acc_lp1
-sudo cp /tmp/soc_eq_acc_lp2 ${OPENWBBASEDIR}/modules/soc_eq/soc_eq_acc_lp2
+# only master branch yet
+sudo git reset --hard "origin/master"
 
-sleep 2
-
-# now treat system as in booting state
+# now reboot system
 sudo reboot now
