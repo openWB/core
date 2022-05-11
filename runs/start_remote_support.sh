64c83ec2d4adbc11432c57ffb355932959e19ba5
Lutz Bender <user.email=github@lutz-bender.de>
2022-05-11 13:35:40 +0200

    fix warnings

--- runs/start_remote_support.sh
+++ runs/start_remote_support.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-OPENWBBASEDIR=$(cd `dirname $0`/../ && pwd)
+OPENWBBASEDIR=$(cd "$(dirname "$0")/../" && pwd)
 re='^[0-9]+$'
 if ! [[ $2 =~ $re ]] ; then
 	port=2223
 else
 	port=$2
 fi
-sshpass -p $1 ssh -tt -o StrictHostKeyChecking=no  -o "ServerAliveInterval 60" -R $port:localhost:22 getsupport@remotesupport.openwb.de &
+sshpass -p "$1" ssh -tt -o StrictHostKeyChecking=no  -o "ServerAliveInterval 60" -R "$port":localhost:22 getsupport@remotesupport.openwb.de &
 
-echo $! > ${OPENWBBASEDIR}/ramdisk/remotesupportpid
+echo $! > "${OPENWBBASEDIR}/ramdisk/remotesupportpid"
