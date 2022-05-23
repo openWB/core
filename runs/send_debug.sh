648746d4639f6ae00a4c6d8e41ab1e2b3ce01960
Lutz Bender <user.email=github@lutz-bender.de>
2022-05-23 11:13:15 +0200

    fix shellcheck warnings

--- runs/send_debug.sh
+++ runs/send_debug.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
-OPENWBBASEDIR=$(cd `dirname $0`/../ && pwd)
+OPENWBBASEDIR=$(cd "$(dirname "$0")/../" && pwd)
 sleep 60
 
 debugFile=${OPENWBBASEDIR}/ramdisk/debug.log
-echo $1 > $debugFile
-debugemail=$2
-echo "############################ system ###############" >> $debugFile
-uptime >> $debugFile
-free >> $debugFile
-echo "############################ storage ###############" >> $debugFile
-df -h >> $debugFile
-echo "############################ network ##############" >> $debugFile
-ifconfig >> $debugFile
-echo "############################ version ##############" >> $debugFile
-cat ${OPENWBBASEDIR}/web/version >> $debugFile
-cat ${OPENWBBASEDIR}/web/lastcommit >> $debugFile
-echo "############################ main.log ##############" >> $debugFile
-echo "$(tail -1000 ${OPENWBBASEDIR}/ramdisk/main.log)" >> $debugFile
-echo "############################ mqtt ##############" >> $debugFile
-echo "$(tail -500 ${OPENWBBASEDIR}/ramdisk/mqtt.log)" >> $debugFile
+touch "$debugFile"
+{
+	echo "$1" > "$debugFile"
+	debugemail=$2
+	echo "############################ system ###############"
+	uptime
+	free
+	echo "############################ storage ###############"
+	df -h
+	echo "############################ network ##############"
+	ifconfig
+	echo "############################ version ##############"
+	cat "${OPENWBBASEDIR}/web/version"
+	cat "${OPENWBBASEDIR}/web/lastcommit"
+	echo "############################ main.log ##############"
+	tail -2500 "${OPENWBBASEDIR}/ramdisk/main.log"
+	echo "############################ mqtt ##############"
+	tail -1000 "${OPENWBBASEDIR}/ramdisk/mqtt.log"
 
-for currentConfig in /etc/mosquitto/conf.d/99-bridge-*; do
-	if [ -f "$currentConfig" ]; then
-		echo "############################ mqtt bridge '$currentConfig' ######" >> $debugFile
-		sudo grep -F -v -e password "$currentConfig" | sed '/^#/ d'>> $debugFile
-	fi
-done
+	for currentConfig in /etc/mosquitto/conf.d/99-bridge-*; do
+		if [ -f "$currentConfig" ]; then
+			echo "############################ mqtt bridge '$currentConfig' ######"
+			sudo grep -F -v -e password "$currentConfig" | sed '/^#/ d'
+		fi
+	done
 
-echo "############################ mqtt topics ##############" >> $debugFile
-timeout 1 mosquitto_sub -v -t 'openWB/#' >> $debugFile
+	echo "############################ mqtt topics ##############"
+	timeout 1 mosquitto_sub -v -t 'openWB/#'
 
-#echo "############################ smarthome.log ##############" >> $debugFile
-#echo "$(tail -200 ${OPENWBBASEDIR}/ramdisk/smarthome.log)" >> $debugFile
+	# echo "############################ smarthome.log ##############"
+	# tail -200 "${OPENWBBASEDIR}/ramdisk/smarthome.log"
+} >> "$debugFile"
 
-curl --upload $debugFile "https://openwb.de/tools/debug2.php?debugemail=$debugemail"
+echo "***** uploading debuglog..." >> "$RAMDISKDIR/main.log"
+curl --upload "$debugFile" "https://openwb.de/tools/debug2.php?debugemail=$debugemail"
 
-rm $debugFile
+echo "***** cleanup..." >> "$RAMDISKDIR/main.log"
+rm "$debugFile"
+
+echo "***** debuglog end" >> "$RAMDISKDIR/main.log"
