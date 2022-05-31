aaa8e15cc290321058a2d98270c68055a300b3f6
Lutz Bender <user.email=github@lutz-bender.de>
2022-05-31 15:35:50 +0200

    create group openwb

--- runs/upgrade2openwbuser.sh
+++ runs/upgrade2openwbuser.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 OPENWBBASEDIR=/var/www/html/openWB
 OPENWB_USER=openwb
+OPENWB_GROUP=openwb
 
 if (( $(id -u) != 0 )); then
 	echo "this script has to be run as user root or with sudo"
@@ -13,9 +14,16 @@ echo "stopping openWB2 system service..."
 systemctl stop openwb2.service
 echo "done"
 
+echo "create group $OPENWB_GROUP"
+# Will do nothing if group already exists:
+/usr/sbin/groupadd "$OPENWB_GROUP"
+echo "done"
+
 echo "create user $OPENWB_USER"
 # Will do nothing if user already exists:
-/usr/sbin/useradd "$OPENWB_USER" -g "$OPENWB_USER" --create-home
+/usr/sbin/useradd "$OPENWB_USER" -g "OPENWB_GROUP" --create-home
+echo "done"
+
 # The user "openwb" is still new and we might need sudo in many places. Thus for now we give the user
 # unrestricted sudo. This should be restricted in the future
 echo "adding new user to sudoers"
@@ -32,8 +40,8 @@ systemctl daemon-reload
 systemctl enable openwb2.service
 echo "done"
 
-echo "setting permissions in $OPENWBBASEDIR..."
-chown -R "$OPENWB_USER:$OPENWB_USER" "$OPENWBBASEDIR"
+echo "setting permissions in $OPENWBBASEDIR to $OPENWB_USER:$OPENWB_GROUP..."
+chown -R "$OPENWB_USER:$OPENWB_GROUP" "$OPENWBBASEDIR"
 echo "done"
 
 echo "upgrade to openwb user finished, rebooting..."
