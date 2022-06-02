137cce34bf88b0baa973d2909e16d1d6ea696793
LKuemmel <lena.kuemmel@openwb.de>
2022-06-02 13:50:16 +0200

    rewrite all tesla components

--- packages/modules/tesla/inverter.py
+++ packages/modules/tesla/inverter.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
+from typing import Dict
+
+from modules.common.abstract_component import AbstractConfiguration, AbstractSetup, from_dict
 from modules.common.component_state import InverterState
 from modules.common.fault_state import ComponentInfo
 from modules.common.store import get_inverter_value_store
 from modules.tesla.http_client import PowerwallHttpClient
 
 
-def get_default_config() -> dict:
-    return {
-        "name": "Tesla Wechselrichter",
-        "id": 0,
-        "type": "inverter",
-        "configuration": {}
-    }
+@dataclass
+class Configuration(AbstractConfiguration):
+    pass
+
+
+@dataclass
+class Setup(AbstractSetup):
+    name: str = "Tesla Wechselrichter"
+    type: str = "inverter"
+    id: int = 0
+    configuration: Configuration = Configuration()
 
 
 class TeslaInverter:
-    def __init__(self, component_config: dict) -> None:
-        self.component_config = component_config
-        self.__store = get_inverter_value_store(component_config["id"])
-        self.component_info = ComponentInfo.from_component_config(component_config)
+    def __init__(self, component_config: Dict) -> None:
+        self.component_config = component_config if isinstance(
+            component_config, Setup) else from_dict(component_config, Setup)
+        self.__store = get_inverter_value_store(self.component_config.id)
+        self.component_info = ComponentInfo.from_component_config(self.component_config)
 
     def update(self, client: PowerwallHttpClient, aggregate) -> None:
         pv_watt = aggregate["solar"]["instant_power"]
