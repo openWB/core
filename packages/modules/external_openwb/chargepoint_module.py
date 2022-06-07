894f228c145cd6d523826248096d62b01708ea80
LKuemmel <lena.kuemmel@openwb.de>
2022-06-07 15:55:55 +0200

    don't overwrite faultstate, set current to 0 if readings fails for 5 cycles

--- packages/modules/external_openwb/chargepoint_module.py
+++ packages/modules/external_openwb/chargepoint_module.py
@@ -4,7 +4,7 @@ from typing import Dict
 from control import data
 from helpermodules import pub
 from modules.common.abstract_chargepoint import AbstractChargepoint
-from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.component_context import ErrorCounterContext, SingleComponentUpdateContext
 from modules.common.fault_state import ComponentInfo
 
 
@@ -29,40 +29,49 @@ class ChargepointModule(AbstractChargepoint):
         self.component_info = ComponentInfo(
             self.id,
             "Ladepunkt", "chargepoint")
+        self.__client_error_context = ErrorCounterContext(
+            "Anhaltender Fehler beim Auslesen des Ladepunkts. Sollstromstärke wird zurückgesetzt.")
 
     def set_current(self, current: float) -> None:
-        with SingleComponentUpdateContext(self.component_info):
-            if self.connection_module["configuration"]["duo_num"] == 1:
-                pub.pub_single("openWB/set/isss/Current", current,
-                               hostname=self.connection_module["configuration"]["ip_address"])
-            else:
-                pub.pub_single("openWB/set/isss/Lp2Current", current,
-                               hostname=self.connection_module["configuration"]["ip_address"])
+        if self.__client_error_context.error_counter_exceeded():
+            current = 0
+        with SingleComponentUpdateContext(self.component_info, False):
+            with self.__client_error_context:
+                if self.connection_module["configuration"]["duo_num"] == 1:
+                    pub.pub_single("openWB/set/isss/Current", current,
+                                   hostname=self.connection_module["configuration"]["ip_address"])
+                else:
+                    pub.pub_single("openWB/set/isss/Lp2Current", current,
+                                   hostname=self.connection_module["configuration"]["ip_address"])
 
     def get_values(self) -> None:
         with SingleComponentUpdateContext(self.component_info):
-            ip_address = self.connection_module["configuration"]["ip_address"]
-            cp_num = self.id
-            my_ip_address = data.data.system_data["system"].data["ip_address"]
-            pub.pub_single("openWB/set/isss/heartbeat", 0, hostname=ip_address)
-            pub.pub_single("openWB/set/isss/parentWB", my_ip_address,
-                           hostname=ip_address, no_json=True)
-            if (self.connection_module["configuration"]["duo_num"] == 2):
-                pub.pub_single("openWB/set/isss/parentCPlp2", str(cp_num), hostname=ip_address)
-            else:
-                pub.pub_single("openWB/set/isss/parentCPlp1", str(cp_num), hostname=ip_address)
+            with self.__client_error_context:
+                ip_address = self.connection_module["configuration"]["ip_address"]
+                cp_num = self.id
+                my_ip_address = data.data.system_data["system"].data["ip_address"]
+                pub.pub_single("openWB/set/isss/heartbeat", 0, hostname=ip_address)
+                pub.pub_single("openWB/set/isss/parentWB", my_ip_address,
+                               hostname=ip_address, no_json=True)
+                if (self.connection_module["configuration"]["duo_num"] == 2):
+                    pub.pub_single("openWB/set/isss/parentCPlp2", str(cp_num), hostname=ip_address)
+                else:
+                    pub.pub_single("openWB/set/isss/parentCPlp1", str(cp_num), hostname=ip_address)
+                self.__client_error_context.reset_error_counter()
 
     def switch_phases(self, phases_to_use: int, duration: int) -> None:
-        with SingleComponentUpdateContext(self.component_info):
-            pub.pub_single("openWB/set/isss/U1p3p", phases_to_use,
-                           self.connection_module["configuration"]["ip_address"])
-            time.sleep(6+duration-1)
+        with SingleComponentUpdateContext(self.component_info, False):
+            with self.__client_error_context:
+                pub.pub_single("openWB/set/isss/U1p3p", phases_to_use,
+                               self.connection_module["configuration"]["ip_address"])
+                time.sleep(6+duration-1)
 
     def interrupt_cp(self, duration: int) -> None:
-        with SingleComponentUpdateContext(self.component_info):
-            ip_address = self.connection_module["configuration"]["ip_address"]
-            if (self.connection_module["configuration"]["duo_num"] == 2):
-                pub.pub_single("openWB/set/isss/Cpulp2", "1", hostname=ip_address)
-            else:
-                pub.pub_single("openWB/set/isss/Cpulp1", "1", hostname=ip_address)
-            time.sleep(duration)
+        with SingleComponentUpdateContext(self.component_info, False):
+            with self.__client_error_context:
+                ip_address = self.connection_module["configuration"]["ip_address"]
+                if (self.connection_module["configuration"]["duo_num"] == 2):
+                    pub.pub_single("openWB/set/isss/Cpulp2", "1", hostname=ip_address)
+                else:
+                    pub.pub_single("openWB/set/isss/Cpulp1", "1", hostname=ip_address)
+                time.sleep(duration)
