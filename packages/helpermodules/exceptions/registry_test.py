7407ff946000e26c8a9e80a5314b4f7d0117bd64
LKuemmel <lena.kuemmel@openwb.de>
2022-08-17 15:28:43 +0200

    fix merge

--- packages/helpermodules/exceptions/registry_test.py
+++ packages/helpermodules/exceptions/registry_test.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from typing import Type
 
 import pytest
@@ -32,13 +31,13 @@ class ErrorF(ErrorD):
     pass
 
 
-@pytest.mark.parametrize("exception,expected_message",
-                         [[ErrorRoot, "<class '" + Path(__file__).resolve().parts[-5] +
-                           ".packages.helpermodules.exceptions.registry_test.ErrorRoot'> ErrorRoot"],
-                          [ErrorB, "B"],
-                          [ErrorC, "A"],
-                          [ErrorD, "B"],
-                          [ErrorF, "F"]])
+@pytest.mark.parametrize("exception,expected_message", [
+    [ErrorRoot, "<class 'helpermodules.exceptions.registry_test.ErrorRoot'> ErrorRoot"],
+    [ErrorB, "B"],
+    [ErrorC, "A"],
+    [ErrorD, "B"],
+    [ErrorF, "F"]
+])
 def test_uses_exact_match_if_available(exception: Type, expected_message: str):
     # setup
     registry = ExceptionRegistry()
