d94238b49f9af8bcd154fe4ab0192f2f05aa769f
LKuemmel <lena.kuemmel@openwb.de>
2022-10-27 16:17:54 +0200

    fix home consumption for hybrid systems

---
+++ packages/control/conftest.py
@@ -0,0 +1,90 @@
+from typing import Counter
+from unittest.mock import Mock
+
+import pytest
+
+from control import data
+from control.bat import Bat
+from control.chargepoint import Chargepoint, ChargepointData, Get
+from control.counter import CounterAll
+from control.pv import Pv
+
+
+def hierarchy_standard() -> CounterAll:
+    # counter0
+    #        |
+    #        - cp3
+    #        - cp4
+    #        - cp5
+    #        - inverter1
+    #        - bat2
+    # counter6
+    c = CounterAll()
+    c.data["get"] = {"hierarchy": [{"id": 0, "type": "counter",
+                                   "children": [
+                                       {"id": 3, "type": "cp", "children": []},
+                                       {"id": 4, "type": "cp", "children": []},
+                                       {"id": 5, "type": "cp", "children": []},
+                                       {"id": 1, "type": "inverter", "children": []},
+                                       {"id": 2, "type": "bat", "children": []}]},
+                                   {"id": 6, "type": "counter", "children": []}]}
+    return c
+
+
+def hierarchy_hybrid() -> CounterAll:
+    # counter0
+    #        |
+    #        - cp3
+    #        - cp4
+    #        - cp5
+    #        - inverter1
+    #                  |
+    #                  - bat2
+    # counter6
+    c = CounterAll()
+    c.data["get"] = {"hierarchy": [{"id": 0, "type": "counter",
+                                   "children": [
+                                       {"id": 3, "type": "cp", "children": []},
+                                       {"id": 4, "type": "cp", "children": []},
+                                       {"id": 5, "type": "cp", "children": []},
+                                       {"id": 1, "type": "inverter",
+                                        "children": [
+                                            {"id": 2, "type": "bat", "children": []}]}]},
+                                   {"id": 6, "type": "counter", "children": []}]}
+    return c
+
+
+def hierarchy_nested() -> CounterAll:
+    # counter0
+    #        |
+    #        - cp3
+    #        - counter6
+    #                  |
+    #                   - cp4
+    #                   - cp5
+    #        - inverter1
+    #        - bat2
+    c = CounterAll()
+    c.data["get"] = {"hierarchy": [{"id": 0, "type": "counter",
+                                   "children": [
+                                       {"id": 3, "type": "cp", "children": []},
+                                       {"id": 6, "type": "counter",
+                                        "children": [
+                                            {"id": 4, "type": "cp", "children": []},
+                                            {"id": 5, "type": "cp", "children": []}]},
+                                       {"id": 1, "type": "inverter", "children": []},
+                                       {"id": 2, "type": "bat", "children": []}]}]}
+    return c
+
+
+@pytest.fixture()
+def data_() -> None:
+    data.data_init(Mock())
+    data.data.cp_data = {
+        "cp3": Mock(spec=Chargepoint, data=Mock(spec=ChargepointData, get=Mock(spec=Get, power=2500))),
+        "cp4": Mock(spec=Chargepoint, data=Mock(spec=ChargepointData, get=Mock(spec=Get, power=2500))),
+        "cp5": Mock(spec=Chargepoint, data=Mock(spec=ChargepointData, get=Mock(spec=Get, power=2500)))}
+    data.data.bat_data.update({"bat2": Mock(spec=Bat, data={"get": {"power": -5000}})})
+    data.data.pv_data.update({"pv1": Mock(spec=Pv, data={"get": {"power": -10000}})})
+    data.data.counter_data.update({"counter0": Mock(spec=Counter, data={"get": {"power": 200}}),
+                                   "counter6": Mock(spec=Counter, data={"get": {"power": 5000}})})
---
+++ packages/control/counter_home_consumption_test.py
@@ -0,0 +1,52 @@
+from typing import Callable
+from unittest.mock import Mock
+import pytest
+
+from control import data
+from control.conftest import hierarchy_standard, hierarchy_hybrid, hierarchy_nested
+from control.counter import CounterAll
+from modules.common.fault_state import FaultStateLevel
+
+
+@pytest.mark.parametrize("counter_all",
+                         [pytest.param(hierarchy_standard, id="standard"),
+                          pytest.param(hierarchy_hybrid, id="hybrid"),
+                             pytest.param(hierarchy_nested, id="nested")])
+def test_calc_home_consumption(counter_all: Callable[[], CounterAll], data_):
+    #
+    c = counter_all()
+
+    # execution
+    home_consumption = c._calc_home_consumption()[0]
+
+    # evaluation
+    assert home_consumption == 7700
+
+
+@pytest.mark.parametrize(["home_consumption",
+                          "invalid_home_consumption",
+                          "expected_home_consumption",
+                          "expected_invalid_home_consumption"],
+                         [pytest.param(500, 0, 500, 0, id="valid home consumption"),
+                          pytest.param(-100, 0, 200, 1, id="first invalid home consumption"),
+                             pytest.param(-100, 3, 0, 3, id="invalid home consumption, reset home consumption")])
+def test_set_home_consumption(home_consumption: int,
+                              invalid_home_consumption: int,
+                              expected_home_consumption: int,
+                              expected_invalid_home_consumption: int,
+                              monkeypatch,
+                              data_):
+    # setup
+    c = hierarchy_standard()
+    data.data.counter_data["counter0"].data["get"]["fault_state"] = FaultStateLevel.NO_ERROR
+    c.data["set"]["invalid_home_consumption"] = invalid_home_consumption
+    c.data["set"]["home_consumption"] = 200
+    calc_home_consumption_mock = Mock(return_value=[home_consumption, []])
+    monkeypatch.setattr(CounterAll, "_calc_home_consumption", calc_home_consumption_mock)
+
+    # execution
+    c.set_home_consumption()
+
+    # evaluation
+    assert c.data["set"]["invalid_home_consumption"] == expected_invalid_home_consumption
+    assert c.data["set"]["home_consumption"] == expected_home_consumption
