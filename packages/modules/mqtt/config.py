2b68638e033f74c823be5b441aac1a749cfd3bf3
rleidner <rleidner@gmx.net>
2022-10-29 11:59:30 +0200

    Adding soc component to mqtt module

--- packages/modules/mqtt/config.py
+++ packages/modules/mqtt/config.py
@@ -64,3 +64,20 @@ class MqttInverterSetup:
         self.type = type
         self.id = id
         self.configuration = configuration or MqttInverterConfiguration()
+
+
+class MqttSocConfiguration:
+    def __init__(self):
+        pass
+
+
+class MqttSocSetup:
+    def __init__(self,
+                 name: str = "mqtt",
+                 type: str = "mqtt",
+                 id: int = 0,
+                 configuration: MqttSocConfiguration = None) -> None:
+        self.name = name
+        self.type = type
+        self.id = id
+        self.configuration = configuration or MqttSocConfiguration()
