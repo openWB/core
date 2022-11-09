5b1f21e8ee31ed2b5980da6243629d1ae2adca5b
LKuemmel <lena.kuemmel@openwb.de>
2022-11-09 10:19:15 +0100

    don't pub old values

--- packages/helpermodules/utils/_thread_handler.py
+++ packages/helpermodules/utils/_thread_handler.py
@@ -6,7 +6,7 @@ from control import data
 log = logging.getLogger(__name__)
 
 
-def thread_handler(threads: List[threading.Thread]) -> None:
+def thread_handler(threads: List[threading.Thread]) -> List[str]:
     # Start them all
     for thread in threads:
         thread.start()
@@ -15,7 +15,10 @@ def thread_handler(threads: List[threading.Thread]) -> None:
     for thread in threads:
         thread.join(timeout=data.data.general_data.data.control_interval/3)
 
+    not_finished_threads = []
     for thread in threads:
         if thread.is_alive():
             log.error(f"{thread.name} konnte nicht innerhalb des Timeouts die Werte abfragen, die abgefragten "
                       "Werte werden nicht in der Regelung verwendet.")
+            not_finished_threads.append(thread.name)
+    return not_finished_threads
