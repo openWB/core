3cbbe9262bec773109cfed33d5f1090879c7ee48
LKuemmel <lena.kuemmel@openwb.de>
2023-01-31 10:06:01 +0100

    openWB Pro: vehicle id

--- packages/modules/chargepoints/openwb_pro/chargepoint_module_test.py
+++ packages/modules/chargepoints/openwb_pro/chargepoint_module_test.py
@@ -15,7 +15,7 @@ def test_openwb_pro(monkeypatch, requests_mock: requests_mock.mock):
     monkeypatch.setattr(chargepoint_module, 'get_chargepoint_value_store',
                         Mock(return_value=mock_chargepoint_value_store))
     requests_mock.post(f'http://{SAMPLE_IP}/connect.php')
-    requests_mock.get(f'http://{SAMPLE_IP}/api2.php', json=SAMPLE)
+    requests_mock.get(f'http://{SAMPLE_IP}/connect.php', json=SAMPLE)
 
     cp = chargepoint_module.ChargepointModule(0, {
         "type": "openwb_pro",
@@ -33,37 +33,30 @@ def test_openwb_pro(monkeypatch, requests_mock: requests_mock.mock):
 
 
 SAMPLE_CHARGEPOINT_STATE = ChargepointState(
-    power=13359,
-    currents=[19.415, 19.545, 19.486],
-    imported=8376,
-    exported=3,
+    power=4302.7,
+    currents=[6.133, 6.066, 6.122],
+    imported=10839,
+    exported=0,
     plug_state=True,
     charge_state=True,
-    phases_in_use=3
+    phases_in_use=3,
+    rfid="98:ED:5C:B4:EE:8D"
 )
 
-SAMPLE = {
-    "date": "2021:12:08-17:59:51",
-    "timestamp": 1638986391,
-    "powers": [
-        4422.3,
-        4500.8,
-        4436
-    ],
-    "power_all": 13359,
-    "currents": [
-        19.415,
-        19.545,
-        19.486
-    ],
-    "imported": 8376,
-    "exported": 3,
-    "plug_state": True,
-    "charge_state": True,
-    "phases_actual": 3,
-    "phases_target": 3,
-    "phases_in_use": 3,
-    "offered_current": 32,
-    "evse_signaling": "basic iec61851",
-    "serial": "387107"
-}
+SAMPLE = {'charge_state': True,
+          'currents': [6.133, 6.066, 6.122],
+          'date': '2023:01:30-18:48:31',
+          'evse_signaling': 'fake highlevel + basic iec61851',
+          'exported': 0,
+          'imported': 10839,
+          'offered_current': 6,
+          'phases_actual': 3,
+          'phases_in_use': 3,
+          'phases_target': 3,
+          'plug_state': True,
+          'power_all': 4302.7,
+          'powers': [1444.4, 1423.7, 1438.1],
+          'serial': '823950',
+          'timestamp': 1675104511,
+          'v2g_ready': 0,
+          'vehicle_id': '98:ED:5C:B4:EE:8D'}
