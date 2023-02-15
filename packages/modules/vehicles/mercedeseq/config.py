a91c3f2b78946d1bdbf10afecd273f8c6e4cad4a
Det <76627235+DetMoerk@users.noreply.github.com>
2023-02-15 20:08:43 +0100

    SocEQ Remove loginURL

--- packages/modules/vehicles/mercedeseq/config.py
+++ packages/modules/vehicles/mercedeseq/config.py
@@ -1,24 +1,27 @@
 from typing import Optional
 
-#from modules.common.component_setup import ComponentSetup
+# from modules.common.component_setup import ComponentSetup
 
 class MercedesEQSocToken:
     def __init__(self,
                  refresh_token: str = "",
                  access_token: str = "",
-                 expires_in: int = 0) -> None:
+                 expires_in: int = 0,
+                 id_token: str = "",
+                 token_type: str = "") -> None:
         self.refresh_token = refresh_token
         self.access_token = access_token
         self.expires_in = expires_in
+        self.id_token = id_token
+        self.token_type = token_type
 
 
 class MercedesEQSocConfiguration:
-    def __init__(self, client_id: Optional[str] = None, client_secret: Optional[str] = None, vin: Optional[str] = None, callbackurl: Optional[str] = None, loginurl: Optional[str] = None, token: MercedesEQSocToken = None) -> None:
+    def __init__(self, client_id: Optional[str] = None, client_secret: Optional[str] = None, vin: Optional[str] = None, callbackurl: Optional[str] = None, token: MercedesEQSocToken = None) -> None:
         self.client_id = client_id
         self.client_secret = client_secret
         self.vin = vin
         self.callbackurl = callbackurl
-        self.loginurl = loginurl
         self.token = token or MercedesEQSocToken()
 
 
