23e52b00d12c6d151c1e9032cbfae2bb182c7b70
Det <76627235+DetMoerk@users.noreply.github.com>
2023-03-01 09:08:58 +0100

    callback_ev.php sec fix
    escapeshellargs and commands

--- packages/modules/vehicles/mercedeseq/callback_ev.php
+++ packages/modules/vehicles/mercedeseq/callback_ev.php
@@ -1,7 +1,11 @@
 <?php
    //Callback procedure for mercedes SoC API EV 
    if( $_GET["code"] ) {
-      system( "/var/www/html/openWB/packages/modules/vehicles/mercedeseq/auth.py " . $_GET['state'] . " " . $_GET['code']) ;
+      $state= escapeshellarg($_GET['state']);
+      $code= escapeshellarg($_GET['code']);
+      $command = escapeshellcmd("/var/www/html/openWB/packages/modules/vehicles/mercedeseq/auth.py " );
+      //system( "/var/www/html/openWB/packages/modules/vehicles/mercedeseq/auth.py " . $_GET['state'] . " " . $_GET['code']) ;
+      system( $command . $state . " " . $code);
    }
    else {
       echo "<html>";
