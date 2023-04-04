84b3be7eba49f94dad3c338d39ddb0b84aa68737
vuffiraa72 <u.mersewsky+github@gmail.com>
2023-04-04 14:35:08 +0200

    SOC Modul über SkodaConnect (#762)
    * skodaconnect inital version
    
    * handle refresh tokens
    
    * implement api as class
    
    * implement config to dict in class
    
    * Update packages/modules/vehicles/skodaconnect/soc.py
    
    Co-authored-by: LKuemmel <76958050+LKuemmel@users.noreply.github.com>
    
    * Update packages/modules/vehicles/skodaconnect/api.py
    
    Co-authored-by: LKuemmel <76958050+LKuemmel@users.noreply.github.com>
    
    * add logging for communication errors
    
    * keep name in Json config
    
    * create JSON directly
    
    * test config with Optional of dict
    
    ---------
    
    Co-authored-by: LKuemmel <76958050+LKuemmel@users.noreply.github.com>

---
+++ packages/modules/vehicles/skodaconnect/soc.py
@@ -0,0 +1,42 @@
+from typing import Union, List
+
+import logging
+
+from dataclass_utils import dataclass_from_dict
+from helpermodules.cli import run_using_positional_cli_args
+from modules.common import store
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.abstract_soc import AbstractSoc
+from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.fault_state import ComponentInfo
+from modules.vehicles.skodaconnect.api import SkodaConnectApi
+from modules.vehicles.skodaconnect.config import SkodaConnect, SkodaConnectConfiguration
+
+
+log = logging.getLogger("soc."+__name__)
+
+
+class Soc(AbstractSoc):
+    def __init__(self, device_config: Union[dict, SkodaConnect], vehicle: int):
+        self.config = dataclass_from_dict(SkodaConnect, device_config)
+        self.vehicle = vehicle
+        self.store = store.get_car_value_store(self.vehicle)
+        self.component_info = ComponentInfo(self.vehicle, self.config.name, "vehicle")
+
+    def update(self, charge_state: bool = False) -> None:
+        with SingleComponentUpdateContext(self.component_info):
+            soc, range = SkodaConnectApi(self.config, self.vehicle).fetch_soc()
+
+
+def skodaconnect_update(user_id: str, password: str, vin: str, refresh_token: str, charge_point: int):
+    log.debug("skodaconnect: userid="+user_id+"vin="+vin+"charge_point="+str(charge_point))
+    Soc(
+        SkodaConnect(configuration=SkodaConnectConfiguration(user_id, password, vin, refresh_token)),
+        charge_point).update()
+
+
+def main(argv: List[str]):
+    run_using_positional_cli_args(skodaconnect_update, argv)
+
+
+device_descriptor = DeviceDescriptor(configuration_factory=SkodaConnect)
