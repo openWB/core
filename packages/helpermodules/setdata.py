6fcde7769e63a8ac97d5e0db6a8c21f6bf393f6a
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2023-04-12 12:02:33 +0200

    activate plans per direct topic (#859)
    * activate plans per direct topic
    
    * ammend

--- packages/helpermodules/setdata.py
+++ packages/helpermodules/setdata.py
@@ -12,7 +12,8 @@ import logging
 from helpermodules import subdata
 from helpermodules.broker import InternalBrokerClient
 from helpermodules.pub import Pub
-from helpermodules.utils.topic_parser import decode_payload, get_index
+from helpermodules.utils.topic_parser import (decode_payload, get_index, get_index_position, get_second_index,
+                                              get_second_index_position)
 from helpermodules.update_config import UpdateConfig
 import dataclass_utils
 
@@ -25,10 +26,14 @@ class SetData:
                  event_ev_template: threading.Event,
                  event_charge_template: threading.Event,
                  event_cp_config: threading.Event,
+                 event_scheduled_charging_plan: threading.Event,
+                 event_time_charging_plan: threading.Event,
                  event_subdata_initialized: threading.Event):
         self.event_ev_template = event_ev_template
         self.event_charge_template = event_charge_template
         self.event_cp_config = event_cp_config
+        self.event_scheduled_charging_plan = event_scheduled_charging_plan
+        self.event_time_charging_plan = event_time_charging_plan
         self.event_subdata_initialized = event_subdata_initialized
         self.heartbeat = False
 
@@ -134,14 +139,31 @@ class SetData:
                 else:
                     # aktuelles json-Objekt liegt in subdata
                     index = get_index(msg.topic)
-                    if "charge_template" in msg.topic:
+                    if "time_charging" in msg.topic and "plans" in msg.topic:
+                        index_second = get_second_index(msg.topic)
+                        event = self.event_time_charging_plan
+                        try:
+                            template = dataclasses.asdict(copy.deepcopy(
+                                subdata.SubData.ev_charge_template_data[
+                                    "ct"+index].data.time_charging.plans[index_second]))
+                        except IndexError:
+                            template = {}
+                    elif "scheduled_charging" in msg.topic and "plans" in msg.topic:
+                        index_second = get_second_index(msg.topic)
+                        event = self.event_scheduled_charging_plan
+                        try:
+                            template = dataclasses.asdict(copy.deepcopy(
+                                subdata.SubData.ev_charge_template_data[
+                                    "ct"+index].data.chargemode.scheduled_charging.plans[index_second]))
+                        except IndexError:
+                            template = {}
+                    elif "charge_template" in msg.topic:
                         event = self.event_charge_template
                         if "ct"+str(index) in subdata.SubData.ev_charge_template_data:
                             template = dataclass_utils.asdict(copy.deepcopy(
                                 subdata.SubData.ev_charge_template_data["ct"+str(index)].data))
                             # Wenn eine Einzeleinstellung empfangen wird, muss das gesamte Template veröffentlicht
-                            # werden (pub_json=True), allerdings ohne Pläne. Diese sind in einem Extra-Topic und werden
-                            # immer als komplettes json empfangen (mit pub_json=False).
+                            # werden (pub_json=True), allerdings ohne Pläne. Diese sind in einem Extra-Topic.
                             try:
                                 template["chargemode"]["scheduled_charging"].pop("plans")
                             except KeyError:
@@ -171,16 +193,17 @@ class SetData:
                     # Wert, der aktualisiert werden soll, erstellen/finden und updaten
                     if event == self.event_cp_config:
                         key_list = msg.topic.split("/")[5:]
+                    elif event == self.event_scheduled_charging_plan or event == self.event_time_charging_plan:
+                        key_list = msg.topic.split("/")[-1:]
                     else:
                         key_list = msg.topic.split("/")[6:]
                     self._change_key(template, key_list, value)
                     # publish
-                    regex = re.search('(?!/)([0-9]*)(?=/|$)', msg.topic)
-                    if regex is None:
-                        raise Exception(f"Couldn't find index in {msg.topic}")
-                    index_pos = regex.end()
+                    index_pos = get_index_position(msg.topic)
                     if event == self.event_cp_config:
                         topic = msg.topic[:index_pos]+"/config"
+                    elif event == self.event_scheduled_charging_plan or event == self.event_time_charging_plan:
+                        topic = msg.topic[:get_second_index_position(msg.topic)]
                     else:
                         topic = msg.topic[:index_pos]
                     topic = topic.replace('set/', '', 1)
@@ -435,11 +458,15 @@ class SetData:
                     self._validate_value(msg, int, [(6, 32)], pub_json=True)
                 elif "/chargemode/pv_charging/max_soc" in msg.topic:
                     self._validate_value(msg, int, [(0, 101)], pub_json=True)
+                elif "/chargemode/scheduled_charging/plans/" in msg.topic and "/active" in msg.topic:
+                    self._validate_value(msg, bool, pub_json=True)
                 elif "/chargemode/scheduled_charging/plans" in msg.topic:
                     self._validate_value(msg, "json")
                 elif "/chargemode/scheduled_charging" in msg.topic:
                     self._validate_value(msg, "json", pub_json=True)
                 elif "/time_charging/active" in msg.topic:
+                    self._validate_value(msg, bool, pub_json=True)
+                elif "/time_charging/plans/" in msg.topic and "/active" in msg.topic:
                     self._validate_value(msg, bool, pub_json=True)
                 elif "/time_charging/plans" in msg.topic:
                     self._validate_value(msg, "json")
--- packages/helpermodules/utils/topic_parser.py
+++ packages/helpermodules/utils/topic_parser.py
@@ -6,10 +6,17 @@ from typing import Any
 def get_index(topic: str) -> str:
     """extrahiert den Index aus einem Topic (Zahl zwischen zwei // oder am Stringende)
     """
-    regex = re.search('(?!/)([0-9]*)(?=/|$)', topic)
+    regex = re.search(r"\d", topic)
     if regex is None:
         raise Exception(f"Couldn't find index in {topic}")
     return regex.group()
+
+
+def get_index_position(topic: str) -> int:
+    regex = re.search(r"\d", topic)
+    if regex is None:
+        raise Exception(f"Couldn't find index in {topic}")
+    return regex.start()
 
 
 def get_second_index(topic: str) -> str:
@@ -19,6 +26,13 @@ def get_second_index(topic: str) -> str:
     if regex is None:
         raise Exception(f"Couldn't find index in {topic}")
     return regex.group(2)
+
+
+def get_second_index_position(topic: str) -> int:
+    regex = re.search('^.+/([0-9]*)/.+/([0-9]+)/*.*$', topic)
+    if regex is None:
+        raise Exception(f"Couldn't find index in {topic}")
+    return regex.end(2)
 
 
 def decode_payload(payload) -> Any:
