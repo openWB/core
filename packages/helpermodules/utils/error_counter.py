2665db4a7fefe15f94095db5b7ca81c44f6b7c82
LKuemmel <lena.kuemmel@openwb.de>
2023-06-23 14:40:37 +0200

    fix default value

--- packages/helpermodules/utils/error_counter.py
+++ packages/helpermodules/utils/error_counter.py
@@ -5,7 +5,7 @@ log = logging.getLogger(__name__)
 
 
 class ErrorCounterContext:
-    def __init__(self, exceeded_msg: str, max_errors: int = 15, hide_exception: bool = False):
+    def __init__(self, exceeded_msg: str, max_errors: int = 2, hide_exception: bool = False):
         self.max_errors = max_errors
         self.hide_exception = hide_exception
         self.__error_counter = 0
