c575e4021af0527796d526d2ed788ba440f917e9
LKuemmel <lena.kuemmel@openwb.de>
2023-06-23 14:27:05 +0200

    fix message

--- packages/control/chargepoint/chargepoint.py
+++ packages/control/chargepoint/chargepoint.py
@@ -727,23 +727,23 @@ class Chargepoint:
         if self.data.get.rfid is not None:
             if data.data.optional_data.data.rfid.active:
                 rfid = self.data.get.rfid
-                if rfid in self.template.data.valid_tags or len(self.template.data.valid_tags) == 0:
-                    if self.data.get.rfid_timestamp is None:
-                        self.data.get.rfid_timestamp = timecheck.create_timestamp()
-                        Pub().pub(f"openWB/set/chargepoint/{self.num}/get/rfid_timestamp",
-                                  self.data.get.rfid_timestamp)
+                if self.data.get.rfid_timestamp is None:
+                    self.data.get.rfid_timestamp = timecheck.create_timestamp()
+                    Pub().pub(f"openWB/set/chargepoint/{self.num}/get/rfid_timestamp",
+                              self.data.get.rfid_timestamp)
+                    return
+                else:
+                    if (timecheck.check_timestamp(self.data.get.rfid_timestamp, 300) or
+                            self.data.get.plug_state is True):
                         return
                     else:
-                        if (timecheck.check_timestamp(self.data.get.rfid_timestamp, 300) or
-                                self.data.get.plug_state is True):
-                            return
-                        else:
-                            self.data.get.rfid_timestamp = None
+                        self.data.get.rfid_timestamp = None
+                        if rfid in self.template.data.valid_tags or len(self.template.data.valid_tags) == 0:
                             Pub().pub(f"openWB/set/chargepoint/{self.num}/get/rfid_timestamp", None)
                             msg = "Es ist in den letzten 5 Minuten kein EV angesteckt worden, dem " \
                                 f"der RFID-Tag/Code {rfid} zugeordnet werden kann. Daher wird dieser verworfen."
-                else:
-                    msg = f"Der Tag {rfid} ist an diesem Ladepunkt nicht gültig."
+                        else:
+                            msg = f"Der Tag {rfid} ist an diesem Ladepunkt nicht gültig."
             else:
                 msg = "RFID ist nicht aktiviert."
             self.data.get.rfid = None
