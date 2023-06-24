085024d40e4f3c19dc013d388a4281720735fddd
MartinRinas <martrin@microsoft.com>
2023-06-24 21:43:18 +0000

    retrieve scope from config

--- packages/modules/backup_clouds/onedrive/backup_cloud.py
+++ packages/modules/backup_clouds/onedrive/backup_cloud.py
@@ -9,25 +9,17 @@ from modules.backup_clouds.onedrive.config import OneDriveBackupCloud, OneDriveB
 from modules.common.abstract_device import DeviceDescriptor
 from modules.common.configurable_backup_cloud import ConfigurableBackupCloud
 import pathlib
+import base64
 
 
 log = logging.getLogger(__name__)
 
-# Define the scope of access
-scope = ["https://graph.microsoft.com/Files.ReadWrite"]
 
-# Define the authority and the token endpoint for MSA/Live accounts
-authority = "https://login.microsoftonline.com/consumers/"
-clientID = "e529d8d2-3b0f-4ae4-b2ba-2d9a2bba55b2"
-
-localbackuppath = '/var/www/html/op'
-
-# to-do move into one or more functions
-def get_tokens(persistent_tokencache: str) -> dict:
+def get_tokens(config: OneDriveBackupCloudConfiguration) -> dict:
     result = None
     cache = msal.SerializableTokenCache()
 
-    # to-do: add write to config after update
+    '''    # to-do: add write to config after update
     if os.path.exists("/var/www/html/openWB/packages/modules/backup_clouds/onedrive/my_cache.bin"):  # to do: read from config
         log.debug("reading token cache from file")
         cache.deserialize(open("/var/www/html/openWB/packages/modules/backup_clouds/onedrive/my_cache.bin", "r").read())
@@ -35,14 +27,14 @@ def get_tokens(persistent_tokencache: str) -> dict:
         log.debug("token cache not found")
     atexit.register(lambda: open("my_cache.bin", "w").write(cache.serialize())  # to-do: write to config
                     if cache.has_state_changed else None
-                    )
+                    )'''
 
-    #if persistent_tokencache:
-    #    cache.deserialize(persistent_tokencache)
+    if config.persistent_tokencache:
+        cache.deserialize(base64.b64decode(config.persistent_tokencache))
 
     # Create a public client application with msal
     log.debug("creating MSAL public client application")
-    app = msal.PublicClientApplication(client_id=clientID, authority=authority, token_cache=cache)
+    app = msal.PublicClientApplication(client_id=config.clientID, authority=config.authority, token_cache=cache)
 
     log.debug("getting accounts")
     accounts = app.get_accounts()
@@ -50,12 +42,14 @@ def get_tokens(persistent_tokencache: str) -> dict:
         chosen = accounts[0]  # assume that we only will have a single account in cache
         log.debug("selected account " + str(chosen["username"]))
         # Now let's try to find a token in cache for this account
-        result = app.acquire_token_silent(scopes=scope, account=chosen)
+        result = app.acquire_token_silent(scopes=config.scope, account=chosen)
 
     log.debug("done acquring tokens")
     if not result:  # We have no token for this account, so the end user shall sign-in
+        # to-do: stop execution if no authcode is provided, log error
 
-        flow = app.initiate_device_flow(scope)
+        flow = app.initiate_device_flow(config.scope)
+
         if "user_code" not in flow:
             raise ValueError(
                 "Fail to create device flow. Err: %s" % json.dumps(flow, indent=4))
@@ -84,19 +78,21 @@ def get_tokens(persistent_tokencache: str) -> dict:
 def upload_backup(config: OneDriveBackupCloudConfiguration, backup_filename: str, backup_file: bytes) -> None:
     # upload a single file to onedrive useing credentials from OneDriveBackupCloudConfiguration
     # https://docs.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_put_content?view=odsp-graph-online
-    # to-do: acquire tokens
-    # tokens = get_tokens(config.tokencache)
-    tokens = get_tokens("sudo token")
+    tokens = get_tokens(config)  # type: ignore
     log.debug("token object retrieved, access_token: %s", tokens.__len__)
+    log.debug("instantiate OneDrive connection")
     onedrive = OneDrive(access_token=tokens["access_token"])
-    log.debug("instantiated OneDrive object")
 
     localbackup = os.path.join(pathlib.Path().resolve(), 'data', 'backup', backup_filename)
-    remote_filename = backup_filename.replace(':','-')  # file won't upload when name contains ':'
-    
+    remote_filename = backup_filename.replace(':', '-')  # file won't upload when name contains ':'
+
+    if not config.backuppath.endswith("/"):
+        log.debug("fixing  missing ending slash in backuppath: " + config.backuppath)
+        config.backuppath = config.backuppath + "/"
+
+    log.debug("uploading file %s to OneDrive", backup_filename)
     onedrive.upload_item(item_path=(config.backuppath+remote_filename), file_path=localbackup,
                          conflict_behavior="replace")
-    log.debug("uploaded file %s to OneDrive", backup_filename)
 
 
 def create_backup_cloud(config: OneDriveBackupCloud):
