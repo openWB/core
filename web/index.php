086904ad75844b4470e711b584935f9d10e4c225
Lutz Bender <github@lutz-bender.de>
2023-06-30 09:57:27 +0200

    check for usage terms in main page

--- web/index.php
+++ web/index.php
@@ -1,8 +1,17 @@
 <?php
+define("BASE_WEB_PATH", "/openWB/web/");
+
+function redirect($target)
+{
+	header("Location: " . BASE_WEB_PATH . $target);
+	exit;
+}
+
 $updateInProgress = false;
 $bootInProgress = false;
 $isExtern = false;
 $dataProtectionAcknowledged = false;
+$usageTermsAcknowledged = false;
 
 // check if update.sh is still running
 $output = null;
@@ -18,48 +27,50 @@ exec("mosquitto_sub -t 'openWB/system/boot_done' -C 1 -W 1", $output, $resultCod
 if ($resultCode == 0 && $output[0] !== "true") {
 	$bootInProgress = true;
 }
-// if yes, show placeholder. If not, show theme
+// if updating or booting, show placeholder. If not, show theme
 if ($bootInProgress or $updateInProgress) {
 	//atreboot.sh or update.sh still in progress, wait 5 seconds and retry
-	include 'notReady.html';
-} else {
-	// check for acknowledgement of data protection
-	// $output = null;
-	// $resultCode = null;
-	// exec("mosquitto_sub -t 'openWB/system/dataprotection_acknowledged' -C 1 -W 1", $output, $resultCode);
-	// if($resultCode == 0 && $output[0] == "true"){
-	// 	$dataProtectionAcknowledged = true;
-	// }
-	// if ( ! $dataProtectionAcknowledged ) {
-	//	// load dataprotection page
-	// 	include 'settings/#/System/DataProtection';
-	// } else {
+	include $_SERVER["DOCUMENT_ROOT"] . BASE_WEB_PATH . "notReady.html";
+	exit;
+}
 
-	// check for external mode
-	$output = null;
-	$resultCode = null;
-	exec("mosquitto_sub -t 'openWB/general/extern' -C 1 -W 1", $output, $resultCode);
-	if ($resultCode == 0 && $output[0] == "true") {
-		$isExtern = true;
-	}
-	if ($isExtern) {
-		// load chargepoint only page
-		include 'isExtern.html';
-	} else {
-		// load normal UI
-		// check if theme cookie exists and theme is installed
-		// else set standard theme
-		if (!(isset($_COOKIE['openWBTheme']) === true) || !(is_dir('themes/' . $_COOKIE['openWBTheme']) === true)) {
-			$_COOKIE['openWBTheme'] = 'standard';
-		}
-		// expand expiring-date to now + 2 years
-		$expire = time() + (60 * 60 * 24 * 365 * 2);
-		setcookie('openWBTheme', $_COOKIE['openWBTheme'], $expire, '/openWB/');
-		if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/openWB/web/themes/' . $_COOKIE['openWBTheme'] . '/theme.php')) {
-			include 'themes/' . $_COOKIE['openWBTheme'] . '/theme.php';
-		} else {
-			include 'themes/' . $_COOKIE['openWBTheme'] . '/theme.html';
-		}
-	}
-	// }
+// check for acknowledgement of usage terms
+$output = null;
+$resultCode = null;
+exec("mosquitto_sub -t 'openWB/system/usage_terms_acknowledged' -C 1 -W 1", $output, $resultCode);
+if ($resultCode == 0 && $output[0] == "true") {
+	$usageTermsAcknowledged = true;
+}
+if (!$usageTermsAcknowledged) {
+	// load usage terms page
+	redirect("settings/#/System/LegalSettings");
+	exit;
+}
+
+// check for external mode
+$output = null;
+$resultCode = null;
+exec("mosquitto_sub -t 'openWB/general/extern' -C 1 -W 1", $output, $resultCode);
+if ($resultCode == 0 && $output[0] == "true") {
+	$isExtern = true;
+}
+if ($isExtern) {
+	// load chargepoint only page
+	include $_SERVER["DOCUMENT_ROOT"] . BASE_WEB_PATH . "isExtern.html";
+	exit;
+}
+
+// load normal UI
+// check if theme cookie exists and theme is installed
+// else set standard theme
+if (!(isset($_COOKIE["openWBTheme"]) === true) || !(is_dir("themes/" . $_COOKIE["openWBTheme"]) === true)) {
+	$_COOKIE["openWBTheme"] = "standard";
+}
+// expand expiring-date to now + 2 years
+$expire = time() + (60 * 60 * 24 * 365 * 2);
+setcookie("openWBTheme", $_COOKIE["openWBTheme"], $expire, "/openWB/");
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . BASE_WEB_PATH . "themes/" . $_COOKIE["openWBTheme"] . "/theme.php")) {
+	include  $_SERVER["DOCUMENT_ROOT"] . BASE_WEB_PATH . "themes/" . $_COOKIE["openWBTheme"] . "/theme.php";
+} else {
+	include  $_SERVER["DOCUMENT_ROOT"] . BASE_WEB_PATH . "themes/" . $_COOKIE["openWBTheme"] . "/theme.html";
 }
