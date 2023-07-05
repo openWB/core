71ccf37af372ff15cfaeaabb4ad81bee5d0b818c
LKuemmel <lena.kuemmel@openwb.de>
2023-07-05 12:38:25 +0200

    draft

---
+++ packages/helpermodules/conv_1_9/id_mapping.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class MapId:
+    cp1: Optional[int] = 0
+    cp2: Optional[int] = 0
+    cp3: Optional[int] = 0
+    cp4: Optional[int] = 0
+    cp5: Optional[int] = 0
+    cp6: Optional[int] = 0
+    cp7: Optional[int] = 0
+    cp8: Optional[int] = 0
+    evu: Optional[int] = 0
+    pv1: Optional[int] = 0
+    pv2: Optional[int] = 0
+    bat: Optional[int] = 0
+    consumer1: Optional[int] = 0
+    consumer2: Optional[int] = 0
+    consumer3: Optional[int] = 0
+    smarthome_device1: Optional[int] = 0
+    smarthome_device2: Optional[int] = 0
+    smarthome_device3: Optional[int] = 0
+    smarthome_device4: Optional[int] = 0
+    smarthome_device5: Optional[int] = 0
+    smarthome_device6: Optional[int] = 0
+    smarthome_device7: Optional[int] = 0
+    smarthome_device8: Optional[int] = 0
+    smarthome_device9: Optional[int] = 0
+    smarthome_device10: Optional[int] = 0
