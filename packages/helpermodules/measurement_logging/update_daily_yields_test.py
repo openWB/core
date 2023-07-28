8cf653d8ec2bbe33001b0a1e48af6877f4423698
LKuemmel <lena.kuemmel@openwb.de>
2023-07-28 14:01:42 +0200

    diagramm data

--- packages/helpermodules/measurement_logging/update_daily_yields_test.py
+++ packages/helpermodules/measurement_logging/update_daily_yields_test.py
@@ -1,15 +1,31 @@
 import pytest
 
-from helpermodules.measurement_logging.conftest import EXPECTED, TOTALS
 from helpermodules.measurement_logging.update_daily_yields import update_module_yields
 
 
-def test_update_module_yields(mock_pub):
+def test_update_module_yields(daily_log_totals, mock_pub):
     # setup and execution
-    [update_module_yields(type, TOTALS) for type in ("bat", "counter", "cp", "pv")]
+    [update_module_yields(type, daily_log_totals) for type in ("bat", "counter", "cp", "pv")]
 
     # evaluation
-    for topic, value in EXPECTED.items():
+    expected = {
+        "openWB/set/bat/get/daily_imported": 0,
+        "openWB/set/bat/get/daily_exported": 550.857,
+        "openWB/set/bat/2/get/daily_imported": 0,
+        "openWB/set/bat/2/get/daily_exported": 550.857,
+        "openWB/set/counter/0/get/daily_imported": 1492.011,
+        "openWB/set/counter/0/get/daily_exported": 0,
+        "openWB/set/chargepoint/get/daily_imported": 1919.625999999999,
+        "openWB/set/chargepoint/get/daily_exported": 0,
+        "openWB/set/chargepoint/4/get/daily_imported": 383.942,
+        "openWB/set/chargepoint/4/get/daily_exported": 0,
+        "openWB/set/chargepoint/5/get/daily_imported": 191.928,
+        "openWB/set/chargepoint/5/get/daily_exported": 0,
+        "openWB/set/chargepoint/6/get/daily_imported": 0,
+        "openWB/set/chargepoint/6/get/daily_exported": 0,
+        "openWB/set/pv/get/daily_exported": 251,
+        "openWB/set/pv/1/get/daily_exported": 251}
+    for topic, value in expected.items():
         for call in mock_pub.mock_calls:
             try:
                 if call.args[0] == topic:
--- packages/main.py
+++ packages/main.py
@@ -123,7 +123,7 @@ class HandlerAlgorithm:
     @exit_after(10)
     def handler_midnight(self):
         try:
-            measurement_log.save_log("monthly")
+            save_log("monthly")
         except KeyboardInterrupt:
             log.critical("Ausführung durch exit_after gestoppt: "+traceback.format_exc())
         except Exception:
@@ -144,7 +144,7 @@ def schedule_jobs():
     [schedule.every().minute.at(f":{i:02d}").do(soc.update).tag("algorithm") for i in range(0, 60, 10)]
     [schedule.every().minute.at(f":{i:02d}").do(smarthome_handler).tag("algorithm") for i in range(0, 60, 5)]
     [schedule.every().hour.at(f":{i:02d}").do(handler.handler5Min) for i in range(0, 60, 5)]
-    [schedule.every().hour.at(f":{i:02d}").do(handler.handler5MinAlgorithm).tag("algorithm") for i in range(1, 60, 5)]
+    [schedule.every().hour.at(f":{i:02d}").do(handler.handler5MinAlgorithm).tag("algorithm") for i in range(0, 60, 5)]
     schedule.every().day.at("00:00:00").do(handler.handler_midnight).tag("algorithm")
     schedule.every().day.at(f"0{randrange(0, 5)}:{randrange(0, 59):02d}:{randrange(0, 59):02d}").do(
         handler.handler_random_nightly)
