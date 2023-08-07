f6d5cf4062af3322e629af0d5a03b5012438fbf9
Det <76627235+DetMoerk@users.noreply.github.com>
2023-08-07 18:52:21 +0200

    Cleanup MercedesEQ Soc

--- packages/modules/vehicles/mercedeseq/soc.py
+++ packages/modules/vehicles/mercedeseq/soc.py
@@ -1,12 +1,8 @@
-from typing import Union
 import logging
-from dataclass_utils import dataclass_from_dict
-from modules.common import store
 from modules.common.abstract_device import DeviceDescriptor
-from modules.common.abstract_soc import AbstractSoc
-from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.abstract_soc import SocUpdateData
+from modules.common.configurable_vehicle import ConfigurableVehicle
 from modules.common.component_state import CarState
-from modules.common.fault_state import ComponentInfo
 from modules.vehicles.mercedeseq.config import MercedesEQSoc
 import modules.vehicles.mercedeseq.api as api
 
@@ -14,26 +10,15 @@ import modules.vehicles.mercedeseq.api as api
 log = logging.getLogger("soc."+__name__)
 
 
-class Soc(AbstractSoc):
-    def __init__(self, device_config: Union[dict, MercedesEQSoc], vehicle: int):
-        self.config = dataclass_from_dict(MercedesEQSoc, device_config)
-        self.vehicle = vehicle
-        self.store = store.get_car_value_store(self.vehicle)
-        self.component_info = ComponentInfo(self.vehicle, self.config.name, "vehicle")
+def fetch(vehicle_config: MercedesEQSoc, soc_update_data: SocUpdateData, vehicle) -> CarState:
+    soc, range = api.fetch_soc(vehicle_config, vehicle)
+    return CarState(soc, range)
 
-    def update(self, charge_state: bool = False) -> None:
-        # # if self.config.configuration.code == None and self.config.configuration.token.access_token == None:
-        # if  self.config.configuration.token.access_token == None:
-        #     # error: "Bitte Link in der Konfiguration anklicken" triggert Aufruf der Callback-URL
-        #     pass
-        # # elif self.config.configuration.code != None and self.config.configuration.token.access_token == None:
-        #     # auth.py um Token abzuholen
 
-        # else:
-        with SingleComponentUpdateContext(self.component_info):
-            soc, range = api.fetch_soc(self.config, self.vehicle)
-            # log.info("eq: vehicle="+str(self.vehicle) + ", return: soc=" + str(soc)+", range=" + str(range))
-            self.store.set(CarState(soc, range))
+def create_vehicle(vehicle_config: MercedesEQSoc, vehicle: int):
+    def updater(soc_update_data: SocUpdateData) -> CarState:
+        return fetch(vehicle_config, soc_update_data, vehicle)
+    return ConfigurableVehicle(vehicle_config=vehicle_config, component_updater=updater, vehicle=vehicle)
 
 
 device_descriptor = DeviceDescriptor(configuration_factory=MercedesEQSoc)
