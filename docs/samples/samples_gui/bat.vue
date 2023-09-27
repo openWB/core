977d36cb23a2c15bf396e2dd1634f5ab7cbe856a
LKuemmel <lena.kuemmel@openwb.de>
2023-09-27 13:31:11 +0200

    Wiki-Beitrag Einstellungs-Seiten

---
+++ docs/samples/samples_gui/bat.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="device-sample-bat">
+		<openwb-base-heading>
+			Einstellungen für Sample Batteriespeicher
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-alert subtype="info">
+			Diese Komponente benötigt keine Einstellungen.
+		</openwb-base-alert>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceSampleBat",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
---
+++ docs/samples/samples_gui/counter.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="device-sample-counter">
+		<openwb-base-heading>
+			Einstellungen für Sample Zähler
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-alert subtype="info">
+			Diese Komponente benötigt keine Einstellungen.
+		</openwb-base-alert>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceSampleCounter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
