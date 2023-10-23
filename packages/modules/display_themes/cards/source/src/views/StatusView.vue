537da5298291e40cf22a82d87beb9b967060f9b5
Lutz Bender <github@lutz-bender.de>
2023-10-23 15:51:23 +0200

    add cards display status view

--- packages/modules/display_themes/cards/source/src/views/StatusView.vue
+++ packages/modules/display_themes/cards/source/src/views/StatusView.vue
@@ -11,9 +11,73 @@ export default {
       mqttStore: useMqttStore(),
     };
   },
+  methods: {
+    reloadDisplay() {
+      console.debug("reload requested");
+      location.reload();
+    },
+  },
 };
 </script>
 
-<template>Status</template>
+<template>
+  <i-container>
+    <i-form>
+      <i-row>
+        <i-column>
+          <i-form-group>
+            <i-form-label>
+              IP-Adresse
+            </i-form-label>
+            <i-input
+              id="input_system_ip"
+              plaintext
+              :model-value="mqttStore.getSystemIp"
+            />
+            <i-form-label>
+              Systemzeit
+            </i-form-label>
+            <i-input
+              id="input_system_time"
+              plaintext
+              :model-value="mqttStore.getSystemTime"
+            />
+            <i-form-label>
+              Version
+            </i-form-label>
+            <i-input
+              id="input_system_version"
+              plaintext
+              :model-value="mqttStore.getSystemVersion"
+            />
+            <i-form-label>
+              Installierte Version (Details)
+            </i-form-label>
+            <i-input
+              id="input_system_branch"
+              plaintext
+              :model-value="mqttStore.getSystemCurrentCommit"
+            />
+            <i-form-label>
+              Entwicklungszweig
+            </i-form-label>
+            <i-input
+              id="input_system_branch"
+              plaintext
+              :model-value="mqttStore.getSystemBranch"
+            />
+          </i-form-group>
+        </i-column>
+      </i-row>
+      <i-row class="_margin-top:5">
+        <i-column>
+          <i-form-group v-if="!changesLocked">
+            <i-button color="danger" @click="reloadDisplay()">Display neu laden</i-button>
+          </i-form-group>
+        </i-column>
+      </i-row>
+    </i-form>
+  </i-container>
+</template>
 
 <style scoped></style>
