46e14918b36ab2cbba7cfeaa176a230640912b60
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2023-10-25 16:45:55 +0200

    Fronius secondary inverter (#1192)
    * secondary inverter
    
    * add test
    
    * review

--- packages/modules/devices/fronius/inverter_secondary.py
+++ packages/modules/devices/fronius/inverter_secondary.py
@@ -4,18 +4,18 @@ from typing import Dict, Union
 from dataclass_utils import dataclass_from_dict
 from modules.common.component_state import InverterState
 from modules.common.component_type import ComponentDescriptor
-from modules.common.fault_state import ComponentInfo
+from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.simcount import SimCounter
 from modules.common.store import get_inverter_value_store
-from modules.devices.fronius.config import FroniusInverterSetup
+from modules.devices.fronius.config import FroniusSecondaryInverterSetup
 
 
-class FroniusInverter:
+class FroniusSecondaryInverter:
     def __init__(self,
                  device_id: int,
-                 component_config: Union[Dict, FroniusInverterSetup]) -> None:
+                 component_config: Union[Dict, FroniusSecondaryInverterSetup]) -> None:
         self.__device_id = device_id
-        self.component_config = dataclass_from_dict(FroniusInverterSetup, component_config)
+        self.component_config = dataclass_from_dict(FroniusSecondaryInverterSetup, component_config)
         self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="pv")
         self.store = get_inverter_value_store(self.component_config.id)
         self.component_info = ComponentInfo.from_component_config(self.component_config)
@@ -26,7 +26,13 @@ class FroniusInverter:
             power = 0.0
         else:
             try:
-                power = float(response["Body"]["Data"]["Site"]["P_PV"]) * -1
+                secondary_data = response["Body"]["Data"]["SecondaryMeters"][str(
+                    self.component_config.configuration.id)]
+                if secondary_data["Category"] == "METER_CAT_WR":
+                    power = float(secondary_data["P"]) * -1
+                else:
+                    raise FaultState.error(f"Sekundäres Gerät {self.component_config.configuration.id} "
+                                           "ist kein Wechselrichter.")
             except TypeError:
                 # Ohne PV Produktion liefert der WR 'null', ersetze durch Zahl 0
                 power = 0
@@ -39,4 +45,4 @@ class FroniusInverter:
         ))
 
 
-component_descriptor = ComponentDescriptor(configuration_factory=FroniusInverterSetup)
+component_descriptor = ComponentDescriptor(configuration_factory=FroniusSecondaryInverterSetup)
