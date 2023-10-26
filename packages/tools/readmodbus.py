65a3813793ff0313ff2035ee1090c68c1b4f5255
LKuemmel <lena.kuemmel@openwb.de>
2023-10-26 14:01:01 +0200

    read/write modbus

---
+++ packages/tools/readmodbus.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+from pymodbus.client.sync import ModbusSerialClient
+
+seradd = str(sys.argv[1])
+modbusid = int(sys.argv[2])
+readreg = int(sys.argv[3])
+reganzahl = int(sys.argv[4])
+
+client = ModbusSerialClient(method="rtu", port=seradd, baudrate=9600, stopbits=1, bytesize=8, timeout=1)
+request = client.read_holding_registers(readreg, reganzahl, unit=modbusid)
+if request.isError():
+    print('Modbus Error:', request)
+else:
+    print(request.registers[0])
