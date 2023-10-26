951daa78cfd9007be110dd124b7ecc3c62dfee18
LKuemmel <lena.kuemmel@openwb.de>
2023-10-26 15:05:36 +0200

    fix add unused evse current for precise current

--- packages/control/algorithm/surplus_controlled.py
+++ packages/control/algorithm/surplus_controlled.py
@@ -109,9 +109,10 @@ class SurplusControlled:
     def _add_unused_evse_current(self, limited_current, chargepoint: Chargepoint) -> float:
         """Wenn Autos nicht die volle Ladeleistung nutzen, wird unnötig eingespeist. Dann kann um den noch nicht
         genutzten Sollstrom hochgeregelt werden."""
-        if chargepoint.data.get.evse_current:
-            current_with_offset = limited_current + \
-                max(chargepoint.data.get.evse_current - max(chargepoint.data.get.currents), 0)
+        evse_current = chargepoint.data.get.evse_current
+        if evse_current:
+            formatted_evse_current = evse_current if evse_current < 32 else evse_current / 1000
+            current_with_offset = limited_current + max(formatted_evse_current - max(chargepoint.data.get.currents), 0)
             current = min(current_with_offset, chargepoint.data.control_parameter.required_current)
             if current != limited_current:
                 log.debug(f"Ungenutzten Sollstrom aufschlagen ergibt {current}A.")
