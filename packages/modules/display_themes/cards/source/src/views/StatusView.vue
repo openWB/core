4b8bb43643ee41960b0e6dd1ac03e8f9bc9f2040
Sebastian Kessler <sebastian.kessler@sebkit.de>
2023-10-27 12:19:56 +0200

    move status view in dash-noard-card and rezise columns

--- packages/modules/display_themes/cards/source/src/views/StatusView.vue
+++ packages/modules/display_themes/cards/source/src/views/StatusView.vue
@@ -3,6 +3,7 @@ import { useMqttStore } from "@/stores/mqtt.js";
 import ReloadButton from "@/components/Status/ReloadButton.vue";
 import RebootButton from "@/components/Status/RebootButton.vue";
 import ShutdownButton from "@/components/Status/ShutdownButton.vue";
+import DashBoardCard from "@/components/DashBoardCard.vue";
 
 export default {
   name: "StatusView",
@@ -13,6 +14,7 @@ export default {
     ReloadButton,
     RebootButton,
     ShutdownButton,
+    DashBoardCard,
   },
   data() {
     return {
@@ -23,41 +25,78 @@ export default {
 </script>
 
 <template>
+  <div class="status-card-wrapper">
+  <dash-board-card
+    color="primary"
+  >
+    <template #headerLeft>
+      Status
+    </template>
   <i-container>
     <i-form>
       <i-row>
         <i-column>
           <i-form-group>
-            <i-form-label> IP-Adresse </i-form-label>
-            <i-input
-              id="input_system_ip"
-              plaintext
-              :model-value="mqttStore.getSystemIp"
-            />
-            <i-form-label> Systemzeit </i-form-label>
-            <i-input
-              id="input_system_time"
-              plaintext
-              :model-value="mqttStore.getSystemTime"
-            />
-            <i-form-label> Version </i-form-label>
-            <i-input
-              id="input_system_version"
-              plaintext
-              :model-value="mqttStore.getSystemVersion"
-            />
-            <i-form-label> Installierte Version (Details) </i-form-label>
-            <i-input
-              id="input_system_commit"
-              plaintext
-              :model-value="mqttStore.getSystemCurrentCommit"
-            />
-            <i-form-label> Entwicklungszweig </i-form-label>
-            <i-input
-              id="input_system_branch"
-              plaintext
-              :model-value="mqttStore.getSystemBranch"
-            />
+            <i-row class="_margin-top:2">
+              <i-column id="label-column">
+                <i-form-label> IP-Adresse </i-form-label>
+              </i-column>
+              <i-column class="_padding-left:0">
+                <i-input
+                  id="input_system_ip"
+                  plaintext
+                  :model-value="mqttStore.getSystemIp"
+                />
+              </i-column>
+            </i-row >
+            <i-row class="_margin-top:1">
+              <i-column id="label-column">
+                <i-form-label> Systemzeit </i-form-label>
+              </i-column>
+              <i-column class="_padding-left:0">
+                <i-input
+                  id="input_system_time"
+                  plaintext
+                  :model-value="mqttStore.getSystemTime"
+                />
+              </i-column>
+            </i-row>       
+            <i-row class="_margin-top:1">
+              <i-column id="label-column">
+                <i-form-label> Version </i-form-label>
+              </i-column>
+              <i-column class="_padding-left:0">
+                <i-input
+                  id="input_system_version"
+                  plaintext
+                  :model-value="mqttStore.getSystemVersion"
+                />
+              </i-column>
+            </i-row>
+            <i-row class="_margin-top:1">
+              <i-column id="label-column">
+                <i-form-label> Installierte Version (Details) </i-form-label>
+              </i-column>
+              <i-column class="_padding-left:0">
+                <i-input
+                  id="input_system_commit"
+                  plaintext
+                  :model-value="mqttStore.getSystemCurrentCommit"
+                />
+              </i-column>
+            </i-row>
+            <i-row class="_margin-top:1">
+              <i-column id="label-column">
+                <i-form-label> Entwicklungszweig </i-form-label>
+              </i-column>
+              <i-column class="_padding-left:0">
+                <i-input
+                  id="input_system_branch"
+                  plaintext
+                  :model-value="mqttStore.getSystemBranch"
+                />
+              </i-column>
+            </i-row>
           </i-form-group>
         </i-column>
       </i-row>
@@ -76,6 +115,19 @@ export default {
       </i-row>
     </i-form>
   </i-container>
+  </dash-board-card>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#label-column.column{
+  max-width: 230px;;  
+}
+
+.status-card-wrapper {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(36rem, 1fr));
+  grid-gap: var(--spacing);
+}
+</style>
+
