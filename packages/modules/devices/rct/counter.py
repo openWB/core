a519720e7d8a533671b047e1cd34a8ae90a479f6
LKuemmel <lena.kuemmel@openwb.de>
2023-10-27 12:43:24 +0200

    comment

--- packages/modules/devices/rct/counter.py
+++ packages/modules/devices/rct/counter.py
@@ -48,6 +48,7 @@ class RctCounter:
         )
         self.store.set(counter_state)
         if (stat1.value + stat2.value + stat3.value + stat4.value) > 0:
+            # Werte werden trotz Fehlercode übermittelt.
             raise FaultState.warning(
                 f"Alarm Status Speicher ist ungleich 0. Status 1: {stat1.value}, Status 2: {stat2.value}, "
                 f"Status 3: {stat3.value}, Status 4: {stat4.value},")
