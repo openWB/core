baebde590572887de2a12024ff7781adf93f1455
LKuemmel <lena.kuemmel@openwb.de>
2023-12-05 16:26:56 +0100

    add veltego

--- packages/modules/electricity_tariffs/voltego/tariff.py
+++ packages/modules/electricity_tariffs/voltego/tariff.py
@@ -1,68 +1,75 @@
 #!/usr/bin/env python3
 import datetime
 import logging
-
+from typing import Dict
 import pytz
+
+from dataclass_utils import asdict
 from helpermodules import timecheck
 from helpermodules.pub import Pub
-
 from modules.common import configurable_tariff, req
 from modules.common.abstract_device import DeviceDescriptor
 from modules.common.component_state import TariffState
-from modules.electricity_tariffs.voltego.config import VoltegoTariff, VoltegoTariffConfiguration, VoltegoToken
+from modules.electricity_tariffs.voltego.config import VoltegoTariff, VoltegoToken
 
 log = logging.getLogger(__name__)
 
 
-def validate_token(config: VoltegoTariffConfiguration) -> None:
-    expiration = config.token.created_at + config.token.expires_in
-    log.debug("No need to authenticate. Valid token already present.")
-    if timecheck.create_timestamp_unix() > expiration:
-        log.debug("Access token expired. Refreshing token.")
+def validate_token(config: VoltegoTariff) -> None:
+    if config.configuration.token.expires_in:
+        expiration = config.configuration.token.created_at + config.configuration.token.expires_in
+        log.debug("No need to authenticate. Valid token already present.")
+        if timecheck.create_timestamp_unix() > expiration:
+            log.debug("Access token expired. Refreshing token.")
+            _refresh_token(config)
+    else:
         _refresh_token(config)
 
 
-def _refresh_token(config: VoltegoTariffConfiguration):
+def _refresh_token(config: VoltegoTariff):
     response = req.get_http_session().post(
         "https://api.voltego.de/oauth/token",
         data={"grant_type": "client_credentials", "scope": 'market_data:read'},
-        auth=(config.client_id, config.client_secret),
+        auth=(config.configuration.client_id, config.configuration.client_secret),
     ).json()
-    config.token = VoltegoToken(token=response["access_token"],
-                                expires_in=response["expires_in"],
-                                created_at=timecheck.create_timestamp_unix())
-    Pub().pub("openWB/set/optional/et/provider", config)
+    config.configuration.token = VoltegoToken(token=response["access_token"],
+                                              expires_in=response["expires_in"],
+                                              created_at=timecheck.create_timestamp_unix())
+    Pub().pub("openWB/set/optional/et/provider", asdict(config))
 
 
-def fetch(config: VoltegoTariffConfiguration) -> None:
-    if validate_token():
-        _refresh_token(config)
+def fetch(config: VoltegoTariff) -> None:
+    validate_token(config)
+    # start_date von voller Stunde sonst liefert die API die nächste Stunde
     start_date = datetime.datetime.fromtimestamp(
-        timecheck.create_unix_timestamp_current_full_hour()).astimezone(pytz.timezone("Europe/Berlin")).isoformat(sep="T", timespec="seconds")
+        timecheck.create_unix_timestamp_current_full_hour()).astimezone(
+            pytz.timezone("Europe/Berlin")).isoformat(sep="T", timespec="seconds")
     if datetime.datetime.today().astimezone(pytz.timezone("Europe/Berlin")).dst().total_seconds()/3600:
-        timezone = "UTC(+1)"
+        # Sommerzeit
+        timezone = "UTC+2:00"
     else:
-        timezone = "UTC"
-    response = req.get_http_session().get(
-        f"https://api.voltego.de/market_data/day_ahead/DE_LU/60?from={start_date}&tz={timezone}",
+        timezone = "UTC+1:00"
+    raw_prices = req.get_http_session().get(
+        "https://api.voltego.de/market_data/day_ahead/DE_LU/60",
         headers={"Content-Type": "application/json;charset=UTF-8",
-                 "Authorization": f'Bearer {config.token.token}'},
-        params={"from": "2023-12-04T08:00:00+00:00", "tz": "UTC+1:00"}
-    ).json()
-# /1000000  # €/MWh -> €/Wh
-    return TariffState(prices=response["price_list"])
+                 "Authorization": f'Bearer {config.configuration.token.token}'},
+        params={"from": start_date, "tz": timezone}
+    ).json()["elements"]
+    prices: Dict[int, float] = {}
+    for data in raw_prices:
+        formatted_price = data["price"]/1000000  # €/MWh -> €/Wh
+        timestamp = datetime.datetime.fromisoformat(data["begin"]).astimezone(
+            pytz.timezone("Europe/Berlin")).timestamp()
+        prices.update({int(timestamp): formatted_price})
+    return prices
 
 
 def create_electricity_tariff(config: VoltegoTariff):
-    if config.configuration.token.expires_in:
-        if validate_token():
-            _refresh_token(config.configuration)
-    else:
-        _refresh_token(config.configuration)
+    validate_token(config)
 
     def updater():
-        return fetch(config.configuration)
-    return configurable_tariff(config=config, component_updater=updater)
+        return TariffState(prices=fetch(config))
+    return configurable_tariff.ConfigurableElectricityTariff(config=config, component_updater=updater)
 
 
 device_descriptor = DeviceDescriptor(configuration_factory=VoltegoTariff)
