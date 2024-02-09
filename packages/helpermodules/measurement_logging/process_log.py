2cad26f96d82ae38fcbe5a79fd5802379297d889
LKuemmel <lena.kuemmel@openwb.de>
2024-02-09 14:47:22 +0100

    fix negative energy source

--- packages/helpermodules/measurement_logging/process_log.py
+++ packages/helpermodules/measurement_logging/process_log.py
@@ -310,11 +310,21 @@ def analyse_percentage(entry):
                 grid_exported = counter["energy_exported"]
         consumption = grid_imported - grid_exported + pv + bat_exported - bat_imported + cp_exported
         try:
+            if grid_exported > pv:
+                # Ins Netz eingespeiste Leistung kam nicht von der PV-Anlage sondern aus dem Speicher
+                consumption += grid_exported - pv
+            elif bat_imported > pv:
+                # Die geladene Energie des Speichers kam nicht von der PV-Anlage sondern aus dem Netz
+                consumption += bat_imported - pv
+            grid_energy_source = format(grid_imported / consumption)
+            cp_energy_source = format(cp_exported/consumption)
+            bat_energy_source = format(bat_exported/consumption)
+            pv_energy_source = format(1 - grid_energy_source - bat_energy_source - cp_energy_source)
             entry["energy_source"] = {
-                "grid": format(grid_imported / consumption),
-                "pv": format((pv - grid_exported - bat_imported) / consumption),
-                "bat": format(bat_exported/consumption),
-                "cp": format(cp_exported/consumption)}
+                "grid": grid_energy_source,
+                "pv": pv_energy_source,
+                "bat": bat_energy_source,
+                "cp": cp_energy_source}
         except ZeroDivisionError:
             entry["energy_source"] = {"grid": 0, "pv": 0, "bat": 0, "cp": 0}
         for source in ("grid", "pv", "bat", "cp"):
--- packages/helpermodules/measurement_logging/process_log_test.py
+++ packages/helpermodules/measurement_logging/process_log_test.py
@@ -19,7 +19,7 @@ def test_get_totals(daily_log_sample, daily_log_totals):
 def test_analyse_percentage(daily_log_entry_kw):
     # setup
     expected = deepcopy(daily_log_entry_kw)
-    expected.update({"energy_source":  {'bat': 0.2398, 'cp': 0.0, 'grid': 0.6504, 'pv': 0.1099}})
+    expected.update({"energy_source":  {'bat': 0.2398, 'cp': 0.0, 'grid': 0.6504, 'pv': 0.1098}})
     expected["cp"]["all"].update({
         "energy_imported_bat": 0.23,
         "energy_imported_cp": 0.0,
