b89ca9a8637c3b61df3c4f6a3275d7d9a45ee4a1
vuffiraa72 <u.mersewsky+github@gmail.com>
2024-02-16 19:25:51 +0100

    fix tests to match pv charging

--- packages/control/loadmanagement_test.py
+++ packages/control/loadmanagement_test.py
@@ -6,17 +6,18 @@ from control.loadmanagement import LimitingValue, Loadmanagement
 
 
 @pytest.mark.parametrize(
-    "available_currents, raw_power_left, expected_currents",
+    "available_currents, voltage, raw_power_left, expected_currents",
     [
-        pytest.param([5, 10, 15], 6900, ([5, 10, 15], None)),
-        pytest.param([5, 10, 25], 1000, ([0.5434782608695652, 1.0869565217391304,
+        pytest.param([5, 10, 15], 230, 6900, ([5, 10, 15], None)),
+        pytest.param([5, 10, 25], 230, 1000, ([0.5434782608695652, 1.0869565217391304,
                      2.717391304347826], LimitingValue.POWER)),
-        pytest.param([5, 10, 25], 5000, ([2.717391304347826, 5.434782608695652,
+        pytest.param([5, 10, 25], 230, 5000, ([2.717391304347826, 5.434782608695652,
                      13.58695652173913], LimitingValue.POWER)),
     ])
-def test_limit_by_power(available_currents: List[float], raw_power_left: float, expected_currents: List[float]):
+def test_limit_by_power(available_currents: List[float], voltage: float, raw_power_left: float,
+                        expected_currents: List[float]):
     # setup & evaluation
-    currents = Loadmanagement()._limit_by_power(available_currents, raw_power_left, None)
+    currents = Loadmanagement()._limit_by_power(available_currents, voltage, raw_power_left, None)
 
     # assertion
     assert currents == expected_currents
