176414a75749130d25ec5edf3ccfc64b1d2c1ef6
LKuemmel <lena.kuemmel@openwb.de>
2024-03-06 14:43:35 +0100

    flake8

--- packages/modules/chargepoints/openwb_series2_satellit/chargepoint_module.py
+++ packages/modules/chargepoints/openwb_series2_satellit/chargepoint_module.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# import asyncio
 import logging
-# import telnetlib3
-import telnetlib
 import time
-# import time
 from typing import Optional
 
 from helpermodules.utils.error_counter import ErrorCounterContext
@@ -121,41 +117,3 @@ class ChargepointModule(AbstractChargepoint):
 
 
 chargepoint_descriptor = DeviceDescriptor(configuration_factory=OpenWBseries2Satellit)
-
-# Telnetlib
-# Verbindungen werden nicht geschlossen
-# def get_or_create_eventloop() -> asyncio.AbstractEventLoop:
-#     try:
-#         return asyncio.get_event_loop()
-#     except RuntimeError:
-#         loop = asyncio.new_event_loop()
-#         asyncio.set_event_loop(loop)
-#         return asyncio.get_event_loop()
-# loop = get_or_create_eventloop()
-# coro = telnetlib3.open_connection(self.config.configuration.ip_address, 8898, shell=self._shell)
-# reader, writer = loop.run_until_complete(coro)
-# loop.run_until_complete(writer.protocol.waiter_closed)
-# time.sleep(2)
-# writer.close()
-# reader.close()
-
-# async def _shell(self, reader: telnetlib3.TelnetReader, writer: telnetlib3.TelnetWriter):
-#     for i in range(0, 3):
-#         try:
-#             outp = await asyncio.wait_for(reader.readline(), timeout=3)
-#         except asyncio.exceptions.TimeoutError:
-#             # writer.close()
-#             raise FaultState.error(
-#                 "Firmware des openWB satellit ist nicht mit openWB software2 kompatibel. "
-#                 "Bitte den Support kontaktieren.")
-#         if not outp:
-#             # End of File
-#             break
-#         for version in self.VALID_VERSIONS:
-#             if version in outp:
-#                 self.version = True
-#                 log.debug("Firmware des openWB satellit ist mit openWB software2 kompatibel.")
-#                 return
-#     else:
-#         self.version = False
-#         raise ValueError
