18e1f3055c639cc91dbaa69e63835ac505348496
LKuemmel <lena.kuemmel@openwb.de>
2024-03-07 09:33:30 +0100

    error handling

--- packages/modules/chargepoints/openwb_series2_satellit/chargepoint_module.py
+++ packages/modules/chargepoints/openwb_series2_satellit/chargepoint_module.py
@@ -123,14 +123,27 @@ class ChargepointModule(AbstractChargepoint):
                         self._validate_version()
 
     def switch_phases(self, phases_to_use: int, duration: int) -> None:
-        if phases_to_use == 1:
-            self._client.client.delegate.write_register(0x0001, 256, unit=self.ID_PHASE_SWITCH_UNIT)
-            time.sleep(1)
-            self._client.client.delegate.write_register(0x0001, 512, unit=self.ID_PHASE_SWITCH_UNIT)
-        else:
-            self._client.client.delegate.write_register(0x0002, 512, unit=self.ID_PHASE_SWITCH_UNIT)
-            time.sleep(1)
-            self._client.client.delegate.write_register(0x0002, 256, unit=self.ID_PHASE_SWITCH_UNIT)
+        if self.version is not None:
+            with SingleComponentUpdateContext(self.fault_state, update_always=False):
+                with self.__client_error_context:
+                    try:
+                        with SeriesHardwareCheckContext(self._client):
+                            with self._client.client:
+                                if phases_to_use == 1:
+                                    self._client.client.delegate.write_register(
+                                        0x0001, 256, unit=self.ID_PHASE_SWITCH_UNIT)
+                                    time.sleep(1)
+                                    self._client.client.delegate.write_register(
+                                        0x0001, 512, unit=self.ID_PHASE_SWITCH_UNIT)
+                                else:
+                                    self._client.client.delegate.write_register(
+                                        0x0002, 512, unit=self.ID_PHASE_SWITCH_UNIT)
+                                    time.sleep(1)
+                                    self._client.client.delegate.write_register(
+                                        0x0002, 256, unit=self.ID_PHASE_SWITCH_UNIT)
+                    except AttributeError:
+                        self._create_client()
+                        self._validate_version()
 
 
 chargepoint_descriptor = DeviceDescriptor(configuration_factory=OpenWBseries2Satellit)
