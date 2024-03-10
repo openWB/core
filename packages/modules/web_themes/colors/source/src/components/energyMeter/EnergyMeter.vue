bd60074abf59b52f3e22c9eae3ff4f65c82ee9e5
Claus Hagen <cshagen@hagens.ch>
2024-03-10 16:09:37 +0100

    correct button naming in energy graph

--- packages/modules/web_themes/colors/source/src/components/energyMeter/EnergyMeter.vue
+++ packages/modules/web_themes/colors/source/src/components/energyMeter/EnergyMeter.vue
@@ -8,6 +8,7 @@
 				widgetid="graphsettings"
 				:show-left-button="true"
 				:show-right-button="true"
+				:ignore-live="true"
 				@shift-left="shiftLeft"
 				@shift-right="shiftRight"
 				@shift-up="shiftUp"
