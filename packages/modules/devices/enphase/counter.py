4330021b5a7a6ce62d75e3830841ef2295674c62
Lutz Bender <github@lutz-bender.de>
2024-03-12 12:18:20 +0100

    use auth token

--- packages/modules/devices/enphase/counter.py
+++ packages/modules/devices/enphase/counter.py
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 
 
 class EnphaseCounter:
-    def __init__(self, component_config: Union[Dict, EnphaseCounterSetup]) -> None:
+    def __init__(self, device_id: int, component_config: Union[Dict, EnphaseCounterSetup]) -> None:
         self.component_config = dataclass_from_dict(EnphaseCounterSetup, component_config)
         self.store = get_counter_value_store(self.component_config.id)
         self.fault_state = FaultState(ComponentInfo.from_component_config(self.component_config))
