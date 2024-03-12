435b0cf8d33d9f9221de6267fa162d654ae7153d
Lutz Bender <github@lutz-bender.de>
2024-03-12 12:59:24 +0100

    implement version handling

--- packages/modules/devices/enphase/bat.py
+++ packages/modules/devices/enphase/bat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import logging
-from typing import Dict, Union
+from typing import Any, Dict, Optional, Union
 
 from dataclass_utils import dataclass_from_dict
 from modules.common.component_state import BatState
@@ -21,20 +21,19 @@ class EnphaseBat:
         self.__device_id = device_id
         self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="speicher")
 
-    def update(self, response, live_data):
+    def update(self, response, live_data: Optional[Dict[str, Any]] = None) -> None:
+        if live_data is None:
+            raise ValueError("Es stehen keine Daten vom Speicher zur Verfügung.")
         bat_data = live_data["meters"]["storage"]
-
         if bat_data is None:
             # configuration wrong or error
             raise ValueError("Es konnten keine Daten vom Speicher gelesen werden.")
-
         soc = live_data["meters"]["soc"]
         power = bat_data['agg_p_mw'] / -1000  # negative is charging
         # powers = [bat_data['agg_p_ph_a_mw'] / 1000,
         #           bat_data['agg_p_ph_b_mw'] / 1000,
         #           bat_data['agg_p_ph_c_mw'] / 1000]
         imported, exported = self.sim_counter.sim_count(power)
-
         bat_state = BatState(
             imported=imported,
             exported=exported,
@@ -42,7 +41,6 @@ class EnphaseBat:
             power=power,
             # powers=powers
         )
-
         self.store.set(bat_state)
 
 
