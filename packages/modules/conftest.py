8c7a9048fa29ad486150bb928456086e70c23ddb
MartinRinas <martrin@microsoft.com>
2024-03-13 13:48:14 +0000

    fix pytest

--- packages/modules/conftest.py
+++ packages/modules/conftest.py
@@ -18,6 +18,7 @@ sys.modules['smb'] = type(sys)('smb')
 sys.modules['skodaconnect'] = type(sys)('skodaconnect')
 sys.modules['skodaconnect.Connection'] = type(sys)('skodaconnect.Connection')
 sys.modules['socketserver'] = type(sys)('socketserver')
+sys.modules['grpc'] = type(sys)('grpc')
 
 # sys.modules['telnetlib3'] = type(sys)('telnetlib3')
 
