9a61410c05ec1092e3512145a0c46b178aba25ea
LKuemmel <lena.kuemmel@openwb.de>
2024-03-22 08:49:55 +0100

    renaming

--- packages/modules/common/evse.py
+++ packages/modules/common/evse.py
@@ -6,12 +6,13 @@ from typing import Optional, Tuple
 from helpermodules.logger import ModifyLoglevelContext
 
 from modules.common import modbus
+from modules.common.component_state import EvseState
 from modules.common.modbus import ModbusDataType
 
 log = logging.getLogger(__name__)
 
 
-class EvseState(IntEnum):
+class EvseStatusCode(IntEnum):
     READY = (1, False, False)
     EV_PRESENT = (2, True, False)
     CHARGING = (3, True, True)
@@ -41,8 +42,8 @@ class Evse:
         set_current = int(set_current)
         log.debug("Gesetzte Stromstärke EVSE: "+str(set_current) +
                   ", Status: "+str(state_number)+", Modbus-ID: "+str(self.id))
-        state = EvseState(state_number)
-        if state == EvseState.FAILURE:
+        state = EvseStatusCode(state_number)
+        if state == EvseStatusCode.FAILURE:
             raise ValueError("Unbekannter Zustand der EVSE: State " +
                              str(state)+", Soll-Stromstärke: "+str(set_current))
         plugged = state.plugged
