35608fe81ca73311c394a280ab1f96888a39872b
PK <pk.hh8@online.de>
2024-04-03 22:50:33 +0200

    trailing space removed in polestar/api.py

--- packages/modules/vehicles/polestar/api.py
+++ packages/modules/vehicles/polestar/api.py
@@ -78,7 +78,7 @@ class PolestarApi:
             # get list of cars and store the ones not matching our vin
             cars = result['data']['getConsumerCarsV2']
             if len(cars) == 0:
-                raise Exception("Es konnten keine Fahrzeuge im Account gefunden werden. Bitte in den Einstellungen " + 
+                raise Exception("Es konnten keine Fahrzeuge im Account gefunden werden. Bitte in den Einstellungen " +
                                 "prüfen, ob der Besitzer-Account des Polestars eingetragen ist.")
             for i in range(0, len(cars)):
                 if cars[i]['vin'] == self.vin:
