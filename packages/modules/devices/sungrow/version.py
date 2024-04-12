efa81f81723175931c0cd2b715fa74a5340b590f
Mantelinho <mantelinho@yahoo.com>
2024-04-12 12:49:52 +0200

    Preserving original version numbers
    Co-authored-by: LKuemmel <76958050+LKuemmel@users.noreply.github.com>

--- packages/modules/devices/sungrow/version.py
+++ packages/modules/devices/sungrow/version.py
@@ -3,6 +3,6 @@ from enum import IntEnum
 
 class Version(IntEnum):
     SH = 0
-    SH_winet_dongle = 1
-    SG = 2
-    SG_winet_dongle = 3
+    SG = 1
+    SG_winet_dongle = 2
+    SH_winet_dongle = 3
