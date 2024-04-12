256c2d89064afcbfca8e9348586bc139dc234756
LKuemmel <lena.kuemmel@openwb.de>
2024-04-12 13:52:04 +0200

    fix

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -1414,9 +1414,8 @@ class UpdateConfig:
                     elif ("openWB/general/chargemode_config/pv_charging/rundown_power" == mode_topic and
                             decode_payload(mode_payload) == 0 and old_bat_prio):
                         return {"openWB/general/chargemode_config/pv_charging/bat_mode": "bat_mode"}
-                    else:
-                        return {"openWB/general/chargemode_config/pv_charging/bat_mode": "min_soc_mode"}
-                return {"openWB/general/chargemode_config/pv_charging/bat_mode": "min_soc_mode"}
+                else:
+                    return {"openWB/general/chargemode_config/pv_charging/bat_mode": "min_soc_mode"}
             elif "openWB/general/chargemode_config/pv_charging/rundown_soc" == topic:
                 return {"openWB/general/chargemode_config/pv_charging/min_bat_soc": decode_payload(payload)}
             elif "openWB/general/chargemode_config/pv_charging/rundown_power" == topic:
