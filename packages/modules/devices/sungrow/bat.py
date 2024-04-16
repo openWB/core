6233bc70370972f98bba83dc374f6e919d46924a
Mantelinho <mantelinho@yahoo.com>
2024-04-16 11:02:00 +0200

    Fixed PEP 8 errors

--- packages/modules/devices/sungrow/bat.py
+++ packages/modules/devices/sungrow/bat.py
@@ -6,7 +6,7 @@ from modules.common import modbus
 from modules.common.component_state import BatState
 from modules.common.component_type import ComponentDescriptor
 from modules.common.fault_state import ComponentInfo, FaultState
-from modules.common.modbus import ModbusDataType, Endian
+from modules.common.modbus import ModbusDataType
 from modules.common.simcount import SimCounter
 from modules.common.store import get_bat_value_store
 from modules.devices.sungrow.config import SungrowBatSetup, Sungrow
