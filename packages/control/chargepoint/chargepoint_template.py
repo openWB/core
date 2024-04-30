7898000b3f9dc8c52ef29935b5bcd44c2ad0c2f6
pama87 <patrick.mayer@openwb.de>
2024-04-30 11:14:50 +0200

    rfid fix

--- packages/control/chargepoint/chargepoint_template.py
+++ packages/control/chargepoint/chargepoint_template.py
@@ -90,11 +90,11 @@ class CpTemplate:
             if data.data.optional_data.data.rfid.active and (rfid is not None or vehicle_id is not None):
                 vehicle = ev_module.get_ev_to_rfid(rfid, vehicle_id)
                 if vehicle is None:
-                    if self.data.rfid_enabling:
+                    if self.data.rfid_enabling and len(self.data.valid_tags) == 0 or not self.data.rfid_enabling:
                         num = -1
                         message = (
                             f"Keine Ladung, da dem ID-Tag {rfid} kein Fahrzeug-Profil zugeordnet werden kann. Eine "
-                            "Freischaltung ist nur mit gültigen ID-Tag möglich.")
+                            "Freischaltung ist nur mit gültigem ID-Tag möglich.")
                     else:
                         num = assigned_ev
                 else:
