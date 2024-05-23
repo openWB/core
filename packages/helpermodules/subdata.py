7588f2e61de06a0d3d81008cfd112eb81aebb5b8
LKuemmel <lena.kuemmel@openwb.de>
2024-05-23 12:44:10 +0200

    handle errors during configurable initialization - electronic tariffs

--- packages/helpermodules/subdata.py
+++ packages/helpermodules/subdata.py
@@ -33,6 +33,7 @@ from dataclass_utils import dataclass_from_dict
 from modules.common.abstract_vehicle import CalculatedSocState, GeneralVehicleConfig
 from modules.common.configurable_backup_cloud import ConfigurableBackupCloud
 from modules.common.configurable_ripple_control_receiver import ConfigurableRcr
+from modules.common.configurable_tariff import ConfigurableElectricityTariff
 from modules.common.simcount.simcounter_state import SimCounterState
 from modules.internal_chargepoint_handler.internal_chargepoint_handler_config import (
     GlobalHandlerData, InternalChargepoint, RfidData)
@@ -653,7 +654,7 @@ class SubData:
                             mod = importlib.import_module(
                                 f".electricity_tariffs.{config_dict['type']}.tariff", "modules")
                             config = dataclass_from_dict(mod.device_descriptor.configuration_factory, config_dict)
-                            var.et_module = mod.create_electricity_tariff(config)
+                            var.et_module = ConfigurableElectricityTariff(config, mod.create_electricity_tariff)
                             var.et_get_prices()
                     else:
                         self.set_json_payload_class(var.data.et, msg)
