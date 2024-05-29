88b456231fcbe01e60b44bea5e1702c59504913b
pama87 <patrick.mayer@openwb.de>
2024-05-29 14:12:53 +0200

    topic von phase_switch_delay geändert und von Klasse PVCharging in ChargeModeConfig migriert

--- packages/control/general.py
+++ packages/control/general.py
@@ -37,7 +37,6 @@ class PvCharging:
     bat_power_reserve_active: bool = False
     control_range: List = field(default_factory=control_range_factory)
     feed_in_yield: int = 15000
-    phase_switch_delay: int = 7
     phases_to_use: int = 1
     bat_power_discharge: int = 1500
     bat_power_discharge_active: bool = False
@@ -81,6 +80,7 @@ class ChargemodeConfig:
     time_charging: TimeCharging = field(default_factory=time_charging_factory)
     unbalanced_load_limit: int = 18
     unbalanced_load: bool = False
+    phase_switch_delay: int = 7
 
 
 def chargemode_config_factory() -> ChargemodeConfig:
--- packages/helpermodules/setdata.py
+++ packages/helpermodules/setdata.py
@@ -750,7 +750,7 @@ class SetData:
                 self._validate_value(msg, int, [(0, float("inf"))])
             elif "openWB/set/general/chargemode_config/pv_charging/switch_off_threshold" in msg.topic:
                 self._validate_value(msg, float)
-            elif "openWB/set/general/chargemode_config/pv_charging/phase_switch_delay" in msg.topic:
+            elif "openWB/set/general/chargemode_config/phase_switch_delay" in msg.topic:
                 self._validate_value(msg, int, [(1, 15)])
             elif "openWB/set/general/chargemode_config/pv_charging/control_range" in msg.topic:
                 self._validate_value(msg, int, collection=list)
