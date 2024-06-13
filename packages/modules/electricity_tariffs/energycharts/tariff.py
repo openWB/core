76fe002fa3a007bf55b10b162363fd669c72ee55
pama87 <patrick.mayer@openwb.de>
2024-06-13 08:55:55 +0200

    Fix Börsenmodul Zeit

--- packages/modules/electricity_tariffs/energycharts/tariff.py
+++ packages/modules/electricity_tariffs/energycharts/tariff.py
@@ -1,6 +1,8 @@
 from typing import Dict
 from datetime import datetime, timedelta
 from modules.common import req
+import pytz
+from helpermodules import timecheck
 
 from modules.common.abstract_device import DeviceDescriptor
 from modules.common.component_state import TariffState
@@ -12,7 +14,15 @@ from modules.electricity_tariffs.energycharts.config import EnergyChartsTariff
 def fetch_prices(config: EnergyChartsTariffConfiguration) -> Dict[int, float]:
     current_dateTime = datetime.now()
     tomorrow = datetime.now() + timedelta(1)
-    start_time = current_dateTime.strftime("%Y-%m-%d") + 'T00%3A00%2B01%3A00'
+    if datetime.today().astimezone(pytz.timezone("Europe/Berlin")).dst().total_seconds()/3600:
+        # Sommerzeit if = 1
+        current_dateTime = current_dateTime - timedelta(hours=1)
+        start_time = current_dateTime.strftime("%Y-%m-%d") + 'T' + current_dateTime.strftime("%H") + \
+            '%3A00' + '%2B01%3A00'
+    else:
+        # keine Sommerzeit
+        start_time = current_dateTime.strftime("%Y-%m-%d") + 'T' + current_dateTime.strftime("%H") + \
+            '%3A00' + '%2B01%3A00'
     end_time = tomorrow.strftime("%Y-%m-%d") + 'T23%3A59%2B01%3A00'
     url = f'https://api.energy-charts.info/price?bzn={config.country}&start={start_time}&end={end_time}'
     raw_prices = req.get_http_session().get(url).json()
