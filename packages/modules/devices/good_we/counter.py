c0bf390509623168b1e263d645b6d3aeb4f3fb9e
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2024-07-05 08:36:21 +0200

    Goodwe v1 1 (#1727)
    * add version and firmware information
    
    * add alternative registers for battery and counter
    
    * fix breaking changes
    
    * solve breaking changes
    
    * fix breaking changes, add comments
    
    * fix SimCounter
    
    * remove duplicate lines

--- packages/modules/devices/good_we/counter.py
+++ packages/modules/devices/good_we/counter.py
@@ -7,6 +7,7 @@ from modules.common.component_state import CounterState
 from modules.common.component_type import ComponentDescriptor
 from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.modbus import ModbusDataType
+from modules.common.simcount import SimCounter
 from modules.common.store import get_counter_value_store
 from modules.devices.good_we.config import GoodWeCounterSetup
 from modules.devices.good_we.version import GoodWeVersion
@@ -22,6 +23,7 @@ class GoodWeCounter:
         self.__modbus_id = modbus_id
         self.version = version
         self.firmware = firmware
+        self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="bezug")
         self.component_config = dataclass_from_dict(GoodWeCounterSetup, component_config)
         self.__tcp_client = tcp_client
         self.store = get_counter_value_store(self.component_config.id)
