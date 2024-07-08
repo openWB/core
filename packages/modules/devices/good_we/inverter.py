97a397ab787a647ba0899ec97707e93e6d734622
ndrsnhs <andreas.neuhaus@openwb.de>
2024-07-08 15:23:12 +0200

    correct pv total power reg for v1.1

--- packages/modules/devices/good_we/inverter.py
+++ packages/modules/devices/good_we/inverter.py
@@ -30,8 +30,12 @@ class GoodWeInverter:
 
     def update(self) -> None:
         with self.__tcp_client:
-            power = sum([self.__tcp_client.read_holding_registers(reg, ModbusDataType.UINT_32,
-                        unit=self.__modbus_id) for reg in [35105, 35109, 35113, 35117]]) * -1
+            if self.version == GoodWeVersion.V_1_7:
+                power = sum([self.__tcp_client.read_holding_registers(reg, ModbusDataType.UINT_32,
+                                                                      unit=self.__modbus_id) for reg in [35105, 35109, 35113, 35117]]) * -1
+            else:
+                power = self.__tcp_client.read_holding_registers(
+                    35137, ModbusDataType.UINT_32, unit=self.__modbus_id) * -1
             exported = self.__tcp_client.read_holding_registers(
                 35191, ModbusDataType.UINT_32, unit=self.__modbus_id) * 100
 
