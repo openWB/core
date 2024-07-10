28e4c5efd55c85304e1e3641ba66a79a636c5703
ndrsnhs <andreas.neuhaus@openwb.de>
2024-07-10 13:27:34 +0200

    adjust inverter total power

--- packages/modules/devices/good_we/inverter.py
+++ packages/modules/devices/good_we/inverter.py
@@ -30,13 +30,9 @@ class GoodWeInverter:
 
     def update(self) -> None:
         with self.__tcp_client:
-            if self.version == GoodWeVersion.V_1_7:
-                power = sum([self.__tcp_client.read_holding_registers(
-                    reg, ModbusDataType.UINT_32, unit=self.__modbus_id)
-                    for reg in [35105, 35109, 35113, 35117]]) * -1
-            else:
-                power = self.__tcp_client.read_holding_registers(
-                    35137, ModbusDataType.INT_32, unit=self.__modbus_id) * -1
+            power = sum([self.__tcp_client.read_holding_registers(
+                reg, ModbusDataType.INT_32, unit=self.__modbus_id)
+                for reg in [35105, 35109, 35113, 35117]]) * -1
             exported = self.__tcp_client.read_holding_registers(
                 35191, ModbusDataType.UINT_32, unit=self.__modbus_id) * 100
 
