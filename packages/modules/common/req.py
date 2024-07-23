5f138b98d29f8cb639983aebc04ca61a2cac8815
LKuemmel <lena.kuemmel@openwb.de>
2024-07-23 10:54:13 +0200

    default timeout for requests

--- packages/modules/common/req.py
+++ packages/modules/common/req.py
@@ -5,8 +5,18 @@ from requests import Session
 log = logging.getLogger(__name__)
 
 
-def get_http_session() -> Session:
-    session = Session()
+class CustomSession(Session):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.default_timeout = 5
+
+    def request(self, method, url, *args, **kwargs):
+        kwargs.setdefault('timeout', self.default_timeout)
+        return super().request(method, url, *args, **kwargs)
+
+
+def get_http_session() -> CustomSession:
+    session = CustomSession()
     session.hooks['response'].append(lambda r, *args, **kwargs: r.raise_for_status())
     session.hooks['response'].append(lambda r, *args, **kwargs: log.debug("Get-Response: " + r.text))
     return session
