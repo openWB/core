1513f9891fc910bf4d43f4eae9740d4d522ba1b0
pama87 <patrick.mayer@openwb.de>
2024-07-31 11:57:06 +0200

    flake8

--- packages/modules/devices/powerdog/powerdog/device.py
+++ packages/modules/devices/powerdog/powerdog/device.py
@@ -9,7 +9,8 @@ from modules.common.abstract_device import AbstractDevice, DeviceDescriptor
 from modules.common.component_context import MultiComponentUpdateContext, SingleComponentUpdateContext
 from modules.devices.powerdog.powerdog import counter
 from modules.devices.powerdog.powerdog import inverter
-from modules.devices.powerdog.powerdog.config import Powerdog, PowerdogConfiguration, PowerdogCounterSetup, PowerdogInverterSetup
+from modules.devices.powerdog.powerdog.config import (
+    Powerdog, PowerdogConfiguration, PowerdogCounterSetup, PowerdogInverterSetup)
 
 log = logging.getLogger(__name__)
 
