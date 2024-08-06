04320847375d75b6a5e2134963582565e349cec9
Claus Hagen <cshagen@hagens.ch>
2024-08-06 08:33:45 +0200

    add change pages to display

--- packages/modules/display_themes/colors/source/src/views/StatusPage.vue
+++ packages/modules/display_themes/colors/source/src/views/StatusPage.vue
@@ -23,10 +23,10 @@ import { globalData } from '@/assets/js/model.ts'
 	display: grid;
 	grid-template-columns: 35% 65%;
 	font-size: 16px;
-	color: black;
+	color: white;
 }
 
 .header {
-	color: var(--color-charging);
+	color: var(--color-fg);
 }
 </style>
