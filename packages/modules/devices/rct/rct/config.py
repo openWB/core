4f5a70d1ba80cfb1f81fd05c4a844b9f079157b0
Lutz Bender <github@lutz-bender.de>
2024-08-08 11:50:46 +0200

    fix unused imports

--- packages/modules/devices/rct/rct/config.py
+++ packages/modules/devices/rct/rct/config.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional
 from helpermodules.auto_str import auto_str
 from modules.common.component_setup import ComponentSetup
 
--- packages/modules/devices/sungrow/sungrow/config.py
+++ packages/modules/devices/sungrow/sungrow/config.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional
 
 from modules.common.component_setup import ComponentSetup
 from modules.devices.sungrow.sungrow.version import Version
