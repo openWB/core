8e5a450d06f30e243ce7ee07ea25cea0df8eeac7
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2024-09-24 09:28:06 +0200

    Use create device: siemens sentron, sma sunny boy, kits, enphase&sunny island (#1774)
    * siemens sentron
    
    * sma sunny boy
    
    * kits
    
    * enphase
    
    * sunny island
    
    * remove legacy test
    
    * flake8

--- packages/modules/devices/enphase/enphase/device.py
+++ packages/modules/devices/enphase/enphase/device.py
@@ -1,179 +1,81 @@
 #!/usr/bin/env python3
 import jwt
 import logging
-import re
 import time
-from typing import Dict, List, Union
-from dataclass_utils import asdict, dataclass_from_dict
-from helpermodules.cli import run_using_positional_cli_args
+from typing import Iterable, Union
+
+from dataclass_utils import asdict
 from helpermodules.pub import Pub
 from modules.common import req
-from modules.common.abstract_device import AbstractDevice, DeviceDescriptor
-from modules.common.component_context import MultiComponentUpdateContext
-from modules.devices.enphase.enphase import counter, inverter, bat
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.configurable_device import ComponentFactoryByType, ConfigurableDevice, MultiComponentUpdater
+from modules.devices.enphase.enphase.bat import EnphaseBat
 from modules.devices.enphase.enphase.config import (EnphaseVersion,
                                                     Enphase,
-                                                    EnphaseConfiguration,
-                                                    EnphaseCounterConfiguration,
                                                     EnphaseCounterSetup,
-                                                    EnphaseInverterConfiguration,
                                                     EnphaseInverterSetup,
-                                                    EnphaseBatConfiguration,
                                                     EnphaseBatSetup)
+from modules.devices.enphase.enphase.counter import EnphaseCounter
+from modules.devices.enphase.enphase.inverter import EnphaseInverter
 
 log = logging.getLogger(__name__)
 
-enphase_component_classes = Union[counter.EnphaseCounter, inverter.EnphaseInverter, bat.EnphaseBat]
 
+def create_device(device_config: Enphase):
+    def create_bat_component(component_config: EnphaseBatSetup):
+        nonlocal read_live_data
+        read_live_data = True
+        return EnphaseBat(device_config.id, component_config)
 
-class Device(AbstractDevice):
-    COMPONENT_TYPE_TO_CLASS = {
-        "counter": counter.EnphaseCounter,
-        "inverter": inverter.EnphaseInverter,
-        "bat": bat.EnphaseBat,
-    }
+    def create_counter_component(component_config: EnphaseCounterSetup):
+        return EnphaseCounter(device_config.id, component_config)
 
-    def __init__(self, device_config: Union[Dict, Enphase]) -> None:
-        self.components = {}  # type: Dict[str, enphase_component_classes]
-        self.read_live_data = False
-        self.token_tries = 0
-        self.token_fails = 0
-        try:
-            self.device_config = dataclass_from_dict(Enphase, device_config)
-        except Exception:
-            log.exception("Fehler im Modul "+self.device_config.name)
+    def create_inverter_component(component_config: EnphaseInverterSetup):
+        return EnphaseInverter(device_config.id, component_config)
 
-    def add_component(
-        self,
-        component_config: Union[Dict, EnphaseCounterSetup, EnphaseInverterSetup, EnphaseBatSetup]
-    ) -> None:
-        if isinstance(component_config, Dict):
-            component_type = component_config["type"]
-        else:
-            component_type = component_config.type
-        component_config = dataclass_from_dict(COMPONENT_TYPE_TO_MODULE[
-            component_type].component_descriptor.configuration_factory, component_config)
-        if component_type in self.COMPONENT_TYPE_TO_CLASS:
-            self.components["component"+str(component_config.id)] = self.COMPONENT_TYPE_TO_CLASS[component_type](
-                self.device_config.id, component_config)
-            if "bat" in component_type:
-                self.read_live_data = True
-        else:
-            raise Exception(
-                "illegal component type " + component_type + ". Allowed values: " +
-                ','.join(self.COMPONENT_TYPE_TO_CLASS.keys())
-            )
-
-    def auto_set_version(self) -> None:
-        response = req.get_http_session().get(
-            'http://'+self.device_config.configuration.hostname+'/',
-            timeout=5, allow_redirects=False)
-        if response.status_code == 200:
-            self.device_config.configuration.version = EnphaseVersion.V1.value
-        elif response.status_code == 301:
-            if response.headers.get('Location', '').startswith('https'):
-                self.device_config.configuration.version = EnphaseVersion.V2.value
-            else:
-                self.device_config.configuration.version = EnphaseVersion.V1.value
-        else:
-            log.error("Could not identify Envoy firmware version")
-            return
-        try:
-            log.debug("saving envoy version")
-            Pub().pub("openWB/set/system/device/" + str(self.device_config.id) + "/config", asdict(self.device_config))
-        except Exception as e:
-            log.exception('Token mqtt write exception ' + str(e))
-
-    def auto_set_serial(self) -> None:
-        if self.device_config.configuration.version == EnphaseVersion.V1.value:
-            response = req.get_http_session().get(
-                'http://'+self.device_config.configuration.hostname+'/',
-                timeout=5)
-        elif self.device_config.configuration.version == EnphaseVersion.V2.value:
-            response = req.get_http_session().get(
-                'https://'+self.device_config.configuration.hostname+'/',
-                timeout=5, verify=False)
-        else:
-            log.error(f"unknown version: {self.device_config.configuration.version}")
-            return
-        pattern = re.compile('<title.*>.* ([0-9]+)</title>')
-        match = re.search(pattern, response.text)
-        if match is None:
-            log.error("could not find Envoy serial")
-            return
-        self.device_config.configuration.serial = match.group(1)
-        try:
-            log.debug("saving envoy serial")
-            Pub().pub("openWB/set/system/device/" + str(self.device_config.id) + "/config", asdict(self.device_config))
-        except Exception as e:
-            log.exception('Token mqtt write exception ' + str(e))
-
-    def find_counter_eid(self, measurement_type: str) -> str:
-        if self.device_config.configuration.version == EnphaseVersion.V1.value:
-            json_response = req.get_http_session().get(
-                'http://'+self.device_config.configuration.hostname+'/ivp/meters', timeout=5).json()
-        elif self.device_config.configuration.version == EnphaseVersion.V2.value:
-            # v2 requires token authentication
-            if self.check_token() is False:
-                log.error("no valid token to connect to envoy")
-                return
-            response = req.get_http_session().get(
-                'https://'+self.device_config.configuration.hostname+'/ivp/meters',
-                timeout=5, verify=False,
-                headers={"Authorization": f"Bearer {self.device_config.configuration.token}"})
-            if response.ok is False:
-                log.info("token invalid, will be renewed if credentials are set")
-                self.device_config.configuration.token = None
-                self.token_fails += 1
-                return
-            self.token_fails = 0
-            json_response = response.json()
-        for cnt in json_response:
-            if cnt.get('measurementType') == measurement_type:
-                return str(cnt.get('eid'))
-
-    def check_token(self) -> bool:
-        if (self.device_config.configuration.token is None or
-                self.device_config.configuration.token == ""):
+    def check_token() -> bool:
+        if (device_config.configuration.token is None or
+                device_config.configuration.token == ""):
             log.info("no valid token found, trying to receive token")
-            if self.receive_token() is False:
+            if receive_token() is False:
                 return False
         try:
             token_exp = jwt.decode(
-                self.device_config.configuration.token, options={"verify_signature": False})["exp"]
+                device_config.configuration.token, options={"verify_signature": False})["exp"]
         except jwt.exceptions.InvalidTokenError as e:
             log.error("invalid token stored, trying to receive new token: " + str(e))
-            if self.receive_token() is False:
+            if receive_token() is False:
                 return False
             try:
                 token_exp = jwt.decode(
-                    self.device_config.configuration.token, options={"verify_signature": False})["exp"]
+                    device_config.configuration.token, options={"verify_signature": False})["exp"]
             except jwt.exceptions.InvalidTokenError as e:
                 log.error("received token invalid: " + str(e))
                 return False
         if token_exp < (time.time() + 3600*24):
             log.info("token will expire in less than one day, trying to receive new token")
-            self.receive_token()
+            receive_token()
         return True
 
-    def receive_token(self) -> bool:
-        if self.token_tries > 5:
+    def receive_token() -> bool:
+        nonlocal token_tries
+        if token_tries > 5:
             log.error("unsuccessfully tried 5 times to receive valid token, giving up. check log")
             return False
-        if self.token_fails > 5:
+        if token_fails > 5:
             log.error("received 5 times a non-working token, giving up. check Envoy / gateway serial number")
             return False
-        self.token_tries += 1
-        if (self.device_config.configuration.user is None or
-                self.device_config.configuration.password is None or
-                self.device_config.configuration.serial is None):
+        token_tries += 1
+        if (device_config.configuration.user is None or
+                device_config.configuration.password is None or
+                device_config.configuration.serial is None):
             log.error("no credentials to authenticate to get token!")
             return False
         with req.get_http_session() as session:
             response = session.post(
                 'https://enlighten.enphaseenergy.com/login/login.json?',
-                data={'user[email]': self.device_config.configuration.user,
-                      'user[password]': self.device_config.configuration.password},
+                data={'user[email]': device_config.configuration.user,
+                      'user[password]': device_config.configuration.password},
                 timeout=5)
             if response.ok is False:
                 log.error(f"could not authenticate at enphase, check credentials. "
@@ -183,106 +85,77 @@ class Device(AbstractDevice):
             response = session.post(
                 'https://entrez.enphaseenergy.com/tokens',
                 json={'session_id': response_json['session_id'],
-                      'serial_num': self.device_config.configuration.serial,
-                      'username': self.device_config.configuration.user},
+                      'serial_num': device_config.configuration.serial,
+                      'username': device_config.configuration.user},
                 timeout=5)
             if response.ok is False:
                 log.error(f"could not receive token from enphase, check Envoy serial number. "
                           f"HTTP status code returned: {response.status_code}")
                 return False
-            self.token_tries = 0
+            token_tries = 0
             token = response.text.strip()
             log.info(f"received new token: {token}")
-            self.device_config.configuration.token = token
+            device_config.configuration.token = token
             try:
                 log.debug("saving new access token")
-                Pub().pub("openWB/set/system/device/" + str(self.device_config.id) + "/config",
-                          asdict(self.device_config))
+                Pub().pub("openWB/set/system/device/" + str(device_config.id) + "/config",
+                          asdict(device_config))
             except Exception as e:
                 log.exception('Token mqtt write exception ' + str(e))
                 return False
             return True
 
-    def update(self) -> None:
-        if self.components:
-            with MultiComponentUpdateContext(self.components):
-                if self.device_config.configuration.version == EnphaseVersion.V2.value:
-                    # v2 requires token authentication
-                    if self.check_token() is False:
-                        log.error("no valid token to connect to envoy")
-                        return
-                log.debug("Start device reading " + str(self.components))
-                with req.get_http_session() as session:
-                    json_live_data = None
-                    if self.device_config.configuration.version == EnphaseVersion.V1.value:
-                        json_response = session.get(
-                            'http://'+self.device_config.configuration.hostname+'/ivp/meters/readings',
-                            timeout=5).json()
-                        # json_live_data does not exist on older firmware
-                    elif self.device_config.configuration.version == EnphaseVersion.V2.value:
-                        response = session.get(
-                            'https://'+self.device_config.configuration.hostname+'/ivp/meters/readings',
-                            timeout=5, verify=False,
-                            headers={"Authorization": f"Bearer {self.device_config.configuration.token}"})
-                        if response.ok is False:
-                            log.info("token invalid, will be renewed if credentials are set")
-                            self.device_config.configuration.token = None
-                            self.token_fails += 1
-                            return
-                        self.token_fails = 0
-                        json_response = response.json()
-                        log.debug(f"meters/readings json response: {json_response}")
-                        if self.read_live_data:
-                            json_live_data = session.get(
-                                'https://'+self.device_config.configuration.hostname+'/ivp/livedata/status',
-                                timeout=5, verify=False,
-                                headers={"Authorization": f"Bearer {self.device_config.configuration.token}"}).json()
-                            log.debug(f"livedata/status json response: {json_live_data}")
-                    else:
-                        log.error(f"unknown version: {self.device_config.configuration.version}")
-                        return
-                    for component in self.components:
-                        self.components[component].update(json_response, json_live_data)
-        else:
-            log.warning(
-                self.device_config.name +
-                ": Es konnten keine Werte gelesen werden, da noch keine Komponenten konfiguriert wurden."
-            )
+    def update_components(components: Iterable[Union[EnphaseBat, EnphaseCounter, EnphaseInverter]]):
+        nonlocal token_fails
+        if device_config.configuration.version == EnphaseVersion.V2.value:
+            # v2 requires token authentication
+            if check_token() is False:
+                log.error("no valid token to connect to envoy")
+                return
+        log.debug("Start device reading " + str(components))
+        with req.get_http_session() as session:
+            json_live_data = None
+            if device_config.configuration.version == EnphaseVersion.V1.value:
+                json_response = session.get(
+                    'http://'+device_config.configuration.hostname+'/ivp/meters/readings',
+                    timeout=5).json()
+                # json_live_data does not exist on older firmware
+            elif device_config.configuration.version == EnphaseVersion.V2.value:
+                response = session.get(
+                    'https://'+device_config.configuration.hostname+'/ivp/meters/readings',
+                    timeout=5, verify=False,
+                    headers={"Authorization": f"Bearer {device_config.configuration.token}"})
+                if response.ok is False:
+                    log.info("token invalid, will be renewed if credentials are set")
+                    device_config.configuration.token = None
+                    token_fails += 1
+                    return
+                token_fails = 0
+                json_response = response.json()
+                log.debug(f"meters/readings json response: {json_response}")
+                if read_live_data:
+                    json_live_data = session.get(
+                        'https://'+device_config.configuration.hostname+'/ivp/livedata/status',
+                        timeout=5, verify=False,
+                        headers={"Authorization": f"Bearer {device_config.configuration.token}"}).json()
+                    log.debug(f"livedata/status json response: {json_live_data}")
+            else:
+                log.error(f"unknown version: {device_config.configuration.version}")
+                return
+            for component in components:
+                components[component].update(json_response, json_live_data)
 
-
-COMPONENT_TYPE_TO_MODULE = {
-    "counter": counter,
-    "inverter": inverter,
-    "bat": bat
-}
-
-
-def read_legacy(hostname: str, component_config: Union[EnphaseCounterSetup, EnphaseInverterSetup]) -> None:
-    dev = Device(Enphase(configuration=EnphaseConfiguration(hostname=hostname)))
-    dev.add_component(component_config)
-    dev.update()
-
-
-def read_legacy_counter(ip_address: str, eid: str):
-    config = EnphaseCounterConfiguration(eid=eid)
-    read_legacy(
-        ip_address,
-        counter.component_descriptor.configuration_factory(id=None, configuration=config))
-
-
-def read_legacy_inverter(ip_address: str, eid: str, num: int):
-    config = EnphaseInverterConfiguration(eid=eid)
-    read_legacy(ip_address, inverter.component_descriptor.configuration_factory(id=num, configuration=config))
-
-
-def read_legacy_bat(ip_address: str, eid: str, num: int):
-    config = EnphaseBatConfiguration()
-    read_legacy(ip_address, bat.component_descriptor.configuration_factory(id=num, configuration=config))
-
-
-def main(argv: List[str]):
-    run_using_positional_cli_args(
-        {"counter": read_legacy_counter, "inverter": read_legacy_inverter}, argv
+    read_live_data = False
+    token_tries = 0
+    token_fails = 0
+    return ConfigurableDevice(
+        device_config=device_config,
+        component_factory=ComponentFactoryByType(
+            bat=create_bat_component,
+            counter=create_counter_component,
+            inverter=create_inverter_component,
+        ),
+        component_updater=MultiComponentUpdater(update_components)
     )
 
 
--- packages/modules/devices/openwb/openwb_pv_kit/device.py
+++ packages/modules/devices/openwb/openwb_pv_kit/device.py
@@ -1,84 +1,37 @@
 import logging
-from typing import Dict, Optional, List, Union
+from typing import Iterable
 
-from dataclass_utils import dataclass_from_dict
-from helpermodules.cli import run_using_positional_cli_args
 from modules.common import modbus
-from modules.common.abstract_device import AbstractDevice, DeviceDescriptor
+from modules.common.abstract_device import DeviceDescriptor
 from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.configurable_device import ComponentFactoryByType, ConfigurableDevice, MultiComponentUpdater
 from modules.devices.openwb.openwb_pv_kit import inverter
-from modules.devices.openwb.openwb_pv_kit.config import PvKit, PvKitInverterSetup
+from modules.devices.openwb.openwb_pv_kit.config import PvKitSetup, PvKitInverterSetup
 
 log = logging.getLogger(__name__)
 
 
-class Device(AbstractDevice):
-    def __init__(self, device_config: Union[Dict, PvKit]) -> None:
-        self.device_config = dataclass_from_dict(PvKit, device_config)
-        self.components = {}  # type: Dict[str, inverter.PvKit]
-        self.client = modbus.ModbusTcpClient_("192.168.193.13", 8899)
+def create_device(device_config: PvKitSetup):
+    def create_inverter_component(component_config: PvKitInverterSetup):
+        return inverter.PvKit(device_config.id, component_config, client)
 
-    def add_component(self, component_config: Union[Dict, PvKitInverterSetup]) -> None:
-        if isinstance(component_config, Dict):
-            component_type = component_config["type"]
-        else:
-            component_type = component_config.type
-        component_config = dataclass_from_dict(COMPONENT_TYPE_TO_MODULE[
-            component_type].component_descriptor.configuration_factory, component_config)
-        if component_type == "inverter":
-            self.components["component"+str(component_config.id)] = inverter.PvKit(
-                self.device_config.id, component_config, self.client)
-        else:
-            raise Exception("illegal component type " + component_type)
+    def update_components(components: Iterable[inverter.PvKit]):
+        with client:
+            for component in components:
+                with SingleComponentUpdateContext(component.fault_state):
+                    component.update()
 
-    def update(self) -> None:
-        log.debug("Start device reading " + str(self.components))
-        if self.components:
-            for component in self.components:
-                # Auch wenn bei einer Komponente ein Fehler auftritt, sollen alle anderen noch ausgelesen werden.
-                with SingleComponentUpdateContext(self.components[component].fault_state):
-                    self.components[component].update()
-        else:
-            log.warning(
-                self.device_config.name +
-                ": Es konnten keine Werte gelesen werden, da noch keine Komponenten konfiguriert wurden."
-            )
+    try:
+        client = modbus.ModbusTcpClient_("192.168.193.13", 8899)
+    except Exception:
+        log.exception("Fehler in create_device")
+    return ConfigurableDevice(
+        device_config=device_config,
+        component_factory=ComponentFactoryByType(
+            inverter=create_inverter_component,
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
 
 
-COMPONENT_TYPE_TO_MODULE = {
-    "inverter": inverter
-}
-
-
-def read_legacy(component_type: str, version: int, num: Optional[int] = None, evu_version: Optional[int] = None):
-    """ Ausführung des Moduls als Python-Skript
-    """
-    dev = Device(PvKit())
-
-    component_config = get_component_config(component_type, version, num)
-    dev.add_component(component_config)
-    log.debug('openWB Version: ' + str(version))
-
-    if component_type == "evu_inverter" and evu_version:
-        component_config = get_component_config("counter", evu_version, None)
-        dev.add_component(component_config)
-        log.debug('openWB EVU-Version: ' + str(evu_version))
-
-    dev.update()
-
-
-def get_component_config(component_type: str, version: int, num: Optional[int] = None) -> Dict:
-    if component_type == "inverter":
-        component_config = inverter.component_descriptor.configuration_factory()
-    else:
-        raise Exception("illegal component type " + component_type)
-    component_config.id = num
-    component_config.configuration.version = version
-    return component_config
-
-
-def main(argv: List[str]):
-    run_using_positional_cli_args(read_legacy, argv)
-
-
-device_descriptor = DeviceDescriptor(configuration_factory=PvKit)
+device_descriptor = DeviceDescriptor(configuration_factory=PvKitSetup)
