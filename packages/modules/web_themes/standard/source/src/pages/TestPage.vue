50fe73c7edf31945937ec62cc95ba4e646b716d8
Brett-S-OWB <brett.scott@openwb.de>
2024-10-04 14:45:05 +0200

    start page and index page

--- packages/modules/web_themes/standard/source/src/pages/TestPage.vue
+++ packages/modules/web_themes/standard/source/src/pages/TestPage.vue
@@ -60,10 +60,62 @@
         </q-carousel>
       </q-tab-panel>
       <q-tab-panel name="speicher" class="scroll">
-        <!-- Speicher content -->
+        <div class="q-pa-md">
+            <p>Speicher</p>
+            <div class="q-mt-md">
+              <q-list bordered>
+                <q-item>
+                  <q-item-section>
+                    <q-item-label>Leistung</q-item-label>
+                    <q-item-label caption>22.0 kW</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Geladen</q-item-label>
+                    <q-item-label caption>7.2 kWh</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item>
+                  <q-item-section>
+                    <q-item-label>Ladestand</q-item-label>
+                    <q-item-label caption>30% (100 km)</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Ladeziel</q-item-label>
+                    <q-item-label caption>90% (360 km)</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+          </div>
       </q-tab-panel>
       <q-tab-panel name="smartHome" class="scroll">
-        <!-- Smart Home content -->
+        <div class="q-pa-md">
+            <p>Smart Home</p>
+            <div class="q-mt-md">
+              <q-list bordered>
+                <q-item>
+                  <q-item-section>
+                    <q-item-label>Leistung</q-item-label>
+                    <q-item-label caption>22.0 kW</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Geladen</q-item-label>
+                    <q-item-label caption>7.2 kWh</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item>
+                  <q-item-section>
+                    <q-item-label>Ladestand</q-item-label>
+                    <q-item-label caption>30% (100 km)</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>Ladeziel</q-item-label>
+                    <q-item-label caption>90% (360 km)</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+          </div>
       </q-tab-panel>
     </q-tab-panels>
   </q-page>
