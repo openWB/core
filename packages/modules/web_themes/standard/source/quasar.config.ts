facfb2d5b6eb59c50d655e0488c75d365d388b1e
Lutz Bender <github@lutz-bender.de>
2024-10-07 13:28:08 +0200

    initialize store in boot file

--- packages/modules/web_themes/standard/source/quasar.config.ts
+++ packages/modules/web_themes/standard/source/quasar.config.ts
@@ -13,7 +13,7 @@ export default configure((/* ctx */) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [],
+    boot: ['store-init'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.scss'],
