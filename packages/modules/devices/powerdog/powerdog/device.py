0f78046d0f23fda6c3fbbe1ed78af52eb2ebb150
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2024-10-07 11:21:34 +0200

    fix feature device list (#1912)
    * fix feature device list
    
    * fix powerdog

--- packages/modules/devices/powerdog/powerdog/device.py
+++ packages/modules/devices/powerdog/powerdog/device.py
@@ -1,113 +1,64 @@
 #!/usr/bin/env python3
 import logging
-from typing import Dict, Union, Optional, List
+from typing import Iterable, Union
 
-from dataclass_utils import dataclass_from_dict
-from helpermodules.cli import run_using_positional_cli_args
 from modules.common import modbus
-from modules.common.abstract_device import AbstractDevice, DeviceDescriptor
-from modules.common.component_context import MultiComponentUpdateContext, SingleComponentUpdateContext
-from modules.devices.powerdog.powerdog import counter
-from modules.devices.powerdog.powerdog import inverter
-from modules.devices.powerdog.powerdog.config import (
-    Powerdog, PowerdogConfiguration, PowerdogCounterSetup, PowerdogInverterSetup)
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.configurable_device import ComponentFactoryByType, ConfigurableDevice, MultiComponentUpdater
+from modules.devices.powerdog.powerdog.config import Powerdog, PowerdogCounterSetup, PowerdogInverterSetup
+from modules.devices.powerdog.powerdog.counter import PowerdogCounter
+from modules.devices.powerdog.powerdog.inverter import PowerdogInverter
 
 log = logging.getLogger(__name__)
 
 
-class Device(AbstractDevice):
-    COMPONENT_TYPE_TO_CLASS = {
-        "counter": counter.PowerdogCounter,
-        "inverter": inverter.PowerdogInverter
-    }
+def create_device(device_config: Powerdog):
+    def create_counter_component(component_config: PowerdogCounterSetup):
+        return PowerdogCounter(device_config.id, component_config, client, device_config.configuration.modbus_id)
 
-    def __init__(self, device_config: Union[Dict, Powerdog]) -> None:
-        self.components = {}  # type: Dict[str, Union[counter.PowerdogCounter, inverter.PowerdogInverter]]
-        try:
-            self.device_config = dataclass_from_dict(Powerdog, device_config)
-            self.client = modbus.ModbusTcpClient_(
-                self.device_config.configuration.ip_address, self.device_config.configuration.port)
-        except Exception:
-            log.exception("Fehler im Modul "+self.device_config.name)
+    def create_inverter_component(component_config: PowerdogInverterSetup):
+        return PowerdogInverter(device_config.id, component_config, client, device_config.configuration.modbus_id)
 
-    def add_component(self, component_config: Union[Dict, PowerdogCounterSetup, PowerdogInverterSetup]) -> None:
-        if isinstance(component_config, Dict):
-            component_type = component_config["type"]
-        else:
-            component_type = component_config.type
-        component_config = dataclass_from_dict(COMPONENT_TYPE_TO_MODULE[
-            component_type].component_descriptor.configuration_factory, component_config)
-        if component_type in self.COMPONENT_TYPE_TO_CLASS:
-            self.components["component"+str(component_config.id)] = (
-                self.COMPONENT_TYPE_TO_CLASS[component_type](
-                    self.device_config.id, component_config, self.client,
-                    self.device_config.configuration.modbus_id))
-        else:
-            raise Exception(
-                "illegal component type " + component_type + ". Allowed values: " +
-                ','.join(self.COMPONENT_TYPE_TO_CLASS.keys())
-            )
-
-    def update(self) -> None:
-        log.debug("Start device reading " + str(self.components))
-        with MultiComponentUpdateContext(self.components):
-            if len(self.components) == 1:
-                for component in self.components:
-                    if isinstance(self.components[component], inverter.PowerdogInverter):
-                        with SingleComponentUpdateContext(self.components[component].fault_state):
-                            self.components[component].update()
+    def update_components(components: Iterable[Union[PowerdogCounter, PowerdogInverter]]):
+        with client:
+            if len(components) == 1:
+                for component in components:
+                    if isinstance(component, PowerdogInverter):
+                        with SingleComponentUpdateContext(component.fault_state):
+                            component.update()
                     else:
                         raise Exception(
                             "Wenn ein EVU-Zähler konfiguriert wurde, muss immer auch ein WR konfiguriert sein.")
-            elif len(self.components) == 2:
-                for component in self.components:
-                    if isinstance(self.components[component], inverter.PowerdogInverter):
-                        inverter_power = self.components[component].update()
+            elif len(components) == 2:
+                for component in components:
+                    if isinstance(component, PowerdogInverter):
+                        inverter_power = component.update()
                         break
                 else:
                     inverter_power = 0
-                for component in self.components:
-                    if isinstance(self.components[component], counter.PowerdogCounter):
-                        self.components[component].update(inverter_power)
+                for component in components:
+                    if isinstance(component, PowerdogCounter):
+                        component.update(inverter_power)
             else:
                 log.warning(
-                    self.device_config.name +
+                    device_config.name +
                     ": Es konnten keine Werte gelesen werden, da noch keine oder zu viele Komponenten konfiguriert "
                     + "wurden."
                 )
 
-
-COMPONENT_TYPE_TO_MODULE = {
-    "counter": counter,
-    "inverter": inverter
-}
-
-
-def read_legacy(component_type: str, ip_address: str, num: Optional[int] = None) -> None:
-    dev = Device(Powerdog(configuration=PowerdogConfiguration(ip_address=ip_address)))
-    if component_type in COMPONENT_TYPE_TO_MODULE:
-        component_config = COMPONENT_TYPE_TO_MODULE[component_type].component_descriptor.configuration_factory()
-    else:
-        raise Exception(
-            "illegal component type " + component_type + ". Allowed values: " +
-            ','.join(COMPONENT_TYPE_TO_MODULE.keys())
-        )
-    component_config.id = num
-    dev.add_component(component_config)
-
-    # Wenn der EVU-Zähler ausgelesen werden soll, wird auch noch der Inverter benötigt.
-    if component_type in COMPONENT_TYPE_TO_MODULE and component_type == "counter":
-        inverter_config = PowerdogInverterSetup()
-        inverter_config.id = 1
-        dev.add_component(inverter_config)
-
-    log.debug('Powerdog IP-Adresse: ' + ip_address)
-
-    dev.update()
-
-
-def main(argv: List[str]):
-    run_using_positional_cli_args(read_legacy, argv)
+    try:
+        client = modbus.ModbusTcpClient_(device_config.configuration.ip_address, device_config.configuration.port)
+    except Exception:
+        log.exception("Fehler in create_device")
+    return ConfigurableDevice(
+        device_config=device_config,
+        component_factory=ComponentFactoryByType(
+            counter=create_counter_component,
+            inverter=create_inverter_component,
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
 
 
 device_descriptor = DeviceDescriptor(configuration_factory=Powerdog)
--- packages/modules/devices/siemens/siemens/device.py
+++ packages/modules/devices/siemens/siemens/device.py
@@ -1,99 +1,51 @@
 #!/usr/bin/env python3
 import logging
-from typing import Dict, Union, Optional, List
+from typing import Iterable, Union
 
-from dataclass_utils import dataclass_from_dict
-from helpermodules.cli import run_using_positional_cli_args
 from modules.common import modbus
-from modules.common.abstract_device import AbstractDevice, DeviceDescriptor
+from modules.common.abstract_device import DeviceDescriptor
 from modules.common.component_context import SingleComponentUpdateContext
-from modules.devices.siemens.siemens import bat, counter, inverter
+from modules.common.configurable_device import ComponentFactoryByType, ConfigurableDevice, MultiComponentUpdater
+from modules.devices.siemens.siemens.bat import SiemensBat
 from modules.devices.siemens.siemens.config import Siemens, SiemensBatSetup, SiemensCounterSetup, SiemensInverterSetup
+from modules.devices.siemens.siemens.counter import SiemensCounter
+from modules.devices.siemens.siemens.inverter import SiemensInverter
 
 log = logging.getLogger(__name__)
 
 
-siemens_component_classes = Union[bat.SiemensBat, counter.SiemensCounter, inverter.SiemensInverter]
+siemens_component_classes = Union[SiemensBat, SiemensCounter, SiemensInverter]
 
 
-class Device(AbstractDevice):
-    COMPONENT_TYPE_TO_CLASS = {
-        "bat": bat.SiemensBat,
-        "counter": counter.SiemensCounter,
-        "inverter": inverter.SiemensInverter
-    }
+def create_device(device_config: Siemens):
+    def create_bat_component(component_config: SiemensBatSetup):
+        return SiemensBat(device_config.id, component_config, client, device_config.configuration.modbus_id)
 
-    def __init__(self, device_config: Union[Dict, Siemens]) -> None:
-        self.components = {}  # type: Dict[str, siemens_component_classes]
-        try:
-            self.device_config = dataclass_from_dict(Siemens, device_config)
-            self.client = modbus.ModbusTcpClient_(
-                self.device_config.configuration.ip_address, self.device_config.configuration.port)
-        except Exception:
-            log.exception("Fehler im Modul "+self.device_config.name)
+    def create_counter_component(component_config: SiemensCounterSetup):
+        return SiemensCounter(device_config.id, component_config, client, device_config.configuration.modbus_id)
 
-    def add_component(self, component_config: Union[Dict,
-                                                    SiemensBatSetup,
-                                                    SiemensCounterSetup,
-                                                    SiemensInverterSetup]) -> None:
-        if isinstance(component_config, Dict):
-            component_type = component_config["type"]
-        else:
-            component_type = component_config.type
-        component_config = dataclass_from_dict(COMPONENT_TYPE_TO_MODULE[
-            component_type].component_descriptor.configuration_factory, component_config)
-        if component_type in self.COMPONENT_TYPE_TO_CLASS:
-            self.components["component"+str(component_config.id)] = (self.COMPONENT_TYPE_TO_CLASS[component_type](
-                self.device_config.id, component_config, self.client,
-                self.device_config.configuration.modbus_id))
-        else:
-            raise Exception(
-                "illegal component type " + component_type + ". Allowed values: " +
-                ','.join(self.COMPONENT_TYPE_TO_CLASS.keys())
-            )
+    def create_inverter_component(component_config: SiemensInverterSetup):
+        return SiemensInverter(device_config.id, component_config, client, device_config.configuration.modbus_id)
 
-    def update(self) -> None:
-        log.debug("Start device reading" + str(self.components))
-        if self.components:
-            for component in self.components:
-                # Auch wenn bei einer Komponente ein Fehler auftritt, sollen alle anderen noch ausgelesen werden.
-                with SingleComponentUpdateContext(self.components[component].fault_state):
-                    self.components[component].update()
-        else:
-            log.warning(
-                self.device_config.name +
-                ": Es konnten keine Werte gelesen werden, da noch keine Komponenten konfiguriert wurden."
-            )
+    def update_components(components: Iterable[siemens_component_classes]):
+        with client:
+            for component in components:
+                with SingleComponentUpdateContext(component.fault_state):
+                    component.update()
 
-
-COMPONENT_TYPE_TO_MODULE = {
-    "bat": bat,
-    "counter": counter,
-    "inverter": inverter
-}
-
-
-def read_legacy(component_type: str, ip_address: str, num: Optional[int] = None) -> None:
-    device_config = Siemens()
-    device_config.configuration.ip_address = ip_address
-    dev = Device(device_config)
-    if component_type in COMPONENT_TYPE_TO_MODULE:
-        component_config = COMPONENT_TYPE_TO_MODULE[component_type].component_descriptor.configuration_factory()
-    else:
-        raise Exception(
-            "illegal component type " + component_type + ". Allowed values: " +
-            ','.join(COMPONENT_TYPE_TO_MODULE.keys())
-        )
-    component_config.id = num
-    dev.add_component(component_config)
-
-    log.debug('Siemens IP-Adresse: ' + ip_address)
-
-    dev.update()
-
-
-def main(argv: List[str]):
-    run_using_positional_cli_args(read_legacy, argv)
+    try:
+        client = modbus.ModbusTcpClient_(device_config.configuration.ip_address, device_config.configuration.port)
+    except Exception:
+        log.exception("Fehler in create_device")
+    return ConfigurableDevice(
+        device_config=device_config,
+        component_factory=ComponentFactoryByType(
+            bat=create_bat_component,
+            counter=create_counter_component,
+            inverter=create_inverter_component,
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
 
 
 device_descriptor = DeviceDescriptor(configuration_factory=Siemens)
