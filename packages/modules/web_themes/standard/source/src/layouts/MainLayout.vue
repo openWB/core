e3ec94a81cf59cf54b71a659b4801ca6ead00fdf
Lutz Bender <github@lutz-bender.de>
2024-10-08 09:31:22 +0200

    cleanup color classes

--- packages/modules/web_themes/standard/source/src/layouts/MainLayout.vue
+++ packages/modules/web_themes/standard/source/src/layouts/MainLayout.vue
@@ -2,8 +2,15 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title>openWB</q-toolbar-title>
         <q-btn flat round icon="menu" aria-label="Menu" />
+        <q-toolbar-title>openWB</q-toolbar-title>
+        <q-btn
+          flat
+          round
+          :icon="colorModeIcon"
+          @click="toggleColorMode()"
+          aria-label="Color-Mode"
+        />
       </q-toolbar>
     </q-header>
 
@@ -15,9 +22,27 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useQuasar } from 'quasar';
+const $q = useQuasar();
+
 defineOptions({
   name: 'MainLayout',
 });
+
+/**
+ * Computed property that returns the icon name for the color mode button.
+ */
+const colorModeIcon = computed(() => {
+  return $q.dark.isActive ? 'dark_mode' : 'light_mode';
+});
+
+/**
+ * Toggles the color mode of the application.
+ */
+function toggleColorMode() {
+  $q.dark.toggle();
+}
 </script>
 
 <style>
