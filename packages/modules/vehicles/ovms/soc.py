62eb3a65e92bc3e04442a2e31a6fe36846014e5d
rleidner <89418596+rleidner@users.noreply.github.com>
2024-10-10 10:48:03 +0200

    Update soc.py - flake8 fix

--- packages/modules/vehicles/ovms/soc.py
+++ packages/modules/vehicles/ovms/soc.py
@@ -10,8 +10,6 @@ from modules.common.component_state import CarState
 from modules.common.configurable_vehicle import ConfigurableVehicle
 from modules.vehicles.ovms import api
 from modules.vehicles.ovms.config import OVMS, OVMSConfiguration
-import datetime
-from json import dumps
 
 date_fmt = '%Y-%m-%d %H:%M:%S'
 log = logging.getLogger(__name__)
