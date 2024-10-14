deb8ff9aef1cb05aefcab692d52c0959d4e64b26
Lutz Bender <github@lutz-bender.de>
2024-10-14 09:10:56 +0200

    fix apiv2 token after rebase

--- packages/modules/devices/sonnen/sonnenbatterie/counter.py
+++ packages/modules/devices/sonnen/sonnenbatterie/counter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import logging
-from typing import Dict, Union
+from typing import Dict, Optional, Union
 
 from dataclass_utils import dataclass_from_dict
 from modules.common import req
@@ -20,7 +20,7 @@ class SonnenbatterieCounter(AbstractCounter):
                  device_id: int,
                  device_address: str,
                  device_variant: int,
-                 api_v2_token: str,
+                 api_v2_token: Optional[str],
                  component_config: Union[Dict, SonnenbatterieCounterSetup]) -> None:
         self.__device_id = device_id
         self.__device_address = device_address
