8aa150ce84c171e17778110e57243a995c637cdf
BrettS <168732306+Brett-S-OWB@users.noreply.github.com>
2024-10-15 09:33:23 +0200

    Füge Komponenten zur ChargePoint-Karte hinzu (#1929)
    * Test chargePoint page
    * Test page with mqtt store
    * Layout: Remove 100vh, add Quasar css classes
    * Separate component ChargePoint into ChargePoint & ChargePointCarousel
    * Update carousel component in tab panel
    * Carousel navigation arrows and points responsive, >600px width = Quasar size= xs
    * Charge point properties

--- packages/modules/web_themes/standard/source/src/components/ChargePoint.vue
+++ packages/modules/web_themes/standard/source/src/components/ChargePoint.vue
@@ -1,71 +1,95 @@
 <template>
-  <q-carousel
-    v-model="slideBottom"
-    swipeable
-    animated
-    navigation
-    arrows
-    control-color="primary"
-    infinite
-    class="full-height"
-    @mousedown.prevent
-  >
-    <q-carousel-slide
-      v-for="(item, index) in carouselItems"
-      :key="index"
-      :name="item.name"
-      class="column items-center justify-center"
+  <div class="q-ma-sm q-pa-sm"  v-if="chargePoint" style="max-width: 23em;" >
+    <div class="row items-center text-h6" style="font-weight: bold">
+      {{ chargePoint.name }}
+      <q-icon
+        :name="chargePoint.locked ? 'lock' : 'lock_open'"
+        size="sm"
+        :color="chargePoint.locked ? 'red' : 'green'"
+        @click="toggleLock(chargePoint.id, chargePoint.locked)"
+        style="cursor: pointer"
+        class="q-ml-sm"
+      />
+      <q-icon
+        :name="chargePoint.state ? 'power' : 'power_off'"
+        size="sm"
+        :color="chargePoint.state ? 'green' : 'red'"
+      />
+      <q-space />
+      <q-icon name="settings" size="sm" />
+    </div>
+    <div
+      class="row q-mt-md q-pa-sm bg-primary text-white"
+      style="border-radius: 10px"
     >
-      <q-icon :name="item.icon" size="56px" />
-      <div class="text-center q-mt-md">
-        {{ chargePointNames }}
-        {{ item.text }}
-      </div>
-      <SliderQuasar />
-    </q-carousel-slide>
-  </q-carousel>
+      {{ chargePoint.message }}
+    </div>
+    <div style="margin-left: auto; margin-right: auto" display="block">
+      <q-btn-group push rounded class="q-mt-md">
+        <q-btn
+          v-for="mode in chargeModes"
+          :key="mode.value"
+          :flat="selectedButton !== mode.value"
+          :outline="selectedButton === mode.value"
+          :glossy="selectedButton === mode.value"
+          :label="mode.label"
+          :color="mode.color"
+          size="sm"
+          @click="setChargeMode(mode.value)"
+        />
+      </q-btn-group>
+    </div>
+    <div class="row q-mt-sm">
+      {{ chargePoint.power }}
+    </div>
+    <SliderQuasar class="q-mb-xl"></SliderQuasar>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import SliderQuasar from './SliderQuasar.vue';
 import { useMqttStore } from 'src/stores/mqtt-store';
 
+const props = defineProps<{
+  chargePointId: number;
+}>();
+
 const mqttStore = useMqttStore();
-const topicsToSubscribe = <string[]>['openWB/chargepoint/+/config'];
 
-const slideBottom = ref<string>('style');
-
-interface CarouselItem {
-  name: string;
-  icon: string;
-  text: string;
-}
-
-const carouselItems: CarouselItem[] = [
-  {
-    name: 'style',
-    icon: 'style',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    name: 'tv',
-    icon: 'live_tv',
-    text: 'Praesent bibendum, neque at hendrerit pretium, nunc nisi tempus nunc.',
-  },
-  {
-    name: 'layers',
-    icon: 'layers',
-    text: 'Donec euismod, nisl eget ultricies ultricies, nunc nunc ultricies nunc.',
-  },
-  {
-    name: 'map',
-    icon: 'terrain',
-    text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  },
+const topicsToSubscribe = <string[]>[
+  'openWB/chargepoint/+/config',
+  'openWB/chargepoint/+/set/manual_lock',
+  'openWB/chargepoint/+/get/power',
+  'openWB/chargepoint/+/get/state_str',
+  'openWB/chargepoint/+/get/plug_state',
+  'openWB/chargepoint/+/get/conected_vehicle',
+  'openWB/chargepoint/+/get/connected_vehicle/config',
+  'openWB/vehicle/template/charge_template/0',
 ];
 
-const chargePointNames = computed(() => mqttStore.getChargePointNames);
+const chargePoint = computed(() =>
+  mqttStore.getChargePointDetails(props.chargePointId),
+);
+const selectedButton = computed(() => mqttStore.getChargeMode);
+
+const chargeModes = [
+  { value: 'sofort', label: 'Sofort', color: 'negative' },
+  { value: 'pv', label: 'PV', color: 'positive' },
+  { value: 'scheduled', label: 'Zeil', color: 'primary' },
+  { value: 'standby', label: 'Standby', color: 'warning' },
+  { value: 'stop', label: 'Stop', color: 'light' },
+];
+
+const toggleLock = (chargePointId: number, currentLockState: boolean) => {
+  const topic = `openWB/chargepoint/${chargePointId}/set/manual_lock`;
+  const newLockState = !currentLockState;
+  mqttStore.updateTopic(topic, newLockState);
+};
+
+const setChargeMode = (mode: string) => {
+  mqttStore.setChargeMode(mode);
+};
 
 onMounted(() => {
   mqttStore.subscribe(topicsToSubscribe);
