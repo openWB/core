5a70b7673bcd391336ae511d94fb8270bdb4082d
LKuemmel <lena.kuemmel@openwb.de>
2024-10-15 11:22:17 +0200

    improve message

--- packages/helpermodules/utils/error_handling.py
+++ packages/helpermodules/utils/error_handling.py
@@ -55,8 +55,9 @@ class ImportErrorContext:
     def __exit__(self, exception_type, exception, exception_traceback) -> bool:
         if isinstance(exception, ModuleNotFoundError):
             try:
-                msg = ("Importfehler: Konnte Modul " +
-                       exception.args[0].split("'")[1] + " nicht finden. Bitte die Internetverbindung prüfen.")
+                msg = ("Importfehler: Das Python-Modul " + exception.args[0].split("'")[1] + " wurde nicht gefunden." +
+                       " Bitte sicherstellen, dass diese openWB eine Verbindung zum Internet besitzt. Beim nächsten " +
+                       "Neustart wird versucht, fehlende Software-Pakete zu installieren.")
             except IndexError:
                 msg = "Importfehler: " + str(exception)
             # pub_system_message() publlished an openWB/set/, dass wird beim Starten gelöscht
