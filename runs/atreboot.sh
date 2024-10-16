f375f4c3aaaa5cea78824d65ed9f5f3e1e17e771
LKuemmel <lena.kuemmel@openwb.de>
2024-10-16 10:19:49 +0200

    fix boot config

--- runs/atreboot.sh
+++ runs/atreboot.sh
@@ -93,8 +93,7 @@ chmod 666 "$LOGFILE"
 		fi
 	) &
 
-	revision=$(grep "Revision" /proc/cpuinfo | awk '{print $3}')
-
+	boot_config_source="${OPENWBBASEDIR}/data/config/boot_config.txt"
 	boot_config_target="/boot/config.txt"
 	echo "checking init in $boot_config_target..."
 	if versionMatch "$boot_config_source" "$boot_config_target"; then
