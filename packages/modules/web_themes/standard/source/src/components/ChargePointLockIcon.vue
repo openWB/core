5d2227b42b5cb8110b2fd885f8c7b737c933f59e
Lutz Bender <github@lutz-bender.de>
2024-10-23 08:49:08 +0200

    extend store and refactor charge point view

---
+++ packages/modules/web_themes/standard/source/src/components/ChargePointLockIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-icon
+    :name="locked ? 'lock' : 'lock_open'"
+    size="sm"
+    :color="locked ? 'red' : 'green'"
+    @click="locked = !locked"
+    style="cursor: pointer"
+    class="q-ml-sm"
+  />
+</template>
+
+<script setup lang="ts">
+import { useMqttStore } from 'src/stores/mqtt-store';
+// import { computed } from 'vue';
+
+const props = defineProps<{
+  chargePointId: number;
+}>();
+
+const mqttStore = useMqttStore();
+
+const locked = mqttStore.chargePointManualLock(props.chargePointId);
+</script>
