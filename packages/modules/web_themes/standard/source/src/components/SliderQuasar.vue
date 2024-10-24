4def9bf9eea9590b3daef2896ab3beedc5efd978
Lutz Bender <github@lutz-bender.de>
2024-10-24 11:18:27 +0200

    integrate changes from PR#1963

--- packages/modules/web_themes/standard/source/src/components/SliderQuasar.vue
+++ packages/modules/web_themes/standard/source/src/components/SliderQuasar.vue
@@ -6,9 +6,10 @@
         :min="0"
         :max="100"
         color="green-7"
-        track-size="20px"
+        track-size="1em"
         thumb-size="0px"
         readonly
+        no-focus
         @touchstart.stop
         @touchmove.stop
         @touchend.stop
@@ -20,13 +21,31 @@
         :max="100"
         color="light-green-5"
         inner-track-color="blue-grey-2"
-        track-size="20px"
-        thumb-size="37px"
+        track-size="1em"
+        :thumb-size="props.readonly ? '0' : '2em'"
+        :readonly="props.readonly"
         @touchstart.stop
         @touchmove.stop
         @touchend.stop
-        style="position: absolute; width: 100%; z-index:"
+        style="position: absolute; width: 100%"
       />
+    </div>
+
+    <div class="row justify-between no-wrap">
+      <div class="col">
+        <div>Ladestand</div>
+        <div>{{ currentCharge }}%</div>
+        <div>100km</div>
+      </div>
+      <div class="col text-center">
+        <div>Zielzeit</div>
+        <div>keine</div>
+      </div>
+      <div class="col text-right">
+        <div>Ladeziel</div>
+        <div>{{ targetCharge }}%</div>
+        <div>360km</div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +55,13 @@ import { ref } from 'vue';
 
 defineOptions({
   name: 'SliderQuasar',
+});
+
+const props = defineProps({
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const currentCharge = ref<number>(35);
