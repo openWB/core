223ed414e137bf296a6013bdc271ad576b665aaa
LKuemmel <lena.kuemmel@openwb.de>
2024-10-31 08:33:30 +0100

    pub configurable

--- packages/modules/io_actions/groups.py
+++ packages/modules/io_actions/groups.py
@@ -2,4 +2,9 @@ from enum import Enum
 
 
 class ActionGroup(Enum):
-    CONTROLLABLE_CONSUMERS_ACTIONS = "controllable_consumers_actions"
+    CONTROLLABLE_CONSUMERS = "controllable_consumers"
+
+
+READBALE_GROUP_NAME = {
+    ActionGroup.CONTROLLABLE_CONSUMERS: "Steuerbare Verbrauchseinrichtungen (§14a)"
+}
