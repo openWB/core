d0406d3611da45807704cd90b4333a9922a34321
Lutz Bender <github@lutz-bender.de>
2024-11-06 10:01:21 +0100

    move charts into components

---
+++ packages/modules/web_themes/standard/source/src/components/charts/HistoryChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    :src="image"
+    draggable="false"
+    style="width: 100%; height: 100%; object-fit: contain"
+  />
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'HistoryChart',
+});
+
+const image = '/src/assets/Dia_2.png';
+</script>
