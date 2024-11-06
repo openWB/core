2fa7f13bc1a6cf648f45e5d7e46a461d73e68543
LKuemmel <lena.kuemmel@openwb.de>
2024-11-06 13:58:13 +0100

    EZA

---
+++ packages/modules/io_actions/generator_systems/power_levels/config.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field
+from typing import List
+from dataclass_utils.factories import empty_list_factory
+from modules.io_actions.groups import ActionGroup
+
+
+@dataclass
+class PowerLevelsConfig:
+    io_device: int = 0
+    digital_input: List[int] = field(default_factory=empty_list_factory)
+    component_id: int = 0
+
+
+class PowerLevelsSetup:
+    def __init__(self,
+                 name: str = "Leistungsstufen für Erzeugungsanlagen (EZA)",
+                 type: str = "power_levels",
+                 id: int = 0,
+                 configuration: PowerLevelsConfig = None):
+        self.name = name
+        self.id = id
+        self.configuration = configuration or PowerLevelsConfig()
+        self.type = type
+        self.group = ActionGroup.GENERATOR_SYSTEMS.value
