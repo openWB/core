b728b680fbb83f466b0e8a98d0bd566f4f0ee7d4
LKuemmel <lena.kuemmel@openwb.de>
2024-11-11 15:28:26 +0100

    improve config (review)

--- packages/modules/common/io_setup.py
+++ packages/modules/common/io_setup.py
@@ -13,15 +13,11 @@ class IoDeviceSetup(Generic[T]):
                  type: str,
                  id: int,
                  configuration: T,
-                 analog_input: Optional[Dict[int, float]] = None,
-                 analog_output: Optional[Dict[int, float]] = None,
-                 digital_input: Optional[Dict[int, float]] = None,
-                 digital_output: Optional[Dict[int, float]] = None) -> None:
+                 input: Optional[Dict[str, Dict[int, float]]] = None,
+                 output: Optional[Dict[str, Dict[int, float]]] = None) -> None:
         self.name = name
         self.type = type
         self.id = id
         self.configuration = configuration
-        self.analog_input = analog_input if analog_input is not None else empty_dict_factory()
-        self.analog_output = analog_output if analog_output is not None else empty_dict_factory()
-        self.digital_input = digital_input if digital_input is not None else empty_dict_factory()
-        self.digital_output = digital_output if digital_output is not None else empty_dict_factory()
+        self.input = input if input is not None else empty_dict_factory()
+        self.output = output if output is not None else empty_dict_factory()
--- packages/modules/io_devices/add_on/config.py
+++ packages/modules/io_devices/add_on/config.py
@@ -7,20 +7,24 @@ class AddOnConfiguration:
         pass
 
 
-def digital_input_init():
-    return {"21": False,
-            "24": False,
-            "31": False,
-            "32": False,
-            "33": False,
-            "36": False,
-            "40": False}
+def init_input():
+    return {"analog": {},
+            "digital": {"21": None,
+                        "24": None,
+                        "31": None,
+                        "32": None,
+                        "33": None,
+                        "36": None,
+                        "40": None}
+            }
 
 
-def digital_output_init():
-    return {"7": False,
-            "16": False,
-            "18": False}
+def init_output():
+    return {"analog": {},
+            "digital": {"7": False,
+                        "16": False,
+                        "18": False}
+            }
 
 
 class AddOn(IoDeviceSetup[AddOnConfiguration]):
@@ -29,15 +33,14 @@ class AddOn(IoDeviceSetup[AddOnConfiguration]):
                  type: str = "add_on",
                  id: int = 0,
                  configuration: AddOnConfiguration = None,
-                 digital_input: Dict[int, bool] = None,
-                 digital_output: Dict[int, bool] = None) -> None:
+                 input: Dict[str, Dict[int, float]] = None,
+                 output: Dict[str, Dict[int, float]] = None) -> None:
         self.name = name
         self.type = type
         self.id = id
         self.configuration = configuration or AddOnConfiguration()
-        if digital_input is None:
-            digital_input = digital_input_init()
-        if digital_output is None:
-            digital_output = digital_output_init()
-        super().__init__(name, type, id, configuration or AddOnConfiguration(),
-                         digital_input=digital_input, digital_output=digital_output)
+        if input is None:
+            input = init_input()
+        if output is None:
+            output = init_output()
+        super().__init__(name, type, id, configuration or AddOnConfiguration(), input=input, output=output)
