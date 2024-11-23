9e2caa19f2d1a69633ca776afee95d970e923dfd
rleidner <rleidner@gmx.net>
2024-11-23 14:42:41 +0100

    soc_bmwbc: add new captcha for initial login, store token file in data/bmwbc folder

--- packages/modules/vehicles/bmwbc/api.py
+++ packages/modules/vehicles/bmwbc/api.py
@@ -20,6 +20,9 @@ with ImportErrorContext():
 
 from modules.common.component_state import CarState
 from modules.common.store import RAMDISK_PATH
+from pathlib import Path
+
+dataPath = "/var/www/html/openWB/data/bmwbc"
 
 log = logging.getLogger(__name__)
 
@@ -36,6 +39,14 @@ def init_store():
 
 # load store from file, if no store file exists initialize store structure
 def load_store():
+    global storeFile
+    try:
+        Path(dataPath).mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        log.error("init: dataPath creation failed, dataPath: " +
+                  dataPath + ", error=" + str(e))
+        store = init_store()
+        return store
     try:
         with open(storeFile, 'r', encoding='utf-8') as tf:
             store = json.load(tf)
@@ -54,6 +65,7 @@ def load_store():
 
 # write store file
 def write_store(store: dict):
+    global storeFile
     with open(storeFile, 'w', encoding='utf-8') as tf:
         json.dump(store, tf, indent=4)
 
@@ -66,9 +78,9 @@ def dump_json(data: dict, fout: str):
 
 
 # ---------------fetch Function called by core ------------------------------------
-async def _fetch_soc(user_id: str, password: str, vin: str, vnum: int) -> Union[int, float]:
+async def _fetch_soc(user_id: str, password: str, vin: str, captcha_token: str, vnum: int) -> Union[int, float]:
     global storeFile
-    storeFile = str(RAMDISK_PATH) + '/soc_bmwbc_vh_' + str(vnum) + '.json'
+    storeFile = str(dataPath) + '/soc_bmwbc_vh_' + str(vnum) + '.json'
 
     try:
         # set loggin in httpx to WARNING to prevent unwanted messages
@@ -85,12 +97,15 @@ async def _fetch_soc(user_id: str, password: str, vin: str, vnum: int) -> Union[
                                        expires_at=expires_at)
         else:
             # no token, authenticate via user_id and password only
-            auth = MyBMWAuthentication(user_id, password, Regions.REST_OF_WORLD)
+            auth = MyBMWAuthentication(user_id,
+                                       password,
+                                       Regions.REST_OF_WORLD,
+                                       hcaptcha_token=captcha_token)
 
         clconf = MyBMWClientConfiguration(auth)
         # account = MyBMWAccount(user_id, password, Regions.REST_OF_WORLD, config=clconf)
         # user, password and region already set in BMWAuthentication/ClientConfiguration!
-        account = MyBMWAccount(None, None, None, config=clconf)
+        account = MyBMWAccount(None, None, None, config=clconf, hcaptcha_token=captcha_token)
 
         # get vehicle list - needs to be called async
         await account.get_vehicles()
@@ -130,13 +145,13 @@ async def _fetch_soc(user_id: str, password: str, vin: str, vnum: int) -> Union[
 
 
 # main entry - _fetch needs to be run async
-def fetch_soc(user_id: str, password: str, vin: str, vnum: int) -> CarState:
+def fetch_soc(user_id: str, password: str, vin: str, captcha_token: str, vnum: int) -> CarState:
 
     # prepare and call async method
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     # get soc, range from server
-    soc, range = loop.run_until_complete(_fetch_soc(user_id, password, vin, vnum))
+    soc, range = loop.run_until_complete(_fetch_soc(user_id, password, vin, captcha_token, vnum))
 
     return CarState(soc, range)
