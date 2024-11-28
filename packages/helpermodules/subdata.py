9992ce9fb1089e523dee270e9f1325c919f16a2a
ndrsnhs <andreas.neuhaus@openwb.de>
2024-11-28 14:27:47 +0100

    write to config only on changes, not on startup

--- packages/helpermodules/subdata.py
+++ packages/helpermodules/subdata.py
@@ -683,11 +683,15 @@ class SubData:
                     config_dict = decode_payload(msg.payload)
                     var.data.ocpp = dataclass_from_dict(Ocpp, config_dict)
                 elif re.search("/optional/zabbix/", msg.topic) is not None:
-                    config = decode_payload(msg.payload)
-                    mod = importlib.import_module(".zabbix.zabbix", "modules")
-                    config = dataclass_from_dict(mod.device_descriptor.configuration_factory, config)
-                    # var.zabbix_module = mod.create_config_files(config)
-                    mod.create_config(config)
+                    # do not reconfigure zabbix if topic is received on startup
+                    if self.event_subdata_initialized.is_set():
+                        config = decode_payload(msg.payload)
+                        mod = importlib.import_module(".zabbix.zabbix", "modules")
+                        config = dataclass_from_dict(mod.device_descriptor.configuration_factory, config)
+                        # var.zabbix_module = mod.create_config_files(config)
+                        mod.create_config(config)
+                    else:
+                        log.debug("skipping zabbix message on startup")
                 else:
                     self.set_json_payload_class(var.data, msg)
         except Exception:
