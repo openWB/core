34b849ca5184856159aaf29cd1d81979fa8a9cf9
andlem74 <73437243+andlem74@users.noreply.github.com>
2024-12-02 22:43:09 +0100

    Create config.py

---
+++ packages/modules/vehicles/psacc/config.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+class PSACCVehicleSocConfiguration:
+    def __init__(self,
+                 psacc_server_or_ip: Optional[str] = None,
+                 psacc_port: Optional[int] = None,
+                 vehicle_vin: Optional[str] = None) -> None:
+        self.psacc_server_or_ip = psacc_server_or_ip
+        self.psacc_port = psacc_port
+        self.vehicle_vin = vehicle_vin
+
+
+class PSACCVehicleSoc:
+    def __init__(self,
+                 name: str = "PSA Car Controller",
+                 type: str = "psacc",
+                 configuration: PSACCVehicleSocConfiguration = None) -> None:
+        self.name = name
+        self.type = type
+        self.configuration = configuration or PSACCVehicleSocConfiguration()
