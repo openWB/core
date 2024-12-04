ee3cf8ed1a9e5bb7ace36182a8ad7f49aef4ed4f
LKuemmel <lena.kuemmel@openwb.de>
2024-12-04 10:00:16 +0100

    fix install

--- runs/install_packages.sh
+++ runs/install_packages.sh
@@ -4,6 +4,7 @@ if [ ! -e zabbix-release_6.2-3+debian11_all.deb ]
 then
 	wget https://repo.zabbix.com/zabbix/6.2/raspbian/pool/main/z/zabbix-release/zabbix-release_6.2-3%2Bdebian11_all.deb
 	sudo dpkg -i zabbix-release_6.2-3+debian11_all.deb
+	echo "download"
 fi
  sudo apt-get -q update
  sudo apt-get -q -y install \
@@ -14,5 +15,6 @@ fi
  	mosquitto mosquitto-clients \
  	python3-pip \
  	xserver-xorg x11-xserver-utils openbox-lxde-session lightdm lightdm-autologin-greeter accountsservice \ 	
-	chromium chromium-l10n zabbix-agent2 zabbix-agent2-plugin-*
+	chromium chromium-l10n
+sudo apt install zabbix-agent2 zabbix-agent2-plugin-*
 echo "done"
