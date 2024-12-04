27123cf315a991a0329914389453c6902c7d3b75
Brett-S-OWB <brett.scott@openwb.de>
2024-12-04 10:12:04 +0100

    Fix: changes to config not necessary for viewing on mobile device in local WiFi network

--- packages/modules/web_themes/standard/source/quasar.config.ts
+++ packages/modules/web_themes/standard/source/quasar.config.ts
@@ -78,11 +78,9 @@ export default configure((/* ctx */) => {
     devServer: {
       // https: true
       open: true, // opens browser window automatically
-      host: '0.0.0.0', // Allow external access
       proxy: {
         '/ws': {
-          //target: 'ws://localhost:9000',
-          target: 'ws://0.0.0.0:9001', // Changed from localhost
+          target: 'ws://localhost:9001',
           ws: true,
         },
       },
