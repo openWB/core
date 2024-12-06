a6ef51e5393b2a8ae1ec698109fa77cbca5b6f0c
Brett-S-OWB <brett.scott@openwb.de>
2024-12-06 11:20:31 +0100

    Custom button styles for mobile view (larger buttons)

--- packages/modules/web_themes/standard/source/src/components/ChargePointInstantSettings.vue
+++ packages/modules/web_themes/standard/source/src/components/ChargePointInstantSettings.vue
@@ -82,3 +82,24 @@ const instantEnergy = computed(() =>
   ),
 );
 </script>
+
+<style lang="scss" scoped>
+body.desktop {
+  .q-btn-group {
+    .q-btn {
+      min-width: 100px !important;
+    }
+  }
+}
+
+body.mobile {
+  .q-btn-group {
+    .q-btn {
+      padding: 4px 8px;
+      font-size: 12px !important;
+      min-height: 30px;
+      min-width: 100px !important;
+    }
+  }
+}
+</style>
