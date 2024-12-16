6341bbdeef854217bf1136a580f32dc4b9fa6b52
MartinRinas <martrin@microsoft.com>
2024-12-16 21:41:16 +0000

    add soc calc while charging

--- packages/modules/vehicles/json/config.py
+++ packages/modules/vehicles/json/config.py
@@ -3,12 +3,15 @@ from helpermodules.auto_str import auto_str
 
 @auto_str
 class JsonSocConfiguration:
-    def __init__(self, soc_url=None, range_url=None, soc_pattern=None, range_pattern=None, timeout=None):
+    def __init__(
+            self, soc_url=None, range_url=None, soc_pattern=None, range_pattern=None, timeout=None, calculate_soc=False
+            ):
         self.soc_url = soc_url
         self.soc_pattern = soc_pattern
         self.range_url = range_url
         self.range_pattern = range_pattern
         self.timeout = timeout
+        self.calculate_soc = calculate_soc
 
 
 @auto_str
