9bdcb20d1cd756986bfbb527e691a526a313339e
ndrsnhs <andreas.neuhaus@openwb.de>
2024-12-17 11:54:54 +0100

    fix typo

--- packages/modules/devices/sungrow/sungrow/config.py
+++ packages/modules/devices/sungrow/sungrow/config.py
@@ -12,7 +12,7 @@ class SungrowConfiguration:
                  port: int = 502,
                  modbus_id: int = 1,
                  version: Version = Version.SG,
-                 firmware: Version = Firmware.v111):
+                 firmware: Firmware = Firmware.v111):
         self.ip_address = ip_address
         self.port = port
         self.modbus_id = modbus_id
