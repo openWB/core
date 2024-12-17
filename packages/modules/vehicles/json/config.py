92a7270d44d5318dfe9008210e378755977c6c80
MartinRinas <martrin@microsoft.com>
2024-12-17 08:09:11 +0000

    single url

--- packages/modules/vehicles/json/config.py
+++ packages/modules/vehicles/json/config.py
@@ -5,16 +5,14 @@ from helpermodules.auto_str import auto_str
 class JsonSocConfiguration:
     def __init__(
             self,
-            soc_url: str = None,
-            range_url: str = None,
+            url: str = None,
             soc_pattern: str = None,
             range_pattern: str = None,
             timeout: int = None,
             calculate_soc: bool = False
             ):
-        self.soc_url = soc_url
+        self.url = url
         self.soc_pattern = soc_pattern
-        self.range_url = range_url
         self.range_pattern = range_pattern
         self.timeout = timeout
         self.calculate_soc = calculate_soc
