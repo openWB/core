ad5d3359934cf7ad8f3822347676ba6b645ba865
andlem74 <73437243+andlem74@users.noreply.github.com>
2024-12-20 20:01:17 +0100

    Changed to inherit from JSON SoC module

--- packages/modules/vehicles/psacc/config.py
+++ packages/modules/vehicles/psacc/config.py
@@ -1,14 +1,30 @@
 from typing import Optional
 
+from modules.vehicles.json.config import JsonSocConfiguration
 
-class PSACCVehicleSocConfiguration:
+
+class PSACCVehicleSocConfiguration(JsonSocConfiguration):
     def __init__(self,
                  psacc_server_or_ip: Optional[str] = None,
                  psacc_port: Optional[int] = None,
                  vehicle_vin: Optional[str] = None) -> None:
+
+        super().__init__(soc_pattern=".energy[0].level",
+                         range_pattern=".energy[0].autonomy",
+                         timeout=10,
+                         calculate_soc=True)
+
         self.psacc_server_or_ip = psacc_server_or_ip
         self.psacc_port = psacc_port
         self.vehicle_vin = vehicle_vin
+
+    @property
+    def url(self):
+        return f'http://{self.psacc_server_or_ip}:{self.psacc_port}/get_vehicleinfo/{self.vehicle_vin}'
+
+    @url.setter
+    def url(self, value):
+        pass
 
 
 class PSACCVehicleSoc:
