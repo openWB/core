4863dfd1598079cca27d3dca9936db372f26e047
okaegi <okaegi@pinball4you.ch>
2025-01-05 09:47:36 +0100

    smarthome....
    Korrektur Faktor vom http Zähler für Smarthome....

--- packages/modules/smarthome/bezug.php
+++ packages/modules/smarthome/bezug.php
@@ -3,6 +3,6 @@ $number = $_GET['d'];
 $file = "/var/www/html/openWB/ramdisk/device" . $number . "_wh";
 if (is_file($file)) {
 	$a = file_get_contents($file);
-	echo $a / 1000;
+	echo $a ;
 } else { echo 0; }
 ?>
