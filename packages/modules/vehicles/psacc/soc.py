d9d67db32aaa5ed9e83754954d762f37fee619cf
andlem74 <73437243+andlem74@users.noreply.github.com>
2025-01-09 17:04:32 +0100

    Simplification

--- packages/modules/vehicles/psacc/soc.py
+++ packages/modules/vehicles/psacc/soc.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
-import logging
-
 from modules.common.abstract_device import DeviceDescriptor
 from modules.vehicles.psacc.config import PSACCVehicleSoc
-from modules.vehicles.json.soc import create_vehicle as create_vehicle_json
-
-log = logging.getLogger(__name__)
-
-
-def create_vehicle(vehicle_config: PSACCVehicleSoc, vehicle: int):
-    return create_vehicle_json(vehicle_config, vehicle)
-
+from modules.vehicles.json.soc import create_vehicle
 
 device_descriptor = DeviceDescriptor(configuration_factory=PSACCVehicleSoc)
