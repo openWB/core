3fc9a05124971fac09a0e0c1605f1133ef098fef
LKuemmel <lena.kuemmel@openwb.de>
2025-01-14 10:13:08 +0100

    fix write

--- packages/modules/common/configurable_io.py
+++ packages/modules/common/configurable_io.py
@@ -30,10 +30,10 @@ class ConfigurableIo(Generic[T_IO_CONFIG], AbstractIoDevice):
             with SingleComponentUpdateContext(self.fault_state):
                 self.store.set(self.component_reader())
 
-    def write(self):
+    def write(self, digital_output):
         if hasattr(self, "component_writer"):
             # Wenn beim Initialisieren etwas schief gelaufen ist, ursprüngliche Fehlermeldung beibehalten
             with SingleComponentUpdateContext(self.fault_state):
-                io_state = self.component_writer()
+                io_state = self.component_writer(digital_output)
                 if io_state is not None:
                     self.store.set(io_state)
