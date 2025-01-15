acedca91efced91300ecc88723437637fe82d3a2
ndrsnhs <andreas.neuhaus@openwb.de>
2025-01-15 13:10:36 +0100

    move functionality to monitoring modul

--- packages/modules/monitoring/zabbix/api.py
+++ packages/modules/monitoring/zabbix/api.py
@@ -8,18 +8,13 @@ KEY_PATH = "/etc/zabbix/encrypt.psk"
 CONFIG_PATH = "/etc/zabbix/zabbix_agent2.conf"
 
 
-def set_value(path, key, value):
-    with open(path, "r+") as file:
-        lines = file.readlines()
-        for i, line in enumerate(lines):
-            if line.startswith(key):
-                lines[i] = f"{key}={value}\n"
-                break
-        else:
-            lines.append(f"{key}={value}\n")
-        file.seek(0)
-        file.writelines(lines)
-        file.truncate()
+def set_value(lines, key, value):
+    for i, line in enumerate(lines):
+        if line.startswith(key):
+            lines[i] = f"{key}={value}\n"
+            break
+    else:
+        lines.append(f"{key}={value}\n")
 
 
 def create_config(config: Zabbix):
@@ -28,12 +23,29 @@ def create_config(config: Zabbix):
     os.system(f"sudo chmod 666 {CONFIG_PATH}")
     with open(KEY_PATH, "w") as key_file:
         key_file.write(config.configuration.psk_key)
-    set_value(CONFIG_PATH, "ServerActive", config.configuration.destination_host)
-    set_value(CONFIG_PATH, "Hostname", config.configuration.hostname)
-    set_value(CONFIG_PATH, "TLSConnect", "psk")
-    set_value(CONFIG_PATH, "TLSAccept", "psk")
-    set_value(CONFIG_PATH, "TLSPSKFile", KEY_PATH)
-    set_value(CONFIG_PATH, "TLSPSKIdentity", config.configuration.psk_identifier)
+    with open(CONFIG_PATH, "r+") as config_file:
+        lines = config_file.readlines()
+        set_value(lines, "ServerActive", config.configuration.destination_host)
+        set_value(lines, "Hostname", config.configuration.hostname)
+        set_value(lines, "TLSConnect", "psk")
+        set_value(lines, "TLSAccept", "psk")
+        set_value(lines, "TLSPSKFile", KEY_PATH)
+        set_value(lines, "TLSPSKIdentity", config.configuration.psk_identifier)
+        config_file.seek(0)
+        config_file.writelines(lines)
+        config_file.truncate()
+
+
+def enable(config: Zabbix):
+    os.system("sudo ./runs/install_zabbix.sh")
+    create_config(config)
+    os.system("sudo systemctl restart zabbix-agent2")
+    os.system("sudo systemctl enable zabbix-agent2")
+
+
+def disable():
+    os.system("sudo systemctl stop zabbix-agent2")
+    os.system("sudo systemctl disable zabbix-agent2")
 
 
 device_descriptor = DeviceDescriptor(configuration_factory=Zabbix)
