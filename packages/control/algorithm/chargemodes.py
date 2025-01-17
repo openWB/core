25ad3ac8492b26471f1777d7988f151450faeb04
LKuemmel <lena.kuemmel@openwb.de>
2025-01-17 09:48:36 +0100

    fixes

--- packages/control/algorithm/chargemodes.py
+++ packages/control/algorithm/chargemodes.py
@@ -8,19 +8,21 @@ CHARGEMODES = ((Chargemode.SCHEDULED_CHARGING, Chargemode.INSTANT_CHARGING, True
                (None, Chargemode.TIME_CHARGING, False),
                (Chargemode.INSTANT_CHARGING, Chargemode.INSTANT_CHARGING, True),
                (Chargemode.INSTANT_CHARGING, Chargemode.INSTANT_CHARGING, False),
+               (Chargemode.ECO_CHARGING, Chargemode.INSTANT_CHARGING, True),
+               (Chargemode.ECO_CHARGING, Chargemode.INSTANT_CHARGING, False),
                (Chargemode.PV_CHARGING, Chargemode.INSTANT_CHARGING, True),
                (Chargemode.PV_CHARGING, Chargemode.INSTANT_CHARGING, False),
                (Chargemode.SCHEDULED_CHARGING, Chargemode.PV_CHARGING, True),
                (Chargemode.SCHEDULED_CHARGING, Chargemode.PV_CHARGING, False),
+               (Chargemode.ECO_CHARGING, Chargemode.PV_CHARGING, True),
+               (Chargemode.ECO_CHARGING, Chargemode.PV_CHARGING, False),
                (Chargemode.PV_CHARGING, Chargemode.PV_CHARGING, True),
                (Chargemode.PV_CHARGING, Chargemode.PV_CHARGING, False),
-               (None, Chargemode.STANDBY, True),
-               (None, Chargemode.STANDBY, False),
                (None, Chargemode.STOP, True),
                (None, Chargemode.STOP, False))
 
-CONSIDERED_CHARGE_MODES_SURPLUS = CHARGEMODES[0:2] + CHARGEMODES[6:12]
-CONSIDERED_CHARGE_MODES_PV_ONLY = CHARGEMODES[8:12]
-CONSIDERED_CHARGE_MODES_ADDITIONAL_CURRENT = CHARGEMODES[0:8]
+CONSIDERED_CHARGE_MODES_SURPLUS = CHARGEMODES[0:2] + CHARGEMODES[6:14]
+CONSIDERED_CHARGE_MODES_PV_ONLY = CHARGEMODES[10:14]
+CONSIDERED_CHARGE_MODES_ADDITIONAL_CURRENT = CHARGEMODES[0:10]
 CONSIDERED_CHARGE_MODES_MIN_CURRENT = CHARGEMODES[0:-1]
-CONSIDERED_CHARGE_MODES_NO_CURRENT = CHARGEMODES[12:16]
+CONSIDERED_CHARGE_MODES_NO_CURRENT = CHARGEMODES[14:16]
