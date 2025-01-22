4b0293b1fee05398e0f916081b6b052a7df0be94
Brett-S-OWB <brett.scott@openwb.de>
2025-01-22 11:49:09 +0100

    Add live chart data / fix chargepoint card display for all browsers

--- packages/modules/web_themes/standard/source/src/components/charts/HistoryChart.vue
+++ packages/modules/web_themes/standard/source/src/components/charts/HistoryChart.vue
@@ -1,15 +1,280 @@
 <template>
-  <img
-    :src="image"
-    draggable="false"
-    style="width: 100%; height: 100%; object-fit: contain"
-  />
+  <div class="chart-container">
+    <LineChart
+      :chartData="lineChartData"
+      :options="chartOptions"
+      :class="'chart'"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-defineOptions({
-  name: 'HistoryChart',
+import { ref, computed } from 'vue';
+import { useQuasar } from 'quasar';
+import { LineChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
+import { useMqttStore } from 'src/stores/mqtt-store';
+import 'chartjs-adapter-luxon';
+
+const $q = useQuasar();
+
+const mobileView = ref<boolean>($q.platform.is.mobile);
+const showLegend = ref<boolean>(!mobileView.value);
+
+const toggleLegend = () => {
+  showLegend.value = !showLegend.value;
+};
+
+const legendDisplay = computed(() => showLegend.value);
+
+Chart.register(...registerables);
+
+const mqttStore = useMqttStore();
+
+const selectedData = computed(() => {
+  const data = mqttStore.chartData;
+  const currentTime = Math.floor(Date.now() / 1000);
+  return data.filter((item) => item.timestamp > currentTime - 3600);
 });
 
-const image = '/src/assets/Dia_2.png';
+const chargePointIds = computed(() => mqttStore.chargePointIds);
+const chargePointNames = computed(() => mqttStore.chargePointName);
+const vehicles = computed(() => mqttStore.vehicleList());
+
+const chargePointDatasets = computed(() =>
+  chargePointIds.value.map((cpId) => ({
+    label: `${chargePointNames.value(cpId)}`,
+    borderColor: '#4766b5',
+    backgroundColor: 'rgba(71, 102, 181, 0.2)',
+    data: selectedData.value.map((item) => item[`cp${cpId}-power`] as number),
+    borderWidth: 2,
+    pointRadius: 0,
+    pointHoverRadius: 4,
+    pointHitRadius: 5,
+    fill: true,
+    yAxisID: 'y',
+  })),
+);
+
+const vehicleDatasets = computed(() =>
+  vehicles.value
+    .map((vehicle) => {
+      const socKey = `ev${vehicle.id}-soc`;
+      if (selectedData.value.some((item) => socKey in item)) {
+        return {
+          label: `${vehicle.name} SoC`,
+          borderColor: '#9F8AFF',
+          borderWidth: 2,
+          borderDash: [10, 5],
+          pointRadius: 0,
+          pointHoverRadius: 4,
+          pointHitRadius: 5,
+          data: selectedData.value.map(
+            (item) => item[socKey as keyof typeof item] as number,
+          ),
+          fill: false,
+          yAxisID: 'y2',
+        };
+      }
+      return undefined;
+    })
+    .filter(
+      (dataset): dataset is NonNullable<typeof dataset> =>
+        dataset !== undefined,
+    ),
+);
+
+const lineChartData = computed(() => {
+  return {
+    labels: selectedData.value.map((item) => item.time),
+    datasets: [
+      {
+        label: 'Grid Power',
+        borderColor: '#a33c42',
+        backgroundColor: 'rgba(239,182,188, 0.2)',
+        data: selectedData.value.map((item) => item.grid),
+        borderWidth: 2,
+        pointRadius: 0,
+        pointHoverRadius: 4,
+        pointHitRadius: 5,
+        fill: true,
+        yAxisID: 'y',
+      },
+      {
+        label: 'House Power',
+        borderColor: '#949aa1',
+        backgroundColor: 'rgba(148, 154, 161, 0.2)',
+        data: selectedData.value.map((item) => item['house-power'] as number),
+        borderWidth: 2,
+        pointRadius: 0,
+        pointHoverRadius: 4,
+        pointHitRadius: 5,
+        fill: true,
+        yAxisID: 'y',
+      },
+      {
+        label: 'PV Power',
+        borderColor: 'green',
+        backgroundColor: 'rgba(144, 238, 144, 0.2)',
+        data: selectedData.value.map((item) => item['pv-all'] as number),
+        borderWidth: 2,
+        pointRadius: 0,
+        pointHoverRadius: 4,
+        pointHitRadius: 5,
+        fill: true,
+        yAxisID: 'y',
+      },
+      {
+        label: 'Speicher ges.',
+        borderColor: '#b5a647',
+        backgroundColor: 'rgba(181, 166, 71, 0.2)',
+        data: selectedData.value.map((item) => item['bat-all-power'] as number),
+        borderWidth: 2,
+        pointRadius: 0,
+        pointHoverRadius: 4,
+        pointHitRadius: 5,
+        fill: true,
+        yAxisID: 'y',
+      },
+      {
+        label: 'CP1 Power',
+        borderColor: '#4766b5',
+        backgroundColor: 'rgba(71, 102, 181, 0.2)',
+        data: selectedData.value.map((item) => item['cp2-power'] as number),
+        borderWidth: 2,
+        pointRadius: 0,
+        pointHoverRadius: 4,
+        pointHitRadius: 5,
+        fill: true,
+        yAxisID: 'y',
+      },
+      {
+        label: 'Speicher SoC',
+        borderColor: '#FFB96E',
+        borderWidth: 2,
+        borderDash: [10, 5],
+        pointRadius: 0,
+        pointHoverRadius: 4,
+        pointHitRadius: 5,
+        data: selectedData.value.map((item) => item['bat-all-soc'] as number),
+        fill: false,
+        yAxisID: 'y2',
+      },
+      ...chargePointDatasets.value,
+      ...vehicleDatasets.value,
+    ],
+  };
+});
+
+const settingsButtonPlugin = {
+  id: 'settingsButton',
+  afterRender(chart: { canvas: { parentNode: HTMLElement } }) {
+    const buttonContainer = document.createElement('div');
+    buttonContainer.innerHTML = `
+      <div>
+        <button style="background: transparent; color: var(--q-icon); border: none; width: 22px; height: 22px; display: flex; align-items: center; justify-content: center; cursor: pointer;">
+          <span>
+            <i class="material-icons" style="font-size: 17px; color: var(--q-icon)">settings</i>
+          </span>
+        </button>
+      </div>
+    `;
+    buttonContainer.style.position = 'absolute';
+    buttonContainer.style.right = '55px';
+    buttonContainer.style.top = '5px';
+    buttonContainer.style.zIndex = '100';
+    buttonContainer.onclick = toggleLegend;
+
+    chart.canvas.parentNode.appendChild(buttonContainer);
+  },
+};
+
+Chart.register(settingsButtonPlugin);
+
+const chartOptions = computed(() => ({
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: legendDisplay.value,
+      position: 'bottom',
+      fullSize: true,
+      labels: {
+        boxWidth: 19,
+        boxHeight: 0.1,
+      },
+    },
+    title: {
+      display: true,
+      text: 'Zeitliche Verlauf',
+      font: {
+        size: 16,
+      },
+    },
+    customSettingsButton: settingsButtonPlugin,
+  },
+  scales: {
+    x: {
+      type: 'time',
+      time: {
+        unit: 'minute',
+        stepSize: 5,
+        displayFormats: {
+          minute: 'HH:mm',
+        },
+      },
+      ticks: {
+        maxTicksLimit: 12,
+        source: 'auto',
+      },
+      grid: {
+        tickLength: 5,
+      },
+    },
+    y: {
+      position: 'left',
+      type: 'linear',
+      display: true,
+      title: {
+        display: true,
+        text: 'Leistung [kW]',
+      },
+      ticks: {
+        stepSize: 0.2,
+        maxTicksLimit: 11,
+      },
+    },
+    y2: {
+      position: 'right',
+      type: 'linear',
+      display: true,
+      title: {
+        display: true,
+        text: 'SoC [%]',
+      },
+      min: 0,
+      max: 100,
+      ticks: {
+        stepSize: 10,
+      },
+      grid: {
+        display: false,
+      },
+    },
+  },
+}));
 </script>
+
+<style scoped>
+.chart-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.chart {
+  object-fit: contain;
+  height: 100%;
+  width: 100%;
+}
+</style>
