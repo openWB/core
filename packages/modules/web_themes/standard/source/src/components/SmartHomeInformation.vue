5819e929651c85694b2d9965ed1644d30989209f
Brett-S-OWB <brett.scott@openwb.de>
2025-01-24 12:47:27 +0100

    Update smart home tab

--- packages/modules/web_themes/standard/source/src/components/SmartHomeInformation.vue
+++ packages/modules/web_themes/standard/source/src/components/SmartHomeInformation.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="q-pa-md">
     <p>Smart Home</p>
-    <div class="q-mt-md">
-      <q-list bordered>
-        <q-item>
-          <q-item-section>
-            <q-item-label>Leistung</q-item-label>
-            <q-item-label caption>22.0 kW</q-item-label>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Geladen</q-item-label>
-            <q-item-label caption>7.2 kWh</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item>
-          <q-item-section>
-            <q-item-label>Ladestand</q-item-label>
-            <q-item-label caption>30% (100 km)</q-item-label>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Ladeziel</q-item-label>
-            <q-item-label caption>90% (360 km)</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </div>
   </div>
 </template>
 
