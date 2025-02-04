4c3ac243091d5a43fd0187c2feaeff11ce38c041
LKuemmel <lena.kuemmel@openwb.de>
2025-02-04 11:03:37 +0100

    fix

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -1972,12 +1972,7 @@ class UpdateConfig:
                     else:
                         dev = importlib.import_module(".io_devices."+payload["type"]+".api", "modules")
                     io_device = dev.device_descriptor.configuration_factory()
-                    for cp_topic, cp_payload in self.all_received_topics.items():
-                        if re.search("openWB/chargepoint/[0-9]+/config", cp_topic) is not None:
-                            cp_config = decode_payload(cp_payload)
-                            if cp_config["type"] == "mqtt":
-                                # if cp_config["type"] == "internal_openwb":
-                                io_device.configuration.io_device = cp_config["id"]
+                    io_device.configuration.host = "localhost"
 
                     action = RippleControlReceiverSetup()
                     for cp_topic in self.all_received_topics.keys():
