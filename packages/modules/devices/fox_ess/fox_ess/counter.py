d9ebfc266c1a8a42ab85c6927792658cce9d2e81
LKuemmel <lena.kuemmel@openwb.de>
2025-02-07 14:21:58 +0100

    fix

--- packages/modules/devices/fox_ess/fox_ess/counter.py
+++ packages/modules/devices/fox_ess/fox_ess/counter.py
@@ -20,7 +20,7 @@ class FoxEssCounter(AbstractCounter):
         unit = self.component_config.configuration.modbus_id
 
         powers = [val * -1 for val in
-                  client.read_holding_registers(31026, [ModbusDataType.INT_16]*3, unit=unit)]
+                  self.client.read_holding_registers(31026, [ModbusDataType.INT_16]*3, unit=unit)]
         power = sum(powers)
         frequency = self.client.read_holding_registers(31015, ModbusDataType.UINT_16, unit=unit) / 100
         imported = self.client.read_holding_registers(32018, ModbusDataType.UINT_32, unit=unit) * 100
