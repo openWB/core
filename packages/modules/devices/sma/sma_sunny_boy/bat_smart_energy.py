96c08b44a15390d1e067da7afa8cdeba489ea611
MartinRinas <martrin@microsoft.com>
2025-02-21 14:56:14 +0000

    refactor for readability, skip unecessary modbus writes when there is no change

--- packages/modules/devices/sma/sma_sunny_boy/bat_smart_energy.py
+++ packages/modules/devices/sma/sma_sunny_boy/bat_smart_energy.py
@@ -43,6 +43,7 @@ class SunnyBoySmartEnergyBat(AbstractBat):
         self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="speicher")
         self.store = get_bat_value_store(self.component_config.id)
         self.fault_state = FaultState(ComponentInfo.from_component_config(self.component_config))
+        self.last_mode = None
 
     def update(self) -> None:
         self.store.set(self.read())
@@ -61,7 +62,7 @@ class SunnyBoySmartEnergyBat(AbstractBat):
         ]
 
         # Read all values
-        values = self.read_registers(registers_to_read, unit)
+        values = self._read_registers(registers_to_read, unit)
 
         if values["Battery_SoC"] == self.SMA_UINT32_NAN:
             # If the storage is empty and nothing is produced on the DC side, the inverter does not supply any values.
@@ -94,22 +95,26 @@ class SunnyBoySmartEnergyBat(AbstractBat):
     def set_power_limit(self, power_limit: Optional[int]) -> None:
         unit = self.component_config.configuration.modbus_id
 
-        if power_limit is None:
+        if power_limit is None and self.last_mode is not None:
+            # Kein Powerlimit gefordert, externe Steuerung war aktiv, externe Steuerung deaktivieren
             log.debug("Keine Batteriesteuerung gefordert, deaktiviere externe Steuerung.")
             values_to_write = {
                 "Externe_Steuerung": 803,
                 "Wirkleistungsvorgabe": 0,
             }
+            self._write_registers(values_to_write, unit)
+            self.last_mode = None
         else:
+            # Powerlimit gefordert, externe Steuerung aktivieren, Limit setzen
             log.debug("Aktive Batteriesteuerung vorhanden. Setze externe Steuerung.")
             values_to_write = {
                 "Externe_Steuerung": 802,
                 "Wirkleistungsvorgabe": power_limit
             }
+            self._write_registers(values_to_write, unit)
+            self.last_mode = 'limited'
 
-        self.write_registers(values_to_write, unit)
-
-    def read_registers(self, register_names: list, unit: int) -> Dict[str, Union[int, float]]:
+    def _read_registers(self, register_names: list, unit: int) -> Dict[str, Union[int, float]]:
         values = {}
         for key in register_names:
             address, data_type = self.REGISTERS[key]
@@ -117,24 +122,24 @@ class SunnyBoySmartEnergyBat(AbstractBat):
         log.debug("Bat raw values {}: {}".format(self.__tcp_client.address, values))
         return values
 
-    def write_registers(self, values_to_write: Dict[str, Union[int, float]], unit: int) -> None:
+    def _write_registers(self, values_to_write: Dict[str, Union[int, float]], unit: int) -> None:
         for key, value in values_to_write.items():
             address, data_type = self.REGISTERS[key]
-            encoded_value = self.encode_value(value, data_type)
+            encoded_value = self._encode_value(value, data_type)
             self.__tcp_client.write_registers(address, encoded_value, unit=unit)
             log.debug(f"Neuer Wert {encoded_value} in Register {address} geschrieben.")
 
-    def encode_value(self, value: Union[int, float], data_type: ModbusDataType) -> list:
+    def _encode_value(self, value: Union[int, float], data_type: ModbusDataType) -> list:
         builder = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Big)
 
         if data_type == ModbusDataType.UINT_32:
-            builder.add_32bit_uint(value)
+            builder.add_32bit_uint(int(value))
         elif data_type == ModbusDataType.INT_32:
-            builder.add_32bit_int(value)
+            builder.add_32bit_int(int(value))
         elif data_type == ModbusDataType.UINT_16:
-            builder.add_16bit_uint(value)
+            builder.add_16bit_uint(int(value))
         elif data_type == ModbusDataType.INT_16:
-            builder.add_16bit_int(value)
+            builder.add_16bit_int(int(value))
         else:
             raise ValueError(f"Unsupported data type: {data_type}")
 
