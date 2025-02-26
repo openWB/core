83913ee7ffb3e5927baf58eb7ad7ff48392f91d1
Lutz Bender <github@lutz-bender.de>
2025-02-26 10:33:47 +0100

    modify display of schedule plans

--- packages/modules/web_themes/koala/source/src/components/ChargePointScheduledPlanButton.vue
+++ packages/modules/web_themes/koala/source/src/components/ChargePointScheduledPlanButton.vue
@@ -1,54 +1,65 @@
 <template>
   <q-btn
     no-caps
-    align="between"
-    class="cursor-pointer row"
+    align="center"
+    class="cursor-pointer"
     :color="planActive.value ? 'positive' : 'negative'"
     @click="planActive.value = !planActive.value"
   >
-    <div class="row items-center no-wrap full-width">
-      <div class="col text-left">{{ plan.name }}</div>
-      <div class="col-auto row items-center">
-        <q-icon
-          :name="
-            plan.frequency.selected === 'once'
-              ? 'today'
-              : plan.frequency.selected === 'daily'
-                ? 'date_range'
-                : 'calendar_month'
-          "
-          size="sm"
-          :title="
-            plan.frequency.selected === 'once'
-              ? 'Einmalig'
-              : plan.frequency.selected === 'daily'
-                ? 'Täglich'
-                : 'Wöchentlich'
-          "
-        />
-        <div v-if="plan.frequency.selected === 'once'">
-          {{ formattedDate }}
+    <div class="column">
+      <div class="plan-name">{{ plan.name }}</div>
+      <div class="plan-details">
+        <div>
+          <q-icon
+            :name="
+              plan.frequency.selected === 'once'
+                ? 'today'
+                : plan.frequency.selected === 'daily'
+                  ? 'date_range'
+                  : 'calendar_month'
+            "
+            size="sm"
+            :title="
+              plan.frequency.selected === 'once'
+                ? 'Einmalig'
+                : plan.frequency.selected === 'daily'
+                  ? 'Täglich'
+                  : 'Wöchentlich'
+            "
+          />
+          <div v-if="plan.frequency.selected === 'once'">
+            {{ formattedDate }}
+          </div>
+          <div v-if="plan.frequency.selected === 'weekly'">
+            {{ selectedWeekDays }}
+          </div>
+          <div v-if="plan.frequency.selected === 'daily'">
+            täglich
+          </div>
         </div>
-        <div v-if="plan.frequency.selected === 'weekly'">
-          {{ selectedWeekDays }}
+        <div>
+          <q-icon name="schedule" size="sm" />
+          <div>{{ plan.time }}</div>
         </div>
-        <q-icon name="schedule" size="sm" />
-        <div>{{ plan.time }}</div>
-        <q-icon
-          :name="plan.limit.selected === 'soc' ? 'battery_full' : 'bolt'"
-          size="sm"
-        />
-        <div v-if="plan.limit.selected === 'soc'">
-          {{ plan.limit.soc_scheduled }}%
+        <div>
+          <q-icon
+            :name="plan.limit.selected === 'soc' ? 'battery_full' : 'bolt'"
+            size="sm"
+          />
+          <div v-if="plan.limit.selected === 'soc'">
+            {{ plan.limit.soc_scheduled }}%
+          </div>
+          <div v-if="plan.limit.selected === 'amount'">
+            {{ plan.limit.amount ? plan.limit.amount / 1000 : '' }}kWh
+          </div>
         </div>
-        <div v-if="plan.limit.selected === 'amount'">
-          {{ plan.limit.amount ? plan.limit.amount / 1000 : '' }}kWh
+        <div>
+          <q-icon
+            v-if="planEtActive.value"
+            name="bar_chart"
+            size="sm"
+          />
         </div>
-        <q-icon
-          v-if="planEtActive.value"
-          name="bar_chart"
-          size="sm"
-        />
       </div>
     </div>
   </q-btn>
@@ -83,11 +94,42 @@ const planEtActive = computed(() =>
 );
 
 const selectedWeekDays = computed(() => {
-  return props.plan.frequency.weekly
-    ? weekdays
-        .filter((day, index) => props.plan.frequency.weekly[index])
-        .join(', ')
-    : '-';
+  // return props.plan.frequency.weekly
+  //   ? weekdays
+  //       .filter((day, index) => props.plan.frequency.weekly[index])
+  //       .join(', ')
+  //   : '-';
+
+  let planDays: string[] = [];
+  let rangeStart: number | null = null;
+
+  props.plan.frequency.weekly.forEach((dayValue, index) => {
+      if (dayValue) {
+          if (rangeStart === null) {
+              rangeStart = index;
+          }
+      } else {
+          if (rangeStart !== null) {
+              if (rangeStart === index - 1) {
+                  planDays.push(weekdays[rangeStart]);
+              } else {
+                  planDays.push(`${weekdays[rangeStart]}-${weekdays[index - 1]}`);
+              }
+              rangeStart = null;
+          }
+      }
+  });
+
+  // Handle the case where the last day(s) of the week are true
+  if (rangeStart !== null) {
+      if (rangeStart === props.plan.frequency.weekly.length - 1) {
+          planDays.push(weekdays[rangeStart]);
+      } else {
+          planDays.push(`${weekdays[rangeStart]}-${weekdays[props.plan.frequency.weekly.length - 1]}`);
+      }
+  }
+
+  return planDays.join(', ');
 });
 
 const formattedDate = computed(() => {
@@ -104,6 +146,20 @@ const formattedDate = computed(() => {
 <style scoped>
 .full-width {
   width: 100%;
+}
+.plan-name {
+  font-weight: bold;
+}
+.plan-details {
+  display: flex;
+  justify-content: center;
+}
+.plan-details > div {
+  display: flex;
+  align-items: center;
+}
+.plan-details > div:not(:last-child) {
+  margin-right: 0.5em;
 }
 body.mobile .height {
   height: 2.5em;
