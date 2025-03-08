b59ce8463c836f1e823fd8326d36add8493714eb
mekrapp <158028484+mekrapp@users.noreply.github.com>
2025-03-08 10:30:07 +0100

    Add files via upload

--- packages/modules/vehicles/bmwbc/soc.py
+++ packages/modules/vehicles/bmwbc/soc.py
@@ -8,38 +8,33 @@ from modules.common.abstract_device import DeviceDescriptor
 from modules.common.abstract_vehicle import VehicleUpdateData
 from modules.common.component_state import CarState
 from modules.common.configurable_vehicle import ConfigurableVehicle
-from modules.vehicles.bmwbc import api
-from modules.vehicles.bmwbc.config import BMWbc, BMWbcConfiguration
+from modules.vehicles.leaf import api
+from modules.vehicles.leaf.config import LeafSoc, LeafConfiguration
 
 
 log = logging.getLogger(__name__)
 
 
-def create_vehicle(vehicle_config: BMWbc, vehicle: int):
+def create_vehicle(vehicle_config: LeafSoc, vehicle: int):
     def updater(vehicle_update_data: VehicleUpdateData) -> CarState:
         return api.fetch_soc(
             vehicle_config.configuration.user_id,
             vehicle_config.configuration.password,
-            vehicle_config.configuration.vin,
             vehicle)
-    return ConfigurableVehicle(vehicle_config=vehicle_config,
-                               component_updater=updater,
-                               vehicle=vehicle,
-                               calc_while_charging=vehicle_config.configuration.calculate_soc)
+    return ConfigurableVehicle(vehicle_config=vehicle_config, component_updater=updater, vehicle=vehicle)
 
 
-def bmwbc_update(user_id: str, password: str, vin: str, charge_point: int):
-    log.debug("bmwbc: user_id="+user_id+"vin="+vin+"charge_point="+str(charge_point))
-    vehicle_config = BMWbc(configuration=BMWbcConfiguration(charge_point, user_id, password, vin))
+def leaf_update(user_id: str, password: str, charge_point: int):
+    log.debug("Leaf: user_id="+user_id+"charge_point="+str(charge_point))
+    vehicle_config = LeafSoc(configuration=LeafConfiguration(charge_point, user_id, password))
     store.get_car_value_store(charge_point).store.set(api.fetch_soc(
         vehicle_config.configuration.user_id,
         vehicle_config.configuration.password,
-        vehicle_config.configuration.vin,
         charge_point))
 
 
 def main(argv: List[str]):
-    run_using_positional_cli_args(bmwbc_update, argv)
+    run_using_positional_cli_args(leaf_update, argv)
 
 
-device_descriptor = DeviceDescriptor(configuration_factory=BMWbc)
+device_descriptor = DeviceDescriptor(configuration_factory=LeafSoc)
