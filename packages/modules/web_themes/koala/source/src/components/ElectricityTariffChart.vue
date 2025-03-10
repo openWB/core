e423f4537b5f56dde07e6eb84ea6e0e796fecc62
Lutz Bender <github@lutz-bender.de>
2025-03-10 08:34:33 +0100

    switch to setup api in ElectricityPriceChart.vue

--- packages/modules/web_themes/koala/source/src/components/ElectricityTariffChart.vue
+++ packages/modules/web_themes/koala/source/src/components/ElectricityTariffChart.vue
@@ -11,7 +11,8 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, useTemplateRef, computed } from 'vue';
 import { useMqttStore } from 'src/stores/mqtt-store';
 import { useQuasar } from 'quasar';
 
@@ -28,232 +29,237 @@ import {
   TimeScale,
   Filler,
 } from 'chart.js';
-Chart.register(Legend, LineController, LineElement, PointElement, LinearScale, TimeScale, Filler, annotationPlugin);
+Chart.register(
+  Legend,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  TimeScale,
+  Filler,
+  annotationPlugin,
+);
 
-export default {
-  name: 'ElectricityTariffChart',
-  components: {
-    ChartjsLine,
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    required: false,
+    default: undefined,
   },
-  props: {
-    modelValue: {
-      type: Number,
-      required: false,
-      default: undefined,
-    },
-  },
-  emits: ['update:modelValue'],
-  data() {
-    return {
-      mqttStore: useMqttStore(),
-      $q: useQuasar(),
-      chartDatasets: {
-        datasets: [
-          {
-            label: 'Stromtarif',
-            unit: 'ct/kWh',
-            type: 'line',
-            stepped: true,
-            borderColor: 'rgb(18, 111, 142)',
-            backgroundColor: 'rgb(18, 111, 142)',
-            fill: false,
-            pointStyle: 'circle',
-            pointRadius: 0,
-            pointHoverRadius: 4,
-            cubicInterpolationMode: 'monotone',
-            hidden: false,
-            borderWidth: 2,
-            data: undefined,
-            yAxisID: 'y',
-            parsing: {
-              xAxisKey: 'timestamp',
-              yAxisKey: 'price',
-            },
-          },
-        ],
+});
+const emit = defineEmits(['update:modelValue']);
+
+const mqttStore = useMqttStore();
+const $q = useQuasar();
+
+const priceChart = useTemplateRef('priceChart');
+
+const chartDatasets = ref({
+  datasets: [
+    {
+      label: 'Stromtarif',
+      unit: 'ct/kWh',
+      type: 'line',
+      stepped: true,
+      borderColor: 'rgb(18, 111, 142)',
+      backgroundColor: 'rgb(18, 111, 142)',
+      fill: false,
+      pointStyle: 'circle',
+      pointRadius: 0,
+      pointHoverRadius: 4,
+      cubicInterpolationMode: 'monotone',
+      hidden: false,
+      borderWidth: 2,
+      data: undefined,
+      yAxisID: 'y',
+      parsing: {
+        xAxisKey: 'timestamp',
+        yAxisKey: 'price',
       },
-    };
-  },
-  computed: {
-    chartDataRead() {
-      return Object.keys(this.chartDataObject.datasets[0].data).length > 0;
     },
-    chartDataObject() {
-      let myData = [];
-      const chartEntries = this.mqttStore.etPrices;
-      if (Object.keys(chartEntries).length > 0) {
-        // proper scaling:
-        // timestamp: seconds -> milliseconds
-        // price: €/Wh -> €/kWh
-        for (const [key, value] of Object.entries(chartEntries)) {
-          myData.push({
-            timestamp: key * 1000,
-            price: value * 100000,
-          });
+  ],
+});
+
+const chartDataObject = computed(() => {
+  let myData = [];
+  const chartEntries = mqttStore.etPrices;
+  if (Object.keys(chartEntries).length > 0) {
+    // proper scaling:
+    // timestamp: seconds -> milliseconds
+    // price: €/Wh -> €/kWh
+    for (const [key, value] of Object.entries(chartEntries)) {
+      myData.push({
+        timestamp: key * 1000,
+        price: value * 100000,
+      });
+    }
+    // repeat last dataset with 59min 59sec offset
+    const lastData = myData.slice(-1)[0];
+    myData.push({
+      timestamp: lastData.timestamp + (60 * 60 - 1) * 1000,
+      price: lastData.price,
+    });
+  }
+  const dataObject = chartDatasets.value;
+  dataObject.datasets[0].data = myData;
+  return dataObject;
+});
+
+const chartDataRead = computed(() => {
+  return Object.keys(chartDataObject.value.datasets[0].data).length > 0;
+});
+
+const priceAnnotations = computed(() => {
+  const colorUnblocked = 'rgba(73, 238, 73, 0.2)'; // ToDo: use theme color
+  const colorBlocked = 'rgba(255, 10, 13, 0.2)'; // ToDo: use theme color
+  const myData = chartDataObject.value.datasets[0].data;
+  class Annotation {
+    type = 'box';
+    drawTime = 'beforeDatasetsDraw';
+    xMin = myData[0].timestamp;
+    xMax = myData[0].timestamp;
+    borderWidth = 2;
+    cornerRadius = 0;
+  }
+  let annotations = [];
+  if (props.modelValue !== undefined) {
+    for (let i = 0; i < myData.length; i++) {
+      if (myData[i].price <= props.modelValue) {
+        let newAnnotation = new Annotation();
+        newAnnotation.borderColor = colorUnblocked;
+        newAnnotation.backgroundColor = colorUnblocked;
+        newAnnotation.xMin = myData[i].timestamp; // set left edge of box
+        while (i < myData.length && myData[i].price <= props.modelValue) {
+          i++;
         }
-        // repeat last dataset with 59min 59sec offset
-        const lastData = myData.slice(-1)[0];
-        myData.push({
-          timestamp: lastData.timestamp + (60 * 60 - 1) * 1000,
-          price: lastData.price,
-        });
-      }
-      const dataObject = this.chartDatasets;
-      dataObject.datasets[0].data = myData;
-      return dataObject;
-    },
-    priceAnnotations() {
-      const colorUnblocked = 'rgba(73, 238, 73, 0.2)'; // ToDo: use theme color
-    	const colorBlocked = 'rgba(255, 10, 13, 0.2)'; // ToDo: use theme color
-      const myData = this.chartDataObject.datasets[0].data;
-      class Annotation {
-        type = 'box';
-        drawTime = 'beforeDatasetsDraw';
-        xMin = myData[0].timestamp;
-        xMax = myData[0].timestamp;
-        borderWidth = 2;
-        cornerRadius = 0;
-      }
-      let annotations = [];
-      if ( this.modelValue !== undefined ) {
-        for ( let i = 0; i < myData.length; i++ ) {
-          if ( myData[i].price <= this.modelValue ) {
-            let newAnnotation = new Annotation();
-            newAnnotation.borderColor = colorUnblocked;
-            newAnnotation.backgroundColor = colorUnblocked;
-            newAnnotation.xMin = myData[i].timestamp;  // set left edge of box
-            while ( i < myData.length && myData[i].price <= this.modelValue ) {
-              i++;
-            }
-            if ( i == myData.length ) {
-              // correct index if out of bounds
-              i--;
-            }
-            newAnnotation.xMax = myData[i].timestamp;  // first index myData[i] > maxPrice is right edge of box
-            annotations.push(newAnnotation);  // add box to annotations
-          }
+        if (i == myData.length) {
+          // correct index if out of bounds
+          i--;
         }
-        for ( let i = 0; i < myData.length; i++ ) {
-          if ( myData[i].price > this.modelValue ) {
-            let newAnnotation = new Annotation();
-            newAnnotation.borderColor = colorBlocked;
-            newAnnotation.backgroundColor = colorBlocked;
-            newAnnotation.xMin = myData[i].timestamp;  // set left edge of box
-            while ( i < myData.length && myData[i].price > this.modelValue ) {
-              i++;
-            }
-            if ( i == myData.length ) {
-              // correct index if out of bounds
-              i--;
-            }
-            newAnnotation.xMax = myData[i].timestamp;  // first index myData[i] > maxPrice is right edge of box
-            annotations.push(newAnnotation);  // add box to annotations
-          }
+        newAnnotation.xMax = myData[i].timestamp; // first index myData[i] > maxPrice is right edge of box
+        annotations.push(newAnnotation); // add box to annotations
+      }
+    }
+    for (let i = 0; i < myData.length; i++) {
+      if (myData[i].price > props.modelValue) {
+        let newAnnotation = new Annotation();
+        newAnnotation.borderColor = colorBlocked;
+        newAnnotation.backgroundColor = colorBlocked;
+        newAnnotation.xMin = myData[i].timestamp; // set left edge of box
+        while (i < myData.length && myData[i].price > props.modelValue) {
+          i++;
         }
+        if (i == myData.length) {
+          // correct index if out of bounds
+          i--;
+        }
+        newAnnotation.xMax = myData[i].timestamp; // first index myData[i] > maxPrice is right edge of box
+        annotations.push(newAnnotation); // add box to annotations
       }
-      return annotations;
+    }
+  }
+  return annotations;
+});
+
+const myChartOptions = computed(() => {
+  return {
+    plugins: {
+      title: {
+        display: false,
+      },
+      legend: {
+        display: false,
+      },
+      annotation: {
+        annotations: priceAnnotations.value,
+      },
     },
-    myChartOptions() {
-      return {
-        plugins: {
-          title: {
-            display: false,
-          },
-          legend: {
-            display: false,
-          },
-          annotation: {
-            annotations: this.priceAnnotations,
-          }
-        },
-        elements: {
-          point: {
-            radius: 2,
-          },
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        interaction: {
-          mode: 'index',
-          intersect: false,
-        },
-        scales: {
-          x: {
-            type: 'time',
-            time: {
-              unit: 'hour',
-              text: 'Zeit',
-              maxTicksLimit: 24,
-            },
-            display: true,
-            title: {
-              display: true,
-              text: 'Uhrzeit',
-              color: this.$q.dark.isActive
-                ? 'rgb(255, 255, 255)'
-                : 'rgb(0, 0, 0)',
-            },
-            ticks: {
-              font: {
-                size: 12,
-              },
-              color: this.$q.dark.isActive
-                ? 'rgb(255, 255, 255)'
-                : 'rgb(0, 0, 0)',
-              // maxTicksLimit: 0,
-            },
-            grid: {
-              color: this.$q.dark.isActive
-                ? 'rgba(255, 255, 255, 0.1)'
-                : 'rgba(0, 0, 0, 0.1)',
-            },
-          },
-          y: {
-            position: 'left',
-            type: 'linear',
-            display: 'auto',
-            // suggestedMin: 0,
-            // suggestedMax: 0,
-            title: {
-              font: {
-                size: 12,
-              },
-              display: true,
-              text: 'Preis [ct/kWh]',
-              color: this.$q.dark.isActive
-                ? 'rgb(255, 255, 255)'
-                : 'rgb(0, 0, 0)',
-            },
-            grid: {
-              color: this.$q.dark.isActive
-                ? 'rgba(255, 255, 255, 0.1)'
-                : 'rgba(0, 0, 0, 0.1)',
-            },
-            ticks: {
-              font: {
-                size: 12,
-              },
-              stepSize: 0.1,
-              maxTicksLimit: 11,
-              color: this.$q.dark.isActive
-                ? 'rgb(255, 255, 255)'
-                : 'rgb(0, 0, 0)',
-            },
-          },
-        },
-      };
+    elements: {
+      point: {
+        radius: 2,
+      },
     },
-  },
-  methods: {
-    chartClick(event) {
-      const priceChart = this.$refs.priceChart;
-      const points = priceChart.chart.getElementsAtEventForMode(event, 'index', { intersect: false }, true);
-      if (points.length > 0) {
-        this.$emit('update:modelValue', Math.ceil(this.chartDataObject.datasets[0].data[points[0].index].price * 100) / 100);
-      }
+    responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: 'index',
+      intersect: false,
     },
-  },
-};
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'hour',
+          text: 'Zeit',
+          maxTicksLimit: 24,
+        },
+        display: true,
+        title: {
+          display: true,
+          text: 'Uhrzeit',
+          color: $q.dark.isActive ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)',
+        },
+        ticks: {
+          font: {
+            size: 12,
+          },
+          color: $q.dark.isActive ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)',
+          // maxTicksLimit: 0,
+        },
+        grid: {
+          color: $q.dark.isActive
+            ? 'rgba(255, 255, 255, 0.1)'
+            : 'rgba(0, 0, 0, 0.1)',
+        },
+      },
+      y: {
+        position: 'left',
+        type: 'linear',
+        display: 'auto',
+        // suggestedMin: 0,
+        // suggestedMax: 0,
+        title: {
+          font: {
+            size: 12,
+          },
+          display: true,
+          text: 'Preis [ct/kWh]',
+          color: $q.dark.isActive ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)',
+        },
+        grid: {
+          color: $q.dark.isActive
+            ? 'rgba(255, 255, 255, 0.1)'
+            : 'rgba(0, 0, 0, 0.1)',
+        },
+        ticks: {
+          font: {
+            size: 12,
+          },
+          stepSize: 0.1,
+          maxTicksLimit: 11,
+          color: $q.dark.isActive ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)',
+        },
+      },
+    },
+  };
+});
+
+function chartClick(event) {
+  const points = priceChart.value.chart.getElementsAtEventForMode(
+    event,
+    'index',
+    { intersect: false },
+    true,
+  );
+  if (points.length > 0) {
+    emit(
+      'update:modelValue',
+      Math.ceil(
+        chartDataObject.value.datasets[0].data[points[0].index].price * 100,
+      ) / 100,
+    );
+  }
+}
 </script>
 
 <style scoped>
