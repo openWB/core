02f627ec8b5408efc03cea19053450efd0d71372
mekrapp <158028484+mekrapp@users.noreply.github.com>
2025-03-14 21:11:25 +0100

    Update config.py
    variable "region" added.
    variable "name" changed to "Nissan Leaf/NV200 -05.2019 (experimental)".

--- packages/modules/vehicles/leaf/config.py
+++ packages/modules/vehicles/leaf/config.py
@@ -5,11 +5,12 @@ class LeafConfiguration:
     def __init__(self, user_id: Optional[str] = None, password: Optional[str] = None):
         self.user_id = user_id
         self.password = password
+        self.region = region
 
 
 class LeafSoc:
     def __init__(self,
-                 name: str = "Leaf",
+                 name: str = "Nissan Leaf/NV200 -05.2019 (experimental)",
                  type: str = "leaf",
                  configuration: LeafConfiguration = None) -> None:
         self.name = name
