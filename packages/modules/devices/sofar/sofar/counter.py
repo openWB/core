36dd7489e5c506673af7adc4dfaa2f638a69ad64
ndrsnhs <andreas.neuhaus@openwb.de>
2025-03-18 11:08:41 +0100

    change factors an register type

--- packages/modules/devices/sofar/sofar/counter.py
+++ packages/modules/devices/sofar/sofar/counter.py
@@ -24,49 +24,39 @@ class SofarCounter(AbstractCounter):
     def update(self, client: ModbusTcpClient_):
         # 0x0485 ActivePower_output_total Int16 in kW accuracy 0,01 discharge + charge -
         # 0x0488 ActivePower_PCC_total Int16 0,01 kW
-        power = client.read_input_registers(0x0488, ModbusDataType.INT_16, wordorder=Endian.Little,
-                                            unit=self.__modbus_id) * -1
+        power = client.read_holding_registers(0x0488, ModbusDataType.INT_16, wordorder=Endian.Little,
+                                              unit=self.__modbus_id) * -10
         # 0x0484 Frequency_Grid UInt16 in Hz accuracy 0,01
-        frequency = client.read_input_registers(
+        frequency = client.read_holding_registers(
             0x0484, ModbusDataType.UINT_16, unit=self.__modbus_id) / 100
         try:
-            # 0x048F ActivePower_Output_R UInt16 in V accuracy 0,1
-            # 0x0493 ActivePower_PCC_R Int16 in kW accuracy 0,01
-            powers = [-value for value in client.read_input_registers(
-                0x0493, [ModbusDataType.INT_16] * 1, wordorder=Endian.Little, unit=self.__modbus_id
-            )]
+            powers = [
+                client.read_holding_registers(0x0493, ModbusDataType.INT_16, unit=self.__modbus_id) * -10,
+                client.read_holding_registers(0x049E, ModbusDataType.INT_16, unit=self.__modbus_id) * -10,
+                client.read_holding_registers(0x04A9, ModbusDataType.INT_16, unit=self.__modbus_id) * -10]
         except Exception:
             powers = None
         try:
-            voltages = [client.read_input_registers(
-                # 048D Voltage_Phase_R UInt16 in V accuracy 0,1
-                0x048D, ModbusDataType.UINT_16, unit=self.__modbus_id
-            ) / 10, client.read_input_registers(
-                # 0498 Voltage_Phase_S UInt16 in V accuracy 0,1
-                0x0498, ModbusDataType.UINT_16, unit=self.__modbus_id
-            ) / 10, client.read_input_registers(
-                # 04A3 Voltage_Phase_T UInt16 in V accuracy 0,1
-                0x04A3, ModbusDataType.UINT_16, unit=self.__modbus_id
-            ) / 10]
-            if voltages[0] < 1:
-                voltages[0] = 230
-            if voltages[1] < 1:
-                voltages[1] = 230
-            if voltages[2] < 1:
-                voltages[2] = 230
+            voltages = [
+                client.read_holding_registers(0x048D, ModbusDataType.UINT_16, unit=self.__modbus_id) * 0.1,
+                client.read_holding_registers(0x0498, ModbusDataType.UINT_16, unit=self.__modbus_id) * 0.1,
+                client.read_holding_registers(0x04A3, ModbusDataType.UINT_16, unit=self.__modbus_id) * 0.1]
+            for voltage in voltages:
+                if voltage < 1:
+                    voltage = 230
         except Exception:
             voltages = [230, 230, 230]
         exported = [value * 10
-                    for value in client.read_input_registers(
+                    for value in client.read_holding_registers(
                         # 0x0692 Energy_Selling_Total UInt32 in kwH accuracy 0,01 LSB
                         # 0x0693 Energy_Selling_Total UInt32 in kwH accuracy 0,01
-                        0x0692, [ModbusDataType.UINT_32] * 10,
+                        0x0692, [ModbusDataType.UINT_32] * 0.1,
                         wordorder=Endian.Little, unit=self.__modbus_id)]
         imported = [value * 10
-                    for value in client.read_input_registers(
+                    for value in client.read_holding_registers(
                         # 0x068E Energy_Purchase_Total UInt32 in kwH accuracy 0,01 LSB
                         # 0x068F Energy_Purchase_Total UInt32 in kwH accuracy 0,01
-                        0x068E, [ModbusDataType.UINT_32] * 10,
+                        0x068E, [ModbusDataType.UINT_32] * 0.1,
                         wordorder=Endian.Little, unit=self.__modbus_id)]
 
         counter_state = CounterState(
--- packages/modules/devices/sofar/sofar/inverter.py
+++ packages/modules/devices/sofar/sofar/inverter.py
@@ -27,15 +27,15 @@ class SofarInverter(AbstractInverter):
         # 0x0595 Power_PV6, 0x0598 Power_PV7, 0x059B Power_PV8, 0x059E Power_PV9, 0x05A1 Power_PV10,
         # 0x05A4 Power_PV11, 0x05A7 Power_PV12, 0x05AA Power_PV13,
         # 0x05AD Power_PV14, 0x05B0 Power_PV15, 0x05B3 Power_PV16
-        power = sum([client.read_input_registers(reg, ModbusDataType.UINT_16,
+        power = sum([client.read_holding_registers(reg, ModbusDataType.UINT_16,
                     unit=self.__modbus_id) for reg in [0x0586, 0x0589, 0x058C, 0x058F, 0x0592,
                                                        0x0595, 0x0598, 0x059B, 0x059E, 0x05A1, 0x05A4,
-                                                       0x05A7, 0x05AA, 0x05AD, 0x05B0, 0x05B3]]) * -1
+                                                       0x05A7, 0x05AA, 0x05AD, 0x05B0, 0x05B3]]) * -10
         # 0x05C4 Power_PV_Total UInt16 in kW accuracy 0,1
         # 0x0686 PV_Generation_Total UInt32 0,1 kW LSB
         # 0x0687 PV_Generation_Total UInt32 0,1 kW
-        exported = client.read_input_registers(0x0686, ModbusDataType.UINT_32, wordorder=Endian.Little,
-                                               unit=self.__modbus_id) * 100
+        exported = client.read_holding_registers(0x0686, ModbusDataType.UINT_32, wordorder=Endian.Little,
+                                                 unit=self.__modbus_id) * 100
 
         inverter_state = InverterState(
             power=power,
