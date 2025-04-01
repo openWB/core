b7ba1aa10738e2940b0c39b321082fb96c8e6fda
Brett-S-OWB <brett.scott@openwb.de>
2025-04-01 08:19:09 +0200

    Refactor SliderDouble - two sliders only

--- packages/modules/web_themes/koala/source/src/components/ChargePointCard.vue
+++ packages/modules/web_themes/koala/source/src/components/ChargePointCard.vue
@@ -31,32 +31,27 @@
         </div>
         <div class="col q-pl-sm">
           <div class="text-subtitle2">geladen</div>
-          <!-- {{ energyCharged }} -->
           {{ energyChargedPlugged }}
         </div>
       </div>
       <SliderDouble
         v-if="showSocTargetSlider"
         class="q-mt-sm"
+        :model-value="target"
         :readonly="true"
         :charge-mode="chargeMode"
-        :connected-vehicle-soc="connectedVehicleSoc"
-        :target-soc="targetSoc"
-        :target-time="targetTime"
         :limit-mode="limitMode"
-        :current-energy-charged="currentEnergyCharged"
-        :target-energy-amount="targetEnergyAmount"
+        :current-value="currentValue"
+        :target-time="targetTime"
       />
     </q-card-section>
   </q-card>
-
   <!-- //////////////////////  Settings popup dialog   //////////////////// -->
   <ChargePointSettings
     :chargePointId="props.chargePointId"
     v-model="settingsVisible"
   />
 </template>
-
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import { useMqttStore } from 'src/stores/mqtt-store';
@@ -69,14 +64,10 @@ import ChargePointStateMessage from './ChargePointStateMessage.vue';
 import ChargePointFaultMessage from './ChargePointFaultMessage.vue';
 import ChargePointVehicleSelect from './ChargePointVehicleSelect.vue';
 import ChargePointSettings from './ChargePointSettings.vue';
-
 const mqttStore = useMqttStore();
-
 const props = defineProps<{
   chargePointId: number;
 }>();
-
-// Add these computed properties
 const limitMode = computed(() => {
   switch (chargeMode.value) {
     case 'instant_charging':
@@ -95,47 +86,11 @@ const limitMode = computed(() => {
       return 'soc';
   }
 });
-
-const currentEnergyCharged = computed(() => {
-  const energyValue = mqttStore.chargePointEnergyChargedPlugged(
-    props.chargePointId,
-    'value',
-  );
-  return typeof energyValue === 'number' ? energyValue : 0;
-});
-
-const targetEnergyAmount = computed(() => {
-  if (limitMode.value !== 'amount') return 0;
-  switch (chargeMode.value) {
-    case 'instant_charging':
-      return (
-        (mqttStore.chargePointConnectedVehicleInstantChargeLimitEnergy(
-          props.chargePointId,
-        ).value ?? 0) * 1000
-      );
-    case 'pv_charging':
-      return (
-        (mqttStore.chargePointConnectedVehiclePvChargeLimitEnergy(
-          props.chargePointId,
-        ).value ?? 0) * 1000
-      );
-    case 'eco_charging':
-      return (
-        (mqttStore.chargePointConnectedVehicleEcoChargeLimitEnergy(
-          props.chargePointId,
-        ).value ?? 0) * 1000
-      );
-    default:
-      return 0;
-  }
-});
-
 const settingsVisible = ref<boolean>(false);
 const name = computed(() => mqttStore.chargePointName(props.chargePointId));
 const power = computed(() =>
   mqttStore.chargePointPower(props.chargePointId, 'textValue'),
 );
-
 const energyChargedPlugged = computed(() =>
   mqttStore.chargePointEnergyChargedPlugged(props.chargePointId, 'textValue'),
 );
@@ -145,20 +100,24 @@ const phaseNumber = computed(() =>
 const chargingCurrent = computed(() =>
   mqttStore.chargePointChargingCurrent(props.chargePointId),
 );
-
-const connectedVehicleSoc = computed(() =>
-  Math.round(
-    mqttStore.chargePointConnectedVehicleSoc(props.chargePointId).value?.soc ??
+const currentValue = computed(() => {
+  if (limitMode.value === 'soc') {
+    return Math.round(
+      mqttStore.chargePointConnectedVehicleSoc(props.chargePointId).value?.soc ??
       0,
-  ),
-);
-
+    );
+  } else {
+    return mqttStore.chargePointEnergyChargedPlugged(
+      props.chargePointId,
+      'value',
+    ) as number;
+  }
+});
 const chargeMode = computed(
   () =>
     mqttStore.chargePointConnectedVehicleChargeMode(props.chargePointId).value,
 );
-
-const targetSoc = computed<number | undefined>(() => {
+const target = computed(() => {
   switch (chargeMode.value) {
     case 'scheduled_charging':
       return mqttStore.vehicleScheduledChargingTarget(props.chargePointId).value
@@ -171,8 +130,10 @@ const targetSoc = computed<number | undefined>(() => {
       return instantLimitMode === 'soc'
         ? mqttStore.chargePointConnectedVehicleInstantChargeLimitSoC(
             props.chargePointId,
-          ).value
-        : undefined;
+          ).value ?? 0
+        : (mqttStore.chargePointConnectedVehicleInstantChargeLimitEnergy(
+          props.chargePointId,
+        ).value ?? 0) * 1000;
     case 'pv_charging':
       const pvLimitMode = mqttStore.chargePointConnectedVehiclePvChargeLimit(
         props.chargePointId,
@@ -180,8 +141,10 @@ const targetSoc = computed<number | undefined>(() => {
       return pvLimitMode === 'soc'
         ? mqttStore.chargePointConnectedVehiclePvChargeLimitSoC(
             props.chargePointId,
-          ).value
-        : undefined;
+          ).value ?? 0
+        : (mqttStore.chargePointConnectedVehiclePvChargeLimitEnergy(
+          props.chargePointId,
+        ).value ?? 0) * 1000;
     case 'eco_charging':
       const ecoLimitMode = mqttStore.chargePointConnectedVehicleEcoChargeLimit(
         props.chargePointId,
@@ -189,28 +152,27 @@ const targetSoc = computed<number | undefined>(() => {
       return ecoLimitMode === 'soc'
         ? mqttStore.chargePointConnectedVehicleEcoChargeLimitSoC(
             props.chargePointId,
-          ).value
-        : undefined;
+          ).value ?? 0
+        : (mqttStore.chargePointConnectedVehicleEcoChargeLimitEnergy(
+          props.chargePointId,
+        ).value ?? 0) * 1000;
     default:
-      return undefined;
+      return 0;
   }
 });
-
 const showSocTargetSlider = computed(() => {
-  return chargeMode.value !== undefined && !'stop'.includes(chargeMode.value);
+  return chargeMode.value !== undefined && !'stop'.includes(chargeMode.value) && limitMode.value !== 'none';
 });
-
 const targetTime = computed(() => {
   const target = mqttStore.vehicleScheduledChargingTarget(
     props.chargePointId,
   ).value;
   if (!target || !target.time || chargeMode.value !== 'scheduled_charging') {
-    return 'keine';
+    return undefined;
   }
   return target.time;
 });
 </script>
-
 <style lang="scss" scoped>
 .card-width {
   max-width: 24em;
--- packages/modules/web_themes/koala/source/src/components/SliderDouble.vue
+++ packages/modules/web_themes/koala/source/src/components/SliderDouble.vue
@@ -1,70 +1,49 @@
 <template>
   <div class="my-card">
     <div class="slider-container">
-      <!-- For SoC mode -->
-      <div v-if="props.limitMode !== 'amount'">
-        <q-slider
-          v-model="currentValue"
-          :min="0"
-          :max="100"
-          color="green-7"
-          class="current-slider"
-          track-size="1.5em"
-          thumb-size="0px"
-          readonly
-          no-focus
-          @touchstart.stop
-          @touchmove.stop
-          @touchend.stop
-        />
-        <q-slider
-          v-model="targetValue"
-          :min="0"
-          :max="100"
-          color="light-green-5"
-          inner-track-color="blue-grey-2"
-          class="target-slider"
-          track-size="1.5em"
-          :thumb-size="props.readonly ? '0' : '2em'"
-          :readonly="props.readonly"
-          @touchstart.stop
-          @touchmove.stop
-          @touchend.stop
-        />
-      </div>
-
-      <!-- For Energy Amount mode -->
-      <div v-else>
-        <q-slider
-          v-model="currentValue"
-          :min="0"
-          :max="targetValue"
-          color="green-7"
-          class="current-slider"
-          track-size="1.5em"
-          thumb-size="0px"
-          readonly
-          no-focus
-          @touchstart.stop
-          @touchmove.stop
-          @touchend.stop
-        />
-      </div>
+      <q-slider
+        :model-value="currentValue"
+        :min="0"
+        :max="maxValue"
+        color="green-7"
+        class="current-slider"
+        track-size="1.5em"
+        thumb-size="0px"
+        readonly
+        no-focus
+        @touchstart.stop
+        @touchmove.stop
+        @touchend.stop
+      />
+      <q-slider
+        v-if="props.limitMode == 'soc'"
+        v-model="target"
+        :min="0"
+        :max="100"
+        color="light-green-5"
+        inner-track-color="blue-grey-2"
+        class="target-slider"
+        track-size="1.5em"
+        :thumb-size="props.readonly ? '0' : '2em'"
+        :readonly="props.readonly"
+        @touchstart.stop
+        @touchmove.stop
+        @touchend.stop
+      />
     </div>
-
     <div class="row justify-between no-wrap">
       <div class="col">
-        <div>{{ props.limitMode !== 'amount' ? 'Ladestand' : 'Geladen' }}</div>
+        <div>{{ props.limitMode == 'soc' ? 'Ladestand' : 'Geladen' }}</div>
         <div>
           {{
-            props.limitMode !== 'amount'
+            props.limitMode == 'soc'
               ? currentValue + '%'
               : formatEnergy(currentValue)
           }}
         </div>
       </div>
       <div
-        v-if="props.chargeMode === 'scheduled_charging'"
+        v-if="props.targetTime"
         class="col text-center"
       >
         <div>Zielzeit</div>
@@ -72,28 +51,30 @@
       </div>
       <div class="col text-right">
         <div>
-          {{ props.limitMode !== 'amount' ? 'Ladeziel' : 'Energieziel' }}
+          {{ props.limitMode == 'soc' ? 'Ladeziel' : 'Energieziel' }}
         </div>
         <div>
           {{
-            props.limitMode !== 'amount'
-              ? targetValue + '%'
-              : formatEnergy(targetValue)
+            props.limitMode == 'soc'
+              ? target + '%'
+              : formatEnergy(target)
           }}
         </div>
       </div>
     </div>
   </div>
 </template>
-
 <script setup lang="ts">
 import { computed } from 'vue';
-
 defineOptions({
   name: 'SliderQuasar',
 });
-
+const emit = defineEmits(['update:modelValue']);
 const props = defineProps({
+  modelValue: {
+    type: Number,
+    required: true,
+  },
   readonly: {
     type: Boolean,
     default: false,
@@ -102,43 +83,34 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  connectedVehicleSoc: {
-    type: Number,
-    default: 0,
+  limitMode: {
+    type: String,
+    default: 'soc',
   },
-  targetSoc: {
+  currentValue: {
     type: Number,
     default: 0,
   },
   targetTime: {
     type: String,
-    default: 'keine',
-  },
-  limitMode: {
-    type: String,
-    default: 'soc',
-  },
-  currentEnergyCharged: {
-    type: Number,
-    default: 0,
-  },
-  targetEnergyAmount: {
-    type: Number,
-    default: 0,
+    required: false,
+    default: undefined,
   },
 });
-
-// Unified current and target values that adapt based on the limitMode
-const currentValue = computed(() =>
-  props.limitMode === 'amount'
-    ? props.currentEnergyCharged
-    : props.connectedVehicleSoc,
-);
-
-const targetValue = computed(() =>
-  props.limitMode === 'amount' ? props.targetEnergyAmount : props.targetSoc,
-);
-
+const target = computed({
+  get: () => props.modelValue,
+  set: (value) => {
+    if (!props.readonly) {
+      emit('update:modelValue', value);
+    }
+  },
+});
+const maxValue = computed(() => {
+  if (props.limitMode == 'soc') {
+    return 100;
+  }
+  return target.value;
+});
 const formatEnergy = (value: number) => {
   if (value >= 1000) {
     return (value / 1000).toFixed(2) + ' kWh';
@@ -147,24 +119,20 @@ const formatEnergy = (value: number) => {
   }
 };
 </script>
-
 <style scoped>
 .my-card {
   width: 100%;
   max-width: 300px;
 }
-
 .slider-container {
   position: relative;
   height: 40px;
 }
-
 .current-slider {
   position: absolute;
   width: 100%;
   z-index: 1;
 }
-
 .target-slider {
   position: absolute;
   width: 100%;
