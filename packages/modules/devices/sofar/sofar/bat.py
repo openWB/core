4ef06aecd776c0355579643b2e3fc08c2f4e4e12
ndrsnhs <andreas.neuhaus@openwb.de>
2025-04-01 11:46:34 +0200

    remove whitespace

--- packages/modules/devices/sofar/sofar/bat.py
+++ packages/modules/devices/sofar/sofar/bat.py
@@ -24,7 +24,7 @@ class SofarBat(AbstractBat):
         # 0x900D High 8 bits: the number of battery packs in parallel
         # Lower 8 bits: the number of battery strings in the battery pack
         battery_packs = client.read_holding_registers(0x900D, ModbusDataType.UINT_16, unit=self.__modbus_id) >> 8
-        # Power bat1 - bat12: INT_16 in kW accuracy 0,01 
+        # Power bat1 - bat12: INT_16 in kW accuracy 0,01
         power_regs = [0x0606, 0x060D, 0x0614, 0x061B, 0x0622, 0x0629, 0x0630, 0x0637, 0x0646, 0x064D, 0x0654, 0x065B]
 
         power = sum(client.read_holding_registers(power_regs[idx], ModbusDataType.INT_16, unit=self.__modbus_id)
