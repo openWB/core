a8be4837b35219c9abf6d1633fec1e0c5c4f1793
Sven Lütkemeier <sven.luetkemeier@outlook.com>
2025-04-05 13:22:19 +0200

    ostrom tariff: reuse access token until expired

--- packages/modules/electricity_tariffs/ostrom/config.py
+++ packages/modules/electricity_tariffs/ostrom/config.py
@@ -1,14 +1,26 @@
 from typing import Optional
 
 
+class OstromToken():
+    def __init__(self,
+                 access_token: Optional[str] = None,
+                 expires_in: Optional[str] = None,
+                 created_at: Optional[str] = None) -> None:
+        self.access_token = access_token
+        self.expires_in = expires_in
+        self.created_at = created_at
+
+
 class OstromTariffConfiguration:
     def __init__(self,
                  client_id: Optional[str] = None,
                  client_secret: Optional[str] = None,
-                 zip: Optional[str] = None):
+                 zip: Optional[str] = None,
+                 token: OstromToken = None):
         self.client_id = client_id
         self.client_secret = client_secret
         self.zip = zip
+        self.token = token or OstromToken()
 
 
 class OstromTariff:
