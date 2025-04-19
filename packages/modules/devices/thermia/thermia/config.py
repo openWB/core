701cc7dec9936b59b02f1ef827c004414dad26e8
cr0i <christoph@rischke.net>
2025-04-19 20:04:27 +0200

    thermia-counter

---
+++ packages/modules/devices/thermia/thermia/config.py
@@ -0,0 +1,39 @@
+from typing import Optional
+from helpermodules.auto_str import auto_str
+from modules.common.component_setup import ComponentSetup
+
+
+@auto_str
+class ThermiaConfiguration:
+    def __init__(self, ip_address: Optional[str] = None, port: int = 502):
+        self.ip_address = ip_address
+        self.port = port
+
+
+@auto_str
+class Thermia:
+    def __init__(self,
+                 name: str = "Thermia",
+                 type: str = "thermia",
+                 id: int = 0,
+                 configuration: ThermiaConfiguration = None) -> None:
+        self.name = name
+        self.type = type
+        self.id = id
+        self.configuration = configuration or ThermiaConfiguration()
+
+
+@auto_str
+class ThermiaCounterConfiguration:
+    def __init__(self, modbus_id: int = 1):
+        self.modbus_id = modbus_id
+
+
+@auto_str
+class ThermiaCounterSetup(ComponentSetup[ThermiaCounterConfiguration]):
+    def __init__(self,
+                 name: str = "Thermia Zähler",
+                 type: str = "counter",
+                 id: int = 0,
+                 configuration: ThermiaCounterConfiguration = None) -> None:
+        super().__init__(name, type, id, configuration or ThermiaCounterConfiguration())
---
+++ packages/modules/devices/thermia/thermia/device.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import logging
+from typing import Iterable,  Union
+
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.configurable_device import ConfigurableDevice, ComponentFactoryByType, MultiComponentUpdater
+from modules.common.modbus import ModbusTcpClient_
+from modules.devices.thermia.thermia.config import Thermia, ThermiaCounterSetup
+from modules.devices.thermia.thermia.counter import ThermiaCounter
+
+log = logging.getLogger(__name__)
+
+
+def create_device(device_config: Thermia):
+    client = None
+
+    def create_counter_component(component_config: ThermiaCounterSetup):
+        nonlocal client
+        return ThermiaCounter(component_config, device_id=device_config.id, client=client)
+
+    def update_components(components: Iterable[Union[ThermiaCounter]]):
+        with client:
+            for component in components:
+                component.update()
+
+    def initializer():
+        nonlocal client
+        client = ModbusTcpClient_(device_config.configuration.ip_address, device_config.configuration.port)
+
+    return ConfigurableDevice(
+        device_config=device_config,
+        initializer=initializer,
+        component_factory=ComponentFactoryByType(
+            counter=create_counter_component,
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
+
+
+device_descriptor = DeviceDescriptor(configuration_factory=Thermia)
