7f27665516b72eca31933ca47dc8e22c3fac7ba4
ndrsnhs <andreas.neuhaus@openwb.de>
2025-04-22 10:03:17 +0200

    add Kaco Tx inverter

---
+++ packages/modules/devices/kaco/kaco_tx/config.py
@@ -0,0 +1,37 @@
+from modules.common.component_setup import ComponentSetup
+from ..vendor import vendor_descriptor
+
+
+class KacoConfiguration:
+    def __init__(self,
+                 port: int = 502,
+                 ip_address=None):
+        self.port = port
+        self.ip_address = ip_address
+
+
+class Kaco:
+    def __init__(self,
+                 name: str = "Kaco Tx1 & Tx3 Serie",
+                 type: str = "kaco",
+                 id: int = 0,
+                 configuration:  KacoConfiguration = None) -> None:
+        self.name = name
+        self.type = type
+        self.vendor = vendor_descriptor.configuration_factory().type
+        self.id = id
+        self.configuration = configuration or KacoConfiguration()
+
+
+class KacoInverterConfiguration:
+    def __init__(self, modbus_id: int = 1):
+        self.modbus_id = modbus_id
+
+
+class KacoInverterSetup(ComponentSetup[KacoInverterConfiguration]):
+    def __init__(self,
+                 name: str = "Kaco Wechselrichter",
+                 type: str = "inverter",
+                 id: int = 0,
+                 configuration: KacoInverterConfiguration = None) -> None:
+        super().__init__(name, type, id, configuration or KacoInverterConfiguration())
---
+++ packages/modules/devices/kaco/kaco_tx/device.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import logging
+from typing import Iterable
+
+from modules.common import modbus
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.configurable_device import ComponentFactoryByType, ConfigurableDevice, MultiComponentUpdater
+from modules.devices.kaco.kaco_tx.inverter import KacoInverter
+from modules.devices.kaco.kaco_tx.config import (Kaco, KacoInverterSetup)
+
+log = logging.getLogger(__name__)
+
+default_unit_id = 85
+synergy_unit_identifier = 160
+reconnect_delay = 1.2
+
+
+def create_device(device_config: Kaco):
+    client = None
+
+    def create_inverter_component(component_config: KacoInverterSetup):
+        nonlocal client
+        return KacoInverter(component_config, client=client, device_id=device_config.id)
+
+    def update_components(components: Iterable[KacoInverter]):
+        nonlocal client
+        with client:
+            for component in components:
+                component.update()
+
+    def initializer():
+        nonlocal client
+        client = modbus.ModbusTcpClient_(device_config.configuration.ip_address,
+                                         device_config.configuration.port,
+                                         reconnect_delay=reconnect_delay)
+
+    device = ConfigurableDevice(
+        device_config=device_config,
+        initializer=initializer,
+        component_factory=ComponentFactoryByType(
+            inverter=create_inverter_component
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
+    return device
+
+
+device_descriptor = DeviceDescriptor(configuration_factory=Kaco)
