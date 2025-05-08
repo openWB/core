8ccbf97454bddbe2a83c7d630e03352d291d0138
LKuemmel <lena.kuemmel@openwb.de>
2025-05-08 15:40:00 +0200

    build Koala UI

---
+++ packages/modules/web_themes/koala/web/assets/store-init-BoFV1Cc-.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-FW_-FGoo.js";import{u as o}from"./mqtt-store-Rgh6tEll.js";const s=t(()=>{o().initialize()});export{s as default};
--- packages/modules/web_themes/koala/web/assets/store-init-DWJw-IZM.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index-YZoBGXd-.js";import{u as o}from"./mqtt-store-RSZBzMzk.js";const s=t(()=>{o().initialize()});export{s as default};
