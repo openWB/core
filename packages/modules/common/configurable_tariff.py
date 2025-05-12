40e4ca546056fffd32e6f7866b03c09e4a54ee93
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2025-05-12 10:35:39 +0200

    electricity tariff: remove outdated prices (#2383)

--- packages/modules/common/configurable_tariff.py
+++ packages/modules/common/configurable_tariff.py
@@ -3,6 +3,7 @@ from helpermodules.timecheck import create_unix_timestamp_current_full_hour
 
 from modules.common import store
 from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.component_state import TariffState
 from modules.common.component_type import ComponentType
 from modules.common.fault_state import ComponentInfo, FaultState
 
@@ -24,14 +25,19 @@ class ConfigurableElectricityTariff(Generic[T_TARIFF_CONFIG]):
         if hasattr(self, "_component_updater"):
             # Wenn beim Initialisieren etwas schief gelaufen ist, ursprüngliche Fehlermeldung beibehalten
             with SingleComponentUpdateContext(self.fault_state):
-                tariff_state = self._component_updater()
-                current_hour = str(int(create_unix_timestamp_current_full_hour()))
+                tariff_state = self._remove_outdated_prices(self._component_updater())
                 self.store.set(tariff_state)
                 self.store.update()
-                for timestamp in tariff_state.prices.keys():
-                    if timestamp < current_hour:
-                        self.fault_state.warning('Die Preisliste startet nicht mit der aktuellen Stunde.')
                 if len(tariff_state.prices) < 24:
                     self.fault_state.no_error(
                         f'Die Preisliste hat nicht 24, sondern {len(tariff_state.prices)} Einträge. '
                         'Die Strompreise werden vom Anbieter erst um 14:00 für den Folgetag aktualisiert.')
+
+    def _remove_outdated_prices(self, tariff_state: TariffState) -> TariffState:
+        current_hour = str(int(create_unix_timestamp_current_full_hour()))
+        for timestamp in list(tariff_state.prices.keys()):
+            if timestamp < current_hour:
+                self.fault_state.warning(
+                    'Die Preisliste startet nicht mit der aktuellen Stunde. Abgelaufene Einträge wurden entfernt.')
+                tariff_state.prices.pop(timestamp)
+        return tariff_state
