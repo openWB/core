3541c923bd135529ce4a68d1b374a0f30196668e
benderl <benderl@users.noreply.github.com>
2025-06-03 10:08:25 +0000

    Build Display Theme: Cards

--- packages/modules/display_themes/cards/web/assets/vendor-fortawesome-CwlFXTtU.js
+++ packages/modules/display_themes/cards/web/assets/vendor-fortawesome-CwlFXTtU.js
@@ -1,4 +1,4 @@
-import{g as ce,d as Lt,c as K,w as Ot,h as Mt}from"./vendor-ghSDDKiF.js";/*!
+import{g as ce,d as Lt,c as K,w as Ot,h as Mt}from"./vendor-BWLXa0iW.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
