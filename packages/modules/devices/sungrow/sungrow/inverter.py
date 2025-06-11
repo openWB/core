77f6c1aaa528d0e1c47ca0f96437ccae8940c649
SeaSpotter <florian.horch@gmail.com>
2025-06-11 12:44:03 +0200

    Read all phases

--- packages/modules/devices/sungrow/sungrow/inverter.py
+++ packages/modules/devices/sungrow/sungrow/inverter.py
@@ -43,8 +43,10 @@ class SungrowInverter(AbstractInverter):
             dc_power = self.__tcp_client.read_input_registers(5016, ModbusDataType.UINT_32,
                                                               wordorder=Endian.Little, unit=unit) * -1
 
-        currents_raw = self.__tcp_client.read_input_registers(13030, ModbusDataType.INT_16, 3, unit=unit)
-        currents = [value * 0.1 for value in currents_raw]
+        current_L1 = self.__tcp_client.read_input_registers(13030, ModbusDataType.INT_16, unit=unit) * 0.1
+        current_L2 = self.__tcp_client.read_input_registers(13031, ModbusDataType.INT_16, unit=unit) * 0.1
+        current_L3 = self.__tcp_client.read_input_registers(13032, ModbusDataType.INT_16, unit=unit) * 0.1
+        currents = [current_L1, current_L2, current_L3]
 
         imported, exported = self.sim_counter.sim_count(power)
 
