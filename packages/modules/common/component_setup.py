9b943fe97aef9b41cdd61a0bbfd0519ddd9c9491
Lutz Bender <github@lutz-bender.de>
2025-06-11 08:46:11 +0200

    custom component colors

--- packages/modules/common/component_setup.py
+++ packages/modules/common/component_setup.py
@@ -4,8 +4,9 @@ T = TypeVar("T")
 
 
 class ComponentSetup(Generic[T]):
-    def __init__(self, name: str, type: str, id: int, configuration: T) -> None:
+    def __init__(self, name: str, color: str, type: str, id: int, configuration: T) -> None:
         self.name = name
+        self.color = color
         self.info = {"manufacturer": None, "model": None}
         self.type = type
         self.id = id
