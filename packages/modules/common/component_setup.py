dfdcabd7545d53d49f968660b3f049a1440749cc
Lutz Bender <github@lutz-bender.de>
2025-06-12 13:22:21 +0200

    fix component setup

--- packages/modules/common/component_setup.py
+++ packages/modules/common/component_setup.py
@@ -4,10 +4,18 @@ T = TypeVar("T")
 
 
 class ComponentSetup(Generic[T]):
-    def __init__(self, name: str, color: str, type: str, id: int, configuration: T) -> None:
+    def __init__(self, name: str, type: str, id: int, configuration: T) -> None:
         self.name = name
-        self.color = color
         self.info = {"manufacturer": None, "model": None}
         self.type = type
         self.id = id
         self.configuration = configuration
+        if "counter" in type.lower():
+            self.color = "#dc3545"
+        elif "bat" in type.lower():
+            self.color = "#ffc107"
+        elif "inverter" in type.lower():
+            self.color = "#28a745"
+        else:
+            # Default color for other types
+            self.color = "#000000"
