3d52abb6c7f4cb49e62e78a2831a27c3575512a6
LKuemmel <LKuemmel@users.noreply.github.com>
2025-07-02 10:16:04 +0000

    Build Web Theme: Koala

--- packages/modules/web_themes/koala/web/assets/store-init-BWjDqns0.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index-qEOJvHRe.js";import{u as o}from"./mqtt-store-C_pDpUnf.js";const s=t(()=>{o().initialize()});export{s as default};
---
+++ packages/modules/web_themes/koala/web/assets/store-init-BvscZWOS.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-BLq9Qvp9.js";import{u as o}from"./mqtt-store-Bc65k2ub.js";const s=t(()=>{o().initialize()});export{s as default};
