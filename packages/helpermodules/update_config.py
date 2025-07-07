5459e5cabfcab7c994b2abb2f677ff9518c10ffd
ndrsnhs <andreas.neuhaus@openwb.de>
2025-07-07 13:48:42 +0200

    update config

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -56,7 +56,7 @@ NO_MODULE = {"type": None, "configuration": {}}
 
 class UpdateConfig:
 
-    DATASTORE_VERSION = 85
+    DATASTORE_VERSION = 86
 
     valid_topic = [
         "^openWB/bat/config/configured$",
@@ -2307,3 +2307,15 @@ class UpdateConfig:
         self._loop_all_received_topics(upgrade)
         self._loop_all_received_topics(cp_upgrade)
         self.__update_topic("openWB/system/datastore_version", 85)
+
+    def upgrade_datastore_85(self) -> None:
+        def upgrade(topic: str, payload) -> None:
+            if re.search("openWB/system/device/[0-9]+", topic) is not None:
+                payload = decode_payload(payload)
+                # remove phase from device tasmota
+                # phase=int(device_config.configuration.phase)
+                if payload.get("type") == "tasmota" and "phase" in payload["configuration"]:
+                    payload["configuration"].pop("phase")
+                Pub().pub(topic, payload)
+        self._loop_all_received_topics(upgrade)
+        self.__update_topic("openWB/system/datastore_version", 86)
