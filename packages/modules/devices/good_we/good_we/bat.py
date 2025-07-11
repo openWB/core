a2677dd3b08e3fd912e9a8b2dfb5ba732ce6b734
ndrsnhs <andreas.neuhaus@openwb.de>
2025-07-11 12:29:45 +0200

    detect battery index

--- packages/modules/devices/good_we/good_we/bat.py
+++ packages/modules/devices/good_we/good_we/bat.py
@@ -14,6 +14,7 @@ from modules.devices.good_we.good_we.version import GoodWeVersion
 
 
 class KwargsDict(TypedDict):
+    device_id: int
     modbus_id: int
     version: GoodWeVersion
     firmware: int
