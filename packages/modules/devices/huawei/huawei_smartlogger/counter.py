54fb8a2dabe1ac7750a0ecfcf8bc0635efb2988a
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2025-07-16 15:20:48 +0200

    adjust factors of phase values (#2564)

--- packages/modules/devices/huawei/huawei_smartlogger/counter.py
+++ packages/modules/devices/huawei/huawei_smartlogger/counter.py
@@ -31,12 +31,11 @@ class Huawei_SmartloggerCounter(AbstractCounter):
     def update(self) -> None:
         modbus_id = self.component_config.configuration.modbus_id
         power = self.client.read_holding_registers(32278, ModbusDataType.INT_32, unit=modbus_id)
-        currents = [val / 100 for val in self.client.read_holding_registers(
+        currents = [val / 10 for val in self.client.read_holding_registers(
             32272, [ModbusDataType.INT_32] * 3, unit=modbus_id)]
         voltages = [val / 100 for val in self.client.read_holding_registers(
             32260, [ModbusDataType.INT_32] * 3, unit=modbus_id)]
-        powers = [val / 1000 for val in self.client.read_holding_registers(
-            32335, [ModbusDataType.INT_32] * 3, unit=modbus_id)]
+        powers = self.client.read_holding_registers(32335, [ModbusDataType.INT_32] * 3, unit=modbus_id)
         imported, exported = self.sim_counter.sim_count(power)
         counter_state = CounterState(
             currents=currents,
