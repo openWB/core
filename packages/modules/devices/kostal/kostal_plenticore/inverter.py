c78f84307561c24d0a38c9791afbd8a3223634d8
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2025-07-18 15:13:54 +0200

    fix kostal counter power peaks (#2579)
    * simplify code
    
    * use list comprehension
    
    * call simcounter after power calculation

--- packages/modules/devices/kostal/kostal_plenticore/inverter.py
+++ packages/modules/devices/kostal/kostal_plenticore/inverter.py
@@ -1,37 +1,44 @@
 #!/usr/bin/env python3
-from typing import Any, Callable
+from typing import TypedDict, Any
+
 from modules.common.abstract_device import AbstractInverter
 from modules.common.component_state import InverterState
 from modules.common.component_type import ComponentDescriptor
 from modules.common.fault_state import ComponentInfo, FaultState
-from modules.common.modbus import ModbusDataType
+from modules.common.modbus import ModbusDataType, ModbusTcpClient_
+from modules.common.simcount import SimCounter
 from modules.common.store import get_inverter_value_store
 from modules.devices.kostal.kostal_plenticore.config import KostalPlenticoreInverterSetup
 
 
+class KwargsDict(TypedDict):
+    device_id: int
+    modbus_id: int
+    client: ModbusTcpClient_
+
+
 class KostalPlenticoreInverter(AbstractInverter):
-    def __init__(self, component_config: KostalPlenticoreInverterSetup) -> None:
+    def __init__(self, component_config: KostalPlenticoreInverterSetup, **kwargs: Any) -> None:
         self.component_config = component_config
+        self.kwargs: KwargsDict = kwargs
 
     def initialize(self) -> None:
+        self.__device_id: int = self.kwargs['device_id']
+        self.modbus_id: int = self.kwargs['modbus_id']
+        self.client: ModbusTcpClient_ = self.kwargs['client']
         self.store = get_inverter_value_store(self.component_config.id)
         self.fault_state = FaultState(ComponentInfo.from_component_config(self.component_config))
+        self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="pv")
 
-    def read_state(self, reader: Callable[[int, ModbusDataType], Any]) -> InverterState:
-        # PV-Anlage kann nichts verbrauchen, also ggf. Register-/Rundungsfehler korrigieren.
-        power = reader(575, ModbusDataType.INT_16) * -1
-        exported = reader(320, ModbusDataType.FLOAT_32)
+    def update(self) -> None:
+        power = self.client.read_holding_registers(575, ModbusDataType.INT_16, unit=self.modbus_id) * -1
+        exported = self.client.read_holding_registers(320, ModbusDataType.FLOAT_32, unit=self.modbus_id)
 
-        return InverterState(
+        inverter_state = InverterState(
             power=power,
             exported=exported
         )
-
-    def dc_in_string_1_2(self, reader: Callable[[int, ModbusDataType], Any]):
-        return reader(260, ModbusDataType.FLOAT_32) + reader(270, ModbusDataType.FLOAT_32)
-
-    def update(self, state):
-        self.store.set(state)
+        self.store.set(inverter_state)
 
 
 component_descriptor = ComponentDescriptor(configuration_factory=KostalPlenticoreInverterSetup)
