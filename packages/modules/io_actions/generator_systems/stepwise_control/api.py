9d555186205c37ea1c6b54ba8c50bc20654632f7
Lutz Bender <github@lutz-bender.de>
2025-07-21 13:23:23 +0200

    implement outputs for stepwise control

--- packages/modules/io_actions/generator_systems/stepwise_control/api.py
+++ packages/modules/io_actions/generator_systems/stepwise_control/api.py
@@ -6,6 +6,7 @@ from helpermodules.pub import Pub
 from helpermodules.timecheck import create_timestamp
 from modules.common.abstract_device import DeviceDescriptor
 from modules.common.abstract_io import AbstractIoAction
+from modules.common.utils.component_parser import get_component_name_by_id, get_io_name_by_id
 from modules.io_actions.generator_systems.stepwise_control.config import StepwiseControlSetup
 
 control_command_log = logging.getLogger("steuve_control_command")
@@ -19,11 +20,24 @@ class StepwiseControl(AbstractIoAction):
             for key in pattern["input_matrix"].keys():
                 if key not in self.__unique_inputs:
                     self.__unique_inputs.append(key)
-        assigned_devices = [io_device["id"] for io_device in self.config.configuration.devices]
+        assigned_inverters = [
+            f"{device['id']}"
+            for device in self.config.configuration.devices
+            if device["type"] == "inverter"
+        ]
+        assigned_outputs = [
+            f"{device['id']}/{device['digital_output']}"
+            for device in self.config.configuration.devices
+            if device["type"] == "io"
+        ]
+        # Log the configuration details
+        # We cannot use configured names here, as the devices are not yet initialized
+        # and thus the names are not available.
         control_command_log.info(
             f"Stufenweise Steuerung von EZA: I/O-Gerät: {self.config.configuration.io_device}, "
             f"Überwachte digitale Eingänge: {self.__unique_inputs}, "
-            f"zugeordnete Erzeugungsanlagen: {assigned_devices} "
+            f"zugeordnete Erzeugungsanlagen: {assigned_inverters} "
+            f"zugeordnete IO-Ausgänge: {assigned_outputs} "
             "Die Begrenzung muss in den EZA vorgenommen werden!"
         )
         super().__init__()
@@ -52,6 +66,18 @@ class StepwiseControl(AbstractIoAction):
                         if changed:
                             Pub().pub(f"openWB/set/io/action/{self.config.id}/timestamp", create_timestamp())
                             control_command_log.info(f"EZA-Begrenzung mit Wert {int(pattern['value']*100)}% aktiviert.")
+                            for device in self.config.configuration.devices:
+                                if device["type"] == "inverter":
+                                    control_command_log.info(
+                                        f"Erzeugungsanlage {get_component_name_by_id(device['id'])} "
+                                        f"auf {int(pattern['value']*100)}% begrenzt."
+                                    )
+                                elif device["type"] == "io":
+                                    control_command_log.info(
+                                        f"IO-Gerät {get_io_name_by_id(device['id'])} Ausgang "
+                                        f"{device['digital_output']} "
+                                        f"{'aktiviert' if device['value'] == pattern['value'] else 'deaktiviert'}."
+                                    )
                         break
             else:
                 if changed:
