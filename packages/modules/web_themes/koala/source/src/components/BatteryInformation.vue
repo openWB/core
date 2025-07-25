7d022c21eb16f2ad67c5af937a17de126ae35996
Lutz Bender <github@lutz-bender.de>
2025-07-25 09:58:36 +0200

    cleanup

--- packages/modules/web_themes/koala/source/src/components/BatteryInformation.vue
+++ packages/modules/web_themes/koala/source/src/components/BatteryInformation.vue
@@ -1,7 +1,4 @@
 <template>
-  <div v-if="showBatteryOverview" class="row justify-center">
-    <BatteryCard :battery-id="undefined" />
-  </div>
   <BaseCarousel :items="batteryIds">
     <template #item="{ item }">
       <BatteryCard :battery-id="item" />
@@ -18,9 +15,11 @@ import BatteryCard from 'src/components/BatteryCard.vue';
 
 const mqttStore = useMqttStore();
 
-const batteryIds = computed(() => mqttStore.batteryIds);
-
-const showBatteryOverview = computed(() => {
-  return mqttStore.batteryIds.length > 1;
+const batteryIds = computed(() => {
+  let ids = mqttStore.batteryIds;
+  if (ids.length > 1) {
+    ids.unshift(-1); // add overview card if more than one battery
+  }
+  return ids;
 });
 </script>
