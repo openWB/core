ae376003d2f5d60dd308ce12278da517d2653a1f
BrettS <brett.scott@openwb.de>
2025-07-29 17:33:05 +0200

    History chart drop down categories for mobile and large legends

--- packages/modules/web_themes/koala/source/src/components/charts/historyChart/history-chart-model.ts
+++ packages/modules/web_themes/koala/source/src/components/charts/historyChart/history-chart-model.ts
@@ -1,4 +1,4 @@
-import { TooltipItem, Chart } from 'chart.js'; // Importieren des TooltipItem-Typs
+import type { Chart, ChartDataset, TooltipItem } from 'chart.js'; // Importieren des TooltipItem-Typs
 export interface HistoryChartTooltipItem extends TooltipItem<'line'> {
   dataset: TooltipItem<'line'>['dataset'] & {
     unit?: string;
@@ -8,4 +8,12 @@ export interface HistoryChartTooltipItem extends TooltipItem<'line'> {
 // Define a type for the component that contains the Chart instance
 export interface ChartComponentRef {
   chart?: Chart;
+}
+
+export type Category = 'chargepoint' | 'vehicle' | 'battery' | 'component';
+
+// Add category to the chart datasets
+export interface CategorizedDataset
+  extends ChartDataset<'line', { x: number; y: number }> {
+  category: Category;
 }
