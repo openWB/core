613af2c4d20e9918e8d626e3c67134baecfd55e9
SeaSpotter <florian.horch@gmail.com>
2025-07-31 12:48:09 +0200

    Improve fault_handling with Text

--- packages/modules/devices/kostal/kostal_plenticore/inverter.py
+++ packages/modules/devices/kostal/kostal_plenticore/inverter.py
@@ -32,18 +32,23 @@ class KostalPlenticoreInverter(AbstractInverter):
         self.store = get_inverter_value_store(self.component_config.id)
         self.fault_state = FaultState(ComponentInfo.from_component_config(self.component_config))
         self.sim_counter = SimCounter(self.kwargs['device_id'], self.component_config.id, prefix="Wechselrichter")
+        self.fault_text = (
+            "Es kann keine PV Leistung aus dem Wechselrichter ausgelesen werden, "
+            "möglicherweise kann ein Firmware-Update für den Wechselrichter nötig sein."
 
     def update(self) -> None:
         power = self.client.read_holding_registers(
             575, ModbusDataType.INT_16, unit=self.modbus_id, wordorder=self.endianess) * -1
         exported = self.client.read_holding_registers(
             320, ModbusDataType.FLOAT_32, unit=self.modbus_id, wordorder=self.endianess)
-        # Try to read dc_power, if it fails just skip it and set to 0
+        # Try to read dc_power, if it fails just skip it and set to None
         try:
             dc_power = self.client.read_holding_registers(
                 1066, ModbusDataType.FLOAT_32, unit=self.modbus_id, wordorder=self.endianess) * -1
+            self.fault_state.no_error()
         except Exception:
             dc_power = None
+            self.fault_state.no_error(self.fault_text)
         imported, _ = self.sim_counter.sim_count(power)
 
         inverter_state = InverterState(
