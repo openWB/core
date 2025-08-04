49088fb6bb2f4c74f8f24317c7fd1d397ab61bac
ggtimtom <tv@gerold-online.de>
2025-08-04 22:29:23 +0200

    fix: variable names and modbus

--- packages/modules/devices/chint/chint/counter.py
+++ packages/modules/devices/chint/chint/counter.py
@@ -7,7 +7,8 @@ from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.modbus import ModbusDataType, ModbusTcpClient_
 # from modules.common.simcount import SimCounter
 from modules.common.store import get_counter_value_store
-from modules.devices.zcs.zcs.config import ZCSCounterSetup
+from modules.devices.chint.chint.config import CHINTCounterSetup
+from pymodbus.constants import Endian
 
 
 class KwargsDict(TypedDict):
@@ -15,10 +16,11 @@ class KwargsDict(TypedDict):
     client: ModbusTcpClient_
 
 
-class ZCSCounter(AbstractCounter):
-    def __init__(self, component_config: ZCSCounterSetup, **kwargs: Any) -> None:
+class CHINTCounter(AbstractCounter):
+    def __init__(self, component_config: CHINTCounterSetup, **kwargs: Any) -> None:
         self.component_config = component_config
         self.kwargs: KwargsDict = kwargs
+        self.__modbus_id = component_config.configuration.modbus_id
 
     def initialize(self) -> None:
         self.__device_id: int = self.kwargs['device_id']
@@ -29,13 +31,13 @@ class ZCSCounter(AbstractCounter):
 
     def update(self):
         # power = self.client.read_holding_registers(0x2012, ModbusDataType.INT_32, unit=self.__modbus_id)
-        frequency = self.client.read_holding_registers(0x2044, ModbusDataType.INT_32, unit=self.__modbus_id)/100
+        frequency = self.client.read_holding_registers(0x2044, ModbusDataType.FLOAT_32, Endian.Big, Endian.Big, unit=self.__modbus_id)/100
         # imported, exported = self.sim_counter.sim_count(power)
 
         try:
-            power1 = self.client.read_holding_registers(0x2014, ModbusDataType.INT_16, unit=self.__modbus_id) * -4
-            power2 = self.client.read_holding_registers(0x2016, ModbusDataType.INT_16, unit=self.__modbus_id) * -4
-            power3 = self.client.read_holding_registers(0x2018, ModbusDataType.INT_16, unit=self.__modbus_id) * -4
+            power1 = self.client.read_holding_registers(0x2014, ModbusDataType.FLOAT_32, unit=self.__modbus_id) * -4
+            power2 = self.client.read_holding_registers(0x2016, ModbusDataType.FLOAT_32, unit=self.__modbus_id) * -4
+            power3 = self.client.read_holding_registers(0x2018, ModbusDataType.FLOAT_32, unit=self.__modbus_id) * -4
             powers = [
                 power1,
                 power2,
@@ -43,15 +45,15 @@ class ZCSCounter(AbstractCounter):
             power = power1 + power2 + power3
 
             voltages = [
-                self.client.read_holding_registers(0x2006, ModbusDataType.INT_16, unit=self.__modbus_id) / 10,
-                self.client.read_holding_registers(0x2008, ModbusDataType.INT_16, unit=self.__modbus_id) / 10,
-                self.client.read_holding_registers(0x200A, ModbusDataType.INT_16, unit=self.__modbus_id) / 10
+                self.client.read_holding_registers(0x2006, ModbusDataType.FLOAT_32, unit=self.__modbus_id) / 10,
+                self.client.read_holding_registers(0x2008, ModbusDataType.FLOAT_32, unit=self.__modbus_id) / 10,
+                self.client.read_holding_registers(0x200A, ModbusDataType.FLOAT_32, unit=self.__modbus_id) / 10
             ]
 
             currents = [
-                self.client.read_holding_registers(0x200C, ModbusDataType.INT_16, unit=self.__modbus_id) / 10,
-                self.client.read_holding_registers(0x200E, ModbusDataType.INT_16, unit=self.__modbus_id) / 10,
-                self.client.read_holding_registers(0x2010, ModbusDataType.INT_16, unit=self.__modbus_id) / 10
+                self.client.read_holding_registers(0x200C, ModbusDataType.FLOAT_32, unit=self.__modbus_id) / 10,
+                self.client.read_holding_registers(0x200E, ModbusDataType.FLOAT_32, unit=self.__modbus_id) / 10,
+                self.client.read_holding_registers(0x2010, ModbusDataType.FLOAT_32, unit=self.__modbus_id) / 10
             ]
         except Exception:
             powers = None
@@ -69,4 +71,4 @@ class ZCSCounter(AbstractCounter):
         self.store.set(counter_state)
 
 
-component_descriptor = ComponentDescriptor(configuration_factory=ZCSCounterSetup)
+component_descriptor = ComponentDescriptor(configuration_factory=CHINTCounterSetup)
