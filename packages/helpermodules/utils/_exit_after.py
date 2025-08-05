27e14282a1839a10996d6541b9cf71d57376ff40
benderl <benderl@users.noreply.github.com>
2025-08-05 09:03:05 +0200

    Revert "use own exception class in @exit_after"

--- packages/helpermodules/utils/_exit_after.py
+++ packages/helpermodules/utils/_exit_after.py
@@ -1,33 +1,26 @@
+import _thread as thread
 from threading import Timer
-import threading
+import sys
 
 
-class TimeoutException(Exception):
-    pass
+def quit_function(fn_name):
+    sys.stderr.flush()  # Python 3 stderr is likely buffered.
+    thread.interrupt_main()  # raises KeyboardInterrupt
 
 
 def exit_after(s):
-    '''
-    Nutze als Decorator, um einen Timeout für eine Funktion zu erzwingen.
-    Wirft TimeoutException, wenn die Funktion länger als s Sekunden benötigt.
-    Basiert auf https://stackoverflow.com/questions/492519/timeout-on-a-function-call.
+    ''' https://stackoverflow.com/questions/492519/timeout-on-a-function-call
+    use as decorator to exit process if
+    function takes longer than s seconds
     '''
     def outer(fn):
         def inner(*args, **kwargs):
-            timer = Timer(
-                s,
-                lambda: thread_raise(TimeoutException(f"Timeout nach {s} Sekunden in {fn.__name__}")))
+            timer = Timer(s, quit_function, args=[fn.__name__])
             timer.start()
             try:
-                return fn(*args, **kwargs)
+                result = fn(*args, **kwargs)
             finally:
                 timer.cancel()
+            return result
         return inner
     return outer
-
-
-def thread_raise(ex):
-    # Raise Exception im aktuellen Thread (nur für Hauptthread zuverlässig)
-    import ctypes
-    tid = threading.get_ident()
-    ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), ctypes.py_object(ex))
