f5c31bd5e8565e51e7a2a7f021156be784dacb51
SeaSpotter <florian.horch@gmail.com>
2025-08-20 09:32:32 +0200

    Create config.py

---
+++ packages/modules/devices/marstek/marstek/config.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from modules.common.component_setup import ComponentSetup
+from ..vendor import vendor_descriptor
+
+
+class MarstekConfiguration:
+    def __init__(self, modbus_id: int = 64, ip_address: Optional[str] = None, port: int = 3600):
+        self.modbus_id = modbus_id
+        self.ip_address = ip_address
+        self.port = port
+
+
+class Saxpower:
+    def __init__(self,
+                 name: str = "marstek",
+                 type: str = "marstek",
+                 id: int = 0,
+                 configuration: MarstekConfiguration = None) -> None:
+        self.name = name
+        self.type = type
+        self.vendor = vendor_descriptor.configuration_factory().type
+        self.id = id
+        self.configuration = configuration or MarstekConfiguration()
+
+
+class MarstekBatConfiguration:
+    def __init__(self):
+        pass
+
+
+class MarstekBatSetup(ComponentSetup[MarstekBatConfiguration]):
+    def __init__(self,
+                 name: str = "Marstek Speicher",
+                 type: str = "bat",
+                 id: int = 0,
+                 configuration: MarstekBatConfiguration = None) -> None:
+        super().__init__(name, type, id, configuration or MarstekBatConfiguration())
