d062816f793cae5a8253c0088c5990bdfa18ab0d
Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>
2025-08-20 12:52:24 +0200

    Update tariff.py fix flake8 errors

--- packages/modules/electricity_tariffs/energycharts/tariff.py
+++ packages/modules/electricity_tariffs/energycharts/tariff.py
@@ -26,7 +26,7 @@ def create_request_url(config: EnergyChartsTariffConfiguration) -> str:
     start_time = timecheck.create_unix_timestamp_current_full_hour()
     end_time = get_tomorrow_last_hour_timestamp()
     url = f'https://api.energy-charts.info/price?bzn={config.country}&start={start_time}&end={end_time}'
-    log.debug("fetching tariffs: %s",url)
+    log.debug("fetching tariffs: %s", url)
     return url
 
 
@@ -41,17 +41,17 @@ def parse_response(config: EnergyChartsTariffConfiguration, raw_prices: dict) ->
 def fetch_prices(config: EnergyChartsTariffConfiguration) -> Dict[str, float]:
     url = create_request_url(config)
     for attempt in range(MAX_RETRIES):
-       attempt += 1  # one-based indexing
-       try:
-           response = req.get_http_session().get(url, timeout=(10, 20))
-           response.raise_for_status()
-           return parse_response(config, response.json())
-       except requests.exceptions.Timeout as e:
-           if MAX_RETRIES > attempt:
-              delay = (attempt) * random.uniform(attempt, MAX_DELAY)
-              log.warning(f"Timeout beim Abrufen der Preise (Versuch {attempt}/{MAX_RETRIES}) : {str(e)}"
-                          f", neuer Versuch in {delay:.1f} Sekunden...")
-              time.sleep(delay)
+        attempt += 1  # one-based indexing
+        try:
+            response = req.get_http_session().get(url, timeout=(10, 20))
+            response.raise_for_status()
+            return parse_response(config, response.json())
+        except requests.exceptions.Timeout as e:
+            if MAX_RETRIES > attempt:
+                delay = (attempt) * random.uniform(attempt, MAX_DELAY)
+                log.warning(f"Timeout beim Abrufen der Preise (Versuch {attempt}/{MAX_RETRIES}) : {str(e)}"
+                            f", neuer Versuch in {delay:.1f} Sekunden...")
+                time.sleep(delay)
     raise Exception("Timeout beim Abrufen der Preise nach {MAX_RETRIES} Versuchen")
 
 
