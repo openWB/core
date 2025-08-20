f3f60980e1276aac2e2fb236aa6421c36295d420
SeaSpotter <florian.horch@gmail.com>
2025-08-20 09:34:16 +0200

    Create device.py

---
+++ packages/modules/devices/marstek/marstek/device.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import logging
+from typing import Iterable
+
+from modules.common import modbus
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.configurable_device import ComponentFactoryByType, ConfigurableDevice, MultiComponentUpdater
+from modules.devices.marstek.marstek.bat import MarstekBat
+from modules.devices.marstek.marstek.config import Marstek, MarstekBatSetup
+
+log = logging.getLogger(__name__)
+
+
+def create_device(device_config: Marstek):
+    client = None
+
+    def create_bat_component(component_config: MarstekBatSetup):
+        nonlocal client
+        return MarstekBat(component_config,
+                           device_id=device_config.id,
+                           client=client,
+                           modbus_id=device_config.configuration.modbus_id)
+
+    def update_components(components: Iterable[MarstekBat]):
+        nonlocal client
+        with client:
+            for component in components:
+                with SingleComponentUpdateContext(component.fault_state):
+                    component.update()
+
+    def initializer():
+        nonlocal client
+        client = modbus.ModbusTcpClient_(device_config.configuration.ip_address, device_config.configuration.port)
+
+    return ConfigurableDevice(
+        device_config=device_config,
+        initializer=initializer,
+        component_factory=ComponentFactoryByType(
+            bat=create_bat_component,
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
+
+
+device_descriptor = DeviceDescriptor(configuration_factory=Marstek)
