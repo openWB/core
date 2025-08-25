2cbdade86fb0e09806e8f218e3a5d3413a5aab8f
benderl <benderl@users.noreply.github.com>
2025-08-25 10:29:34 +0000

    Build Web Theme: Koala

---
+++ packages/modules/web_themes/koala/web/assets/store-init-3Sl7yfYY.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-BCUNZtLP.js";import{u as o}from"./mqtt-store-BpqQtyBE.js";const s=t(()=>{o().initialize()});export{s as default};
--- packages/modules/web_themes/koala/web/assets/store-init-B2y2b7C6.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index-BffAdZ7B.js";import{u as o}from"./mqtt-store-2RAlxljy.js";const s=t(()=>{o().initialize()});export{s as default};
