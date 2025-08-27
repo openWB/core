801718a0fe5e77c64fd8c87c937880feb31dba07
SeaSpotter <florian.horch@gmail.com>
2025-08-27 20:11:47 +0200

    Fix for zero currents and power

--- packages/modules/common/component_state.py
+++ packages/modules/common/component_state.py
@@ -71,7 +71,11 @@ class BatState:
         if _check_none(currents):
             currents = [0.0]*3
         else:
-            if not ((sum(currents) < 0 and power < 0) or (sum(currents) > 0 and power > 0)):
+            if not (
+                (sum(currents) < 0 and power < 0) or 
+                (sum(currents) > 0 and power > 0) or 
+                (sum(currents) == 0 and power == 0)
+            ):
                 log.debug("currents sign wrong "+str(currents))
         self.currents = currents
 
@@ -131,7 +135,11 @@ class InverterState:
         if _check_none(currents):
             currents = [0.0]*3
         else:
-            if not ((sum(currents) < 0 and power < 0) or (sum(currents) > 0 and power > 0)):
+            if not (
+                (sum(currents) < 0 and power < 0) or 
+                (sum(currents) > 0 and power > 0) or 
+                (sum(currents) == 0 and power == 0)
+            ):
                 log.debug("currents sign wrong "+str(currents))
         self.currents = currents
         self.power = power
