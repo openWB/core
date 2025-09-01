875222667d28295c13b0a3fcb41f00ede6701a21
LKuemmel <lena.kuemmel@openwb.de>
2025-09-01 11:19:02 +0200

    ammend

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -57,7 +57,7 @@ NO_MODULE = {"type": None, "configuration": {}}
 
 class UpdateConfig:
 
-    DATASTORE_VERSION = 94
+    DATASTORE_VERSION = 95
 
     valid_topic = [
         "^openWB/bat/config/bat_control_permitted$",
