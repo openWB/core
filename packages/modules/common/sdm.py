df20951aeb1d2dd72a41f3047ad863e6f44dca96
LKuemmel <lena.kuemmel@openwb.de>
2025-09-09 14:35:52 +0200

    bulk reader

--- packages/modules/common/sdm.py
+++ packages/modules/common/sdm.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from enum import IntEnum
 import time
 from typing import List, Tuple
 
@@ -14,76 +15,69 @@ class Sdm(AbstractCounter):
     def __init__(self, modbus_id: int, client: modbus.ModbusTcpClient_) -> None:
         self.client = client
         self.id = modbus_id
-        self.last_query = self._get_time_ms()
-        self.WAIT_MS_BETWEEN_QUERIES = 100
         with client:
             self.serial_number = str(self.client.read_holding_registers(0xFC00, ModbusDataType.UINT_32, unit=self.id))
 
-    def get_imported(self) -> float:
-        self._ensure_min_time_between_queries()
-        return self.client.read_input_registers(0x0048, ModbusDataType.FLOAT_32, unit=self.id) * 1000
 
-    def get_exported(self) -> float:
-        self._ensure_min_time_between_queries()
-        return self.client.read_input_registers(0x004a, ModbusDataType.FLOAT_32, unit=self.id) * 1000
-
-    def get_frequency(self) -> float:
-        self._ensure_min_time_between_queries()
-        frequency = self.client.read_input_registers(0x46, ModbusDataType.FLOAT_32, unit=self.id)
-        if frequency > 100:
-            frequency = frequency / 10
-        return frequency
-
-    # These meters require some minimum time between subsequent Modbus reads. Some Eastron papers recommend 100 ms.
-    # Sometimes the time between calls to the get_* methods are much shorter so we forcibly wait for the remaining time.
-    def _ensure_min_time_between_queries(self) -> None:
-        current_time = self._get_time_ms()
-        elapsed_time = current_time - self.last_query
-        if elapsed_time < self.WAIT_MS_BETWEEN_QUERIES:
-            time.sleep((self.WAIT_MS_BETWEEN_QUERIES - elapsed_time) / 1e3)
-        self.last_query = current_time
-
-    def _get_time_ms(self) -> float:
-        return time.time_ns() / 1e6
-
-    def get_serial_number(self) -> str:
-        return self.serial_number
+class SdmRegister(IntEnum):
+    VOLTAGE_L1 = 0x00
+    VOLTAGE_L2 = 0x02
+    VOLTAGE_L3 = 0x04
+    CURRENT_L1 = 0x06
+    CURRENT_L2 = 0x08
+    CURRENT_L3 = 0x0A
+    POWER_L1 = 0x0C
+    POWER_L2 = 0x0E
+    POWER_L3 = 0x10
+    POWER_FACTOR_L1 = 0x1E
+    POWER_FACTOR_L2 = 0x20
+    POWER_FACTOR_L3 = 0x22
+    FREQUENCY = 0x46
+    IMPORTED = 0x48
+    EXPORTED = 0x4A
 
 
 class Sdm630_72(Sdm):
+    REG_MAPPING = (
+        (SdmRegister.VOLTAGE_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.VOLTAGE_L2, ModbusDataType.FLOAT_32),
+        (SdmRegister.VOLTAGE_L3, ModbusDataType.FLOAT_32),
+        (SdmRegister.CURRENT_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.CURRENT_L2, ModbusDataType.FLOAT_32),
+        (SdmRegister.CURRENT_L3, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_L2, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_L3, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_FACTOR_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_FACTOR_L2, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_FACTOR_L3, ModbusDataType.FLOAT_32),
+        (SdmRegister.FREQUENCY, ModbusDataType.FLOAT_32),
+        (SdmRegister.IMPORTED, ModbusDataType.FLOAT_32),
+        (SdmRegister.EXPORTED, ModbusDataType.FLOAT_32),
+    )
+
     def __init__(self, modbus_id: int, client: modbus.ModbusTcpClient_, fault_state: FaultState) -> None:
         super().__init__(modbus_id, client)
         self.fault_state = fault_state
 
-    def get_currents(self) -> List[float]:
-        self._ensure_min_time_between_queries()
-        return self.client.read_input_registers(0x06, [ModbusDataType.FLOAT_32]*3, unit=self.id)
-
-    def get_power_factors(self) -> List[float]:
-        self._ensure_min_time_between_queries()
-        return self.client.read_input_registers(0x1E, [ModbusDataType.FLOAT_32]*3, unit=self.id)
-
-    def get_power(self) -> Tuple[List[float], float]:
-        self._ensure_min_time_between_queries()
-        powers = self.client.read_input_registers(0x0C, [ModbusDataType.FLOAT_32]*3, unit=self.id)
-        power = sum(powers)
-        return powers, power
-
-    def get_voltages(self) -> List[float]:
-        self._ensure_min_time_between_queries()
-        return self.client.read_input_registers(0x00, [ModbusDataType.FLOAT_32]*3, unit=self.id)
-
     def get_counter_state(self) -> CounterState:
-        powers, power = self.get_power()
+        resp = self.client.read_input_registers_bulk(
+            SdmRegister.VOLTAGE_L1, 76, mapping=self.REG_MAPPING, unit=self.id)
+        frequency = resp[SdmRegister.FREQUENCY]
+        if frequency > 100:
+            frequency = frequency / 10
         counter_state = CounterState(
-            imported=self.get_imported(),
-            exported=self.get_exported(),
-            power=power,
-            voltages=self.get_voltages(),
-            currents=self.get_currents(),
-            powers=powers,
-            power_factors=self.get_power_factors(),
-            frequency=self.get_frequency(),
+            imported=resp[SdmRegister.IMPORTED]*1000,
+            exported=resp[SdmRegister.EXPORTED]*1000,
+            power=sum(resp[r] for r in (SdmRegister.POWER_L1, SdmRegister.POWER_L2, SdmRegister.POWER_L3)),
+            voltages=[resp[r]
+                      for r in (SdmRegister.VOLTAGE_L1, SdmRegister.VOLTAGE_L2, SdmRegister.VOLTAGE_L3)],
+            currents=[resp[r]
+                      for r in (SdmRegister.CURRENT_L1, SdmRegister.CURRENT_L2, SdmRegister.CURRENT_L3)],
+            powers=[resp[r] for r in (SdmRegister.POWER_L1, SdmRegister.POWER_L2, SdmRegister.POWER_L3)],
+            power_factors=[resp[r] for r in (SdmRegister.POWER_FACTOR_L1,
+                                             SdmRegister.POWER_FACTOR_L2, SdmRegister.POWER_FACTOR_L3)],
+            frequency=frequency,
             serial_number=self.get_serial_number()
         )
         check_meter_values(counter_state, self.fault_state)
@@ -91,37 +85,35 @@ class Sdm630_72(Sdm):
 
 
 class Sdm120(Sdm):
+    REG_MAPPING = (
+        (SdmRegister.VOLTAGE_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.CURRENT_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.POWER_FACTOR_L1, ModbusDataType.FLOAT_32),
+        (SdmRegister.FREQUENCY, ModbusDataType.FLOAT_32),
+        (SdmRegister.IMPORTED, ModbusDataType.FLOAT_32),
+        (SdmRegister.EXPORTED, ModbusDataType.FLOAT_32),
+    )
+
     def __init__(self, modbus_id: int, client: modbus.ModbusTcpClient_, fault_state: FaultState) -> None:
         super().__init__(modbus_id, client)
         self.fault_state = fault_state
 
-    def get_power(self) -> Tuple[List[float], float]:
-        self._ensure_min_time_between_queries()
-        power = self.client.read_input_registers(0x0C, ModbusDataType.FLOAT_32, unit=self.id)
-        return [power, 0, 0], power
-
-    def get_currents(self) -> List[float]:
-        self._ensure_min_time_between_queries()
-        return [self.client.read_input_registers(0x06, ModbusDataType.FLOAT_32, unit=self.id), 0.0, 0.0]
-
-    def get_voltages(self) -> List[float]:
-        self._ensure_min_time_between_queries()
-        voltage = self.client.read_input_registers(0x00, ModbusDataType.FLOAT_32, unit=self.id)
-        return [voltage, 0.0, 0.0]
-
-    def get_power_factors(self) -> List[float]:
-        self._ensure_min_time_between_queries()
-        return [self.client.read_input_registers(0x1E, ModbusDataType.FLOAT_32, unit=self.id), 0.0, 0.0]
-
     def get_counter_state(self) -> CounterState:
-        powers, power = self.get_power()
+        resp = self.client.read_input_registers_bulk(
+            SdmRegister.VOLTAGE_L1, 76, mapping=self.REG_MAPPING, unit=self.id)
+        frequency = resp[SdmRegister.FREQUENCY]
+        if frequency > 100:
+            frequency = frequency / 10
         counter_state = CounterState(
-            imported=self.get_imported(),
-            exported=self.get_exported(),
-            power=power,
-            currents=self.get_currents(),
-            powers=powers,
-            frequency=self.get_frequency(),
+            imported=resp[SdmRegister.IMPORTED]*1000,
+            exported=resp[SdmRegister.EXPORTED]*1000,
+            power=resp[SdmRegister.POWER_L1],
+            voltages=[resp[SdmRegister.VOLTAGE_L1], 0, 0],
+            currents=[resp[SdmRegister.CURRENT_L1], 0, 0],
+            powers=[resp[SdmRegister.POWER_L1], 0, 0],
+            power_factors=[resp[SdmRegister.POWER_FACTOR_L1], 0, 0],
+            frequency=frequency,
             serial_number=self.get_serial_number()
         )
         check_meter_values(counter_state, self.fault_state)
