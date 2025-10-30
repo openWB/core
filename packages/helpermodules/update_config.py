79bc343a24de07f1c94e31c0000a66cb6ab25470
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2025-10-30 16:01:26 +0100

    battery hysteresis (#2706)
    * add max_soc
    
    * add hysterese
    
    * rename variables, add tests
    
    * update config
    
    * remove whitespace
    
    * fix rebase
    
    ---------
    
    Co-authored-by: LKuemmel <lena.kuemmel@openwb.de>

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -57,7 +57,7 @@ NO_MODULE = {"type": None, "configuration": {}}
 
 class UpdateConfig:
 
-    DATASTORE_VERSION = 99
+    DATASTORE_VERSION = 100
 
     valid_topic = [
         "^openWB/bat/config/bat_control_permitted$",
@@ -233,6 +233,7 @@ class UpdateConfig:
         "^openWB/general/chargemode_config/phase_switch_delay$",
         "^openWB/general/chargemode_config/pv_charging/control_range$",
         "^openWB/general/chargemode_config/pv_charging/min_bat_soc$",
+        "^openWB/general/chargemode_config/pv_charging/max_bat_soc$",
         "^openWB/general/chargemode_config/pv_charging/bat_power_discharge$",
         "^openWB/general/chargemode_config/pv_charging/bat_power_discharge_active$",
         "^openWB/general/chargemode_config/pv_charging/bat_power_reserve$",
@@ -536,6 +537,7 @@ class UpdateConfig:
         ("openWB/general/chargemode_config/pv_charging/bat_power_discharge", 1000),
         ("openWB/general/chargemode_config/pv_charging/bat_power_discharge_active", True),
         ("openWB/general/chargemode_config/pv_charging/min_bat_soc", 50),
+        ("openWB/general/chargemode_config/pv_charging/max_bat_soc", 70),
         ("openWB/general/chargemode_config/pv_charging/bat_power_reserve", 200),
         ("openWB/general/chargemode_config/pv_charging/bat_power_reserve_active", True),
         ("openWB/general/chargemode_config/pv_charging/control_range", [0, 230]),
@@ -2588,3 +2590,13 @@ class UpdateConfig:
             MessageType.INFO,
         )
         self.__update_topic("openWB/system/datastore_version", 99)
+
+    def upgrade_datastore_99(self) -> None:
+        # bei Aktualisierung den max_bat_soc auf min_bat_soc setzen
+        # Regelung verhält sich dadurch wie bisher konfiguriert
+        # max_bat_soc kann nicht kleiner als min_bat_soc werden
+        min_bat_soc = decode_payload(self.all_received_topics[
+            "openWB/general/chargemode_config/pv_charging/min_bat_soc"])
+
+        self.__update_topic("openWB/general/chargemode_config/pv_charging/max_bat_soc", min_bat_soc)
+        self.__update_topic("openWB/system/datastore_version", 100)
