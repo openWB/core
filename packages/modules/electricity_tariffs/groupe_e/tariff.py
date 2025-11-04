6ad2022064e398c469742a587b9b39aaa96c3558
cshagen <cshagen@hagens.ch>
2025-11-04 11:00:52 +0100

    Electricity tariffs (EKZ, Groupe E) (#2757)
    * add electricity tariffs for EKZ and Group E
    
    * fix lint errors
    
    * fix github lint issues
    
    * fix github flake issues
    
    * fix github flake issues
    
    * fix github flake issues
    
    * Update packages/modules/electricity_tariffs/group_e/tariff.py
    
    Co-authored-by: LKuemmel <76958050+LKuemmel@users.noreply.github.com>
    
    * switch to 15min interval
    
    * 15 minute intervals for tariffs
    
    * 15 minute intervals for tariffs
    
    * fix ekz price calculation
    
    * update ekz and group-e prices modeules
    
    ---------
    
    Co-authored-by: LKuemmel <76958050+LKuemmel@users.noreply.github.com>

---
+++ packages/modules/electricity_tariffs/groupe_e/tariff.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from datetime import datetime, timedelta
+from dateutil import tz
+from urllib.parse import quote
+from typing import Dict
+from modules.common import req
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.component_state import TariffState
+from modules.electricity_tariffs.groupe_e.config import GroupeETariffConfiguration
+from modules.electricity_tariffs.groupe_e.config import GroupeETariff
+
+
+# Extract timestamp from power price entry
+def timestamp(power):
+    return str(int(datetime.strptime(power['start_timestamp'], "%Y-%m-%dT%H:%M:%S%z")
+                   .astimezone(tz.tzutc()).timestamp()))
+
+
+# Read prices from Groupe E API
+def readApi() -> list[tuple[str, float]]:
+    endpoint = "https://api.tariffs.groupe-e.ch/v1/tariffs"
+    tariffName = "vario_plus"
+    startDate = datetime.now().strftime("%Y-%m-%dT%H:00:00+01:00")
+    endDate = (datetime.now() + timedelta(days=2)).strftime("%Y-%m-%dT%H:00:00+01:00")
+    session = req.get_http_session()
+    prices_raw = session.get(
+        url=endpoint +
+        f"?start_timestamp={ quote(startDate) }&end_timestamp={ quote(endDate) }",
+        ).json()
+    return [(timestamp(power), (power[tariffName]/100000))
+            for power in prices_raw]
+
+
+# Fetch prices and return as a dictionary
+def fetch_prices(config: GroupeETariffConfiguration) -> Dict[str, float]:
+    pricelist = readApi()
+    prices: Dict[str, float] = dict(pricelist)
+    return prices
+
+
+def create_electricity_tariff(config: GroupeETariff):
+    def updater():
+        return TariffState(prices=fetch_prices(config.configuration))
+    return updater
+
+
+device_descriptor = DeviceDescriptor(configuration_factory=GroupeETariff)
