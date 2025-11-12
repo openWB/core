38a6c2c68afa2da103713f0c9a43504ef6ceb9fc
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2025-11-12 15:06:43 +0100

    fix single phase wallboxes on evu L2 and L3 (#2934)

--- packages/helpermodules/phase_handling.py
+++ packages/helpermodules/phase_handling.py
@@ -24,3 +24,11 @@ def convert_single_cp_phase_to_evu_phase(phase_1: int, cp_phase: int) -> int:
 
 def convert_single_evu_phase_to_cp_phase(phase_1: int, evu_phase: int) -> int:
     return EVU_TO_CP_PHASE_MAPPING[phase_1][evu_phase]
+
+
+def voltages_mean(voltages: List[float]) -> float:
+    # Zoes erzeugen bei einphasiger Ladung 140V auf Phase 2
+    filtered_voltages = [v for v in voltages if v > 200]
+    if not filtered_voltages:
+        return 0.0
+    return sum(filtered_voltages) / len(filtered_voltages)
