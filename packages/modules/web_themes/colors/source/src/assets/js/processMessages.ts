1107509281ffbcc749805731d7ea967e907977f1
Claus Hagen <cshagen@hagens.ch>
2025-11-13 22:13:12 +0100

    fix counter display

--- packages/modules/web_themes/colors/source/src/assets/js/processMessages.ts
+++ packages/modules/web_themes/colors/source/src/assets/js/processMessages.ts
@@ -100,7 +100,8 @@ function processCounterMessages(topic: string, message: string) {
 	if (id != undefined) {
 		if (id == globalData.evuId) {
 			processEvuMessages(topic, message)
-		} else if (!counters.has(id)) {
+		}
+		if (!counters.has(id)) {
 			console.warn('Invalid counter index: ' + id)
 		} else if (topic.match(/^openWB\/counter\/[0-9]+\/get\/power$/i)) {
 			counters.get(id)!.power = +message
