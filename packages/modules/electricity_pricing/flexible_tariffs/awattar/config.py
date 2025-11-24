2b431e384f95d0b5ed45baee237693088472aa00
LKuemmel <lena.kuemmel@openwb.de>
2025-11-24 14:12:15 +0100

    flake8

--- packages/modules/electricity_pricing/flexible_tariffs/awattar/config.py
+++ packages/modules/electricity_pricing/flexible_tariffs/awattar/config.py
@@ -1,5 +1,10 @@
 class AwattarTariffConfiguration:
-    def __init__(self, country: str = "de", net: bool = True, fix: float = 0.015, proportional: float = 3, tax: float = 20) -> None:
+    def __init__(self,
+                 country: str = "de",
+                 net: bool = True,
+                 fix: float = 0.015,
+                 proportional: float = 3,
+                 tax: float = 20) -> None:
         self.country = country
         self.net = net
         self.fix = fix
