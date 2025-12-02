ddc75ee178cbedf1d887d7827839208af35d85ec
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2025-12-02 09:34:07 +0100

    mqtt inverter: fix topic parsing (#2996)

--- packages/modules/devices/generic/mqtt/inverter.py
+++ packages/modules/devices/generic/mqtt/inverter.py
@@ -35,9 +35,8 @@ class MqttInverter(AbstractInverter):
         if (received_topics.get(f"{topic_prefix}exported") is None or
                 received_topics.get(f"{topic_prefix}imported") is None):
             imported, exported = self.sim_counter.sim_count(power)
-        if received_topics.get(f"{topic_prefix}exported"):
+        else:
             exported = received_topics[f"{topic_prefix}exported"]
-        if received_topics.get(f"{topic_prefix}imported"):
             imported = received_topics[f"{topic_prefix}imported"]
 
         currents = parse_received_topics("currents")
