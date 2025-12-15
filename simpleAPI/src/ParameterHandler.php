d8c49928ca3738415eca7ae3bfa55fd6662be6a5
KevinWieland <kevin.wieland@openwb.de>
2025-12-15 11:43:33 +0100

    bugfix max_eco_price multiplier

--- simpleAPI/src/ParameterHandler.php
+++ simpleAPI/src/ParameterHandler.php
@@ -919,7 +919,7 @@ class ParameterHandler
     {
         try {
             $value = $value / 100000;
-	    $templateTopic = "openWB/chargepoint/{$chargepointId}/set/charge_template";
+            $templateTopic = "openWB/chargepoint/{$chargepointId}/set/charge_template";
             $templateJson = $this->mqttClient->getValue($templateTopic);
             if (!$templateJson) {
                 return ['success' => false, 'message' => 'Could not read current charge template from set topic'];
