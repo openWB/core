95eb78605e8f91eb212eb6983724bcbff7e75aad
LKuemmel <lena.kuemmel@openwb.de>
2025-12-16 09:58:18 +0100

    draft

--- packages/modules/electricity_pricing/flexible_tariffs/rabot/config.py
+++ packages/modules/electricity_pricing/flexible_tariffs/rabot/config.py
@@ -1,24 +1,12 @@
 from typing import Optional
 
 
-class RabotToken():
-    def __init__(self,
-                 access_token: Optional[str] = None,
-                 expires_in: Optional[str] = None,
-                 created_at: Optional[str] = None) -> None:
-        self.access_token = access_token  # don't show in UI
-        self.expires_in = expires_in  # don't show in UI
-        self.created_at = created_at  # don't show in UI
-
-
 class RabotTariffConfiguration:
     def __init__(self,
-                 client_id: Optional[str] = None,
-                 client_secret: Optional[str] = None,
-                 token: RabotToken = None):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.token = token or RabotToken()
+                 consumer_number: Optional[str] = None,
+                 contract_number: Optional[str] = None):
+        self.consumer_number = consumer_number
+        self.contract_number = contract_number
 
 
 class RabotTariff:
