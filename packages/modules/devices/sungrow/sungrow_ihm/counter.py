1dc61d460c416fd029a1d12fc1a4e497b2d18cbe
SeaSpotter <florian.horch@gmail.com>
2026-01-19 10:46:41 +0100

    Refactor SungrowIHM counter.py

--- packages/modules/devices/sungrow/sungrow_ihm/counter.py
+++ packages/modules/devices/sungrow/sungrow_ihm/counter.py
@@ -8,12 +8,12 @@ from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.modbus import Endian, ModbusDataType, ModbusTcpClient_
 from modules.common.simcount._simcounter import SimCounter
 from modules.common.store import get_counter_value_store
-from modules.devices.sungrow.sungrow_ihm.config import Sungrow, SungrowIHMCounterSetup
+from modules.devices.sungrow.sungrow_ihm.config import SungrowIHM, SungrowIHMCounterSetup
 
 
 class KwargsDict(TypedDict):
     client: ModbusTcpClient_
-    device_config: Sungrow
+    device_config: SungrowIHM
 
 
 class SungrowIHMCounter(AbstractCounter):
@@ -22,7 +22,7 @@ class SungrowIHMCounter(AbstractCounter):
         self.kwargs: KwargsDict = kwargs
 
     def initialize(self) -> None:
-        self.device_config: Sungrow = self.kwargs['device_config']
+        self.device_config: SungrowIHM = self.kwargs['device_config']
         self.__tcp_client: ModbusTcpClient_ = self.kwargs['client']
         self.sim_counter = SimCounter(self.device_config.id, self.component_config.id, prefix="evu")
         self.store = get_counter_value_store(self.component_config.id)
@@ -32,12 +32,15 @@ class SungrowIHMCounter(AbstractCounter):
     def update(self, pv_power: float):
         unit = self.device_config.configuration.modbus_id
         power = self.__tcp_client.read_input_registers(8156, ModbusDataType.INT_32,
-                                                       wordorder=Endian.Little, unit=unit) * -1
+                                                       wordorder=Endian.Little, unit=unit) * -10
+        
         powers = self.__tcp_client.read_input_registers(8558, [ModbusDataType.UINT_32] * 3,
                                                         wordorder=Endian.Little, unit=unit)
+        
         frequency = self.__tcp_client.read_input_registers(8557, ModbusDataType.UINT_16, unit=unit) / 10
-        voltages = self.__tcp_client.read_input_registers(8554, [ModbusDataType.UINT_16] * 3,
-                                                              wordorder=Endian.Little, unit=unit)
+        
+        voltages = self.__tcp_client.read_input_registers(8554, [ModbusDataType.UINT_16] * 3, 
+                                                          wordorder=Endian.Little, unit=unit)
 
         voltages = [value / 10 for value in voltages]
 
