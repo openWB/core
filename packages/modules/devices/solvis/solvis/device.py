46141ef78d466d0d36f24174f1b5def4d7cd1afb
SeaSpotter <florian.horch@gmail.com>
2026-01-19 08:01:06 +0100

    Add Solvis device.py

---
+++ packages/modules/devices/solvis/solvis/device.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import logging
+from typing import Iterable
+
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.configurable_device import ConfigurableDevice, ComponentFactoryByType, MultiComponentUpdater
+from modules.common.modbus import ModbusTcpClient_
+from modules.devices.solvis.solvis.config import Solvis, SolvisCounterSetup
+from modules.devices.solvis.solvis.counter import SolvisCounter
+
+log = logging.getLogger(__name__)
+
+
+def create_device(device_config: Solvis):
+    client = None
+
+    def create_counter_component(component_config: SolvisCounterSetup):
+        nonlocal client
+        return SolvisCounter(component_config,
+                          device_id=device_config.id,
+                          client=client,
+                          modbus_id=device_config.configuration.modbus_id)
+
+    def update_components(components: Iterable[SolvisCounter]):
+        with client:
+            for component in components:
+                with SingleComponentUpdateContext(component.fault_state):
+                    component.update()
+
+    def initializer():
+        nonlocal client
+        client = ModbusTcpClient_(device_config.configuration.ip_address, device_config.configuration.port)
+
+    return ConfigurableDevice(
+        device_config=device_config,
+        initializer=initializer,
+        component_factory=ComponentFactoryByType(
+            counter=create_counter_component,
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
+
+
+device_descriptor = DeviceDescriptor(configuration_factory=Solvis)
