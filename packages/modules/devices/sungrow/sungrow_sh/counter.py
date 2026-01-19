0402fda3e455e524b69dd255c61cb307adcef976
SeaSpotter <florian.horch@gmail.com>
2026-01-19 10:38:22 +0100

    Remove unused parameter
    Removed pv_power parameter from update method.

--- packages/modules/devices/sungrow/sungrow_sh/counter.py
+++ packages/modules/devices/sungrow/sungrow_sh/counter.py
@@ -32,7 +32,7 @@ class SungrowSHCounter(AbstractCounter):
             "keine Leistung der einzelnen Phasen. "\
             "Das Lastmanagement ist daher nur anhand der Gesamtleistung (nicht phasenbasiert) möglich."
 
-    def update(self, pv_power: float):
+    def update(self):
         unit = self.device_config.configuration.modbus_id
         power = self.__tcp_client.read_input_registers(13009, ModbusDataType.INT_32, 
                                                        wordorder=Endian.Little, unit=unit) * -1
