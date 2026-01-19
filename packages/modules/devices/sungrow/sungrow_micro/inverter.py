ca4b5ed52db89b04629e9729a11fe1b25a49dbd8
SeaSpotter <florian.horch@gmail.com>
2026-01-19 11:51:32 +0100

    Update inverter.py

--- packages/modules/devices/sungrow/sungrow_micro/inverter.py
+++ packages/modules/devices/sungrow/sungrow_micro/inverter.py
@@ -9,7 +9,6 @@ from modules.common.modbus import ModbusDataType, Endian, ModbusTcpClient_
 from modules.common.simcount import SimCounter
 from modules.common.store import get_inverter_value_store
 from modules.devices.sungrow.sungrow_micro.config import SungrowMicroInverterSetup, SungrowMicro
-from modules.devices.sungrow.sungrow_micro.version import Version
 
 
 class KwargsDict(TypedDict):
