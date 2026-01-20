e57a4ce5e0063af1bb29b504308b286148f864b8
Lutz Bender <github@lutz-bender.de>
2026-01-20 14:46:15 +0100

    prepare cards theme for mqtt auth

--- packages/modules/display_themes/cards/source/src/main.js
+++ packages/modules/display_themes/cards/source/src/main.js
@@ -4,17 +4,25 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import VueCookies from "vue-cookies";
+
 import { Inkline, components } from "@inkline/inkline";
 import "@inkline/inkline/inkline.scss";
 import "./main.scss";
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-app.use(Inkline, {
-  colorMode: "dark",
-  components,
-});
+const app = createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(Inkline, {
+    colorMode: "dark",
+    components,
+  })
+  .use(VueCookies, {
+    expire: "30d",
+    path: "/",
+    domain: "",
+    secure: true,
+    sameSite: "Lax",
+  });
 
 app.mount("#app");
