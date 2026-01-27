060ed19b8bb3b436ddb275ca870442205015de1f
Lutz Bender <github@lutz-bender.de>
2026-01-27 08:47:09 +0100

    fix typos password reset

--- packages/helpermodules/mosquitto_dynsec.py
+++ packages/helpermodules/mosquitto_dynsec.py
@@ -98,10 +98,10 @@ def send_password_reset_to_server(email: str, token: str, expires_at: int) -> No
         "subject": "openWB Passwort zurücksetzen",
         "message": (f"Hallo,\n\n"
                     "es wurde ein Antrag zum Zurücksetzen deines openWB Passworts gestellt.\n"
-                    "Bitte benutze den folgenden Token, um dein Passwort zurückzusetzen:\n\n"
+                    "Bitte benutze das folgende Token, um dein Passwort zurückzusetzen:\n\n"
                     f"{token}\n\n"
                     "Das Token ist gültig bis "
-                    f"{datetime.fromtimestamp(expires_at).strftime('%d.%m.%Y %H:%M:%S %Z')}\n\n"
+                    f"{datetime.fromtimestamp(expires_at).strftime('%d.%m.%Y %H:%M:%S %Z')}.\n\n"
                     "Falls du Dein Passwort nicht zurücksetzen wolltest, kannst du diese E-Mail ignorieren.")
     }
     error: Optional[str] = None
