ca5fab5cebec4a89f12e9962a6d7c42757fd18f4
Lutz Bender <github@lutz-bender.de>
2026-02-02 09:36:10 +0100

    display wrapper: check for stored credentials

---
+++ runs/dynsec_helper/display.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+$ip = $_SERVER['REMOTE_ADDR'];
+$filename = __DIR__ . "/../../data/clients/display-$ip.json";
+if (file_exists($filename)) {
+	$json = file_get_contents($filename);
+	$data = json_decode($json, true);
+	if (is_array($data)) {
+		$data['ip'] = $ip;
+		echo json_encode($data);
+	} else {
+		http_response_code(500);
+		echo json_encode(["error" => "Ungültige JSON-Struktur in $filename"]);
+	}
+} else {
+	http_response_code(404);
+	echo json_encode(["error" => "Datei nicht gefunden", "ip" => $ip]);
+}
