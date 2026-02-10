71be51a5957032ee79aae7ad38a6ba777d04a8e6
Lutz Bender <github@lutz-bender.de>
2026-02-10 12:22:02 +0100

    fix factory and user management reset

--- runs/reset_user_management.sh
+++ runs/reset_user_management.sh
@@ -8,4 +8,16 @@
 	else
 		echo "No dynamic security file found."
 	fi
+	# remove mosquitto_ctrl file if it exists
+	if [ -f "/home/openwb/.config/mosquitto_ctrl" ]; then
+		echo deleting user management data
+		rm /home/openwb/.config/mosquitto_ctrl
+	fi
+	# find all client files and remove them
+	if [ -d "/var/www/html/openWB/data/clients" ]; then
+		rm /var/www/html/openWB/data/clients/*.json
+		echo "Client files removed."
+	else
+		echo "No client files found."
+	fi
 } >> /var/www/html/openWB/ramdisk/reset_user_management.log 2>&1
