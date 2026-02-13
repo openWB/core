abc906180edce943522df548516a0f88119dd951
AlexHuebi <alexander@huebinger.at>
2026-02-13 02:51:49 +0100

    add Victron 3P75CT functionality

---
+++ packages/modules/devices/victron/victron_3p75ct/config.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from modules.common.component_setup import ComponentSetup
+from ..vendor import vendor_descriptor
+
+
+class VictronConfiguration:
+    def __init__(self, ip_address: Optional[str] = None, port: int = 502):
+        self.ip_address = ip_address
+        self.port = port
+
+
+class Victron:
+    def __init__(self,
+                 name: str = "Victron 3P75CT",
+                 type: str = "victron_3p75ct",
+                 id: int = 0,
+                 configuration: VictronConfiguration = None) -> None:
+        self.name = name
+        self.type = type
+        self.vendor = vendor_descriptor.configuration_factory().type
+        self.id = id
+        self.configuration = configuration or VictronConfiguration()
+
+
+class VictronCounterConfiguration:
+    def __init__(self, energy_meter: bool = True, modbus_id: int = 1):
+        self.energy_meter = energy_meter
+        self.modbus_id = modbus_id
+
+
+class VictronCounterSetup(ComponentSetup[VictronCounterConfiguration]):
+    def __init__(self,
+                 name: str = "Victron Zähler",
+                 type: str = "counter",
+                 id: int = 0,
+                 configuration: VictronCounterConfiguration = None) -> None:
+        super().__init__(name, type, id, configuration or VictronCounterConfiguration())
---
+++ packages/modules/devices/victron/victron_3p75ct/device.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import logging
+from typing import Iterable, Union
+
+from modules.common.abstract_device import DeviceDescriptor
+from modules.common.component_context import SingleComponentUpdateContext
+from modules.common.configurable_device import ComponentFactoryByType, ConfigurableDevice, MultiComponentUpdater
+from modules.common.modbus import ModbusUdpClient_
+from modules.devices.victron.victron_3p75ct.config import Victron, VictronCounterSetup
+from modules.devices.victron.victron_3p75ct.counter import VictronCounter
+
+log = logging.getLogger(__name__)
+
+
+def create_device(device_config: Victron):
+    client = None
+
+    def create_counter_component(component_config: VictronCounterSetup):
+        nonlocal client
+        return VictronCounter(component_config, device_id=device_config.id, client=client)
+
+
+    def update_components(components: Iterable[VictronCounter]):
+        nonlocal client
+        with client:
+            for component in components:
+                with SingleComponentUpdateContext(component.fault_state):
+                    component.update()
+
+    def initializer():
+        nonlocal client
+        client = ModbusUdpClient_(device_config.configuration.ip_address, device_config.configuration.port, timeout=10)
+
+    return ConfigurableDevice(
+        device_config=device_config,
+        initializer=initializer,
+        component_factory=ComponentFactoryByType(
+            counter=create_counter_component
+        ),
+        component_updater=MultiComponentUpdater(update_components)
+    )
+
+
+device_descriptor = DeviceDescriptor(configuration_factory=Victron)
