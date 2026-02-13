b82dc3b13fc0188d66401f6946affd1f458942af
LKuemmel <lena.kuemmel@openwb.de>
2026-02-13 15:22:34 +0100

    fix scripts

--- runs/evse_read_modbus.py
+++ runs/evse_read_modbus.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import sys
+
 sys.path.append("/var/www/html/openWB/packages")
 try:
+    from modules.chargepoints.internal_openwb.config import InternalChargepointMode
     from modules.internal_chargepoint_handler.clients import get_modbus_client
     from modules.common.modbus import ModbusDataType
 except Exception as e:
     # Durch try-except werden die Imports vom Formatierer nicht an den Dateianfang geschoben.
     print(e)
 
-local_chargepoint_num = int(sys.argv[1])
-register = int(sys.argv[2])
-num = int(sys.argv[3])
+mode = sys.argv[1]
+local_charge_point_num = int(sys.argv[2])
+register = int(sys.argv[3])
+num = int(sys.argv[4])
 
-client, evse_ids = get_modbus_client(local_chargepoint_num)
+client, evse_ids = get_modbus_client(InternalChargepointMode(mode), local_charge_point_num)
 print(client.read_holding_registers(register, [ModbusDataType.INT_16]*num, unit=evse_ids[0]))
--- runs/evse_write_modbus.py
+++ runs/evse_write_modbus.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import sys
+
 sys.path.append("/var/www/html/openWB/packages")
 try:
+    from modules.chargepoints.internal_openwb.config import InternalChargepointMode
     from modules.internal_chargepoint_handler.clients import get_modbus_client
 except Exception as e:
     # Durch try-except werden die Imports vom Formatierer nicht an den Dateianfang geschoben.
     print(e)
 
-local_chargepoint_num = int(sys.argv[1])
-register = int(sys.argv[2])
-value = int(sys.argv[3])
+mode = sys.argv[1]
+local_chargepoint_num = int(sys.argv[2])
+register = int(sys.argv[3])
+value = int(sys.argv[4])
 
-client, evse_ids = get_modbus_client(local_chargepoint_num)
+client, evse_ids = get_modbus_client(InternalChargepointMode(mode), local_chargepoint_num)
 client.write_register(register, value, unit=evse_ids[0])
