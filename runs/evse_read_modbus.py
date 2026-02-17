440373689a6221ae935e90640c226b16a6aee4c0
LKuemmel <lena.kuemmel@openwb.de>
2026-02-17 15:13:12 +0100

    fix

--- runs/evse_read_modbus.py
+++ runs/evse_read_modbus.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
+from pathlib import Path
 import sys
+
 
 sys.path.append("/var/www/html/openWB/packages")
 try:
-    from modules.chargepoints.internal_openwb.config import InternalChargepointMode
-    from modules.internal_chargepoint_handler.clients import get_modbus_client
-    from modules.common.modbus import ModbusDataType
+    from modules.common.modbus import ModbusDataType, ModbusSerialClient_
 except Exception as e:
     # Durch try-except werden die Imports vom Formatierer nicht an den Dateianfang geschoben.
     print(e)
 
-mode = sys.argv[1]
-local_charge_point_num = int(sys.argv[2])
-register = int(sys.argv[3])
-num = int(sys.argv[4])
+unit = sys.argv[1]
+register = int(sys.argv[2])
+num = int(sys.argv[3])
 
-client, evse_ids = get_modbus_client(InternalChargepointMode(mode), local_charge_point_num)
-print(client.read_holding_registers(register, [ModbusDataType.INT_16]*num, unit=evse_ids[0]))
+client = ModbusSerialClient_(str(list(Path("/dev/serial/by-path").glob("*"))[0].resolve()))
+print(client.read_holding_registers(register, [ModbusDataType.INT_16]*num, unit=unit))
