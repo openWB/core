8a4ebdf43f219714bc8c5e40845294da878eb66f
BrettS <brett.scott@openwb.de>
2026-02-18 11:55:12 +0100

    1 warning message, update link button

--- packages/modules/web_themes/koala/source/src/stores/mqtt-store.ts
+++ packages/modules/web_themes/koala/source/src/stores/mqtt-store.ts
@@ -2224,21 +2224,6 @@ export const useMqttStore = defineStore('mqtt', () => {
   };
 
   /**
-   * Get permanent time charging plan/s identified by the charge point id
-   * @param chargePointId charge point id
-   * @returns TimeChargingPlan[]
-   */
-  const vehicleTimeChargingPlansPermanent = (chargePointId: number) => {
-    const templateId =
-      chargePointConnectedVehicleChargeTemplate(chargePointId).value?.id;
-    const plans = getValue.value(
-      `openWB/vehicle/template/charge_template/${templateId}`,
-      'time_charging.plans',
-    ) as TimeChargingPlan[] | undefined;
-    return Array.isArray(plans) ? plans : [];
-  };
-
-  /**
    * Get or set the limit selected mode for the time charging plan identified by the time charging plan id
    * @param chargePointId charge point id
    * @param planId time charging plan id
@@ -3003,21 +2988,6 @@ export const useMqttStore = defineStore('mqtt', () => {
       console.warn('Kein Template für ChargePoint gefunden:', chargePointId);
     }
   }
-
-  /**
-   * Get permanent scheduled charging plan/s identified by the charge point id
-   * @param chargePointId charge point id
-   * @returns ScheduledChargingPlan[]
-   */
-  const vehicleScheduledChargingPlansPermanent = (chargePointId: number) => {
-    const templateId =
-      chargePointConnectedVehicleChargeTemplate(chargePointId).value?.id;
-    const plans = getValue.value(
-      `openWB/vehicle/template/charge_template/${templateId}`,
-      'chargemode.scheduled_charging.plans',
-    ) as ScheduledChargingPlan[] | undefined;
-    return Array.isArray(plans) ? plans : [];
-  };
 
   function removeScheduledChargingPlanForChargePoint(
     chargePointId: number,
@@ -4032,12 +4002,10 @@ export const useMqttStore = defineStore('mqtt', () => {
     vehicleTimeChargingPlanName,
     addTimeChargingPlanForChargePoint,
     removeTimeChargingPlanForChargePoint,
-    vehicleScheduledChargingPlansPermanent,
     vehicleTimeChargingPlanActive,
     vehicleTimeChargingPlanStartTime,
     vehicleTimeChargingPlanEndTime,
     vehicleTimeChargingPlanCurrent,
-    vehicleTimeChargingPlansPermanent,
     vehicleTimeChargingPlanLimitSelected,
     vehicleTimeChargingPlanSocLimit,
     vehicleTimeChargingPlanEnergyAmount,
