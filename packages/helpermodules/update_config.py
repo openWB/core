41a967f75f1a810d6540284291dc593c28edebf2
SeaSpotter <florian.horch@gmail.com>
2026-02-24 12:28:02 +0100

    Change upgrade_datastore version to 110

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -2747,7 +2747,7 @@ class UpdateConfig:
         self._loop_all_received_topics(upgrade)
         self._append_datastore_version(109)
 
-    def upgrade_datastore_108(self) -> None:
+    def upgrade_datastore_110(self) -> None:
         """
         Migrate old single 'sungrow' devices into new modules:
          - 'sungrow_sg' for SG family (no version field)
