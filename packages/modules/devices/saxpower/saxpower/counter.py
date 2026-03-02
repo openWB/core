967d277f8ab41a538bb4bad957238933e313558f
ndrsnhs <andreas.neuhaus@openwb.de>
2026-03-02 13:19:41 +0100

    read power only

--- packages/modules/devices/saxpower/saxpower/counter.py
+++ packages/modules/devices/saxpower/saxpower/counter.py
@@ -31,7 +31,7 @@ class SaxpowerCounter(AbstractCounter):
         self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="bezug")
 
     def update(self) -> None:
-        power = self.client.read_holding_registers(48, [ModbusDataType.INT_16]*2, unit=self.__modbus_id)
+        power = self.client.read_holding_registers(48, ModbusDataType.INT_16, unit=self.__modbus_id)
         power = power * -1 + 16384
         imported, exported = self.sim_counter.sim_count(power)
 
