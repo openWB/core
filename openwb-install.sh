0dbef73d96d1661b5adc4134f566fe9cf1e7b2fb
Lutz Bender <github@lutz-bender.de>
2026-03-04 08:58:16 +0100

    remove auth-service

--- openwb-install.sh
+++ openwb-install.sh
@@ -126,11 +126,6 @@ ln -s "${OPENWBBASEDIR}/data/config/openwb2.service" /etc/systemd/system/openwb2
 systemctl daemon-reload
 systemctl enable openwb2
 
-# echo "installing openwb2 auth service..."
-# ln -s "${OPENWBBASEDIR}/data/config/openwbAuthServer.service" /etc/systemd/system/openwbAuthServer.service
-# systemctl daemon-reload
-# systemctl enable openwbAuthServer
-
 echo "installing openwb2-simpleAPI service..."
 ln -s "${OPENWBBASEDIR}/data/config/openwb-simpleAPI.service" /etc/systemd/system/openwb-simpleAPI.service
 systemctl daemon-reload
@@ -143,7 +138,6 @@ systemctl enable openwbRemoteSupport
 systemctl start openwbRemoteSupport
 
 echo "installation finished, now starting openwb services..."
-# systemctl start openwbAuthServer
 systemctl start openwb2
 
 echo "all done"
--- packages/auth_service/auth-frontend/src/pages/IndexPage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <q-page class="row items-center justify-evenly">
-    Home
-  </q-page>
-</template>
-
-<script setup lang="ts">
-</script>
