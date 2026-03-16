abcb264abd7d37d8e415cae2d462bc1655e9f746
MartinRinas <martrin@microsoft.com>
2026-03-16 16:22:00 +0000

    move to vite8, remove unused dependencies

--- packages/modules/display_themes/url_display/source/vite.config.js
+++ packages/modules/display_themes/url_display/source/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import rollupNodePolyfills from "rollup-plugin-polyfill-node";
 
 export default defineConfig({
   base: "/openWB/web/display/themes/url_display/",
-  plugins: [
-    nodePolyfills({
-      protocolImports: true,
-    }),
-  ],
   build: {
     rollupOptions: {
-      plugins: [rollupNodePolyfills()],
       output: {
         entryFileNames: "assets/index.js",
         chunkFileNames: "assets/[name].js",
