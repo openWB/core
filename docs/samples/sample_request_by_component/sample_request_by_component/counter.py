072b33cea87839109b846a2d9e732fdfaebb2805
ndrsnhs <andreas.neuhaus@openwb.de>
2026-03-27 15:10:21 +0100

    add filter to samples

--- docs/samples/sample_request_by_component/sample_request_by_component/counter.py
+++ docs/samples/sample_request_by_component/sample_request_by_component/counter.py
@@ -8,6 +8,8 @@ from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.simcount import SimCounter
 from modules.common.store import get_counter_value_store
 from modules.devices.sample_request_by_component.sample_request_by_component.config import SampleCounterSetup
+from modules.common.utils.peak_filter import PeakFilter
+from modules.common.component_type import ComponentType
 
 
 class KwargsDict(TypedDict):
@@ -26,10 +28,12 @@ class SampleCounter(AbstractCounter):
         self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="bezug")
         self.store = get_counter_value_store(self.component_config.id)
         self.fault_state = FaultState(ComponentInfo.from_component_config(self.component_config))
+        self.peak_filter = PeakFilter(ComponentType.COUNTER, self.component_config.id, self.fault_state)
 
     def update(self):
         resp = req.get_http_session().get(self.ip_address)
         power = resp.json().get("power")
+        self.peak_filter.check_values(power)
         imported, exported = self.sim_counter.sim_count(power)
 
         counter_state = CounterState(
