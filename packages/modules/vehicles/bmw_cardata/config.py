395d82b2671713d0638ff0708f36332a88c2e8f1
GERDerDennis <dennis@dsc-nord.de>
2026-03-29 00:12:09 +0100

    Add files via upload

--- packages/modules/vehicles/bmw_cardata/config.py
+++ packages/modules/vehicles/bmw_cardata/config.py
@@ -1,35 +1,27 @@
-from helpermodules.auto_str import auto_str
+from dataclasses import dataclass
 
 
-@auto_str
+@dataclass
 class BmwCardataConfiguration:
-    def __init__(
-        self,
-        client_id: str = None,
-        vin: str = None,
-        calculate_soc: bool = False,
-        test_mode: bool = False,
-        test_soc: int = 80,
-        test_range: int = 300,
-    ):
-        self.client_id = client_id
-        self.vin = vin
-        self.calculate_soc = calculate_soc
-        self.test_mode = test_mode
-        self.test_soc = test_soc
-        self.test_range = test_range
+    client_id: str = ""
+    vin: str = ""
+    calculate_soc: bool = False
+    test_mode: bool = True
+    test_soc: int = 80
+    test_range: int = 300
+    access_token: str = ""
+    refresh_token: str = ""
+    expires_at: float = 0
+    container_id: str = ""
 
 
-@auto_str
+@dataclass
 class BmwCardataSetup:
-    def __init__(
-        self,
-        name: str = "BMW CarData",
-        type: str = "bmw_cardata",
-        official: bool = False,
-        configuration: BmwCardataConfiguration = None,
-    ) -> None:
-        self.name = name
-        self.type = type
-        self.official = official
-        self.configuration = configuration or BmwCardataConfiguration()
+    name: str = "BMW CarData"
+    type: str = "bmw_cardata"
+    official: bool = False
+    configuration: BmwCardataConfiguration = None
+
+    def __post_init__(self):
+        if self.configuration is None:
+            self.configuration = BmwCardataConfiguration()
