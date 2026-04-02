256275a96c51452c2e96797c442e51bdfff79bc2
GERDerDennis <dennis@dsc-nord.de>
2026-04-03 00:41:23 +0200

    Fix W292: add newline at end of file

--- packages/modules/vehicles/bmw_cardata/soc_test.py
+++ packages/modules/vehicles/bmw_cardata/soc_test.py
@@ -224,6 +224,5 @@ class TestBmwCardata:
         create_vehicle(self._make_config(), 0).update(VehicleUpdateData())
         assert self.mock_value_store.set.call_count == 1
         assert self.mock_value_store.set.call_args[0][0].soc == 47
-        assert self.mock_value_store.set.call_args[0][0].range == 234
-        
+        assert self.mock_value_store.set.call_args[0][0].range == 234        
         assert self.mock_value_store.set.call_args[0][0].odometer == 61762
