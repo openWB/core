d3c7fd52c8208edcb6044bb67ba939d683866c2b
benderl <benderl@users.noreply.github.com>
2026-04-09 15:08:18 +0200

    Prevent merge conflict in update_config.py

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -57,7 +57,7 @@ NO_MODULE = {"type": None, "configuration": {}}
 
 class UpdateConfig:
 
-    DATASTORE_VERSION = 120
+    DATASTORE_VERSION = 121
 
     valid_topic = [
         "^openWB/bat/config/bat_control_permitted$",
@@ -3046,7 +3046,7 @@ class UpdateConfig:
         self._loop_all_received_topics(upgrade)
         self._append_datastore_version(119)
 
-    def upgrade_datastore_120(self) -> None:
+    def upgrade_datastore_121(self) -> None:
         def upgrade(topic: str, payload) -> Optional[dict]:
             if re.search(r"^openWB/bat/[0-9]+/get/max_discharge_power$", topic) is not None:
                 payload = decode_payload(payload)
@@ -3054,4 +3054,4 @@ class UpdateConfig:
                     payload = -abs(payload)
                     return {topic: payload}
         self._loop_all_received_topics(upgrade)
-        self._append_datastore_version(120)
+        self._append_datastore_version(121)
