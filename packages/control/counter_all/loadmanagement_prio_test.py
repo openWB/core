217d9e836bce9c1288a0ce45f65fbb0153889e6e
LKuemmel <lena.kuemmel@openwb.de>
2026-04-13 15:58:51 +0200

    fixes

--- packages/control/counter_all/loadmanagement_prio_test.py
+++ packages/control/counter_all/loadmanagement_prio_test.py
@@ -7,6 +7,27 @@ from control.chargepoint.chargepoint import Chargepoint
 from control.counter_all.counter_all import CounterAll
 
 
+@pytest.fixture
+def cp1():
+    cp = Chargepoint(1, None)
+    cp.data.control_parameter.required_current = 8
+    return cp
+
+
+@pytest.fixture
+def cp2():
+    cp = Chargepoint(2, None)
+    cp.data.control_parameter.required_current = 7
+    return cp
+
+
+@pytest.fixture
+def cp3():
+    cp = Chargepoint(3, None)
+    cp.data.control_parameter.required_current = 6
+    return cp
+
+
 @pytest.mark.parametrize(
     "loadmanagement_prios, id, type, expected_loadmanagement_prios",
     [
@@ -121,47 +142,72 @@ def test_remove_loadmanagement_prio_item(loadmanagement_prios: List[Dict],
     assert c.data.get.loadmanagement_prios == expected_loadmanagement_prios
 
 
-@pytest.mark.parametrize(
-    "config_ev_list, loadmanagement_prios, sorted_cp_ids",
-    [
-        pytest.param([0]*3,
-                     [{"type": "vehicle", "id": 2}, {"type": "vehicle", "id": 0}],
-                     [1, 2, 3],
-                     id="alle LP haben das gleiche Fahrzeug zugeordnet"),
-        pytest.param([1, 2, 3],
-                     [{"type": "vehicle", "id": 3}, {"type": "vehicle", "id": 1}, {"type": "vehicle", "id": 2}],
-                     [3, 1, 2],
-                     id="alle LP haben unterschiedliche Fahrzeuge"),
-        pytest.param([1, 2, 3],
-                     [{"type": "vehicle", "id": 3}, {
-                      "type": "group",
-                      "label": "Wichtige Fahrzeuge",
-                      "children": [{"type": "vehicle", "id": 1}, {
-                          "type": "vehicle", "id": 2}]}],
-                     [3, 1, 2],
-                     id="alle LP haben unterschiedliche Fahrzeuge mit Gruppe"),
-    ]
-)
-def test_sort_cps_by_loadmanagement_prios_nested(config_ev_list: List[int],
-                                                 loadmanagement_prios: List[Dict],
-                                                 sorted_cp_ids: List[int]):
+def test_sort_cps_by_loadmanagement_prios_nested_same_vehicle(cp1, cp2, cp3):
+    """Alle LP haben das gleiche Fahrzeug zugeordnet"""
     # setup
-    cp1 = Chargepoint(1, None)
-    cp1.data.config.ev = config_ev_list[0]
-    cp1.data.control_parameter.required_current = 8
-    cp2 = Chargepoint(2, None)
-    cp2.data.config.ev = config_ev_list[1]
-    cp2.data.control_parameter.required_current = 7
-    cp3 = Chargepoint(3, None)
-    cp3.data.config.ev = config_ev_list[2]
-    cp3.data.control_parameter.required_current = 6
+    cp1.data.config.ev = 0
+    cp2.data.config.ev = 0
+    cp3.data.config.ev = 0
 
     c = CounterAll()
-    c.data.get.loadmanagement_prios = loadmanagement_prios
+    c.data.get.loadmanagement_prios = [{"type": "vehicle", "id": 2}, {"type": "vehicle", "id": 0}]
 
     # execution
-    sorted_cps = c.sort_cps_by_loadmanagement_prios_nested([cp1, cp2, cp3])
+    result = c.sort_cps_by_loadmanagement_prios_nested([cp1, cp2, cp3])
 
-    # assert
-    for i, cp in enumerate(sorted_cps):
-        assert cp.num == sorted_cp_ids[i]
+    # assert - eine Gruppe mit allen CPs (sortiert nach required_current)
+    assert len(result) == 1
+    assert result[0] == [cp3, cp2, cp1]  # direkte Objektvergleiche!
+
+
+def test_sort_cps_by_loadmanagement_prios_nested_different_vehicles(cp1, cp2, cp3):
+    """Alle LP haben unterschiedliche Fahrzeuge"""
+    # setup
+    cp1.data.config.ev = 1
+    cp2.data.config.ev = 2
+    cp3.data.config.ev = 3
+
+    c = CounterAll()
+    c.data.get.loadmanagement_prios = [
+        {"type": "vehicle", "id": 3},
+        {"type": "vehicle", "id": 1},
+        {"type": "vehicle", "id": 2}
+    ]
+
+    # execution
+    result = c.sort_cps_by_loadmanagement_prios_nested([cp1, cp2, cp3])
+
+    # assert - drei separate Gruppen
+    assert len(result) == 3
+    assert result[0] == [cp3]  # vehicle id=3
+    assert result[1] == [cp1]  # vehicle id=1
+    assert result[2] == [cp2]  # vehicle id=2
+
+
+def test_sort_cps_by_loadmanagement_prios_nested_with_group(cp1, cp2, cp3):
+    """LP mit unterschiedlichen Fahrzeugen, einige in Gruppe"""
+    # setup
+    cp1.data.config.ev = 1
+    cp2.data.config.ev = 2
+    cp3.data.config.ev = 3
+
+    c = CounterAll()
+    c.data.get.loadmanagement_prios = [
+        {"type": "vehicle", "id": 3},
+        {
+            "type": "group",
+            "label": "Wichtige Fahrzeuge",
+            "children": [
+                {"type": "vehicle", "id": 1},
+                {"type": "vehicle", "id": 2}
+            ]
+        }
+    ]
+
+    # execution
+    result = c.sort_cps_by_loadmanagement_prios_nested([cp1, cp2, cp3])
+
+    # assert - zwei Gruppen
+    assert len(result) == 2
+    assert result[0] == [cp3]        # vehicle id=3 einzeln
+    assert result[1] == [cp1, cp2]   # vehicles id=1,2 in der Gruppe
