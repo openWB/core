93540122bef948ebe26c68922fddbc2df2323110
GERDerDennis <dennis@dsc-nord.de>
2026-04-24 16:15:16 +0200

    Update soc.py
    Remove ineffective general_config defaults, add kombiRemainingElectricRange fallback, sync token to all BMW CarData vehicles on refresh

--- packages/modules/vehicles/bmw_cardata/soc.py
+++ packages/modules/vehicles/bmw_cardata/soc.py
@@ -8,7 +8,7 @@ from dataclass_utils import asdict
 from helpermodules.pub import Pub
 from modules.common import req
 from modules.common.abstract_device import DeviceDescriptor
-from modules.common.abstract_vehicle import GeneralVehicleConfig, VehicleUpdateData
+from modules.common.abstract_vehicle import VehicleUpdateData
 from modules.common.component_state import CarState
 from modules.common.configurable_vehicle import ConfigurableVehicle
 from modules.vehicles.bmw_cardata.config import BmwCardataSetup, BmwCardataConfiguration
@@ -275,10 +275,6 @@ def create_vehicle(vehicle_config: BmwCardataSetup, vehicle: int):
         component_updater=updater,
         vehicle=vehicle,
         calc_while_charging=vehicle_config.configuration.calculate_soc,
-        general_config=GeneralVehicleConfig(
-            request_interval_charging=1800,
-            request_interval_not_charging=43200,
-        ),
     )
 
 
