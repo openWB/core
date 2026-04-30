eef26710e2a74da9d7354f0e1c10336504743f8d
LKuemmel <lena.kuemmel@openwb.de>
2026-04-30 07:43:42 +0200

    update datastore version

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -57,7 +57,7 @@ NO_MODULE = {"type": None, "configuration": {}}
 
 class UpdateConfig:
 
-    DATASTORE_VERSION = 121
+    DATASTORE_VERSION = 122
 
     valid_topic = [
         "^openWB/bat/config/bat_control_permitted$",
