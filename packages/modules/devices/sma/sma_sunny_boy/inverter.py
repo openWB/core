f75937aa8a2d65d14e51330aadb4ec5f8937cc2e
SeaSpotter <florian.horch@gmail.com>
2026-05-11 14:06:04 +0200

    Simplify inverter.py

--- packages/modules/devices/sma/sma_sunny_boy/inverter.py
+++ packages/modules/devices/sma/sma_sunny_boy/inverter.py
@@ -43,23 +43,13 @@ class SmaSunnyBoyInverter(AbstractInverter):
         self.peak_filter = PeakFilter(ComponentType.INVERTER, self.component_config.id, self.fault_state)
 
     def update(self) -> None:
-        self.store.set(self.read())
-
-    def read(self) -> InverterState:
         unit = self.component_config.configuration.modbus_id
 
         if self.component_config.configuration.version == SmaInverterVersion.default:
-            # AC Wirkleistung über alle Phasen (W) [Pac]
-            power_total = self.tcp_client.read_holding_registers(30775, ModbusDataType.INT_32, unit=unit)
-            # Gesamtertrag (Wh) [E-Total]
+            power_total = self.tcp_client.read_holding_registers(30775, ModbusDataType.INT_32, unit=unit)  * -1
             energy = self.tcp_client.read_holding_registers(30529, ModbusDataType.UINT_32, unit=unit)
-            # Bei Hybrid Wechselrichtern treten Abweichungen auf, die in der Nacht
-            # immer wieder Generatorleistung anzeigen (0-50 Watt). Um dies zu verhindern, schauen wir uns
-            # zunächst an, ob vom DC Teil überhaupt Leistung kommt. Ist dies nicht der Fall, können wir power
-            # gleich auf 0 setzen.
-            # Leistung DC an Eingang 1 und 2
             dc_power = (self.tcp_client.read_holding_registers(30773, ModbusDataType.INT_32, unit=unit) +
-                        self.tcp_client.read_holding_registers(30961, ModbusDataType.INT_32, unit=unit))
+                        self.tcp_client.read_holding_registers(30961, ModbusDataType.INT_32, unit=unit))  * -1
 
             currents = self.tcp_client.read_holding_registers(30977, [ModbusDataType.INT_32]*3, unit=unit)
             if all(c == self.SMA_INT32_NAN for c in currents):
@@ -67,29 +57,20 @@ class SmaSunnyBoyInverter(AbstractInverter):
             else:
                 currents = [current / -1000 if current != self.SMA_INT32_NAN else 0 for current in currents]
         elif self.component_config.configuration.version == SmaInverterVersion.core2:
-            # AC Wirkleistung über alle Phasen (W) [Pac]
-            power_total = self.tcp_client.read_holding_registers(40084, ModbusDataType.INT_16, unit=unit) * 10
-            # Gesamtertrag (Wh) [E-Total] SF=2!
+            power_total = self.tcp_client.read_holding_registers(40084, ModbusDataType.INT_16, unit=unit) * -10
             energy = self.tcp_client.read_holding_registers(40094, ModbusDataType.UINT_32, unit=unit) * 100
-            # Power
-            dc_power = self.tcp_client.read_holding_registers(40101, ModbusDataType.UINT_32, unit=unit) * 100
-            # Phasenstöme
-            current_L1 = self.tcp_client.read_holding_registers(30977, ModbusDataType.INT_32, unit=unit) * -1
-            current_L2 = self.tcp_client.read_holding_registers(30979, ModbusDataType.INT_32, unit=unit) * -1
-            current_L3 = self.tcp_client.read_holding_registers(30981, ModbusDataType.INT_32, unit=unit) * -1
-            currents = [current_L1 / 1000, current_L2 / 1000, current_L3 / 1000]
+            dc_power = self.tcp_client.read_holding_registers(40101, ModbusDataType.UINT_32, unit=unit) * -100
+            currents = self.tcp_client.read_holding_registers(30977, [ModbusDataType.INT_32]*3, unit=unit)
+            currents = [value / -1000 for value in currents]
+
         elif self.component_config.configuration.version == SmaInverterVersion.datamanager:
-            # AC Wirkleistung über alle Phasen (W) [Pac]
-            power_total = self.tcp_client.read_holding_registers(30775, ModbusDataType.INT_32, unit=unit)
-            # Total eingespeiste Energie auf allen Außenleitern (Wh) [E-Total]
+            power_total = self.tcp_client.read_holding_registers(30775, ModbusDataType.INT_32, unit=unit) * -1
             energy = self.tcp_client.read_holding_registers(30513, ModbusDataType.UINT_64, unit=unit)
-            # DC-Power = power_total - Cluster-Controller gibt in Register 30775 immer korrekte Werte aus,
-            # daher ist wie bei SmaInverterVersion.default keine Prüfung auf DC-Leistung notwendig.
             # Aus kompatibilitätsgründen wird dc_power auf den Wert der AC-Wirkleistung gesetzt.
             dc_power = power_total
             # Der Data-Manager/Cluster-Controller bietet keine Modbus-Register mit Phasenströmen an.
             # Daher die Phasenströme berechnen (es wird davon ausgegangen, dass eine symmetrische Erzeugung erfolgt)
-            currents = [(power_total / 3 / 230) * -1] * 3
+            currents = [(power_total / 3 / 230)] * 3
         else:
             raise ValueError("Unbekannte Version "+str(self.component_config.configuration.version))
         if power_total == self.SMA_INT32_NAN or power_total == self.SMA_NAN:
@@ -102,19 +83,16 @@ class SmaSunnyBoyInverter(AbstractInverter):
                 'Sobald PV Ertrag vorhanden ist sollte sich dieser Wert ändern, '
                 'andernfalls kann ein Defekt vorliegen.'
             )
-        _, exported = self.peak_filter.check_values(power_total * -1, None, energy)
-        imported, _ = self.sim_counter.sim_count(power_total * -1)
+        _, exported = self.peak_filter.check_values(power_total, None, energy)
+        imported, _ = self.sim_counter.sim_count(power_total)
 
         inverter_state = InverterState(
-            power=power_total * -1,
-            dc_power=dc_power * -1,
+            power=power_total,
+            dc_power=dc_power,
             exported=exported,
             imported=imported
         )
-        if 'currents' in locals():
-            inverter_state.currents = currents
-        log.debug("WR {}: {}".format(self.tcp_client.address, inverter_state))
-        return inverter_state
+        self.store.set(inverter_state)
 
 
 component_descriptor = ComponentDescriptor(configuration_factory=SmaSunnyBoyInverterSetup)
