ae74f41dd8e36400799938a37bcb221f339a0b3b
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2026-05-21 16:05:24 +0200

    daily log data: fix units (#3379)
    * fix units
    
    * fix
    
    * flake8
    
    * fix
    
    * floating point

--- packages/helpermodules/measurement_logging/process_log_integration_test.py
+++ packages/helpermodules/measurement_logging/process_log_integration_test.py
@@ -1,25 +1,30 @@
 from unittest.mock import Mock
-import pytest
 
 from helpermodules.measurement_logging import process_log
 
-from helpermodules.measurement_logging.process_log_testdata import (counter_jumps_forward,
-                                                                    counter_jumps_forward_processed,
-                                                                    regular_daily_log_entry,
-                                                                    regular_daily_log_entry_processed)
+
+pytest_plugins = ["helpermodules.measurement_logging.process_log_testdata"]
 
 
-@pytest.mark.parametrize("data, expected", [
-    pytest.param(counter_jumps_forward, counter_jumps_forward_processed, id="counter jumps forward"),
-    pytest.param(regular_daily_log_entry, regular_daily_log_entry_processed, id="regular daily log entry")
-])
-def test_get_daily_log(data, expected, monkeypatch):
+def test_get_daily_log_counter_jump(counter_jumps_forward, counter_jumps_forward_processed, monkeypatch):
     # setup
-    collect_daily_log_data_mock = Mock(return_value=data)
+    collect_daily_log_data_mock = Mock(return_value=counter_jumps_forward)
     monkeypatch.setattr(process_log, "_collect_daily_log_data", collect_daily_log_data_mock)
 
     # execution
     daily_log_processed = process_log.get_daily_log("20250616")
 
     # evaluation
-    assert daily_log_processed == expected
+    assert daily_log_processed == counter_jumps_forward_processed
+
+
+def test_get_daily_log_regular(regular_daily_log_entry, regular_daily_log_entry_processed, monkeypatch):
+    # setup
+    collect_daily_log_data_mock = Mock(return_value=regular_daily_log_entry)
+    monkeypatch.setattr(process_log, "_collect_daily_log_data", collect_daily_log_data_mock)
+
+    # execution
+    daily_log_processed = process_log.get_daily_log("20250616")
+
+    # evaluation
+    assert daily_log_processed == regular_daily_log_entry_processed
