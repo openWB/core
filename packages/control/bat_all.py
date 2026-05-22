5dcd4a55720b58f02f8fafdca69646c019c12c98
ndrsnhs <andreas.neuhaus@openwb.de>
2026-05-22 08:21:41 +0200

    correct power_limit

--- packages/control/bat_all.py
+++ packages/control/bat_all.py
@@ -586,7 +586,7 @@ class BatAll:
             elif self.data.config.power_limit_mode == BatPowerLimitMode.MODE_CHARGE_PV_PRODUCTION.value:
                 # PV-Ueberschuss abzueglich Hausverbrauch als Ladeleistung des Speichers nutzen.
                 # Bei geringem Ueberschuss wird Hausverbrauch durch Speicher ausgeglichen
-                left_pv_power = data.data.pv_all_data.data.get.power - data.data.counter_all_data.data.set.home_consumption
+                left_pv_power = data.data.pv_all_data.data.get.power + data.data.counter_all_data.data.set.home_consumption
                 self.data.set.power_limit = left_pv_power * -1
                 log.debug(f"Speicher in Höhe des PV-Ertrags laden: {self.data.set.power_limit/1000}kW")
         elif charge_mode == BatChargeMode.BAT_FORCE_CHARGE:
