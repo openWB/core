83ffd4ea068d313a4c179032fd590d95791290cb
LKuemmel <lena.kuemmel@openwb.de>
2026-05-29 13:20:45 +0200

    migrate new modules

--- packages/modules/common/store/__init__.py
+++ packages/modules/common/store/__init__.py
@@ -1,10 +1,10 @@
 from modules.common.store._api import ValueStore, update_values
-from modules.common.store._battery import get_bat_value_store
+from modules.common.store._battery import get_bat_value_store  # obsolet 2.3.0
 from modules.common.store._car import get_car_value_store
 from modules.common.store._chargepoint import get_chargepoint_value_store
 from modules.common.store._chargepoint_internal import get_internal_chargepoint_value_store
-from modules.common.store._counter import get_counter_value_store
+from modules.common.store._counter import get_counter_value_store  # obsolet 2.3.0
 from modules.common.store._factory import get_component_value_store
-from modules.common.store._inverter import get_inverter_value_store
+from modules.common.store._inverter import get_inverter_value_store  # obsolet 2.3.0
 from modules.common.store._io import get_io_value_store
 from modules.common.store._tariff import get_flexible_tariff_value_store, get_grid_fee_value_store
