c339399d514b2f8b8e0f8ff602000d7388037617
LKuemmel <lena.kuemmel@openwb.de>
2026-06-02 12:19:27 +0200

    update config for prio list

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -11,6 +11,7 @@ import time
 from typing import List, Optional
 from paho.mqtt.client import Client as MqttClient, MQTTMessage
 
+from control.chargemode import Chargemode
 from control.limiting_value import LoadmanagementLimit
 
 from control.chargepoint.chargepoint_template import get_chargepoint_template_default
@@ -57,7 +58,7 @@ NO_MODULE = {"type": None, "configuration": {}}
 
 class UpdateConfig:
 
-    DATASTORE_VERSION = 140
+    DATASTORE_VERSION = 141
 
     valid_topic = [
         "^openWB/bat/config/bat_control_activated$",
@@ -3569,3 +3570,28 @@ class UpdateConfig:
                    f"{old_prefix}/retry_failed_phase_switches")
 
         self._append_datastore_version(140)
+
+    def upgrade_datastore_141(self) -> None:
+        CHARGEMODES = ((Chargemode.SCHEDULED_CHARGING.value, True),
+                       (Chargemode.SCHEDULED_CHARGING.value, False),
+                       (Chargemode.INSTANT_CHARGING.value, True),
+                       (Chargemode.INSTANT_CHARGING.value, False),
+                       (Chargemode.ECO_CHARGING.value, True),
+                       (Chargemode.PV_CHARGING.value, True),
+                       (Chargemode.PV_CHARGING.value, False),
+                       (Chargemode.STOP.value, True),
+                       (Chargemode.STOP.value, False),)
+
+        def upgrade(topic: str, payload) -> None:
+            if re.search("openWB/vehicle/[0-9]+/charge_template", topic) is not None:
+                charge_template_id = decode_payload(payload)
+                charge_template = decode_payload(
+                    self.all_received_topics[f"openWB/vehicle/template/charge_template/{charge_template_id}"])
+                if charge_template["chargemode"]["selected"] == chargemode and charge_template["prio"] == prio:
+                    loadmanagement_prios.append({"type": "vehicle", "id": int(get_index(topic))})
+
+        loadmanagement_prios = []
+        for chargemode, prio in CHARGEMODES:
+            self._loop_all_received_topics(upgrade)
+        self.__update_topic("openWB/counter/get/loadmanagement_prios", loadmanagement_prios)
+        self._append_datastore_version(141)
