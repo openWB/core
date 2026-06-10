11a716f2048a3f3c09f09c2b47ce64ca86ac3497
LKuemmel <lena.kuemmel@openwb.de>
2026-06-10 14:11:04 +0200

    fix

--- packages/control/bat_all.py
+++ packages/control/bat_all.py
@@ -645,24 +645,15 @@ class BatAll:
             self.data.set.current_state = CurrentState.ACTIVE.value
 
     def time_charging_min_bat_soc_allowed(self) -> bool:
-        if (self.data.config.configured and
-                self.data.config.bat_control_permitted and
-                self.data.config.bat_control_activated):
-            if (self.data.config.power_limit_condition == BatPowerLimitCondition.MANUAL.value or
+        if self.data.config.configured and self.data.config.bat_control_activated:
+            # manueller Modus und keine Eigenregelung oder aktive Speichersteuerung bei Fahrzeugladung
+            if ((self.data.config.power_limit_condition == BatPowerLimitCondition.MANUAL.value and
+                 self.data.config.manual_mode != ManualMode.MANUAL_DISABLE.value) or
                     self.data.config.power_limit_condition == BatPowerLimitCondition.VEHICLE_CHARGING.value):
                 return False
             elif self.data.config.power_limit_condition == BatPowerLimitCondition.PRICE_LIMIT.value:
-                if self.data.config.power_limit_mode == BatPowerLimitMode.MODE_CHARGE_PV_PRODUCTION.value:
-                    if ((self.data.config.price_limit_activated and
-                            data.data.optional_data.ep_is_charging_allowed_price_threshold(
-                                self.data.config.price_limit))
-                        or (self.data.config.price_charge_activated and
-                            data.data.optional_data.ep_is_charging_allowed_price_threshold(
-                                self.data.config.charge_limit))):
-                        return True
-                    else:
-                        return False
-                else:
+                # Preisgrenze aktiv und Ladeleistung des Speichers vorgegeben
+                if self.data.set.power_limit is not None:
                     return False
         return True
 
