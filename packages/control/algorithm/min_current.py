65d62900c1ee4246cbe15dc4c2c08bd619a52771
LKuemmel <lena.kuemmel@openwb.de>
2026-06-16 15:57:04 +0200

    draft

--- packages/control/algorithm/min_current.py
+++ packages/control/algorithm/min_current.py
@@ -3,9 +3,10 @@ import logging
 from control import data
 from control.algorithm import common
 from control.algorithm.chargemodes import CONSIDERED_CHARGE_MODES_MIN_CURRENT, CONSIDERED_CHARGE_MODES_PV_ONLY
+from control.chargepoint.chargepoint import Chargepoint
 from control.chargepoint.chargepoint_state import ChargepointState
 from control.loadmanagement import Loadmanagement
-from control.algorithm.filter_chargepoints import get_chargepoints_by_mode_and_counter
+from control.algorithm.filter_chargepoints import filtered_loads_to_str, get_grouped_loads_by_mode_and_counter
 
 log = logging.getLogger(__name__)
 
@@ -16,39 +17,42 @@ class MinCurrent:
         pass
 
     def set_min_current(self) -> None:
-        for mode_tuple, counter in common.mode_and_counter_generator(CONSIDERED_CHARGE_MODES_MIN_CURRENT):
-            preferenced_chargepoints = get_chargepoints_by_mode_and_counter(mode_tuple, f"counter{counter.num}")
-            if preferenced_chargepoints:
-                log.info(f"Mode-Tuple {mode_tuple[0]} - {mode_tuple[1]} - {mode_tuple[2]}, Zähler {counter.num}")
-                common.update_raw_data(preferenced_chargepoints, diff_to_zero=True)
-                for cp, group in data.data.counter_all_data.generator_cps_by_loadmanagement_prios(
-                        preferenced_chargepoints):
-                    missing_currents, counts = common.get_min_current(cp)
+        for counter in common.counter_generator():
+            preferenced_loads_groups = get_grouped_loads_by_mode_and_counter(CONSIDERED_CHARGE_MODES_MIN_CURRENT,
+                                                                             f"counter{counter.num}")
+            for preferenced_loads in preferenced_loads_groups:
+                log.info(f"Zähler {counter.num}, Verbraucher {filtered_loads_to_str(preferenced_loads)}")
+                common.update_raw_data(preferenced_loads, diff_to_zero=True)
+                while len(preferenced_loads):
+                    load = preferenced_loads[0]
+                    missing_currents, counts = common.get_min_current(load)
                     if max(missing_currents) > 0:
                         available_currents, limit = Loadmanagement().get_available_currents(
-                            missing_currents, counter, cp)
+                            missing_currents, counter, load)
                         if limit.limiting_value is not None:
-                            cp.data.control_parameter.limit = limit
-                        available_for_cp = common.available_current_for_cp(
-                            cp, counts, available_currents, missing_currents)
+                            load.data.control_parameter.limit = limit
+                        available_for_load = common.available_current_for_load(
+                            load, counts, available_currents, missing_currents)
                         current = common.get_current_to_set(
-                            cp.data.set.current, available_for_cp, cp.data.set.target_current)
-                        if current < cp.data.control_parameter.min_current:
-                            common.set_current_counterdiff(-(cp.data.set.current or 0), 0, cp)
-                            if limit:
-                                cp.set_state_and_log(
+                            load.data.set.current, available_for_load, load.data.set.target_current)
+                        if isinstance(load, Chargepoint) and current < load.data.control_parameter.min_current:
+                            common.set_current_counterdiff(-(load.data.set.current or 0), 0, load)
+                            if limit.limiting_value is not None:
+                                load.set_state_and_log(
                                     f"Ladung kann nicht gestartet werden{limit.message}")
                         else:
                             common.set_current_counterdiff(
-                                cp.data.set.target_current,
-                                cp.data.control_parameter.min_current,
-                                cp)
+                                load.data.set.target_current,
+                                load.data.control_parameter.min_current,
+                                load)
                     else:
-                        if mode_tuple in CONSIDERED_CHARGE_MODES_PV_ONLY:
+                        if (load.data.control_parameter.chargemode,
+                                load.data.control_parameter.submode) in CONSIDERED_CHARGE_MODES_PV_ONLY:
                             try:
-                                if (cp.data.control_parameter.state == ChargepointState.NO_CHARGING_ALLOWED or
-                                        cp.data.control_parameter.state == ChargepointState.SWITCH_ON_DELAY):
-                                    data.data.counter_all_data.get_evu_counter().switch_on_threshold_reached(cp)
+                                if (load.data.control_parameter.state == ChargepointState.NO_CHARGING_ALLOWED or
+                                        load.data.control_parameter.state == ChargepointState.SWITCH_ON_DELAY):
+                                    data.data.counter_all_data.get_evu_counter().switch_on_threshold_reached(load)
                             except Exception:
-                                log.exception(f"Fehler in der PV-gesteuerten Ladung bei {cp.num}")
-                        cp.data.set.current = 0
+                                log.exception(f"Fehler in der PV-gesteuerten Ladung bei {load.num}")
+                        load.data.set.current = 0
+                    preferenced_loads.pop(0)
--- packages/modules/common/store/_factory.py
+++ packages/modules/common/store/_factory.py
@@ -2,6 +2,7 @@ from typing import Optional
 from modules.common.component_type import ComponentType, special_to_general_type_mapping
 from modules.common.simcount._simcounter import SimCounter
 from modules.common.store._battery import get_bat_value_store
+from modules.common.store._consumer import get_consumer_value_store
 from modules.common.store._counter import get_counter_value_store
 from modules.common.store._inverter import get_inverter_value_store
 
@@ -14,6 +15,8 @@ def get_component_value_store(component_type_str: str,
         component_type_str.value if isinstance(component_type_str, ComponentType) else component_type_str)
     if ComponentType.BAT == component_type:
         return get_bat_value_store(component_num)
+    elif ComponentType.CONSUMER == component_type:
+        return get_consumer_value_store(component_num)
     elif ComponentType.COUNTER == component_type:
         return get_counter_value_store(component_num, add_child_values, simcounter)
     elif ComponentType.INVERTER == component_type:
