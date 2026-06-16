e6e609118a1a181decb84b6c338d63692a193042
LKuemmel <lena.kuemmel@openwb.de>
2026-06-16 15:56:11 +0200

    draft

---
+++ packages/modules/consumers/my_pv/elwa_e/config.py
@@ -0,0 +1,30 @@
+from typing import List, Optional
+
+from control.consumer.consumer_data import ConsumerUsage
+from helpermodules.auto_str import auto_str
+from modules.common.consumer_setup import ConsumerSetup
+from ..vendor import vendor_descriptor
+
+
+@auto_str
+class ElwaConfiguration:
+    def __init__(self,
+                 ip_address: Optional[str] = None,
+                 port: Optional[int] = 502,
+                 modbus_id: Optional[int] = 1):
+        self.ip_address = ip_address
+        self.port = port
+        self.modbus_id = modbus_id
+
+
+@auto_str
+class Elwa(ConsumerSetup[ElwaConfiguration]):
+    def __init__(self,
+                 name: str = "my-PV Elwa-E Heizstab",
+                 type: str = "elwa_e",
+                 id: int = 0,
+                 configuration: ElwaConfiguration = None,
+                 usage: List[ConsumerUsage] = [ConsumerUsage.SUSPENDABLE_TUNABLE,
+                                               ConsumerUsage.METER_ONLY]) -> None:
+        super().__init__(name, type, id, vendor=vendor_descriptor.configuration_factory(
+        ).type, configuration=configuration or ElwaConfiguration(), usage=usage)
