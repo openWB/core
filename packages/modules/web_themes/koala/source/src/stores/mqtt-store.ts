b691ba4943a599851fe73785916e69c1b0262556
Brett-S-OWB <brett.scott@openwb.de>
2026-06-23 09:10:36 +0200

    remove redundant store function

--- packages/modules/web_themes/koala/source/src/stores/mqtt-store.ts
+++ packages/modules/web_themes/koala/source/src/stores/mqtt-store.ts
@@ -2908,19 +2908,6 @@ export const useMqttStore = defineStore('mqtt', () => {
   };
 
   /**
-   * Get the battery color if exactly one battery is configured
-   * @param batteryId battery id
-   * @returns string | null
-   */
-  const batteryAggregateColor = computed(() => {
-    const ids = batteryIds.value;
-    if (ids.length === 1) {
-      return batteryColor.value(ids[0]);
-    }
-    return null;
-  });
-
-  /**
    * Get the battery color identified by the battery id
    * @param batteryId battery id
    * @returns string | null
@@ -4204,7 +4191,6 @@ export const useMqttStore = defineStore('mqtt', () => {
     batteryTotalPower,
     batteryChargePriorityRange,
     batteryMode,
-    batteryAggregateColor,
     batteryColor,
     // Grid data
     gridId,
