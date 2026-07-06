81dd1e8fd5a88bb699696f2b4a9794f38e73d85b
BrettS <168732306+Brett-S-OWB@users.noreply.github.com>
2026-07-06 11:34:00 +0200

    Add Consumer tab and cards to koala web theme (#3630)
    * add consumer tab/cards
    
    * apply new design to consumer cards
    
    * add consumer time on to consumer card
    
    * fix consumer card CSS
    
    * remove redundant placeholders

---
+++ packages/modules/web_themes/koala/source/src/components/ConsumerInformation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="consumer-grid-wrapper">
+    <div class="consumer-grid">
+      <ConsumerCard
+        v-for="item in consumerIds"
+        :key="item"
+        :consumer-id="item"
+        full-height
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useMqttStore } from 'src/stores/mqtt-store';
+
+import ConsumerCard from 'src/components/ConsumerCard.vue';
+
+const mqttStore = useMqttStore();
+
+const consumerIds = computed(() => mqttStore.consumerIds);
+</script>
+
+<style scoped lang="scss">
+.consumer-grid-wrapper {
+  flex: 1 1 auto;
+  min-height: 0;
+  width: 100%;
+  overflow-y: auto;
+}
+
+.consumer-grid {
+  display: grid;
+  gap: 0.5em;
+  grid-template-columns: repeat(
+    auto-fit,
+    minmax(min(22em, calc(50% - 0.5em)), 22em)
+  );
+  justify-content: center;
+  align-content: start;
+  padding: 0.5em;
+}
+</style>
