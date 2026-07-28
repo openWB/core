a2c5870b2c5f5a7671c5defde1d15c840138801b
AlexanderHa98 <alexander.hartung@openwb.de>
2026-07-28 15:27:27 +0200

    Add Universeller Modbus (#3699)
    * Add Universeller Modbus
    
    * fix
    
    * fix
    
    * Fix imports
    
    * Add suggestions and refactor
    
    * Add suggestions
    
    * Add requested changes

---
+++ packages/modules/devices/generic/modbus/helper.py
@@ -0,0 +1,42 @@
+from typing import Any
+
+from modules.common.modbus import ModbusDataType, ModbusTcpClient_
+from modules.devices.generic.modbus.config import RegisterConfig, GenericModbusConfiguration
+
+
+def check_data(register_config: RegisterConfig, device_config: GenericModbusConfiguration):
+    if (register_config.reg_type is None or
+        device_config.byteorder is None or
+            device_config.wordorder is None):
+        raise ValueError(
+            f"Unvollständige Konfiguration für Universeller-Modbus: Register-Adresse {register_config.reg_address}")
+
+
+def read_value(client: ModbusTcpClient_, unit: int,
+               device_config: GenericModbusConfiguration,
+               register_config: RegisterConfig) -> Any:
+    if register_config.reg_address is None:
+        return None
+
+    check_data(register_config, device_config)
+    return client.read_input_registers(
+        register_config.reg_address,
+        ModbusDataType[register_config.reg_type],
+        byteorder=device_config.byteorder,
+        wordorder=device_config.wordorder,
+        unit=unit,
+    )
+
+
+def read_phase_values(client: ModbusTcpClient_,
+                      unit: int,
+                      device_config: GenericModbusConfiguration,
+                      *register_configs: RegisterConfig) -> Any:
+    values = [0.0] * 3
+    has_value = False
+    for index, register_config in enumerate(register_configs):
+        value = read_value(client, unit, device_config, register_config)
+        if value is not None:
+            values[index] = value
+            has_value = True
+    return values if has_value else None
