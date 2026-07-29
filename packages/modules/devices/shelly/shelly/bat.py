8c81a78227cf3f9831bcde574b27395f6ee6c73a
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2026-07-29 11:53:57 +0200

    shelly, pytest (#3730)
    * add shelly, flake8, pytest consumer chargemodes
    
    * fix shelly
    
    * existing pytests running
    
    * review

--- packages/modules/devices/shelly/shelly/bat.py
+++ packages/modules/devices/shelly/shelly/bat.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import logging
 from typing import Optional, TypedDict, Any
-from modules.common import req
 from modules.common.abstract_device import AbstractBat
 from modules.common.component_state import BatState
 from modules.common.component_type import ComponentDescriptor
@@ -9,9 +8,9 @@ from modules.common.fault_state import ComponentInfo, FaultState
 from modules.common.store import get_component_value_store
 from modules.common.simcount._simcounter import SimCounter
 from modules.devices.shelly.shelly.config import ShellyBatSetup
-from modules.devices.shelly.shelly.constants import ALPHABETICAL_INDEX
 from modules.common.utils.peak_filter import PeakFilter
 from modules.common.component_type import ComponentType
+from modules.devices.shelly.shelly.status_handler import parse_data, request_status
 
 log = logging.getLogger(__name__)
 
@@ -41,64 +40,18 @@ class ShellyBat(AbstractBat):
         self.peak_filter = PeakFilter(ComponentType.BAT, self.component_config.id, self.fault_state)
 
     def update(self) -> None:
-        if self.generation == 1:
-            status_url = "http://" + self.address + "/status"
-        else:
-            status_url = "http://" + self.address + "/rpc/Shelly.GetStatus"
-        status = req.get_http_session().get(status_url, timeout=3).json()
+        status = request_status(self.address, self.generation)
+        _, _, currents, _, power, _ = parse_data(self.phase, self.factor, status)
+        self.peak_filter.check_values(power)
+        imported, exported = self.sim_counter.sim_count(power)
 
-        try:
-            power = 0
-            currents = [0.0, 0.0, 0.0]
-            # GEN 1
-            if "meters" in status:
-                meters = status['meters']  # einphasiger shelly?
-                for i in range(0, min(3, len(meters))):
-                    currents[(i+self.phase-1) % 3] = (float(meters[i].get('power', 0)) * self.factor) / 230
-                    power = power + (float(meters[i].get('power', 0)) * self.factor)
-            elif "emeters" in status:
-                meters = status['emeters']  # shellyEM & shelly3EM
-                # shellyEM has one meter, shelly3EM has three meters
-                for i in range(0, min(3, len(meters))):
-                    currents[(i+self.phase-1) % 3] = float(meters[i].get('current', 0)) * self.factor
-                    power = power + float(meters[i].get('power', 0)) * self.factor
-            # GEN 2+
-            # shelly Pro3EM
-            elif "em:0" in status:
-                meters = status['em:0']
-                for i, alphabetical_index in enumerate(ALPHABETICAL_INDEX):
-                    if meters.get(f'{alphabetical_index}_current') is None:
-                        continue
-                    currents[(i+self.phase-1) % 3] = float(meters.get(f'{alphabetical_index}_current', 0)) * self.factor
-                power = float(meters.get('total_act_power', 0)) * self.factor
-            # Shelly MiniPM G3
-            elif "pm1:0" in status:
-                log.debug("single phase shelly")
-                meters = status['pm1:0']
-                currents[self.phase-1] = meters['current'] * self.factor
-                power = meters['apower'] * self.factor
-            elif 'switch:0' in status and 'apower' in status['switch:0']:
-                log.debug("single phase shelly")
-                meters = status['switch:0']
-                currents[self.phase-1] = meters['current'] * self.factor
-                power = meters['apower'] * self.factor
-            else:
-                log.debug("single phase shelly")
-                meters = status['em1:0']
-                currents[self.phase-1] = meters['current'] * self.factor
-                power = meters['act_power'] * self.factor  # shelly Pro EM Gen 2
-            self.peak_filter.check_values(power)
-            imported, exported = self.sim_counter.sim_count(power)
-
-            bat_state = BatState(
-                power=power,
-                currents=currents,
-                imported=imported,
-                exported=exported
-            )
-            self.store.set(bat_state)
-        except KeyError:
-            log.exception("unsupported shelly device.")
+        bat_state = BatState(
+            power=power,
+            currents=currents,
+            imported=imported,
+            exported=exported
+        )
+        self.store.set(bat_state)
 
 
 component_descriptor = ComponentDescriptor(configuration_factory=ShellyBatSetup)
