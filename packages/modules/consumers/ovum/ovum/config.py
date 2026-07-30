cd7b7fef087d9973622f235e0028ee40c30881c1
seaspotter <florian.horch@gmail.com>
2026-07-30 13:38:30 +0200

    Add OVUM CubeSpeicher/MPlus consumer module

---
+++ packages/modules/consumers/ovum/ovum/config.py
@@ -0,0 +1,33 @@
+from typing import Optional, Tuple
+
+from control.consumer.consumer_data import ConsumerUsage
+from helpermodules.auto_str import auto_str
+from modules.common.consumer_setup import ConsumerSetup
+from ..vendor import vendor_descriptor
+
+
+@auto_str
+class OvumConfiguration:
+    def __init__(self,
+                 ip_address: Optional[str] = None,
+                 port: Optional[int] = 502,
+                 modbus_id: Optional[int] = 1,
+                 send_values: bool = False):
+        self.ip_address = ip_address
+        self.port = port
+        self.modbus_id = modbus_id
+        self.send_values = send_values
+
+
+@auto_str
+class Ovum(ConsumerSetup[OvumConfiguration]):
+    def __init__(self,
+                 name: str = "OVUM Wärmepumpe (CubeSpeicher/MPlus)",
+                 type: str = "ovum",
+                 id: int = 0,
+                 configuration: OvumConfiguration = None,
+                 usage: Tuple[ConsumerUsage, ...] = (ConsumerUsage.SUSPENDABLE_TUNABLE,
+                                                     ConsumerUsage.METER_ONLY),
+                 **kwargs) -> None:
+        super().__init__(name, type, id, vendor=vendor_descriptor.configuration_factory(
+        ).type, configuration=configuration or OvumConfiguration(), usage=usage, **kwargs)
