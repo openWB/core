91f9e68718eb682d4b9b664125a8fe467870d8f5
LKuemmel <lena.kuemmel@openwb.de>
2026-08-05 09:55:14 +0200

    fix

--- packages/modules/web_themes/koala/source/src/stores/mqtt-store.ts
+++ packages/modules/web_themes/koala/source/src/stores/mqtt-store.ts
@@ -1949,7 +1949,7 @@ export const useMqttStore = defineStore('mqtt', () => {
     },
     set(newRange: RangeValue) {
       updateTopic(
-        'openWB/general/chargemode_config/bat/min_soc
+        'openWB/general/chargemode_config/bat/min_soc',
         newRange.min,
         undefined,
         true,
