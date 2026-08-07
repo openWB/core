be896bc40e9c1788ab41ade2c95494c169e2ed4e
SeaSpotter <florian.horch@gmail.com>
2026-08-07 17:23:44 +0200

    Refactor send_values to use CurrentValues

--- packages/modules/consumers/idm/idm/consumer.py
+++ packages/modules/consumers/idm/idm/consumer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from enum import IntEnum
 from pymodbus.constants import Endian
 import logging
 
-from control import data
+from modules.common.abstract_consumer import CurrentValues
 from modules.common.abstract_device import DeviceDescriptor
 from modules.common.component_state import ConsumerState
 from modules.common.component_type import ComponentType
@@ -12,6 +13,15 @@ from modules.common.simcount._simcounter import SimCounterConsumer
 from modules.consumers.idm.idm.config import Idm
 
 log = logging.getLogger(__name__)
+
+
+class Register(IntEnum):
+    # Register-Layout siehe IDM "Gebäudeleittechnik-Smartfox.pdf", Kap. 2.2.5.1
+    PV_SURPLUS = 74         # Aktueller PV-Überschuss [kW]
+    PV_PRODUCTION = 78      # Aktuelle PV-Produktion [kW]
+    HOME_CONSUMPTION = 82   # Hausverbrauch [kW], Default 0
+    BATTERY_POWER = 84      # Batterieentladung [kW], Default 0 (negativ = Ladung)
+    BATTERY_SOC = 86        # Batteriefüllstand [%], Default -1 (= kein Speicher)
 
 
 def create_consumer(config: Idm):
@@ -26,53 +36,41 @@ def create_consumer(config: Idm):
     def error_handler() -> None:
         initializer()
 
-    def send_values() -> None:
+    def send_values(values: CurrentValues) -> None:
         # Sendet die aktuellen Systemwerte an die IDM Navigator 2.0-Regelung über die
         # Gebäudeleittechnik/Smartfox-Schnittstelle (Modbus TCP). IDM unterstützt keine
         # direkte Leistungsvorgabe für die WP - die Regelung berechnet ihre Überschuss- und
-        # Speicherstrategie selbst anhand dieser Werte. Register-Layout siehe IDM
-        # "Gebäudeleittechnik-Smartfox.pdf", Kap. 2.2.5.1. Wird nur aufgerufen, wenn
-        # config.configuration.send_values aktiviert ist.
+        # Speicherstrategie selbst anhand dieser Werte. Wird vom Core automatisch aufgerufen,
+        # solange dieser Verbraucher als ConsumerUsage.SELF_CONTROLLED konfiguriert ist.
         modbus_id = config.configuration.modbus_id
 
-        # pv_all_data.get.power ist negativ, solange PV einspeist (gleiche Konvention wie beim
-        # EVU-Zähler) -> Vorzeichen drehen und auf 0 begrenzen, um die reine Erzeugung zu erhalten
-        pv_power = max(-data.data.pv_all_data.data.get.power, 0)
-        hausverbrauch = data.data.counter_all_data.data.set.home_consumption
-        # Ladepunkt-Leistung mit einrechnen, da diese sonst fälschlich als freier Überschuss an die
+        # values.pv_power kommt roh aus process.py (negativ während Produktion, gleiche
+        # Konvention wie beim EVU-Zähler) -> Vorzeichen drehen und auf 0 begrenzen, um die
+        # reine Erzeugung zu erhalten
+        pv_power = max(-values.pv_power, 0)
+        # Ladeleistung mit einrechnen, da diese sonst fälschlich als freier Überschuss an die
         # WP gemeldet würde, obwohl sie bereits von einer Fahrzeugladung verbraucht wird
-        ladeleistung = data.data.cp_all_data.data.get.power
-        ueberschuss = max(pv_power - hausverbrauch - ladeleistung, 0)
+        surplus = max(pv_power - values.home_consumption - values.cp_power, 0)
+        battery_soc = int(values.bat_soc) if values.bat_soc is not None else -1
 
-        bat = data.data.bat_all_data
-        # Get.power ist bei Speichern bereits positiv = Entladung, negativ = Ladung -> passt
-        # direkt zur IDM-Registerdefinition "Batterieentladung"
-        battery_power = bat.data.get.power if bat.data.config.configured else 0
-        battery_soc = bat.data.get.soc if bat.data.config.configured else None
-
-        # Reg 74: Aktueller PV-Überschuss [kW]
-        client.write_register(74, ueberschuss / 1000, wordorder=Endian.Little, unit=modbus_id)
-        # Reg 78: Aktuelle PV Produktion [kW]
-        client.write_register(78, pv_power / 1000, wordorder=Endian.Little, unit=modbus_id)
-        # Reg 82: Hausverbrauch [kW], Default 0
-        client.write_register(82, hausverbrauch / 1000, wordorder=Endian.Little, unit=modbus_id)
-        # Reg 84: Batterieentladung [kW], Default 0 (negativ = Ladung)
-        client.write_register(84, battery_power / 1000, wordorder=Endian.Little, unit=modbus_id)
-        # Reg 86: Batteriefüllstand [%], Default -1 (= kein Speicher)
-        client.write_register(86, int(battery_soc) if battery_soc is not None else -1, unit=modbus_id)
+        client.write_register(Register.PV_SURPLUS, surplus / 1000, wordorder=Endian.Little, unit=modbus_id)
+        client.write_register(Register.PV_PRODUCTION, pv_power / 1000, wordorder=Endian.Little,
+                               unit=modbus_id)
+        client.write_register(Register.HOME_CONSUMPTION, values.home_consumption / 1000, wordorder=Endian.Little,
+                               unit=modbus_id)
+        client.write_register(Register.BATTERY_POWER, values.bat_power / 1000, wordorder=Endian.Little,
+                               unit=modbus_id)
+        client.write_register(Register.BATTERY_SOC, battery_soc, unit=modbus_id)
 
     def update() -> ConsumerState:
         if config.configuration.version == 1:
             power = client.read_holding_registers(4122, ModbusDataType.FLOAT_32,
-                                                  unit=config.configuration.modbus_id)
+                                                    unit=config.configuration.modbus_id)
         else:
             power = client.read_input_registers(4122, ModbusDataType.FLOAT_32,
-                                                unit=config.configuration.modbus_id)
+                                                  unit=config.configuration.modbus_id)
         power *= 100
         imported, exported = sim_counter.sim_count(power)
-
-        if config.configuration.send_values:
-            send_values()
 
         return ConsumerState(
             power=power,
@@ -83,7 +81,8 @@ def create_consumer(config: Idm):
     return ConfigurableConsumer(consumer_config=config,
                                 initializer=initializer,
                                 error_handler=error_handler,
-                                update=update)
+                                update=update,
+                                send_values=send_values)
 
 
 device_descriptor = DeviceDescriptor(configuration_factory=Idm)
