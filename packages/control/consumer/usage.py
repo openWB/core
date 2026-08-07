ca58d34855a439e61eb87fc370db522b199a8554
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2026-08-07 13:19:31 +0200

    usage type self controlled (#3775)
    * usage type self controlled
    
    * fix
    
    * review

--- packages/control/consumer/usage.py
+++ packages/control/consumer/usage.py
@@ -3,6 +3,10 @@ from enum import Enum
 
 class ConsumerUsage(Enum):
     METER_ONLY = "meter_only"
+    SELF_CONTROLLED = "self_controlled"
     SUSPENDABLE_TUNABLE = "suspendable_tunable"
     SUSPENDABLE_ONOFF = "suspendable_onoff"
     CONTINUOUS = "continuous"
+
+
+NOT_CONTROLLED = (ConsumerUsage.METER_ONLY, ConsumerUsage.SELF_CONTROLLED)
