02ce4681fb4339ac1255a832a9d30216878aac85
SeaSpotter <florian.horch@gmail.com>
2026-08-07 17:24:17 +0200

    Refactor LambdaConfiguration by removing send_values
    Removed send_values parameter from LambdaConfiguration.

--- packages/modules/consumers/lambda_/lambda_/config.py
+++ packages/modules/consumers/lambda_/lambda_/config.py
@@ -11,12 +11,10 @@ class LambdaConfiguration:
     def __init__(self,
                  ip_address: Optional[str] = None,
                  port: Optional[int] = 502,
-                 modbus_id: Optional[int] = 1,
-                 send_values: bool = False):
+                 modbus_id: Optional[int] = 1):
         self.ip_address = ip_address
         self.port = port
         self.modbus_id = modbus_id
-        self.send_values = send_values
 
 
 @auto_str
@@ -26,7 +24,7 @@ class Lambda(ConsumerSetup[LambdaConfiguration]):
                  type: str = "lambda",
                  id: int = 0,
                  configuration: LambdaConfiguration = None,
-                 usage: Tuple[ConsumerUsage, ...] = (ConsumerUsage.SUSPENDABLE_TUNABLE,
+                 usage: Tuple[ConsumerUsage, ...] = (ConsumerUsage.SELF_CONTROLLED,
                                                      ConsumerUsage.METER_ONLY),
                  **kwargs) -> None:
         super().__init__(name, type, id, vendor=vendor_descriptor.configuration_factory(
