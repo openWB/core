c77c482876f0a45a4c8b298cfe2d7b56bb2201fd
Alexander Hartung <alexander.hartung@openwb.de>
2026-08-12 09:26:36 +0200

    Remove local debugging setup

--- packages/modules/common/store/_chargepoint.py
+++ packages/modules/common/store/_chargepoint.py
@@ -42,22 +42,13 @@ class ChargepointValueStoreBroker(ValueStore[ChargepointState]):
         pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/evse_current", self.state.evse_current)
         pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/vehicle_id", self.state.vehicle_id)
         pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/max_evse_current", self.state.max_evse_current)
-        # ONLY_TEST Wieder rückgängig machen
-        pub_to_broker("openWB/set/chargepoint/" + str(self.num) +
-                      "/get/max_charge_power",
-                      500000 if self.state.max_charge_power is None else self.state.max_charge_power)
-        # ONLY_TEST Wieder rückgängig machen
-        pub_to_broker("openWB/set/chargepoint/" + str(self.num) +
-                      "/get/max_discharge_power",
-                      -500000 if self.state.max_discharge_power is None else self.state.max_discharge_power)
-
+        pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/max_charge_power", self.state.max_charge_power)
+        pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/max_discharge_power",
+                      self.state.max_discharge_power)
         pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/version", self.state.version)
         pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/current_branch", self.state.current_branch)
         pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/current_commit", self.state.current_commit)
-
-        # ONLY_TEST Wieder rückgängig machen
-        pub_to_broker("openWB/set/chargepoint/" + str(self.num) +
-                      "/get/evse_signaling", "HLC")  # self.state.evse_signaling)  #
+        pub_to_broker("openWB/set/chargepoint/" + str(self.num) + "/get/evse_signaling", self.state.evse_signaling)
 
 
 def get_chargepoint_value_store(id: int) -> ValueStore[ChargepointState]:
