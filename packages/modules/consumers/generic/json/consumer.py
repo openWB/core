9410e72b7dae60329e4a706a8324e60d4e58a5f4
LKuemmel <lena.kuemmel@openwb.de>
2026-08-18 14:56:43 +0200

    review

--- packages/modules/consumers/generic/json/consumer.py
+++ packages/modules/consumers/generic/json/consumer.py
@@ -78,15 +78,15 @@ def create_consumer(config: Json):
 
     def switch_on():
         # Authorization?
-        jq_switch_on(session, params={"state": True})
+        jq_switch_on(params={"state": True})
 
     def switch_off():
         # Authorization?
-        jq_switch_off(session, params={"state": False})
+        jq_switch_off(params={"state": False})
 
     def set_power_limit(power_limit: int):
         # Authorization?
-        jq_set_power_limit(session, params={"power_limit": power_limit})
+        jq_set_power_limit(params={"power_limit": power_limit})
 
     return ConfigurableConsumer(consumer_config=config,
                                 initializer=initializer,
