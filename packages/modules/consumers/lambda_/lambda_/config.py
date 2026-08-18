ab634197f27df11dfe09373939be0649c61cd65d
LKuemmel <lena.kuemmel@openwb.de>
2026-08-18 11:46:52 +0200

    modules

---
+++ packages/modules/consumers/lambda_/lambda_/config.py
@@ -0,0 +1,32 @@
+from typing import Optional, Tuple
+
+from control.consumer.consumer_data import ConsumerUsage
+from helpermodules.auto_str import auto_str
+from modules.common.consumer_setup import ConsumerSetup
+from ..vendor import vendor_descriptor
+
+
+@auto_str
+class LambdaConfiguration:
+    def __init__(self,
+                 ip_address: Optional[str] = None,
+                 port: Optional[int] = 502,
+                 modbus_id: Optional[int] = 1):
+        self.ip_address = ip_address
+        self.port = port
+        self.modbus_id = modbus_id
+
+
+@auto_str
+class Lambda(ConsumerSetup[LambdaConfiguration]):
+    def __init__(self,
+                 name: str = "Lambda Wärmepumpe",
+                 type: str = "lambda_",
+                 id: int = 0,
+                 configuration: LambdaConfiguration = None,
+                 usage: Tuple[ConsumerUsage, ...] = (ConsumerUsage.SUSPENDABLE_TUNABLE,
+                                                     ConsumerUsage.SELF_CONTROLLED,
+                                                     ConsumerUsage.METER_ONLY),
+                 **kwargs) -> None:
+        super().__init__(name, type, id, vendor=vendor_descriptor.configuration_factory(
+        ).type, configuration=configuration or LambdaConfiguration(), usage=usage, **kwargs)
