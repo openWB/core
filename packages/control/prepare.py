62ae01120e716e530c2155253eab08762ecb8ded
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2026-08-18 15:02:30 +0200

    fix loadmanagement, clean up (#3819)
    * clean up
    
    * fix loadmanagement
    
    * review
    
    * review
    
    * flake8

--- packages/control/prepare.py
+++ packages/control/prepare.py
@@ -24,13 +24,14 @@ class Prepare:
             data.data.bat_all_data.calc_power_for_all_components()
             for cp in data.data.cp_data.values():
                 cp.setup_values_at_start()
-            levels = data.data.counter_all_data.get_list_of_elements_per_level()
-            for level in reversed(levels):
+            # Für die Initialisierung des LM muss bekannt sein, ob der Soll-Strom des Verbrauchers in diesem Intervall
+            # angepasst wird (switch_interval_elapsed).
+            for consumer in data.data.consumer_data.values():
+                consumer.update()
+            for level in reversed(data.data.counter_all_data.get_list_of_elements_per_level()):
                 for element in level:
                     if element["type"] == ComponentType.COUNTER.value:
                         data.data.counter_data[f"counter{element['id']}"].setup_counter()
-            for consumer in data.data.consumer_data.values():
-                consumer.update()
             for cp in data.data.cp_data.values():
                 cp.update(data.data.ev_data)
             # Nach cp update, da für die Speicher-Sperre der Lademodus bekannt sein muss.
