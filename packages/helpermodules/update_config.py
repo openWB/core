620dec818eb92258221fc979b08759bfc6c553cd
LKuemmel <lena.kuemmel@openwb.de>
2026-08-19 11:39:59 +0200

    review

--- packages/helpermodules/update_config.py
+++ packages/helpermodules/update_config.py
@@ -3604,6 +3604,7 @@ class UpdateConfig:
                        (Chargemode.INSTANT_CHARGING.value, True),
                        (Chargemode.INSTANT_CHARGING.value, False),
                        (Chargemode.ECO_CHARGING.value, True),
+                       (Chargemode.ECO_CHARGING.value, False),
                        (Chargemode.PV_CHARGING.value, True),
                        (Chargemode.PV_CHARGING.value, False),
                        (Chargemode.STOP.value, True),
