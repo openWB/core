6f9d2f5c5ceac6a0669d27c1779066668ee02c6d
SeaSpotter <florian.horch@gmail.com>
2026-08-20 14:05:48 +0200

    Remove debug logging from counter.py

--- packages/modules/devices/anker/smartmeter_gen2/counter.py
+++ packages/modules/devices/anker/smartmeter_gen2/counter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import logging
 from typing import Any, TypedDict
 
 from modules.common.abstract_device import AbstractCounter
@@ -11,8 +10,6 @@ from modules.common.simcount import SimCounter
 from modules.common.store import get_component_value_store
 from modules.common.utils.peak_filter import PeakFilter
 from modules.devices.anker.smartmeter_gen2.config import AnkerMeter, AnkerMeterCounterSetup
-
-log = logging.getLogger(__name__)
 
 
 class KwargsDict(TypedDict):
@@ -45,10 +42,8 @@ class AnkerMeterCounter(AbstractCounter):
         currents = self.client.read_input_registers(10635, [ModbusDataType.INT_16] * 3,
                                                     wordorder=Endian.Big, unit=unit)
 
-        # DEBUG only: Rohwerte vor Skalierung/Vorzeichen-Anpassung mitloggen, um zu
-        # sehen ob das Strom-Register selbst schon ein Vorzeichen traegt oder nicht.
-        log.debug("DEBUG currents roh (vor /100, vor Vorzeichen-Uebernahme von powers): %s", currents)
-        log.debug("DEBUG powers roh (zum Vergleich): %s", powers)
+        # Currents hat keine eigene Vorzeichen (getestet), daher kommen die Vorzeichen aus
+        # den powers-Werten
 
         voltages = [value / 10 for value in voltages]
         currents = [abs(c) / 100 * (1 if p >= 0 else -1) for c, p in zip(currents, powers)]
