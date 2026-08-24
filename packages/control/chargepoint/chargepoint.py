b9e8ea3c15b37341316be65386881f60a3224b76
LKuemmel <lena.kuemmel@openwb.de>
2026-08-24 12:51:35 +0200

    review

--- packages/control/chargepoint/chargepoint.py
+++ packages/control/chargepoint/chargepoint.py
@@ -1,4 +1,3 @@
-import dataclasses
 import logging
 from threading import Thread, Event
 import traceback
@@ -780,8 +779,7 @@ class Chargepoint(ChargepointRfidMixin):
     def update_charge_template(self, charge_template: ChargeTemplate) -> None:
         # Prüfen, ob ein temporäres Ladeprofil aktiv ist und dieses übernehmen
         self.data.set.charge_template = charge_template
-        Pub().pub(f"openWB/set/chargepoint/{self.num}/set/charge_template",
-                  dataclasses.asdict(charge_template.data))
+        Pub().pub(f"openWB/set/chargepoint/{self.num}/set/charge_template", asdict(charge_template.data))
 
     def _pub_connected_vehicle(self, vehicle: Ev):
         """ published die Daten, die zur Anzeige auf der Hauptseite benötigt werden.
