6e690812a6b355e33dbdc024e2f86e70cfe954d3
LKuemmel <lena.kuemmel@openwb.de>
2026-08-25 16:34:36 +0200

    add dataclass object for set_power_limit

--- packages/modules/consumers/ratiotherm/ratiotherm/consumer.py
+++ packages/modules/consumers/ratiotherm/ratiotherm/consumer.py
@@ -2,7 +2,7 @@
 import logging
 from modules.common.abstract_device import DeviceDescriptor
 from modules.common.component_type import ComponentType
-from modules.common.configurable_consumer import ConfigurableConsumer
+from modules.common.configurable_consumer import ConfigurableConsumer, SetLimitData
 from modules.common.modbus import ModbusDataType, ModbusTcpClient_
 from modules.common.simcount._simcounter import SimCounterConsumer
 from modules.consumers.ratiotherm.ratiotherm.config import Ratiotherm
@@ -22,14 +22,14 @@ def create_consumer(config: Ratiotherm):
     def error_handler() -> None:
         initializer()
 
-    def set_limit(power_limit: float) -> None:
+    def set_power_limit(power_limit: float, data: SetLimitData) -> None:
         # Absturz bei negativen Zahlen
         client.write_register(100, max(power_limit, 0), ModbusDataType.INT_16, unit=config.configuration.modbus_id)
 
     return ConfigurableConsumer(consumer_config=config,
                                 initializer=initializer,
                                 error_handler=error_handler,
-                                set_power_limit=set_limit,)
+                                set_power_limit=set_power_limit,)
 
 
 device_descriptor = DeviceDescriptor(configuration_factory=Ratiotherm)
