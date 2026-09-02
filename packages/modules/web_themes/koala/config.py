1e79c516991d2291fc859475275e6c50cb132844
Brett-S-OWB <brett.scott@openwb.de>
2026-09-02 16:12:29 +0200

    update type information

--- packages/modules/web_themes/koala/config.py
+++ packages/modules/web_themes/koala/config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from helpermodules.auto_str import auto_str
 
 from modules.common.abstract_device import DeviceDescriptor
@@ -12,7 +14,7 @@ class KoalaWebThemeConfiguration:
                  vehicle_card_view_breakpoint: int = 4,
                  chargePoint_table_search_input_field: bool = False,
                  vehicle_table_search_input_field: bool = False,
-                 top_carousel_slide_order: list = None
+                 top_carousel_slide_order: Optional[list[str]] = None
                  ) -> None:
         self.hide_standard_vehicle = hide_standard_vehicle
         self.history_chart_range = history_chart_range
@@ -35,7 +37,7 @@ class KoalaWebTheme:
                  type: str = "koala",
                  official: bool = True,
                  userManagementSupported: bool = True,
-                 configuration: KoalaWebThemeConfiguration = None) -> None:
+                 configuration: Optional[KoalaWebThemeConfiguration] = None) -> None:
         self.name = name
         self.type = type
         self.official = official
