8898cc460ea59c25ad72aadeb61db2d72f049a71
SeaSpotter <florian.horch@gmail.com>
2026-09-03 17:49:42 +0200

    Update counter.py

--- packages/modules/devices/growatt/growatt/counter.py
+++ packages/modules/devices/growatt/growatt/counter.py
@@ -33,41 +33,61 @@ class GrowattCounter(AbstractCounter):
         self.peak_filter = PeakFilter(ComponentType.COUNTER, self.component_config.id, self.fault_state)
 
     def update(self) -> None:
-        if self.version == GrowattVersion.max_series:
-            power_in = self.client.read_input_registers(1021, ModbusDataType.UINT_32, unit=self.__modbus_id) * 0.1
-            power_out = self.client.read_input_registers(1029, ModbusDataType.UINT_32, unit=self.__modbus_id) * -0.1
+        if self.version == GrowattVersion.vpp:
+            # Quelle: Growatt VPP Protocol V2.03, Input 31112 (int32, 0.1W).
+            # Dort bereits: positiv = Bezug, negativ = Einspeisung - keine Invertierung nötig.
+            power = self.client.read_input_registers(31112, ModbusDataType.INT_32,
+                                                     unit=self.__modbus_id) * 0.1
+            # VPP liefert keine separaten Phasenleistungen am Zähler (nur Summe 31112).
+
+            exported = self.client.read_input_registers(31124, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 100
+            imported = self.client.read_input_registers(31120, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 100
+
+        elif self.version == GrowattVersion.sph:
+            # Quelle: Protocol II V1.39, Storage-Block Input 1000-1249 (SPH/SPA-Hybrid).
+            power_in = self.client.read_input_registers(1021, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 0.1
+            power_out = self.client.read_input_registers(1029, ModbusDataType.UINT_32,
+                                                         unit=self.__modbus_id) * -0.1
             power = power_in + power_out
 
             powers = [
-                self.client.read_input_registers(
-                    40, ModbusDataType.INT_32, unit=self.__modbus_id) / 10,
-                self.client.read_input_registers(
-                    44, ModbusDataType.INT_32, unit=self.__modbus_id) / 10,
-                self.client.read_input_registers(
-                    48, ModbusDataType.INT_32, unit=self.__modbus_id) / 10]
+                self.client.read_input_registers(40, ModbusDataType.INT_32,
+                                                 unit=self.__modbus_id) / 10,
+                self.client.read_input_registers(44, ModbusDataType.INT_32,
+                                                 unit=self.__modbus_id) / 10,
+                self.client.read_input_registers(48, ModbusDataType.INT_32,
+                                                 unit=self.__modbus_id) / 10]
 
-            # Einheit 0.1 kWh
-            exported = self.client.read_input_registers(1050, ModbusDataType.UINT_32, unit=self.__modbus_id) * 100
-            imported = self.client.read_input_registers(1046, ModbusDataType.UINT_32, unit=self.__modbus_id) * 100
+            exported = self.client.read_input_registers(1050, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 100
+            imported = self.client.read_input_registers(1046, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 100
 
-        # TL-X Dokumentation hat die gleichen Register wie die MAX Serie,
-        # zusätzlich sind aber auch unten abweichende enthalten
-        else:
-            power_in = self.client.read_input_registers(3041, ModbusDataType.UINT_32, unit=self.__modbus_id) * 0.1
-            power_out = self.client.read_input_registers(3043, ModbusDataType.UINT_32, unit=self.__modbus_id) * -0.1
+        else:  # GrowattVersion.tlx
+            # Quelle: Protocol II V1.39, Bereich 3000-3124 (TL-X/TL-XH/TL3-XH).
+            # TL-X Dokumentation hat die gleichen Register wie die MAX Serie,
+            # zusätzlich sind aber auch unten abweichende enthalten
+            power_in = self.client.read_input_registers(3041, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 0.1
+            power_out = self.client.read_input_registers(3043, ModbusDataType.UINT_32,
+                                                         unit=self.__modbus_id) * -0.1
             power = power_in + power_out
 
             powers = [
-                self.client.read_input_registers(
-                    3028, ModbusDataType.INT_32, unit=self.__modbus_id) / 10,
-                self.client.read_input_registers(
-                    3032, ModbusDataType.INT_32, unit=self.__modbus_id) / 10,
-                self.client.read_input_registers(
-                    3036, ModbusDataType.INT_32, unit=self.__modbus_id) / 10]
+                self.client.read_input_registers(3028, ModbusDataType.INT_32,
+                                                 unit=self.__modbus_id) / 10,
+                self.client.read_input_registers(3032, ModbusDataType.INT_32,
+                                                 unit=self.__modbus_id) / 10,
+                self.client.read_input_registers(3036, ModbusDataType.INT_32,
+                                                 unit=self.__modbus_id) / 10]
 
-            # Einheit 0.1 kWh
-            exported = self.client.read_input_registers(3073, ModbusDataType.UINT_32, unit=self.__modbus_id) * 100
-            imported = self.client.read_input_registers(3069, ModbusDataType.UINT_32, unit=self.__modbus_id) * 100
+            exported = self.client.read_input_registers(3073, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 100
+            imported = self.client.read_input_registers(3069, ModbusDataType.UINT_32,
+                                                        unit=self.__modbus_id) * 100
 
         imported, exported = self.peak_filter.check_values(power, imported, exported)
         counter_state = CounterState(
