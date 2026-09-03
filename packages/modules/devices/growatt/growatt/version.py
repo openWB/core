5b61efc16803907d8a84b8b6ec3f62dfbfaa1617
SeaSpotter <florian.horch@gmail.com>
2026-09-03 18:09:42 +0200

    Update version.py

--- packages/modules/devices/growatt/growatt/version.py
+++ packages/modules/devices/growatt/growatt/version.py
@@ -6,7 +6,7 @@ class GrowattVersion(Enum):
     Growatt-Registerkarten:
     - tlx: Protocol II V1.39, Bereich 3000-3374 (TL-X/TL-XH/TL3-XH inkl. MOD-XH mit BDC-Batterie).
     - sph: Protocol II V1.39, Bereich 0-124 (WR/Zähler) + 1000-1249 (Speicher) - SPH/SPA-Hybrid.
-           Vormals fälschlich "MAX" genannt - die echte MAX-Serie hat laut Growatt keine 
+           Vormals fälschlich "MAX" genannt - die echte MAX-Serie hat laut Growatt keine
            Batterieregister.
     - vpp: Growatt VPP Communication Protocol V2.01/V2.03, Bereich 30000-31499. Neueres,
            paralleles Protokoll auf denselben Geräten (MOD/MIN-TL-XH/SPH/WIT), per Device Type
