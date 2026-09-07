617d0f00cd05559ad67187379c5dbc490e9fa31c
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2026-09-07 14:01:05 +0200

    Module: Ecoflow Stream (#3857)
    * add ecoflow stream
    
    * rename type to ecoflow_stream
    
    * use get_component_value_store

---
+++ packages/modules/devices/ecoflow/stream/counter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from typing import TypedDict, Any
+
+from modules.common.abstract_device import AbstractCounter
+from modules.common.component_state import CounterState
+from modules.common.component_type import ComponentDescriptor
+from modules.common.fault_state import ComponentInfo, FaultState
+from modules.common.simcount._simcounter import SimCounter
+from modules.common.store import get_component_value_store
+from modules.devices.ecoflow.stream.config import EcoflowStreamCounterSetup
+from modules.common.utils.peak_filter import PeakFilter
+from modules.common.component_type import ComponentType
+
+
+class KwargsDict(TypedDict):
+    device_id: int
+
+
+class EcoflowStreamCounter(AbstractCounter):
+    def __init__(self, component_config: EcoflowStreamCounterSetup, **kwargs: Any) -> None:
+        self.component_config = component_config
+        self.kwargs: KwargsDict = kwargs
+
+    def initialize(self) -> None:
+        self.__device_id: int = self.kwargs['device_id']
+        self.sim_counter = SimCounter(self.__device_id, self.component_config.id, prefix="bezug")
+        self.store = get_component_value_store(self.component_config.type, self.component_config.id)
+        self.fault_state = FaultState(ComponentInfo.from_component_config(self.component_config))
+        self.peak_filter = PeakFilter(ComponentType.COUNTER, self.component_config.id, self.fault_state)
+
+    def update(self, response) -> None:
+        power = float(response['data']['powGetSysGrid'])
+        self.peak_filter.check_values(power)
+        imported, exported = self.sim_counter.sim_count(power)
+
+        counter_state = CounterState(
+            imported=imported,
+            exported=exported,
+            power=power
+        )
+        self.store.set(counter_state)
+
+
+component_descriptor = ComponentDescriptor(configuration_factory=EcoflowStreamCounterSetup)
